d47c0a679dbc3697cb6afb9e8ac57ca05142c16f
John Claus <johnclaus@gmail.com>
2011-01-26 23:49:19 -0700

    Initial Capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,22 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "demo.chain.io"
+set :user, "deployer"
+set :repository,  "git@github.com:bglick/OpenChain.git"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm, :git # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :scm_username, user
+default_run_options[:pty] = true # Must be set for the password prompt from git to work
+ssh_options[:forward_agent] = true
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :deploy_to, "/var/www/apps/#{application}"
+
+# Sloppy use of AWS endpoints
+set :ec2_domain, "ec2-50-16-234-181.compute-1.amazonaws.com"
+set :rds_endpoint, "mydbinstance.cfmnpvwryyto.us-east-1.rds.amazonaws.com"
+
+role :web, ec2_domain                          # Your HTTP server, Apache/etc
+role :app, ec2_domain                          # This may be the same as your `Web` server
+#role :db,  rds_endpoint, :primary => true # This is where Rails migrations will run
+#role :db,  rds_endpoint
 
 # If you are using Passenger mod_rails uncomment this:
 # if you're still using the script/reapear helper you will need
