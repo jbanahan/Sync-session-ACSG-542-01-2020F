4b3fdb920df0eff5f6901e67af2532fb3c494970
Brian Glick <brian@brian-glick.com>
2011-02-01 21:24:01 -0500

    Fixed sort for child joins and custom fields

--- app/models/sort_criterion.rb
+++ app/models/sort_criterion.rb
@@ -6,9 +6,32 @@ class SortCriterion < ActiveRecord::Base
   validates :model_field_uid, :presence => true
   
   def apply(p)
+    r_val = p
     mf = ModelField.find_by_uid self.model_field_uid
-    p = p.joins(mf.join_statement) unless mf.join_statement.nil?
-    p.order("#{mf.join_alias}.#{mf.field_name} #{self.descending? ? "DESC" : "ASC"}")
+    if mf.custom?
+      r_val = apply_custom mf, p
+    else
+      r_val = p.where("1=1") if p.class.to_s == "Class"
+      mf_cm = mf.core_module
+      unless(mf_cm.class_name==r_val.klass.to_s)
+        cm = CoreModule.find_by_class_name(r_val.klass.to_s)
+        unless cm.nil?
+          child_join = cm.child_joins[mf_cm]
+          r_val = r_val.joins(child_join) unless child_join.nil?
+        end
+      end
+      r_val = r_val.joins(mf.join_statement) unless mf.join_statement.nil?
+    end
+    r_val
+  end
+  
+  private 
+  def apply_custom(mf,p)
+      k = (p.class == Class) ? p.where("1=1").klass : p.klass
+      cd = CustomDefinition.find(mf.custom_id)
+      a = "cf_#{mf.custom_id}"
+      p.joins("LEFT OUTER JOIN custom_values AS #{a} ON #{a}.customizable_type = '#{k}' and #{a}.customizable_id = #{mf.core_module.table_name}.id and #{a}.custom_definition_id = #{mf.custom_id}").
+        order("#{a}.#{cd.data_type}_value #{self.descending? ? "DESC" : "ASC"}")
   end
   
 end
