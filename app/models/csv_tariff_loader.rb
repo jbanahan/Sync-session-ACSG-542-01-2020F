352d152ba4f0a5ab338243d9ff4b237aebeeb8a7
Brian Glick <bglick@aspect9.com>
2011-02-16 22:12:12 -0500

    Changes to allow only hts_code & full description to load from customsinfo data

--- app/models/csv_tariff_loader.rb
+++ app/models/csv_tariff_loader.rb
@@ -6,13 +6,16 @@ class CsvTariffLoader
   end
   
   def process
+    #clear existing
+    OfficialTariff.where(:country_id=>@country).destroy_all
+    #load new
     CSV.foreach(@file_path, {:headers=>true}) do |row|
       OfficialTariff.create!(:country => @country,
         :hts_code => row[0].strip,
         :full_description => row[1].strip,
-        :special_rates => row[11].strip,
-        :unit_of_quantity => row[8].strip,
-        :general_rate => row[10].strip
+#        :special_rates => row[11].strip,
+#       :unit_of_quantity => row[8].strip,
+#       :general_rate => row[10].strip
       )
     end
   end
