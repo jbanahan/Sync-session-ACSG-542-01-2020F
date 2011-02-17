7a0f31f3662a7db7affad600d3f5e7ae43eacd49
Brian Glick <bglick@aspect9.com>
2011-02-17 15:31:03 -0500

    Initializers no longer failing silently, instead checking for table existence.

--- config/initializers/master_setup_init.rb
+++ config/initializers/master_setup_init.rb
@@ -1,15 +1,6 @@
-begin
+if ActiveRecord::Base.connection.tables.include?("master_setups")
   m = MasterSetup.first
   if m.nil?
     m = MasterSetup.create!(:uuid => UUIDTools::UUID.timestamp_create.to_s)
   end
-rescue
-  # Intentionally failing silently here.  There are three cases where we will
-  # hit an exception here:
-  # 1) Error connecting to the database: Something else will quickly fail louder
-  # 2) The master_setups table doesn't exist and we're starting up the server:
-  #    In this case, we'll throw a loud exception on the first page that tries to load
-  # 3) The master_setups table doesn't exist because it's a new database and we're running
-  #    rake db:migrate, in which case we don't want this to run yet because we're in the 
-  #    process of creating the database tables.
 end
--- config/initializers/prep_countries.rb
+++ config/initializers/prep_countries.rb
@@ -1,14 +1,3 @@
-begin
-  if Rails.env=='production'
-    Country.load_default_countries
-  end
-rescue
-  # Intentionally failing silently here.  There are three cases where we will
-  # hit an exception here:
-  # 1) Error connecting to the database: Something else will quickly fail louder
-  # 2) The countries table doesn't exist and we're starting up the server:
-  #    In this case, we'll throw a loud exception on the first page that tries to load
-  # 3) The countries table doesn't exist because it's a new database and we're running
-  #    rake db:migrate, in which case we don't want this to run yet because we're in the 
-  #    process of creating the database tables.
+if Rails.env=='production' && ActiveRecord::Base.connection.tables.include?("countries")
+  Country.load_default_countries
 end
