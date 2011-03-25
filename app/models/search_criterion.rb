3fc69a086d34ceeb95df9f4933c4f5e5029b5359
John Claus <johnclaus@gmail.com>
2011-03-24 21:26:49 -0600

    Ticket 83: Semi-optimized, initial pass at passes?

--- app/models/search_criterion.rb
+++ app/models/search_criterion.rb
@@ -24,7 +24,73 @@ class SearchCriterion < ActiveRecord::Base
   
   #does the given value pass the criterion test
   def passes?(value_to_test)
-    return value_to_test=="US"
+    mf = model_field
+    d = mf.data_type
+    
+    operators = {:eq => "eq", :co => "co", :sw => "sw", :ew => "ew", :null => "null", 
+      :notnull => "notnull", :gt => "gt", :lt => "lt"}
+    
+    if d == :string
+      if self.operator == operators[:eq]
+        return value_to_test == self.value
+      elsif self.operator == operators[:co]
+        return value_to_test.include?(self.value)
+      elsif self.operator == operators[:sw]
+        return value_to_test.start_with?(self.value)
+      elsif self.operator == operators[:ew]
+        return value_to_test.end_with?(self.value)
+      elsif self.operator == operators[:null]
+        return value_to_test.nil?(self.value)
+      elsif self.operator == operators[:notnull]
+        return !value_to_test.nil?(self.value)
+      end
+    elsif d == :text
+      if self.operator == operators[:eq]
+        return value_to_test == self.value
+      elsif self.operator == operators[:co]
+        return value_to_test.include?(self.value)
+      elsif self.operator == operators[:sw]
+        return value_to_test.start_with?(self.value)
+      elsif self.operator == operators[:ew]
+        return value_to_test.end_with?(self.value)
+      elsif self.operator == operators[:null]
+        return value_to_test.nil?(self.value)
+      elsif self.operator == operators[:notnull]
+        return !value_to_test.nil?(self.value)
+      end
+    elsif d == :date
+      if self.operator == operators[:eq]
+        return value_to_test == self.value
+      elsif self.operator == operators[:null]
+        return value_to_test.nil?(self.value)
+      elsif self.operator == operators[:notnull]
+        return !value_to_test.nil?(self.value)
+      end
+    elsif d == :boolean
+      if self.operator == operators[:eq]
+        return value_to_test == self.value
+      elsif self.operator == operators[:null]
+        return value_to_test.nil?(self.value)
+      elsif self.operator == operators[:notnull]
+        return !value_to_test.nil?(self.value)
+      end  
+    elsif d == :decimal
+      if self.operator == operators[:eq]
+        return value_to_test == self.value
+      elsif self.operator == operators[:gt]
+        return value_to_test > self.value
+      elsif self.operator == operators[:lt]
+        return value_to_test < self.value
+      end
+    elsif d == :integer
+      if self.operator == operators[:eq]
+        return value_to_test == self.value
+      elsif self.operator == operators[:gt]
+        return value_to_test > self.value
+      elsif self.operator == operators[:lt]
+        return value_to_test < self.value
+      end
+    end
   end
 
   private  
