a6f9fe10ddd6f1689a5d2f867bf45793f46c94ff
Brian Glick <bglick@aspect9.com>
2011-03-27 21:07:38 -0400

    First try

---
+++ app/models/FtpWalker.rb
@@ -0,0 +1,37 @@
+require 'timeout'
+require 'net/ftp'
+class FtpWalker
+
+  def go
+    debugger
+  #this will need a lot more config to be multi-user, multi-file, but just getting Vandegrift going right now
+    log = Logger.new(Rails.root.to_s+"/log/ftp.log")
+    tmp_files = []
+    begin
+      Timeout::timeout(90) {
+        f = Net::FTP.open('ftp.chain.io') {
+          f.debug_mode = true
+          f.passive = true
+          f.login 'chainroot', 'czft9918#'
+          f.chdir 'www-vfitrack-net/integration/to_chain/shipment/trackfeed'
+          file_list = f.nlst.sort
+          file_list.each do |fname|
+            t = Tempfile.new(fname)
+            f.getbinaryfile(fname,t.path)
+            tmp_files << t
+          end
+        }
+      } 
+    rescue Timeout::Error
+      log.error "FTP job timed out!"
+    end
+    s = SearchSetup.where(:name=>'trackfeed',:user_id=>User.where(:username=>"integration").first)
+    tmp_files.each do |tmp|
+      imp = s.imported_files.build(:filename => '',:size=>tmp.size,:content_type=>'text/csv',:ignore_first_row=>false)
+      imp.attached = File.new(tmp.path)
+      imp.save!
+      imp.process
+    end
+  end
+
+end
