6c369311770dbcef02989ea6ac78791a2a7b9433
John Claus <johnclaus@gmail.com>
2011-04-10 18:45:03 -0600

    Added search_error_email

--- app/mailers/error_mailer.rb
+++ app/mailers/error_mailer.rb
@@ -1,3 +1,8 @@
 class ErrorMailer < ActionMailer::Base
-  default :from => "from@example.com"
+  default :from => "notifications@aspect9.com"
+  
+  def search_error_email(user)
+    @user = user
+    mail(:to => user.email, :subject => "Aspect 9 - Search Error Notification")
+  end
 end
