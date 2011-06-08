1b981bc8500a3c383195fbf0a2c9d21467d28861
Brian Glick <bglick@aspect9.com>
2011-06-07 21:12:53 -0400

    FTP Sender default to binary & passive

--- app/models/ftp_sender.rb
+++ app/models/ftp_sender.rb
@@ -8,10 +8,11 @@ class FtpSender
   #  :folder (defaults to nil)
   #  :remote_file_name (defaults to local file name)
   def self.send_file server, username, password, file, opts = {}
-    my_opts = {:binary=>false}.merge opts
+    my_opts = {:binary=>true,:passive=>true}.merge opts
     remote_name = my_opts[:remote_file_name].nil? ? File.basename(file) : my_opts[:remote_file_name]
     Net::FTP.open(server) do |f|
       f.login(username,password)
+      f.passive = my_opts[:passive]
       f.chdir(my_opts[:folder]) if my_opts[:folder]
       if my_opts[:binary]
         f.putbinaryfile file, remote_name
