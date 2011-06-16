da1bd7347ec28bb4000456aa9cb61e5d5ec66049
Brian Glick <bglick@aspect9.com>
2011-06-16 14:45:58 -0400

    Ticket 213: Changed cache key to root folder + timestamp of restart.txt

--- config/environment.rb
+++ config/environment.rb
@@ -4,4 +4,6 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 OpenChain::Application.initialize!
 
+time_stamp = File.new('tmp/restart.txt').nil? ? Time.now : File.new('tmp/restart.txt').mtime
+CACHE = Dalli::Client.new ['localhost:11211'], {:namespace=>"#{time_stamp.to_i}#{Rails.root.basename}"}
 Mime::Type.register "application/vnd.ms-excel", :xls
--- config/initializers/zz_caching.rb
+++
@@ -1,7 +0,0 @@
-  #caching support
-  
-  require 'open_chain/test_extensions'
-
-  cache_class = Rails.env=="test" ? TestExtensions::MockCache : Dalli::Client
-
-  CACHE = cache_class.new ['localhost:11211'], {:namespace=>CacheManager.namespace} if MasterSetup.table_exists?
