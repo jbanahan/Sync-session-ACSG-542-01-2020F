161bb6526f6b0fda20442dce43d5ad319878f4e4
Brian Glick <bglick@aspect9.com>
2011-06-16 15:37:02 -0400

    Ticket 213: Fix to set CACHE before initializers run

--- config/environment.rb
+++ config/environment.rb
@@ -2,8 +2,8 @@
 require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
-OpenChain::Application.initialize!
-
 time_stamp = File.new('tmp/restart.txt').nil? ? Time.now : File.new('tmp/restart.txt').mtime
 CACHE = Dalli::Client.new ['localhost:11211'], {:namespace=>"#{time_stamp.to_i}#{Rails.root.basename}"}
+OpenChain::Application.initialize!
+
 Mime::Type.register "application/vnd.ms-excel", :xls
