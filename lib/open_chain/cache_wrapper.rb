a23a3100066a43b77d55bece374663ee62392bf0
Brian Glick <bglick@aspect9.com>
2011-07-07 17:43:09 -0400

    Wrapping CACHE in exception handler

---
+++ lib/open_chain/cache_wrapper.rb
@@ -0,0 +1,29 @@
+class CacheWrapper
+
+  def initialize(cache)
+    @cache = cache
+  end
+
+  def set key, val
+    error_wrap {@cache.set key, val}
+  end
+
+  def get key
+    error_wrap {@cache.get key}
+  end
+
+  def delete key
+    error_wrap {@cache.delete key}
+  end
+
+  private
+  def error_wrap &block
+    begin
+      yield
+    rescue
+      OpenMailer.delay.send_exception($!)
+      return nil
+    end
+  end
+
+end
