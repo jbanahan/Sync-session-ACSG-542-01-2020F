b3d234eedacaedeebdc51c20bcb4646bcea4a204
Brian Glick <bglick@aspect9.com>
2011-07-10 20:00:12 -0400

    Added comments to delayed_job_manager.rb

--- lib/open_chain/delayed_job_manager.rb
+++ lib/open_chain/delayed_job_manager.rb
@@ -1,9 +1,15 @@
 class DelayedJobManager
 
+  #check the operating system to see if delayed_job worker process(es) are running for this instance
   def self.running?
+    #use ps command to get all running processes filtered by "delayed_job", then make each row its own element in an array
     ps_jobs = %x{ps -Ao pid,args | grep delayed_job}.strip.split("\n")
+    #split each row in the array into compontent elements so we end up with an array of arrays like
+    #[[pid,command,other stuff],[pid,command,other stuff]]
     split_jobs = ps_jobs.collect {|j| j.strip.split}
     found = false
+    #for each job, if the command (sj[1]) was exactly "delayed_job", use the pwdx command to see if it was launched from our
+    #Rails.root directory.  If it was, then we know it's for this instance, so return true.  If not, then it's a different instance's worker, so keep moving
     split_jobs.each do |sj|
       if !found && sj[1]=="delayed_job"
         found = true if %x(pwdx #{sj[0]}).split[1].strip==Rails.root.to_s
@@ -12,14 +18,17 @@ class DelayedJobManager
     found
   end
 
+  #execute the system command to start the delayed_job worker process
   def self.start
     `script/delayed_job start`
   end
 
+  #execute the system command to stop the delayed_job worker process
   def self.stop
     `script/delayed_job stop`
   end
 
+  #checks the 'tmp/restart.txt' & 'tmp/stop.txt' files to see if we should have a worker job running
   def self.should_be_running?
     #if the restart file doesn't exist, we probably just installed 
     #and shouldn't run the background jobs yet
