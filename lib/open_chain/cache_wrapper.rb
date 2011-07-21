cf6acb07d3d44aa18b1df108b3c7fc0e00e39c50
John Claus <johnclaus@gmail.com>
2011-07-21 16:49:43 -0400

    Fixed CacheWrapper and CacheWrapperTest test cases

--- lib/open_chain/cache_wrapper.rb
+++ lib/open_chain/cache_wrapper.rb
@@ -23,10 +23,10 @@ class CacheWrapper
     @cache.reset
   end
   
-  def self.get_production_client relative_file_path
+  def self.get_production_client file_path = 'config/memcache.yml'
     # Load memcached settings from config/memcached.yml
     time_stamp = File.new('tmp/restart.txt').nil? ? Time.now : File.new('tmp/restart.txt').mtime
-    settings = YAML::load(File.open("#{Rails.root}#{relative_file_path}"))[Rails.env]
+    settings = File.exists?(file_path) ? YAML::load(File.open(file_path))[Rails.env] : {}
     settings = {"server"=>"localhost", "port"=>"11211"}.merge settings
     Dalli::Client.new(["#{settings["server"]}:#{settings["port"]}", {:namespace=>"#{time_stamp.to_i}#{Rails.root.basename}"}])
   end
--- test/unit/cache_wrapper_test.rb
+++ test/unit/cache_wrapper_test.rb
@@ -4,23 +4,26 @@ class CacheWrapperTest < ActiveSupport::TestCase
   test "read yaml file" do
     # Read memcached.yml and confirm you have valid server configs
     yml_file = Tempfile.new('readymltest')
-    #yml_file << "%{"
+    
     yml_file << "test:\n"
-    yml_file << "server: abc"
-    yml_file << "port: 999"
+    yml_file << "  server: abc\n"
+    yml_file << "  port: 999\n"
     
     yml_file.flush
     client = CacheWrapper.get_production_client(yml_file.path)
     def client.get_server
       @servers
     end
-    assert_equal "abc:999", client.get_server
+    assert client.get_server.include?("abc:999")
   end
   
   test "read default failovers" do
     # Try to read from an invalid file handle
-    client = CacheWrapper.get_production_client("/config/memcarched.yml")
+    client = CacheWrapper.get_production_client("idonotexist.yml")
     # Confirm memcached configs are the defaults
-    
+    def client.get_server
+      @servers
+    end
+    assert client.get_server.include?("localhost:11211")
   end
 end
