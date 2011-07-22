0d2d479421e2423f1b8de113e6df008eb3f178fe
Brian Glick <bglick@aspect9.com>
2011-07-22 09:42:45 -0400

    upgrade script typo fix

--- lib/open_chain/upgrade.rb
+++ lib/open_chain/upgrade.rb
@@ -1,40 +1,64 @@
 require 'open3'
 
-class Upgrade
+module OpenChain
+  class Upgrade
 
-  def self.upgrade target
-    get_source target
-    apply_upgrade
+    #Upgrades the current instance to the target git tag
+    #returns the absolute path to the upgrade log file
+    #raises and OpenChain::UpgradeFailure if there are any problems
+    def self.upgrade target
+      Upgrade.new(target).go
+    end
+
+    #do not initialize this method directly, use the static #upgrade method instead
+    def initialize target
+      @log_path = "#{Rails.root.to_s}/log/upgrade_#{Time.now}_#{target}.log" 
+      @log = Logger.new(@log_path)
+      @target = target
+    end
+    
+    #do not call this directly, use the static #upgrade method instead
+    def go
+      get_source 
+      apply_upgrade
+      @log_path
+    end
+
+    private
+    def get_source 
+      @log.info "Fetching source"
+      capture_and_log 'git fetch'
+      @log.info "Fetch complete, checking out #{@target}"
+      capture_and_log "git checkout #{@target}"
+      @log.info "Source checked out"
+    end
+
+    def apply_upgrade log
+      
+      @log.info "Touching stop.txt"
+      capture_and_log "touch tmp/stop.txt"
+      @log.info "Stopping Delayed Job"
+      DelayedJobManager.stop
+      raise UpgradeFailure.new("Delayed job should be stopped and is still running.") if DelayedJobManager.running?
+      @log.info "Delayed Job stopped, running bundle install"
+      capture_and_log "bundle install"
+      @log.info "Bundle complete, running migrations"
+      capture_and_log "rake db:migrate"
+      @log.info "Migration complete"
+      @log.info "Touching restart.txt"
+      capture_and_log "touch tmp/restart.txt"
+      @log.info "Upgrade complete"
+    end
+
+    def capture_and_log command
+      stdout, stderr, status = Open3.capture3 command
+      @log.info stdout.to_s
+      @log.info stderr.to_s
+      raise UpgradeFailure.new("#{command} failed: #{stderr}") unless status.success?
+    end
   end
 
-private
-  def self.get_source target
-    puts "Fetching source"
-    stdout, stderr, status = Open3.capture3 'git fetch'
-    raise "git fetch failed: #{stderr}" unless status.success?
-    puts "Source fetched, checking out #{target}"
-    stdout, stderr, status = Open3.capture3 "git checkout #{target}"
-    raise "git checkout failed: #{stderr}" unless status.success?
-    puts "Source checked out"
-  end
+  class UpgradeFailure < StandardError
 
-  def self.apply_upgrade
-    puts "Touching stop.txt"
-    `touch tmp/stop.txt`
-    puts "Stopping Delayed Job"
-    DelayedJobManager.stop
-    raise "Delayed job should be stopped and is still running." if DelayedJobManager.running?
-    puts "Delayed Job stopped, running bundle install"
-    stdout, stderr, status = Open3.capture3 "bundle install"
-    raise "Bundle install failed: #{stderr}" unless status.success?
-    puts "Bundle complete, running migrations"
-    stdout, stderr, status = Open3.capture3 "rake db:migrate"
-    puts stdout
-    raise "Migration failed: #{stderr}" unless status.success?
-    puts "Migration complete"
-    puts "Touching restart.txt"
-    `touch tmp/restart.txt`
-    puts "Upgrade complete"
   end
-
 end
