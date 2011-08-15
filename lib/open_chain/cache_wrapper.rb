8cc453b19ad07c9ebbf8a37469abf0c4faf9d7e1
Brian Glick <bglick@aspect9.com>
2011-08-15 15:29:38 -0400

    Cache uses version number instead of time for unique tag

--- lib/open_chain/cache_wrapper.rb
+++ lib/open_chain/cache_wrapper.rb
@@ -25,10 +25,10 @@ class CacheWrapper
   
   def self.get_production_client file_path = 'config/memcached.yml'
     # Load memcached settings from config/memcached.yml
-    time_stamp = File.new('tmp/restart.txt').nil? ? Time.now : File.new('tmp/restart.txt').mtime
+    ver = File.exists?('tmp/version.txt') ? IO.read('tmp/version.txt').strip : "NOVERSION" 
     settings = File.exists?(file_path) ? YAML::load(File.open(file_path))[Rails.env] : {}
     settings = {"server"=>"localhost", "port"=>"11211"}.merge settings
-    Dalli::Client.new(["#{settings["server"]}:#{settings["port"]}"], {:namespace=>"#{time_stamp.to_i}#{Rails.root.basename}"})
+    Dalli::Client.new(["#{settings["server"]}:#{settings["port"]}"], {:namespace=>"#{ver}#{Rails.root.basename}"})
   end
 
   private
