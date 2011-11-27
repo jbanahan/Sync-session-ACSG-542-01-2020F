1e56b144ac0dee1960d1ad07b01dfddfa99db6f2
Brian Glick <bglick@aspect9.com>
2011-11-27 11:30:32 -0500

    328: basic index page

--- app/controllers/tariff_sets_controller.rb
+++ app/controllers/tariff_sets_controller.rb
@@ -2,8 +2,11 @@ class TariffSetsController < ApplicationController
 
   def index 
     t = params[:country_id] ? TariffSet.where(:country_id=>params[:country_id]) : TariffSet.where(true)
-    t = t.order("tariff_sets.label DESC").to_a
-    render :json => t.to_json
+    t = t.includes(:country).order("countries.name ASC, tariff_sets.label DESC").to_a
+    respond_to do |format|
+      format.json {render :json => t.to_json}
+      format.html {@tariff_sets = t}#index.html.erb
+    end 
   end
 
 end
