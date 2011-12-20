77ddd1ebb70516a481b523af40eb3883183a5fe2
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2011-12-20 18:36:06 +0100

    Set default change records ordering

--- app/models/file_import_result.rb
+++ app/models/file_import_result.rb
@@ -1,7 +1,7 @@
 class FileImportResult < ActiveRecord::Base
   belongs_to :imported_file
   belongs_to :run_by, :class_name => "User"
-  has_many :change_records
+  has_many :change_records, :order => "failed DESC"
 
   after_save :update_changed_object_count
   
