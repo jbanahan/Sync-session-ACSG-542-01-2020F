8818af6fd72de3e6f6e98a7157e339a794f5fecc
Bosko Ivanisevic <bosko.ivanisevic@gmail.com>
2011-12-22 23:12:45 +0100

    Attachment handling

--- app/models/power_of_attorney.rb
+++ app/models/power_of_attorney.rb
@@ -1,4 +1,13 @@
 class PowerOfAttorney < ActiveRecord::Base
   belongs_to :company
   belongs_to :user, :foreign_key => :uploaded_by, :class_name => "User"
+
+  has_attached_file :attachment,
+    :storage => :fog,
+    :fog_credentials => FOG_S3,
+    :fog_public => false,
+    :fog_directory => 'chain-io',
+    :path => "#{MasterSetup.get.nil? ? "UNKNOWN" : MasterSetup.get.uuid}/power_of_attorney/:id/:filename" #conditional on MasterSetup to allow migrations to run
+
+  validates_attachment_presence
 end
