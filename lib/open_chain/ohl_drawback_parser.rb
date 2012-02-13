cc8dd3c2f156748d1562d85188b517c36613575a
Brian Glick <bglick@aspect9.com>
2012-02-13 14:11:31 -0500

    OHL drawback parser closes #500

---
+++ lib/open_chain/ohl_drawback_parser.rb
@@ -0,0 +1,67 @@
+module OpenChain
+  class OhlDrawbackParser
+    SOURCE_CODE = 'OHL Drawback'
+    # parse an OHL Drawback File (sample in spec/support/bin folder)
+    def self.parse file_path
+      sheet = Spreadsheet.open(file_path).worksheet(0) #always first sheet in workbook
+      rows = []
+      last_entry_num = nil
+      sheet.each 1 do |row| #start at row 1, skipping headers
+        entry_num = row[1]
+        if entry_num != last_entry_num && !rows.blank?
+          OhlDrawbackParser.new rows
+          rows = []
+        end
+        rows << row
+        last_entry_num = entry_num
+      end
+      OhlDrawbackParser.new rows unless rows.blank?
+      true #return true so we don't return the last parser used
+    end
+
+    # constructor, don't use, instead use static parse method
+    def initialize entry_rows
+      start_time = Time.now
+      process_header entry_rows.first
+      entry_rows.each {|r| process_details r}
+      @entry.save!
+      #set time to process in milliseconds without calling callbacks
+      @entry.connection.execute "UPDATE entries SET time_to_process = #{((Time.now-start_time) * 1000).to_i.to_s} WHERE ID = #{@entry.id}"
+    end
+
+    private 
+    def process_header row
+      importer_name = row[0]
+      raise "Importer Name is required." if importer_name.blank?
+      c = Company.find_or_create_by_name_and_importer importer_name, true
+      @entry = Entry.find_by_entry_number_and_importer_id row[1], c.id
+      @entry ||= Entry.new(:source_system=>SOURCE_CODE,:importer_id=>c.id,:import_country=>Country.find_by_iso_code('US'),:entry_number=>row[1])
+      @entry.commercial_invoices.destroy_all unless @entry.commercial_invoices.blank?
+      @invoice = @entry.commercial_invoices.build(:invoice_number=>'N/A') unless @invoice
+      @entry.entry_port_code = row[4]
+      @entry.arrival_date = datetime row[2]
+      @entry.mpf = row[7]
+      @entry.transport_mode_code = row[8].to_s
+      @entry.total_invoiced_value = row[9]
+      @entry.total_duty = row[10]
+      @entry.merchandise_description = row[11]
+    end
+    def process_details row
+      @line_number ||= 1
+      line = @invoice.commercial_invoice_lines.build(:line_number=>@line_number)
+      line.part_number = row[17]
+      line.po_number = row[15]
+      t = line.commercial_invoice_tariffs.build
+      t.duty_amount = row[19]
+      t.classification_qty_1 = row[20]
+      t.classification_uom_1 = row[21]
+      t.hts_code = row[14].gsub('.','') if row[14]
+      @line_number += 1
+    end
+    
+    #convert excel spreadsheet date column to US Eastern Date/Time
+    def datetime v
+      ActiveSupport::TimeZone["Eastern Time (US & Canada)"].parse(v.strftime("%Y-%m-%d")) unless v.nil?
+    end
+  end
+end
---
+++ spec/lib/ohl_drawback_parser_spec.rb
@@ -0,0 +1,86 @@
+require 'spec_helper'
+
+describe OpenChain::OhlDrawbackParser do
+  before :each do 
+    Factory(:country,:iso_code=>'US')
+    @est = ActiveSupport::TimeZone["Eastern Time (US & Canada)"]
+    @sample_path = 'spec/support/bin/ohl_drawback_sample.xls'
+    OpenChain::OhlDrawbackParser.parse @sample_path
+  end
+  it 'should create entries based on sample' do
+    Entry.all.should have(2).items
+    Entry.all.collect {|e| e.entry_number}.should == ['11350368418','11353554642']
+  end
+  it 'should map header fields' do
+    ent = Entry.find_by_entry_number '11350368418'
+    ent.entry_port_code.should == '1303'
+    ent.arrival_date.should == @est.parse('2010-12-27') 
+    ent.mpf.should == BigDecimal('139.20')
+    ent.transport_mode_code.should == '-1'
+    ent.total_invoiced_value.should == BigDecimal('66285.00') 
+    ent.total_duty.should == BigDecimal('15403.01')
+    ent.merchandise_description.should == 'WEARING APPAREL, FOOTWEAR'
+  end
+  it 'should handle empty mpf' do
+    ent = Entry.find_by_entry_number '11353554642'
+    ent.mpf.should be_nil
+  end
+  it 'should map invoice header fields' do
+    ent = Entry.find_by_entry_number '11350368418'
+    ent.should have(1).commercial_invoices
+    ci = ent.commercial_invoices.first
+    ci.invoice_number = 'N/A'
+  end
+  it 'should map invoice line fields' do
+    lines = Entry.find_by_entry_number('11353554642').commercial_invoices.first.commercial_invoice_lines
+    lines.should have(15).items
+    first_line = lines.first
+    first_line.part_number.should == '1216859-001'
+    first_line.po_number.should == '4500178680'
+    last_line = lines.last
+    last_line.part_number.should == '1217342-100'
+    last_line.po_number.should == '4500187813'
+  end
+  it 'should map hts fields' do
+    lines = Entry.find_by_entry_number('11353554642').commercial_invoices.first.commercial_invoice_lines
+    lines.each {|line| line.should have(1).commercial_invoice_tariffs}
+    first_hts = lines.first.commercial_invoice_tariffs.first
+    first_hts.hts_code.should == '6104632006'
+    first_hts.duty_amount.should == BigDecimal('0.00')
+    first_hts.classification_qty_1.should == BigDecimal('477')
+    first_hts.classification_uom_1.should == 'DOZ'
+
+    other_hts = Entry.find_by_entry_number('11350368418').commercial_invoices.first.commercial_invoice_lines.first.commercial_invoice_tariffs.first
+    other_hts.duty_amount.should == BigDecimal('26.35')
+    other_hts.classification_qty_1.should == BigDecimal('1')
+    other_hts.classification_uom_1.should == 'DOZ'
+  end
+  it 'should replace invoice lines' do
+    #process again
+    OpenChain::OhlDrawbackParser.parse @sample_path
+    lines = Entry.find_by_entry_number('11353554642').commercial_invoices.first.commercial_invoice_lines
+    lines.should have(15).items
+  end
+  it 'should create importer company' do
+    c = Company.find_by_name_and_importer('UNDER ARMOUR INC.',true)
+    Entry.all.each {|ent| ent.importer.should == c}
+  end
+  it 'should update existing entry' do
+    ent = Entry.find_by_entry_number('11350368418')
+    ent.update_attributes(:merchandise_description=>'X')
+    OpenChain::OhlDrawbackParser.parse @sample_path
+    found = Entry.find_by_entry_number('11350368418')
+    found.id.should == ent.id
+    found.merchandise_description.should == 'WEARING APPAREL, FOOTWEAR'
+    Entry.all.should have(2).items
+  end
+  it 'should map source' do
+    Entry.where(:source_system=>'OHL Drawback').all.should have(2).items
+  end
+  it 'should map import country' do
+    Entry.first.import_country.iso_code.should == 'US'
+  end
+  it 'should write time to process' do
+    Entry.first.time_to_process.should > 0
+  end
+end
