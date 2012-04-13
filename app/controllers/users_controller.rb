68739cc894c8050d6d99091865f453c70af61af7
Brian Glick <bglick@aspect9.com>
2012-04-13 09:27:16 -0400

    fix include? statement, closes #539

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,7 @@ class UsersController < ApplicationController
             companies = current_user.company.linked_companies.to_a
             companies << current_user.company
             master = Company.where(:master=>true).first
-            companies << master unless companies.includes?(master)
+            companies << master unless companies.include?(master)
           end
           render :json => companies.to_json(:only=>[:name],:include=>{:users=>{:only=>[:id,:first_name,:last_name],:methods=>:full_name}})
         }
