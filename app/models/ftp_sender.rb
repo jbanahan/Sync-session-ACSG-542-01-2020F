cb9b385499870720bb6e5cd93d58e63ef526b64f
Brian Glick <bglick@aspect9.com>
2012-04-22 10:47:52 -0400

    Added more loging to FTP Session, closes #546

--- app/models/ftp_sender.rb
+++ app/models/ftp_sender.rb
@@ -10,22 +10,31 @@ class FtpSender
   def self.send_file server, username, password, file, opts = {}
     my_opts = {:binary=>true,:passive=>true}.merge opts
     remote_name = my_opts[:remote_file_name].nil? ? File.basename(file) : my_opts[:remote_file_name]
+    log = ["Attempting to send FTP file."]
     Net::FTP.open(server) do |f|
+      log << "Opened connection to #{server}"
       f.login(username,password)
+      log << "Logged in with account #{username}"
       f.passive = my_opts[:passive]
-      f.chdir(my_opts[:folder]) if my_opts[:folder]
+      log << "Set passive to #{my_opts[:passive]}"
+      if my_opts[:folder]
+        f.chdir(my_opts[:folder]) 
+        log << "Changed to folder #{my_opts[:folder]}" 
+      end
       data = nil
       if my_opts[:binary]
         data = File.open(file, "rb") {|f| f.read}
         f.putbinaryfile file, remote_name
+        log << "Put binary file #{file.to_s} as #{remote_name}"
       else
         f.puttextfile file, remote_name
+        log << "Put text file #{file.to_s} as #{remote_name}"
         data = File.open(file, "r") {|f| f.read}
       end
       FtpSession.create!(:username => username,
                           :server => server,
                           :file_name => File.basename(file),
-                          :log => "Uploading file",
+                          :log => log.join("\n"),
                           :data => File.open(file, "rb") { |f| f.read })
     end
   end
