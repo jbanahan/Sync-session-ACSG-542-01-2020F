2549a4f0f2d9aa945456d6edb7426f80e6b59f10
Brian Glick <bglick@aspect9.com>
2012-05-11 10:30:01 -0400

    UA FMI Export Parser

--- lib/open_chain/under_armour_export_parser.rb
+++ lib/open_chain/under_armour_export_parser.rb
@@ -30,6 +30,35 @@ module OpenChain
       d
     end
 
+    def self.parse_fmi_csv_file file_path
+      count = 0
+      File.new(file_path).lines do |line|
+        parse_fmi_csv_line line unless count == 0
+        puts "Processed Line #{count}" if count.modulo(100)==0
+        count += 1
+      end
+    end
+
+    # parses line from FMI outbound format
+    def self.parse_fmi_csv_line line
+      d = DutyCalcExportFileLine.new
+      CSV.parse(line) do |r|
+        d.export_date = Date.strptime(r[4],"%m/%d/%Y")
+        d.ship_date = d.export_date
+        d.part_number = "#{r[6]}-#{r[8]}+#{r[1]}"
+        d.ref_1 = r[0]
+        d.ref_2 = r[3]
+        d.destination_country = r[11]
+        d.quantity = r[19]
+        d.description = r[7]
+        d.uom = "EA"
+        d.exporter = "Under Armour"
+        d.action_code = "E"
+      end
+      d.save!
+      d
+    end
+
 
     def self.numeric_to_string s
       s.to_s.ends_with?(".0") ? s.to_s[0,s.to_s.size-2] : s.to_s
--- spec/lib/under_armour_export_parser_spec.rb
+++ spec/lib/under_armour_export_parser_spec.rb
@@ -2,50 +2,98 @@ require 'spec_helper'
 
 describe OpenChain::UnderArmourExportParser do
   
-  before :each do
-    @lines = [
-      "SO Num,Delivery,Style,Color,Size,SKU,COO,Item Desc.,Units,Sold To Name,City,Ship To Name,State,Zip,Country,PT Sh Date,Ctn Sh Date,HTS,CCI,Cust.,Cust Line Value,Carton,",
-      "3452168,85439724,1000382,001,LG,85439724-1000382-001-LG-BD,BD,TECH TEE SS-BLK,4,A. ROY SPORTS,MONTREAL,A. ROY SPORTS,QC,H1B 2Y8,CA,20100305,20100305,6110.30.3060,5.53,$14.55,$58.20,8838140040135440,",
-      "3452168,85439724,1000382,001,MD,85439724-1000382-001-MD-BD,BD,TECH TEE SS-BLK,4,A. ROY SPORTS,MONTREAL,A. ROY SPORTS,QC,H1B 2Y8,CA,20100305,20100305,6110.30.3060,5.53,$14.55,$58.20,8838140040135440,"
-    ]
+  context "DH Exports" do
+    before :each do
+      @lines = [
+        "SO Num,Delivery,Style,Color,Size,SKU,COO,Item Desc.,Units,Sold To Name,City,Ship To Name,State,Zip,Country,PT Sh Date,Ctn Sh Date,HTS,CCI,Cust.,Cust Line Value,Carton,",
+        "3452168,85439724,1000382,001,LG,85439724-1000382-001-LG-BD,BD,TECH TEE SS-BLK,4,A. ROY SPORTS,MONTREAL,A. ROY SPORTS,QC,H1B 2Y8,CA,20100305,20100305,6110.30.3060,5.53,$14.55,$58.20,8838140040135440,",
+        "3452168,85439724,1000382,001,MD,85439724-1000382-001-MD-BD,BD,TECH TEE SS-BLK,4,A. ROY SPORTS,MONTREAL,A. ROY SPORTS,QC,H1B 2Y8,CA,20100305,20100305,6110.30.3060,5.53,$14.55,$58.20,8838140040135440,"
+      ]
+    end
+    describe :parse_csv_file do
+      it "should parse multi line csv ignoring headers" do
+        content = @lines.join("\n")
+        t = Tempfile.new("xyz")
+        t << content
+        t.flush
+        OpenChain::UnderArmourExportParser.parse_csv_file t.path
+        DutyCalcExportFileLine.all.should have(2).items
+      end
+    end
+
+    describe :parse_csv_line do
+      it "should parse a line into a DutyCalcExportFileLine" do
+        OpenChain::UnderArmourExportParser.parse_csv_line @lines[1]
+        DutyCalcExportFileLine.count.should == 1
+        d = DutyCalcExportFileLine.first
+        d.export_date.should == Date.new(2010,3,5)
+        d.ship_date.should == Date.new(2010,3,5)
+        d.part_number.should == "1000382-001-LG+BD"
+        d.carrier.should be_nil
+        d.ref_1.should == "85439724"
+        d.ref_2.should == "3452168"
+        d.ref_3.should be_nil
+        d.ref_4.should be_nil
+        d.destination_country.should == "CA"
+        d.quantity.should == 4
+        d.schedule_b_code.should == "6110303060"
+        d.description.should == "TECH TEE SS-BLK"
+        d.uom.should == "EA"
+        d.exporter.should == "Under Armour"
+        d.status.should be_nil
+        d.action_code.should == "E"
+        d.nafta_duty.should be_nil
+        d.nafta_us_equiv_duty.should be_nil
+        d.nafta_duty_rate.should be_nil
+        d.duty_calc_export_file.should be_nil
+      end
+    end
   end
-  describe :parse_csv_file do
-    it "should parse multi line csv ignoring headers" do
+
+  context "FMI Exports" do
+    before :each do
+      @lines = [
+        "PO Number,COO,COO,Customer Order (PO),TPM Shipped Date,TPM Orig Locn Id,Material,Material,AFS Grid,TPM Ship To Name,TPM Ship To ID,Country,Country,Ship-To Party,Ship-To Party,Address,City,Container #,TPM Master BoL,Shipped Qty,Net Price,Net Price",
+        "4500110966,CN,China,161051,1/7/2010,FMI Logistics LAX - HUBFMI,1211721-001,CA LOOSE BATTLE LS-BLK/GPH,LG,FORZANI GROUP LTD-DC CENTRE,20006049,CA,Canada,20006049,FORZANI GROUP LTD-DC CENTRE,#,MISSISSAUGA,41902,#,100,$877.00 ,$8.77",
+        "4500110966,CN,China,161051,1/7/2010,FMI Logistics LAX - HUBFMI,1211721-001,CA LOOSE BATTLE LS-BLK/GPH,MD,FORZANI GROUP LTD-DC CENTRE,20006049,CA,Canada,20006049,FORZANI GROUP LTD-DC CENTRE,#,MISSISSAUGA,41902,#,75,$657.75 ,$8.77"
+      ]
+    end
+    it "should parse file ignoring headers" do
       content = @lines.join("\n")
       t = Tempfile.new("xyz")
       t << content
       t.flush
-      OpenChain::UnderArmourExportParser.parse_csv_file t.path
+      OpenChain::UnderArmourExportParser.parse_fmi_csv_file t.path
       DutyCalcExportFileLine.all.should have(2).items
     end
-  end
 
-  describe :parse_csv_line do
-    it "should parse a line into a DutyCalcExportFileLine" do
-      OpenChain::UnderArmourExportParser.parse_csv_line @lines[1]
-      DutyCalcExportFileLine.count.should == 1
-      d = DutyCalcExportFileLine.first
-      d.export_date.should == Date.new(2010,3,5)
-      d.ship_date.should == Date.new(2010,3,5)
-      d.part_number.should == "1000382-001-LG+BD"
-      d.carrier.should be_nil
-      d.ref_1.should == "85439724"
-      d.ref_2.should == "3452168"
-      d.ref_3.should be_nil
-      d.ref_4.should be_nil
-      d.destination_country.should == "CA"
-      d.quantity.should == 4
-      d.schedule_b_code.should == "6110303060"
-      d.description.should == "TECH TEE SS-BLK"
-      d.uom.should == "EA"
-      d.exporter.should == "Under Armour"
-      d.status.should be_nil
-      d.action_code.should == "E"
-      d.nafta_duty.should be_nil
-      d.nafta_us_equiv_duty.should be_nil
-      d.nafta_duty_rate.should be_nil
-      d.duty_calc_export_file.should be_nil
+    describe :parse_fmi_csv_line do
+      it "should parse a line into a DutyCalcExportFileLine" do
+        OpenChain::UnderArmourExportParser.parse_fmi_csv_line @lines[1]
+        DutyCalcExportFileLine.count.should == 1
+        d = DutyCalcExportFileLine.first
+        d.export_date.should == Date.new(2010,1,7)
+        d.ship_date.should == Date.new(2010,1,7)
+        d.part_number.should == "1211721-001-LG+CN"
+        d.carrier.should be_nil
+        d.ref_1.should == "4500110966"
+        d.ref_2.should == "161051"
+        d.ref_3.should be_nil
+        d.ref_4.should be_nil
+        d.destination_country.should == "CA"
+        d.quantity.should == 100
+        d.schedule_b_code.should be_nil
+        d.description.should == "CA LOOSE BATTLE LS-BLK/GPH"
+        d.uom.should == "EA"
+        d.exporter.should == "Under Armour"
+        d.status.should be_nil
+        d.action_code.should == "E"
+        d.nafta_duty.should be_nil
+        d.nafta_us_equiv_duty.should be_nil
+        d.nafta_duty_rate.should be_nil
+        d.duty_calc_export_file.should be_nil
+      end
     end
   end
-
 end
+
