e8ce24536bf196f4548fdf82bb1c27b38a8926da
Brian Glick <brian@brian-glick.com>
2012-06-22 14:34:54 -0400

    Auto width for excel, #595

--- app/models/xls_maker.rb
+++ app/models/xls_maker.rb
@@ -7,18 +7,20 @@ class XlsMaker
                                           :pattern => 1
 
   def make_from_search(current_search, results)
+    @column_widths = {}
     cols = current_search.search_columns.order("rank ASC")
     wb = prep_workbook cols
     sheet = wb.worksheet 0
     row_number = 1
     GridMaker.new(results,cols,current_search.search_criterions,current_search.module_chain,current_search.user).go do |row,obj|
-      process_row sheet, row_number, row, obj
+        process_row sheet, row_number, row, obj
       row_number += 1
     end
     wb
   end
 
   def make_from_results results, columns, module_chain, user, search_criterions=[]
+    @column_widths = {}
     wb = prep_workbook columns
     sheet = wb.worksheet 0
     row_number = 1
@@ -34,10 +36,13 @@ class XlsMaker
     wb = Spreadsheet::Workbook.new
     sheet = wb.create_worksheet :name=>"Results"
     @date_format = Spreadsheet::Format.new(:number_format => 'YYYY-MM-DD')
-    cols.each do |c|
+    cols.each_with_index do |c,i|
       mf = ModelField.find_by_uid c.model_field_uid
+      label = mf.nil? ? "" : mf.label
       sheet.row(0).default_format = HEADER_FORMAT
-      sheet.row(0).push(mf.nil? ? "" : mf.label)
+      sheet.row(0).push(label)
+      @column_widths[i] = (label.size + 3 > 23 ? 23 : label.size + 3)
+      sheet.column(i).width = @column_widths[i]
     end
     wb
   end
@@ -45,7 +50,16 @@ class XlsMaker
   def process_row sheet, row_number, row_data, base_object
       row_data.each_with_index {|cell,col| 
         sheet.row(row_number).push(cell.nil? ? "" : cell)
-        sheet.row(row_number).set_format(col,@date_format) if cell.is_a? Date
+        width = cell.nil? ? 8 : cell.to_s.size + 3
+        if cell.is_a? Date
+          sheet.row(row_number).set_format(col,@date_format) 
+          width = 13
+        end
+        width = 23 if width > 23
+        if @column_widths[col] < width
+          sheet.column(col).width = width
+          @column_widths[col] = width
+        end
       }
   end
 end
