175feb404f1bab5298a63a0069099a911d919fcb
Brian Glick <brian@brian-glick.com>
2012-08-21 12:15:46 -0400

    AAFES parser

--- lib/open_chain/under_armour_export_parser.rb
+++ lib/open_chain/under_armour_export_parser.rb
@@ -59,6 +59,10 @@ module OpenChain
       d
     end
 
+    def self.parse_aafes_csv_file file_path
+      AafesParser.new(file_path).go
+    end
+
 
     def self.numeric_to_string s
       s.to_s.ends_with?(".0") ? s.to_s[0,s.to_s.size-2] : s.to_s
@@ -68,6 +72,41 @@ module OpenChain
       raise "Combined part number should have had 5 components and only had #{a.size}: #{combined_string}" unless a.size==5
       a.each{|x| raise "Combined part number should not have any empty components: #{combined_string}" if x.blank?} 
       "#{a[1]}-#{a[2]}-#{a[3]}+#{a[4]}"
+    end
+
+    class AafesParser
+      def initialize file_path
+        @file_path = file_path
+      end
+
+      def go
+        msgs = []
+        count = 0
+        CSV.foreach(@file_path) do |line|
+          m = parse_line line unless count == 0
+          msgs << m unless m.nil?
+          puts "Processed Line #{count}" if count.modulo(100)==0
+          count += 1
+        end
+        msgs
+      end
+
+      def parse_line line
+        style_color = line[0]
+        color = line[1]
+        date_pieces = line[5].split("/")
+        export_date = Date.new(date_pieces.last.to_i,date_pieces.first.to_i,date_pieces[1].to_i)
+        coo = find_country_of_origin style_color, export_date
+        return "Could not find country of origin for #{style_color} #{export_date}." if coo.blank?
+        DutyCalcExportFileLine.create!(:export_date=>export_date,:ship_date=>export_date,:ref_1=>line[3],:ref_2=>line[4],:ref_3=>"AAFES - NOT FOR ABI",
+          :destination_country=>line[12], :quantity=>line[18],:description=>line[15],:uom=>'EA',:action_code=>'E',:part_number=>"#{style_color}-#{line[1]}+#{coo}")
+        nil
+      end
+
+      def find_country_of_origin style_color, export_date
+        d_line = DrawbackImportLine.where("part_number LIKE ?","#{style_color}%").where("import_date <= ?",export_date).order("import_date DESC").limit(1).first
+        d_line.nil? ? nil : d_line.part_number.split("+").last
+      end
     end
   end
 end
--- spec/lib/under_armour_export_parser_spec.rb
+++ spec/lib/under_armour_export_parser_spec.rb
@@ -2,6 +2,51 @@ require 'spec_helper'
 
 describe OpenChain::UnderArmourExportParser do
   
+  context "AAFES Exports" do
+    before :each do
+      DrawbackImportLine.create!(:part_number=>"1000375-609-LG+CN",:import_date=>"2011-10-01",:quantity=>10,:product_id=>Factory(:product,:unique_identifier=>'1000375-609').id)
+      DrawbackImportLine.create!(:part_number=>"1000377-001-XXL+TW",:import_date=>"2011-10-01",:quantity=>10,:product_id=>Factory(:product,:unique_identifier=>'1000377-001').id)
+      DrawbackImportLine.create!(:part_number=>"1000377-001-XXL+MY",:import_date=>"2011-11-01",:quantity=>10,:product_id=>Product.find_by_unique_identifier('1000377-001').id)
+      @lines = [
+"Style,Color,UPC,PO Number,PO Line Number,Export Date,TCMD CONTAINER,VAN TCN,PO Received Date,Vendor,VENDOR NAME,Facility,ISO,FACNAME,Item,DESC,CRC,STYLE2,Units Received,Cost,Recd $",
+"1000375-609,LG,698611559156,0055404558,5,10/2/2011,MSKU 812950 ,HX7NNW-3903-S0622M2,11-Nov-11,60581514,UNDER ARMOUR,1375142,DE,GRAFENWOEHR MAIN STORE,470566840000001,UA MEN TOP MAROON LARGE,4947811,1000375609,20,$9.50 ,$190.00",
+"1000377-001,XXL,698611562095,0016052969,10,12/25/2011,APZU 474489 ,HX7NNW-4696-M0022M2,6-Feb-12,60581514,UNDER ARMOUR,1463665,KW,KW ARIFJAN Z6,470551939000013,TSHRT MEN XXL BLK,1387244,1000377-080 ,24,$11.88 ,$285.12", 
+"1000377-410,XL,698611562149,0014599787,2,8/14/2011,SBOP,,9-Oct-10,60581507,UNDER ARMOUR,1365550,DE,GE VILSECK PXTRA,470551939000012,TSHRT MEN XL MDN,1387243,1000377-080,6,$12.50 ,$75.00 "
+      ]
+      @t = Tempfile.new("xyz")
+      @t << @lines.join("\n") 
+      @t.flush
+      @messages = OpenChain::UnderArmourExportParser.parse_aafes_csv_file @t.path 
+    end
+    after :each do
+      @t.close
+    end
+    it "should parse multi line CSV" do
+      line = DutyCalcExportFileLine.find_by_part_number '1000375-609-LG+CN'
+      line.export_date.should == Date.new(2011,10,2)
+      line.ship_date.should == Date.new(2011,10,2)
+      line.ref_1.should == '0055404558'
+      line.ref_2.should == '5'
+      line.ref_3.should == "AAFES - NOT FOR ABI"
+      line.destination_country.should == 'DE'
+      line.quantity.should == 20
+      line.description.should == "UA MEN TOP MAROON LARGE"
+      line.uom.should == "EA"
+      line.action_code.should == 'E'
+      line.duty_calc_export_file_id.should be_nil
+    end
+    it "should pick country of origin for most recent import prior to export if multiple are found" do
+      DutyCalcExportFileLine.find_by_part_number('1000377-001-XXL+MY').should_not be_nil
+      DutyCalcExportFileLine.find_by_part_number('1000377-001-XXL+TW').should be_nil
+    end
+    it "should skip lines without country of origin match" do
+      DutyCalcExportFileLine.where("part_number like \"1000377-410%\"").should be_empty
+    end
+    it "should return skipped lines" do
+      @messages.should have(1).message
+      @messages.first.should include "Could not find country of origin for 1000377-410"
+    end
+  end
   context "DH Exports" do
     before :each do
       @lines = [
