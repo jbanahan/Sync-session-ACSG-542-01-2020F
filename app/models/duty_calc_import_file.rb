bde40ed9f49fae392b36b65e793edb045af3a349
Brian Glick <brian@brian-glick.com>
2012-08-22 13:33:37 -0400

    fix includes statement

--- app/models/duty_calc_import_file.rb
+++ app/models/duty_calc_import_file.rb
@@ -6,7 +6,7 @@ class DutyCalcImportFile < ActiveRecord::Base
     dif = DutyCalcImportFile.create!(:user_id=>user.id)
     DutyCalcImportFile.connection.execute("INSERT INTO duty_calc_import_file_lines (duty_calc_import_file_id,drawback_import_line_id) SELECT #{dif.id}, dil.id FROM drawback_import_lines dil left outer join duty_calc_import_file_lines difl ON dil.id = difl.drawback_import_line_id WHERE difl.id is NULL;")
     dif.reload
-    dif.duty_calc_import_file_lines.include(:drawback_import_line).each do |difl|
+    dif.duty_calc_import_file_lines.includes(:drawback_import_line).each do |difl|
       f << difl.drawback_import_line.duty_calc_line
     end
     f.close
