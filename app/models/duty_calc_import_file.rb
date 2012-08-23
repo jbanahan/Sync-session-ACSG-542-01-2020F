fa388d848be26cf38c6962b2a79f447680b5cf4c
Brian Glick <brian@brian-glick.com>
2012-08-23 10:19:44 -0400

    clean up include statement

--- app/models/duty_calc_import_file.rb
+++ app/models/duty_calc_import_file.rb
@@ -2,7 +2,7 @@ class DutyCalcImportFile < ActiveRecord::Base
   has_many :duty_calc_import_file_lines, :dependent=>:destroy
 
 
-  def self.generate_file user, file=Tempfile.new(['duty_calc_import_','.csv'])
+  def self.generate_file user, f=Tempfile.new(['duty_calc_import_','.csv'])
     dif = DutyCalcImportFile.create!(:user_id=>user.id)
     DutyCalcImportFile.connection.execute("INSERT INTO duty_calc_import_file_lines (duty_calc_import_file_id,drawback_import_line_id) SELECT #{dif.id}, dil.id FROM drawback_import_lines dil left outer join duty_calc_import_file_lines difl ON dil.id = difl.drawback_import_line_id WHERE difl.id is NULL;")
     dif.reload
