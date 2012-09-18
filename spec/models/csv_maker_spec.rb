3c06e6bcf4024a98a6c6a06ad7b58b76a0c3d8f0
Brian Glick <brian@brian-glick.com>
2012-09-18 13:51:29 -0400

    Remove carriage returns from CSV values

--- spec/models/csv_maker_spec.rb
+++ spec/models/csv_maker_spec.rb
@@ -10,4 +10,22 @@ describe CsvMaker do
     arrays = CSV.parse r
     arrays[1][0].should == "abc def"
   end
+  it "should strip carriage return characters" do
+    val = "abc\rdef"
+    Factory(:product,:unique_identifier=>val)
+    ss = Factory(:search_setup,:module_type=>"Product",:user=>Factory(:master_user))
+    ss.search_columns.create!(:model_field_uid=>'prod_uid')
+    r = CsvMaker.new.make_from_search(ss,ss.search)
+    arrays = CSV.parse r
+    arrays[1][0].should == "abc def"
+  end
+  it "should strip crlf" do
+    val = "abc\r\ndef"
+    Factory(:product,:unique_identifier=>val)
+    ss = Factory(:search_setup,:module_type=>"Product",:user=>Factory(:master_user))
+    ss.search_columns.create!(:model_field_uid=>'prod_uid')
+    r = CsvMaker.new.make_from_search(ss,ss.search)
+    arrays = CSV.parse r
+    arrays[1][0].should == "abc  def"
+  end
 end
