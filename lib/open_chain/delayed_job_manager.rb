49edade3b727ca2347e146fdf8793e5663d3b389
Brian Glick <brian@brian-glick.com>
2012-09-26 14:39:04 -0400

    system code in delayed_job monitor, closes #655

--- lib/open_chain/delayed_job_manager.rb
+++ lib/open_chain/delayed_job_manager.rb
@@ -47,7 +47,7 @@ class DelayedJobManager
     begin
       c = Delayed::Job.count
       oldest = Delayed::Job.find(:first, :order => 'created_at ASC')
-      raise "Delayed Job Queue Too Big: #{c} Items" if c > max_messages and oldest.created_at < 15.minutes.ago 
+      raise "#{MasterSetup.get.system_code} - Delayed Job Queue Too Big: #{c} Items" if c > max_messages and oldest.created_at < 15.minutes.ago 
     rescue
       @@dont_send_until = 30.minutes.from_now
       $!.log_me [], [], true #don't delay the send since we know that the queue is backed up
