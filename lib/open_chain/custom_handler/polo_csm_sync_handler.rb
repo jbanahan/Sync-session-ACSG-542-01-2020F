722b3cba5648e1801e4d0c92f189926bb9468e19
Brian Glick <brian@brian-glick.com>
2012-10-12 13:16:42 -0400

    rescue and save on error

--- lib/open_chain/custom_handler/polo_csm_sync_handler.rb
+++ lib/open_chain/custom_handler/polo_csm_sync_handler.rb
@@ -12,28 +12,33 @@ module OpenChain
         italy = Country.find_by_iso_code "IT"
         xlc = XLClient.new(@custom_file.attached.path)
         last_row = xlc.last_row_number(0)
-        (1..last_row).each do |n|
-          matched = 'not matched'
-          style = xlc.get_cell(0, n, 8)['cell']['value']
-          next if style.blank?
-          style.strip!
-          style = style[0,style.size-2] if style.end_with? '.0' #fix accidental numerics
-          p = Product.find_by_unique_identifier style
-          if !p.blank?
-            raise "User does not have permission to edit product #{p.unique_identifier}" unless p.can_edit? user
-            csm_number = string_val(xlc,0,n,2) + string_val(xlc,0,n,3) + string_val(xlc,0,n,4)
-            p.update_custom_value! @csm_cd, csm_number
-            p.create_snapshot user
-            matched = 'matched - no tariff'
-            italy_classification = p.classifications.find_by_country_id italy.id
-            if italy_classification
-              tr = italy_classification.tariff_records.first
-              matched = 'matched' if tr && !tr.hts_1.blank?
+        begin
+          (1..last_row).each do |n|
+            matched = 'not matched'
+            style = xlc.get_cell(0, n, 8)['cell']['value']
+            next if style.blank?
+            style.strip!
+            style = style[0,style.size-2] if style.end_with? '.0' #fix accidental numerics
+            p = Product.find_by_unique_identifier style
+            if !p.blank?
+              raise "User does not have permission to edit product #{p.unique_identifier}" unless p.can_edit? user
+              csm_number = string_val(xlc,0,n,2) + string_val(xlc,0,n,3) + string_val(xlc,0,n,4)
+              p.update_custom_value! @csm_cd, csm_number
+              p.create_snapshot user
+              matched = 'matched - no tariff'
+              italy_classification = p.classifications.find_by_country_id italy.id
+              if italy_classification
+                tr = italy_classification.tariff_records.first
+                matched = 'matched' if tr && !tr.hts_1.blank?
+              end
             end
+            xlc.set_cell 0, n, 16, matched
           end
-          xlc.set_cell 0, n, 16, matched
+          xlc.save
+        rescue
+          xlc.save
+          $!.log_me ["Custom File ID: #{@custom_file.id}","Last Row: #{last_row}"]
         end
-        xlc.save
         user.messages.create(:subject=>"CSM Sync Complete",:body=>"Your CSM Sync job has completed.  You can download the updated file <a href='/custom_features/csm_sync'>here</a>.")
       end
       private
