68bc68301be58addae72eda4e70886bb78bca195
Jeremy Hulford <jhulford@vandegriftinc.com>
2013-01-14 09:11:54 -0600

    Append user timezone to datetime search criterions. #730

--- app/controllers/search_setups_controller.rb
+++ app/controllers/search_setups_controller.rb
@@ -11,8 +11,12 @@ class SearchSetupsController < ApplicationController
   def update
     search_setup = SearchSetup.for_user(current_user).find(params[:id])
     if search_setup.nil?
-      error_redirect "Search wtih ID #{params[:id]} not found."
+      error_redirect "Search with ID #{params[:id]} not found."
     else
+      # Modify the search criterion values for date times for lt, gt, eq, ne operators
+      # adding in the user's time zone.
+      append_timezone_to_datetime params
+
       search_setup.search_columns.destroy_all if !params[:search_setup][:search_columns_attributes].blank? #clear, they will be reloaded
       search_setup.sort_criterions.destroy_all #clear, they will be reloaded
       search_setup.update_attributes(params[:search_setup])
@@ -64,5 +68,18 @@ class SearchSetupsController < ApplicationController
   def sticky_close
     current_user.update_attributes({:search_open=>false})
     render :text => ""
+  end
+
+  private 
+  def append_timezone_to_datetime p
+    p[:search_setup][:search_criterions_attributes].each do |id, criterion|
+      mf = ModelField.find_by_uid(criterion[:model_field_uid])
+      if mf.data_type == :datetime && SearchCriterion.date_time_operators_requiring_timezone.include?(criterion[:operator])
+
+        unless criterion[:value].nil? || criterion[:value].strip.length == 0
+          criterion[:value] = criterion[:value] + " " + Time.zone.name
+        end
+      end
+    end 
   end
 end
--- spec/models/search_criterion_spec.rb
+++ spec/models/search_criterion_spec.rb
@@ -77,25 +77,110 @@ describe SearchCriterion do
           sc = SearchCriterion.new(:model_field_uid=>:prod_created_at,:operator=>"pm",:value=>2)
           sc.apply(Product.where("1=1")).all.should include @product
         end
-        it "should find something using a string field from a list of values using unix newlines" do
-          sc = SearchCriterion.new(:model_field_uid=>:prod_uid, :operator=>"in", :value=>"val\n#{@product.unique_identifier}\nval2")
-          sc.apply(Product.where("1=1")).all.should include @product
-        end
-        it "should find something using a string field from a list of values using windows newlines" do
-          sc = SearchCriterion.new(:model_field_uid=>:prod_uid, :operator=>"in", :value=>"val\r\n#{@product.unique_identifier}\r\nval2")
-          sc.apply(Product.where("1=1")).all.should include @product
-        end
-        it "should not add blank strings in the IN list when using windows newlines" do
-          sc = SearchCriterion.new(:model_field_uid=>:prod_uid, :operator=>"in", :value=>"val\r\n#{@product.unique_identifier}\r\nval2")
-          sc.apply(Product.where("1=1")).to_sql.should match /\('val',\s?'#{@product.unique_identifier}',\s?'val2'\)/
-        end
-        it "should find something using a numeric field from a list of values" do
-          sc = SearchCriterion.new(:model_field_uid=>:prod_class_count, :operator=>"in", :value=>"1\n0\r\n3")
-          sc.apply(Product.where("1=1")).all.should include @product        
-        end
       end
     end
   end
+
+  context "string field IN list" do
+    it "should find something using a string field from a list of values using unix newlines" do
+      sc = SearchCriterion.new(:model_field_uid=>:prod_uid, :operator=>"in", :value=>"val\n#{@product.unique_identifier}\nval2")
+      sc.apply(Product.where("1=1")).all.should include @product
+    end
+    
+    it "should find something using a string field from a list of values using windows newlines" do
+      sc = SearchCriterion.new(:model_field_uid=>:prod_uid, :operator=>"in", :value=>"val\r\n#{@product.unique_identifier}\r\nval2")
+      sc.apply(Product.where("1=1")).all.should include @product
+    end
+    it "should not add blank strings in the IN list when using windows newlines" do
+      sc = SearchCriterion.new(:model_field_uid=>:prod_uid, :operator=>"in", :value=>"val\r\n#{@product.unique_identifier}\r\nval2")
+      sc.apply(Product.where("1=1")).to_sql.should match /\('val',\s?'#{@product.unique_identifier}',\s?'val2'\)/
+    end
+    it "should find something using a numeric field from a list of values" do
+      sc = SearchCriterion.new(:model_field_uid=>:prod_class_count, :operator=>"in", :value=>"1\n0\r\n3")
+      sc.apply(Product.where("1=1")).all.should include @product        
+    end
+  end
+  
+  context 'date time field' do
+    it "should translate datetime values to UTC for lt operator" do
+      # Run these as central timezone
+      tz = "Central Time (US & Canada)"
+      value = "2013-01-01 " + tz
+      expected_value = Time.use_zone(tz) do
+        Time.zone.parse(value).utc.to_formatted_s(:db)
+      end
+
+      sc = SearchCriterion.new(:model_field_uid=>:prod_created_at, :operator=>"lt", :value=>value)
+      sc.apply(Product.where("1=1")).to_sql.should =~ /#{expected_value}/
+    end
+      
+    it "should translate datetime values to UTC for gt operator" do
+      # Make sure we're also allowing actual time values as well
+      tz = "Central Time (US & Canada)"
+      value = "2012-01-01 07:08:09 " + tz
+      expected_value = Time.use_zone(tz) do
+      Time.zone.parse(value).utc.to_formatted_s(:db)
+      end
+      sc = SearchCriterion.new(:model_field_uid=>:prod_created_at, :operator=>"gt", :value=>value)
+      sc.apply(Product.where("1=1")).to_sql.should =~ /#{expected_value}/ 
+    end
+      
+    it "should translate datetime values to UTC for eq operator" do
+      # Make sure that if the timezone is not in the value, that we add eastern timezone to it
+      value = "2012-01-01"
+      sc = SearchCriterion.new(:model_field_uid=>:prod_created_at, :operator=>"eq", :value=>value)
+      expected_value = nil
+      Time.use_zone("Eastern Time (US & Canada)") do
+        expected_value = Time.zone.parse(value + " 00:00:00").utc.to_formatted_s(:db)
+      end
+
+      sc.apply(Product.where("1=1")).to_sql.should =~ /#{expected_value}/ 
+      
+      #verify the nq operator is translated too
+      sc.operator = "nq"
+      sc.apply(Product.where("1=1")).to_sql.should =~ /#{expected_value}/ 
+    end
+
+    it "should not translate date values to UTC for lt, gt, or eq operators" do
+      value = "2012-01-01"
+      # There's no actual date field in product, we'll use Entry.duty_due_date instead
+      sc = SearchCriterion.new(:model_field_uid=>:ent_duty_due_date, :operator=>"eq", :value=>value)
+      sc.apply(Entry.where("1=1")).to_sql.should =~ /#{value}/
+
+      sc.operator = "lt"
+      sc.apply(Entry.where("1=1")).to_sql.should =~ /#{value}/
+
+      sc.operator = "gt"
+      sc.apply(Entry.where("1=1")).to_sql.should =~ /#{value}/
+    end
+
+    it "should not translate datetime values to UTC for any operator other than lt, gt, eq, or nq" do
+      sc = SearchCriterion.new(:model_field_uid=>:prod_created_at, :operator=>"bda", :value=>10)
+      sc.apply(Entry.where("1=1")).to_sql.should =~ /10/
+      
+      sc.operator = "ada"
+      sc.apply(Entry.where("1=1")).to_sql.should =~ /10/
+      
+      sc.operator = "bdf"
+      sc.apply(Entry.where("1=1")).to_sql.should =~ /10/
+
+      sc.operator = "adf"
+      sc.apply(Entry.where("1=1")).to_sql.should =~ /10/
+
+      sc.operator = "pm"
+      sc.apply(Entry.where("1=1")).to_sql.should =~ /10/
+
+      sc.operator = "null"
+      sc.apply(Entry.where("1=1")).to_sql.should =~ /NULL/
+
+      sc.operator = "notnull"
+      sc.apply(Entry.where("1=1")).to_sql.should =~ /NOT NULL/
+
+
+      
+    end
+  end
+
   context 'boolean custom field' do
     before :each do
       @definition = Factory(:custom_definition,:data_type=>'boolean')
