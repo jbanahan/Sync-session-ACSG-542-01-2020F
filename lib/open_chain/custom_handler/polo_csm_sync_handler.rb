29a7c47a8e874f7b51e0f6c316b4656806dcce95
Brian Glick <brian@brian-glick.com>
2013-03-01 13:21:37 -0500

    interim commit #778

--- lib/open_chain/custom_handler/polo_csm_sync_handler.rb
+++ lib/open_chain/custom_handler/polo_csm_sync_handler.rb
@@ -5,76 +5,37 @@ module OpenChain
     class PoloCsmSyncHandler
       def initialize(custom_file)
         @custom_file = custom_file
-        @csm_cd = CustomDefinition.find_or_create_by_label("CSM Number",:module_type=>'Product',:data_type=>'string')
+        @csm_cd = CustomDefinition.find_or_create_by_label("CSM Number",:module_type=>'Product',:data_type=>'text')
       end
 
       def process user, first_row = 1
         had_errors = false
-        italy = Country.find_by_iso_code "IT"
-        us = Country.find_by_iso_code "US"
-        xlc = XLClient.new(@custom_file.attached.path)
-        xlc.raise_errors = true
-        last_row = xlc.last_row_number(0)
+        current_style = ''
         begin
+          xlc = XLClient.new(@custom_file.attached.path)
+          xlc.raise_errors = true
+          last_row = xlc.last_row_number(0)
+          style_map = {}
           (first_row..last_row).each do |n|
-            begin
-              matched = 'Style Not Found'
-              us_hts = nil
-              style = xlc.get_cell(0, n, 8)['cell']['value']
-              next if style.blank?
-              style = style.to_s
-              style.strip!
-              style = style[0,style.size-2] if style.end_with? '.0' #fix accidental numerics
-              p = Product.includes(:classifications=>:tariff_records).where(:unique_identifier=>style).first
-              unless p.blank?
-                raise "User does not have permission to edit product #{p.unique_identifier}" unless p.can_edit? user
-                matched = 'Style Found, No US / IT HTS'
-                csm_number = string_val(xlc,0,n,2) + string_val(xlc,0,n,3) + string_val(xlc,0,n,4)
-                csm_custom_val = p.get_custom_value @csm_cd
+            row_hash = xlc.get_row_as_column_hash 0, n
+            us_style_cell = row_hash[8]
+            us_style = (us_style_cell.blank? || us_style_cell['value'].blank?) ? nil : us_style_cell['value']
+            csm = get_csm_number row_hash
+            next if csm.blank? || us_style.blank?
+            raise "File failed: CSM Number at row #{n} was not 18 digits \"#{csm}\"" unless csm.size == 18
 
-                # We only want to insert csm_number into this product if the product doesn't already have a CSM Number
-                if csm_custom_val.value.blank?
-                  # Now we need to ensure that this CSM value isn't used on another product
-                  other_product = Product.joins(:custom_values).where(:custom_values => {:string_value => csm_number, :custom_definition_id=>@csm_cd.id}).first
-
-                  if other_product.nil?
-                    csm_custom_val.value = csm_number
-                    csm_custom_val.save!
-                    p.create_snapshot user
-                  else
-                    matched = "Rejected: CSM Number is already assigned to US Style #{other_product.unique_identifier}"
-                  end
-                elsif csm_custom_val.value != csm_number
-                  # This product already has a different CSM number, reject it
-                  matched = "Rejected: US Style already has the CSM Number #{csm_custom_val.value} assigned."
-                end
-                italy_classification = p.classifications.find_by_country_id italy.id
-                if italy_classification
-                  tr = italy_classification.tariff_records.first
-                  matched = 'Style Found with IT HTS' if tr && !tr.hts_1.blank?
-                else
-                  us_classification = p.classifications.find_by_country_id(us.id)
-                  if us_classification && us_classification.tariff_records.first && !us_classification.tariff_records.first.hts_1.blank?
-                    us_hts = us_classification.tariff_records.first.hts_1.hts_format
-                    matched = "Style Found, No IT HTS"
-                  end
-                end
-              end
-              xlc.set_cell 0, n, 16, matched
-              xlc.set_cell 0, n, 17, us_hts.hts_format unless us_hts.nil?
-            rescue
-              raise $! if $!.message.include?("User does not have permission to edit product")
-              had_errors = true
-              xlc.set_cell 0, n, 16, "Error. Contact support."
-              $!.log_me ["Custom File ID: #{@custom_file.id}","Last Row: #{last_row}"]
-            end
+            style_map[us_style] ||= Set.new
+            style_map[us_style] << csm
           end
-          xlc.save
+          style_map.each do |us_style,csm_set|
+            current_style = us_style
+            p = Product.where(:unique_identifier=>us_style).first_or_create!
+            raise "File failed: #{user.full_name} can't edit product #{p.unique_identifier}" unless p.can_edit?(user)
+            p.update_custom_value! @csm_cd, csm_set.to_a.join("\n")
+          end
         rescue
           had_errors = true
-          xlc.save
-          $!.log_me ["Custom File ID: #{@custom_file.id}","Last Row: #{last_row}"]
-          raise $! if $!.message.include?("User does not have permission to edit product")
+          $!.log_me ["Custom File ID: #{@custom_file.id}","Style: #{current_style}"]
         end
         if had_errors
           user.messages.create(:subject=>"CSM Sync Complete with Errors",:body=>"<p>Your CSM Sync job has completed.  You can download the updated file <a href='/custom_features/csm_sync'>here</a>.</p><p>Support has received notification regarding the errors in your file and will be researching them.</p>")
@@ -83,8 +44,17 @@ module OpenChain
         end
       end
       private
-      def string_val xlc, sheet, row, cell
-        r = xlc.get_cell(sheet,row,cell)['cell']['value']
+      def get_csm_number row_hash
+        r = ""
+        (2..5).each do |i|
+          cell_hash = row_hash[i]
+          return nil unless cell_hash && cell_hash['value']
+          r << string_val(cell_hash['value'])
+        end
+        r
+      end
+      def string_val val 
+        r = val
         return r if r.nil?
         r = r[0,r.size-2] if r.end_with? '.0' #fix numerics
         r
--- spec/lib/custom_handler/polo_csm_sync_hander_spec.rb
+++ spec/lib/custom_handler/polo_csm_sync_hander_spec.rb
@@ -2,8 +2,6 @@ require 'spec_helper'
 
 describe OpenChain::CustomHandler::PoloCsmSyncHandler do
   before :each do
-    @italy = Factory(:country,:iso_code=>"IT")
-    @us = Factory(:country,:iso_code=>"US")
     @xlc = mock('xl_client')
     @xlc.stub(:raise_errors=)
     @cf = mock('custom_file')
@@ -12,139 +10,123 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
     @cf.stub(:attached).and_return(@att)
     @cf.stub(:update_attributes)
     OpenChain::XLClient.should_receive(:new).with('/path/to').and_return(@xlc)
-    @csm = Factory(:custom_definition,:module_type=>'Product',:label=>"CSM Number",:data_type=>'string')
-  end
-  it "should update lines that match" do
-    p = Factory(:product)
-    p.classifications.create!(:country_id=>@italy.id).tariff_records.create!(:hts_1=>"1234567890",:line_number=>1)
-    p2 = Factory(:product)
-    p3 = Factory(:product)
-    p3.classifications.create!(:country_id=>@us.id).tariff_records.create!(:hts_1=>'1234567890',:line_number=>1)
-    p1_csm = "ABC123XYZ"
-    p2_csm = "DEF654GHI"
-    p3_csm = 'GHI123ABC'
+    @csm = Factory(:custom_definition,:module_type=>'Product',:label=>"CSM Number",:data_type=>'text')
+    @h = described_class.new @cf 
     Product.any_instance.stub(:can_edit?).and_return(true)
+  end
+
+  # CSM Number is columns C-F in the source spreadsheet concatenated
+  # US Style Number is column I
+
+  it "should set CSM numbers for existing product with no CSM custom value" do
+    p = Factory(:product)
+    @xlc.should_receive(:last_row_number).and_return(2)
+    @xlc.should_receive(:get_row_as_column_hash).with(0,1).and_return(
+      2=>{'value'=>'140','datatype'=>'string'},
+      3=>{'value'=>'ABCDE','datatype'=>'string'},
+      4=>{'value'=>'FGHIJ','datatype'=>'string'},
+      5=>{'value'=>'KLMNO','datatype'=>'string'},
+      8=>{'value'=>p.unique_identifier,'datatype'=>'string'}
+    )
+    @xlc.should_receive(:get_row_as_column_hash).with(0,2).and_return(
+      2=>{'value'=>'ZZZ','datatype'=>'string'},
+      3=>{'value'=>'PQRST','datatype'=>'string'},
+      4=>{'value'=>'UVWXY','datatype'=>'string'},
+      5=>{'value'=>'Z1234','datatype'=>'string'},
+      8=>{'value'=>p.unique_identifier,'datatype'=>'string'}
+    )
+    @h.process Factory(:user)
+    p.get_custom_value(@csm).value.should == "140ABCDEFGHIJKLMNO\nZZZPQRSTUVWXYZ1234"
+  end
+  it "should include non-contiguous CSM numbers for a product" do
+    p = Factory(:product)
     @xlc.should_receive(:last_row_number).and_return(3)
-    @xlc.should_receive(:get_cell).with(0,1,8).and_return({'cell'=>{'value'=>p.unique_identifier}})
-    @xlc.should_receive(:get_cell).with(0,1,2).and_return({'cell'=>{'value'=>'ABC'}})
-    @xlc.should_receive(:get_cell).with(0,1,3).and_return({'cell'=>{'value'=>'123'}})
-    @xlc.should_receive(:get_cell).with(0,1,4).and_return({'cell'=>{'value'=>'XYZ'}})
-    @xlc.should_receive(:set_cell).with(0,1,16,'Style Found with IT HTS')
-    @xlc.should_receive(:get_cell).with(0,2,8).and_return({'cell'=>{'value'=>p2.unique_identifier}})
-    @xlc.should_receive(:get_cell).with(0,2,2).and_return({'cell'=>{'value'=>'DEF'}})
-    @xlc.should_receive(:get_cell).with(0,2,3).and_return({'cell'=>{'value'=>'654'}})
-    @xlc.should_receive(:get_cell).with(0,2,4).and_return({'cell'=>{'value'=>'GHI'}})
-    @xlc.should_receive(:set_cell).with(0,2,16,'Style Found, No US / IT HTS')
-    @xlc.should_receive(:get_cell).with(0,3,8).and_return({'cell'=>{'value'=>p3.unique_identifier}})
-    @xlc.should_receive(:get_cell).with(0,3,2).and_return({'cell'=>{'value'=>'GHI'}})
-    @xlc.should_receive(:get_cell).with(0,3,3).and_return({'cell'=>{'value'=>'123'}})
-    @xlc.should_receive(:get_cell).with(0,3,4).and_return({'cell'=>{'value'=>'ABC'}})
-    @xlc.should_receive(:set_cell).with(0,3,16,"Style Found, No IT HTS")
-    @xlc.should_receive(:set_cell).with(0,3,17,p3.classifications.first.tariff_records.first.hts_1.hts_format)
-
-    @xlc.should_receive(:save)
-    OpenChain::CustomHandler::PoloCsmSyncHandler.new(@cf).process Factory(:user)
-    p.get_custom_value(@csm).value.should == p1_csm 
-    p2.get_custom_value(@csm).value.should == p2_csm
-    p3.get_custom_value(@csm).value.should == p3_csm
-    p.should have(1).entity_snapshots
+    @xlc.should_receive(:get_row_as_column_hash).with(0,1).and_return(
+      2=>{'value'=>'140','datatype'=>'string'},
+      3=>{'value'=>'ABCDE','datatype'=>'string'},
+      4=>{'value'=>'FGHIJ','datatype'=>'string'},
+      5=>{'value'=>'KLMNO','datatype'=>'string'},
+      8=>{'value'=>p.unique_identifier,'datatype'=>'string'}
+    )
+    @xlc.should_receive(:get_row_as_column_hash).with(0,2).and_return(
+      2=>{'value'=>'140','datatype'=>'string'},
+      3=>{'value'=>'ABCDE','datatype'=>'string'},
+      4=>{'value'=>'FGHIJ','datatype'=>'string'},
+      5=>{'value'=>'KLMNO','datatype'=>'string'},
+      8=>{'value'=>'something else','datatype'=>'string'}
+    )
+    @xlc.should_receive(:get_row_as_column_hash).with(0,3).and_return(
+      2=>{'value'=>'ZZZ','datatype'=>'string'},
+      3=>{'value'=>'PQRST','datatype'=>'string'},
+      4=>{'value'=>'UVWXY','datatype'=>'string'},
+      5=>{'value'=>'Z1234','datatype'=>'string'},
+      8=>{'value'=>p.unique_identifier,'datatype'=>'string'}
+    )
+    @h.process Factory(:user)
+    p.get_custom_value(@csm).value.should == "140ABCDEFGHIJKLMNO\nZZZPQRSTUVWXYZ1234"
+    Product.find_by_unique_identifier('something else').get_custom_value(@csm).value.should == "140ABCDEFGHIJKLMNO"
   end
-  it "should not update with csm numbers that are on different products" do
+  it "should create new CSM number for new product" do
+    @xlc.should_receive(:last_row_number).and_return(1)
+    @xlc.should_receive(:get_row_as_column_hash).with(0,1).and_return(
+      2=>{'value'=>'140','datatype'=>'string'},
+      3=>{'value'=>'ABCDE','datatype'=>'string'},
+      4=>{'value'=>'FGHIJ','datatype'=>'string'},
+      5=>{'value'=>'KLMNO','datatype'=>'string'},
+      8=>{'value'=>'something else','datatype'=>'string'}
+    )
+    @h.process Factory(:user)
+    Product.find_by_unique_identifier('something else').get_custom_value(@csm).value.should == "140ABCDEFGHIJKLMNO"
+  end
+  it "should drop existing CSM numbers not in file" do
     p = Factory(:product)
-    csm = p.get_custom_value @csm
-    csm.value = "ABC123XYZ"
-    csm.save!
-
-    p2 = Factory(:product)
-    Product.any_instance.stub(:can_edit?).and_return(true)
-
+    p.update_custom_value!(@csm,'XZY')
     @xlc.should_receive(:last_row_number).and_return(1)
-    @xlc.should_receive(:get_cell).with(0,1,8).and_return({'cell'=>{'value'=>p2.unique_identifier}})
-    @xlc.should_receive(:get_cell).with(0,1,2).and_return({'cell'=>{'value'=>'ABC'}})
-    @xlc.should_receive(:get_cell).with(0,1,3).and_return({'cell'=>{'value'=>'123'}})
-    @xlc.should_receive(:get_cell).with(0,1,4).and_return({'cell'=>{'value'=>'XYZ'}})
-    @xlc.should_receive(:set_cell).with(0,1,16,"Rejected: CSM Number is already assigned to US Style #{p.unique_identifier}")
-    @xlc.should_receive(:save)
-
-    OpenChain::CustomHandler::PoloCsmSyncHandler.new(@cf).process Factory(:user)
-    p2.get_custom_value(@csm).value.should be_nil 
+    @xlc.should_receive(:get_row_as_column_hash).with(0,1).and_return(
+      2=>{'value'=>'140','datatype'=>'string'},
+      3=>{'value'=>'ABCDE','datatype'=>'string'},
+      4=>{'value'=>'FGHIJ','datatype'=>'string'},
+      5=>{'value'=>'KLMNO','datatype'=>'string'},
+      8=>{'value'=>p.unique_identifier,'datatype'=>'string'}
+    )
+    @h.process Factory(:user)
+    p.get_custom_value(@csm).value.should == "140ABCDEFGHIJKLMNO"
   end
-
-  it "should update if csm number is in a different custom value" do
-    other_cd = Factory(:custom_definition)
+  it "should fail if CSM number is not 18 digits" do
+    @cf.stub(:id).and_return(1)
     p = Factory(:product)
-    p.update_custom_value! other_cd, "ABC123XYZ"
-
-    p2 = Factory(:product)
-    Product.any_instance.stub(:can_edit?).and_return(true)
-
     @xlc.should_receive(:last_row_number).and_return(1)
-    @xlc.should_receive(:get_cell).with(0,1,8).and_return({'cell'=>{'value'=>p2.unique_identifier}})
-    @xlc.should_receive(:get_cell).with(0,1,2).and_return({'cell'=>{'value'=>'ABC'}})
-    @xlc.should_receive(:get_cell).with(0,1,3).and_return({'cell'=>{'value'=>'123'}})
-    @xlc.should_receive(:get_cell).with(0,1,4).and_return({'cell'=>{'value'=>'XYZ'}})
-    @xlc.should_receive(:set_cell).with(0,1,16,'Style Found, No US / IT HTS')
-    @xlc.should_receive(:save)
-
-    OpenChain::CustomHandler::PoloCsmSyncHandler.new(@cf).process Factory(:user)
-    p2.get_custom_value(@csm).value.should == "ABC123XYZ"
+    @xlc.should_receive(:get_row_as_column_hash).with(0,1).and_return(
+      2=>{'value'=>'140XX','datatype'=>'string'},
+      3=>{'value'=>'ABCDE','datatype'=>'string'},
+      4=>{'value'=>'FGHIJ','datatype'=>'string'},
+      5=>{'value'=>'KLMNO','datatype'=>'string'},
+      8=>{'value'=>p.unique_identifier,'datatype'=>'string'}
+    )
+    Exception.any_instance.should_receive(:log_me)
+    @h.process Factory(:user)
+    p.get_custom_value(@csm).value.should be_blank 
   end
-  
-  it "should warn if product already has a different CSM number" do
+  it "should not fail for empty lines" do
+    @xlc.should_receive(:last_row_number).and_return(1)
+    @xlc.should_receive(:get_row_as_column_hash).with(0,1).and_return({})
+    Exception.any_instance.should_not_receive(:log_me)
+    @h.process Factory(:user)
+  end
+  it "should fail if user cannot edit products" do
+    Product.any_instance.stub(:can_edit?).and_return false
     p = Factory(:product)
-    csm = p.get_custom_value @csm
-    csm.value = "1234567890"
-    csm.save!
-
-    Product.any_instance.stub(:can_edit?).and_return(true)
-
+    @cf.stub(:id).and_return(1)
     @xlc.should_receive(:last_row_number).and_return(1)
-    @xlc.should_receive(:get_cell).with(0,1,8).and_return({'cell'=>{'value'=>p.unique_identifier}})
-    @xlc.should_receive(:get_cell).with(0,1,2).and_return({'cell'=>{'value'=>'ABC'}})
-    @xlc.should_receive(:get_cell).with(0,1,3).and_return({'cell'=>{'value'=>'123'}})
-    @xlc.should_receive(:get_cell).with(0,1,4).and_return({'cell'=>{'value'=>'XYZ'}})
-    @xlc.should_receive(:set_cell).with(0,1,16,"Rejected: US Style already has the CSM Number #{csm.value} assigned.")
-    @xlc.should_receive(:save)
-
-    OpenChain::CustomHandler::PoloCsmSyncHandler.new(@cf).process Factory(:user)
-    p.get_custom_value(@csm).value.should == csm.value
-  end
-  
-  it "should not save products where csm number has not changed" do
-    Product.any_instance.stub(:can_edit?).and_return(true)
-    d = 1.day.ago
-    p = Factory(:product)
-    p.update_custom_value! @csm, 'ABC123XYZ'
-    p.update_attributes(:updated_at=>d)
-    @xlc.should_receive(:last_row_number).and_return(1)
-    @xlc.should_receive(:get_cell).with(0,1,8).and_return({'cell'=>{'value'=>p.unique_identifier}})
-    @xlc.should_receive(:get_cell).with(0,1,2).and_return({'cell'=>{'value'=>'ABC'}})
-    @xlc.should_receive(:get_cell).with(0,1,3).and_return({'cell'=>{'value'=>'123'}})
-    @xlc.should_receive(:get_cell).with(0,1,4).and_return({'cell'=>{'value'=>'XYZ'}})
-    @xlc.should_receive(:set_cell).with(0,1,16,'Style Found, No US / IT HTS')
-    @xlc.should_receive(:save)
-    OpenChain::CustomHandler::PoloCsmSyncHandler.new(@cf).process Factory(:user)
-    p.reload
-    p.get_custom_value(@csm).value.should == 'ABC123XYZ'
-    p.entity_snapshots.should be_empty
-    p.updated_at.to_i.should == d.to_i
-  end
-  it "should not update lines that don't match" do
-    @xlc.should_receive(:last_row_number).and_return(1)
-    @xlc.should_receive(:get_cell).with(0,1,8).and_return({'cell'=>{'value'=>'abc'}})
-    @xlc.should_receive(:set_cell).with(0,1,16,'Style Not Found')
-    @xlc.should_receive(:save)
-    OpenChain::CustomHandler::PoloCsmSyncHandler.new(@cf).process Factory(:user)
-  end
-  context :security do
-    it "should not allow users who can't edit products" do
-      p = Factory(:product)
-      Product.any_instance.stub(:can_edit?).and_return(false)
-      @xlc.stub(:save)
-      @cf.stub(:id).and_return("1")
-      @xlc.should_receive(:last_row_number).and_return(2)
-      @xlc.should_receive(:get_cell).with(0,1,8).and_return({'cell'=>{'value'=>p.unique_identifier}})
-      lambda {OpenChain::CustomHandler::PoloCsmSyncHandler.new(@cf).process Factory(:user)}.should raise_error "User does not have permission to edit product #{p.unique_identifier}"
-    end
+    @xlc.should_receive(:get_row_as_column_hash).with(0,1).and_return(
+      2=>{'value'=>'140','datatype'=>'string'},
+      3=>{'value'=>'ABCDE','datatype'=>'string'},
+      4=>{'value'=>'FGHIJ','datatype'=>'string'},
+      5=>{'value'=>'KLMNO','datatype'=>'string'},
+      8=>{'value'=>p.unique_identifier,'datatype'=>'string'}
+    )
+    Exception.any_instance.should_receive(:log_me)
+    @h.process Factory(:user)
+    p.get_custom_value(@csm).value.should be_blank 
   end
 end
