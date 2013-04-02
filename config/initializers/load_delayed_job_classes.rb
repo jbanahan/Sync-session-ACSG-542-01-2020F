0860c13d4b162dee826841cd3fd6e5b976f7ec82
Brian Glick <brian@brian-glick.com>
2013-04-02 08:51:35 -0400

    change max run time for delayed_job, closes #795

--- config/initializers/load_delayed_job_classes.rb
+++ config/initializers/load_delayed_job_classes.rb
@@ -1,3 +1,6 @@
+#Set delayed job worker timeout
+Delayed::Worker.max_run_time = 12.hours
+
 #reference any classes that will be sent to delayed job here to make sure the environment is aware of them
 
 require 'open_chain/bulk_update'
