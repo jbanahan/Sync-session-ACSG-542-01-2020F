bbe19edace4553cd59989a5cc50184bf84a0ba7a
Brian Glick <brian@brian-glick.com>
2013-04-03 15:24:27 -0400

    disable delayed job timeout override

--- config/initializers/load_delayed_job_classes.rb
+++ config/initializers/load_delayed_job_classes.rb
@@ -1,5 +1,5 @@
 #Set delayed job worker timeout
-Delayed::Worker.max_run_time = 12.hours
+#Delayed::Worker.max_run_time = 12.hours
 
 #reference any classes that will be sent to delayed job here to make sure the environment is aware of them
 
