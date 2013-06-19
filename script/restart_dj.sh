f3b1292d89348ca98a4db2f0126c9d36ee65e576
Brian Glick <brian@brian-glick.com>
2013-06-19 15:27:54 -0400

    2013.3.6

--- script/restart_dj.sh
+++ script/restart_dj.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
 ls_out=`lsof -t ./log/delayed_job.log`
-restart_count=`cat ./config/dj_count.txt`
-if [[ $restart_count > 0 ]]; then
-  echo "Restarting with $restart_count jobs."
-else
-  restart_count=1
-  echo "Restarting with $restart_count jobs."
-fi
 
 for i in ${ls_out}
 	do
@@ -15,4 +8,3 @@ for i in ${ls_out}
 		kill -9 $i
 	done
 
-./script/delayed_job start -n $restart_count
