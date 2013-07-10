cbfd0aa4ff1b09bb217628e9dc6d0eaacda815e8
Jeremy Hulford <jhulford@vandegriftinc.com>
2013-07-10 10:04:00 -0500

    Fix test case. #822

--- spec/models/ftp_session_spec.rb
+++ spec/models/ftp_session_spec.rb
@@ -6,7 +6,7 @@ describe FtpSession do
     it "should report ftp codes starting with 2 followed by other digits as successful" do
       f = FtpSession.new
       [200..299].each do |i|
-        f.status_code = "#{i} This is a test."
+        f.last_server_response = "#{i} This is a test."
         f.successful?.should be_true
       end
     end
@@ -14,15 +14,15 @@ describe FtpSession do
     it "should report all non 2xx codes as unsuccesful" do
       f = FtpSession.new
       f.successful?.should be_false
-      f.status_code = ""
+      f.last_server_response = ""
       f.successful?.should be_false
-      f.status_code = "400 Error"
+      f.last_server_response = "400 Error"
       f.successful?.should be_false
-      f.status_code = "500 Error"
+      f.last_server_response = "500 Error"
       f.successful?.should be_false
-      f.status_code = "2 Error"
+      f.last_server_response = "2 Error"
       f.successful?.should be_false
-      f.status_code = "20 Error"
+      f.last_server_response = "20 Error"
       f.successful?.should be_false
     end
   end
