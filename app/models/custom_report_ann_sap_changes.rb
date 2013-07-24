6f015d038a16bc4968034ec14c3989b19abc34d3
Brian Glick <brian@brian-glick.com>
2013-07-24 13:15:33 -0400

    base report created, #882

---
+++ app/models/custom_report_ann_sap_changes.rb
@@ -0,0 +1,112 @@
+require 'open_chain/custom_handler/ann_inc/ann_custom_definition_support'
+require 'open_chain/custom_handler/ann_inc/ann_sap_product_handler'
+class CustomReportAnnSapChanges < CustomReport
+  include OpenChain::CustomHandler::AnnInc::AnnCustomDefinitionSupport 
+  def self.template_name
+    'SAP Changes'
+  end
+
+  def self.description
+    "Show all SAP records that have had key fields changes since they were approved."
+  end
+
+  def self.column_fields_available user
+    CoreModule::PRODUCT.model_fields(user).values + CoreModule::CLASSIFICATION.model_fields(user).values
+  end
+
+  def self.criterion_fields_available user
+    column_fields_available user
+  end
+
+  def self.can_view? user
+    user.company.master? && MasterSetup.get.custom_feature?('Ann SAP')
+  end
+
+  def run run_by, row_limit=nil
+    raise "User #{run_by.username} does not have permission to view this report." unless self.class.can_view?(run_by) 
+    left_columns_count = 0
+    rpf = OpenChain::CustomHandler::AnnInc::AnnSapProductHandler::SAP_REVISED_PRODUCT_FIELDS
+    cdefs = prep_custom_definitions rpf + [:approved_date,:sap_revised_date]
+    qry = "
+#{make_select(cdefs)}
+FROM products
+INNER JOIN classifications on products.id = classifications.product_id
+INNER JOIN custom_values appr on appr.customizable_id = classifications.id and appr.custom_definition_id = #{cdefs[:approved_date].id}
+INNER JOIN custom_values sap on sap.customizable_id = products.id and sap.custom_definition_id = #{cdefs[:sap_revised_date].id}
+#{make_where} #{row_limit.blank? ? "" : " LIMIT #{row_limit}"}"
+    r = ActiveRecord::Base.connection.execute qry
+    row_cursor = 0
+    col_cursor = 0
+    heading_row 0
+    if self.include_links?
+      left_columns_count += 1
+      write row_cursor, col_cursor, "Web Links"
+      col_cursor += 1
+    end
+    left_columns_count += search_columns.size
+    search_columns.each do |sc|
+      write row_cursor, col_cursor, sc.model_field.label
+      col_cursor += 1
+    end
+    rpf.each do |d|
+      mf = cdefs[d].model_field
+      write row_cursor, col_cursor, "Old #{mf.label}"
+      col_cursor += 1
+      write row_cursor, col_cursor, "New #{mf.label}"
+      col_cursor += 1
+    end
+    row_cursor += 1
+    request_host = MasterSetup.get.request_host
+    r.each do |row|
+      col_cursor = 0
+      if self.include_links?
+        write row_cursor, col_cursor, "#{request_host}/products/#{row.last}"
+        col_cursor += 1
+      end
+      search_columns.each_with_index do |sc,idx|
+        write row_cursor, col_cursor, sc.model_field.process_query_result(row[idx],run_by)
+        col_cursor += 1
+      end
+      es_json = row[search_columns.size + rpf.size]
+      es_hash = es_json.blank? ? {'entity'=>{'model_fields'=>{}}} : JSON.parse(es_json)
+      rpf.each_with_index do |d,i|
+        mf = cdefs[d].model_field
+        new_col = left_columns_count+1+(i*2)
+        new_val_position = left_columns_count+i
+        write row_cursor, new_col, mf.process_query_result(row[new_val_position],run_by)
+
+        old_col = left_columns_count+(i*2)
+        old_val = ""
+        old_val = es_hash['entity']['model_fields'][mf.uid.to_s] unless es_hash['entity']['model_fields'][mf.uid.to_s].blank?
+        write row_cursor, old_col, old_val
+      end
+      row_cursor += 1
+    end
+  end
+
+  private
+  def make_select cdefs
+    flds = []
+    search_columns.each_with_index {|sc,idx| flds << "#{sc.model_field.qualified_field_name} AS \"#{idx}\""}
+    OpenChain::CustomHandler::AnnInc::AnnSapProductHandler::SAP_REVISED_PRODUCT_FIELDS.each do |uid|
+      mf = cdefs[uid].model_field
+      flds << "#{mf.qualified_field_name} AS \"CD#{cdefs[uid].id}\""
+    end
+    flds << "(IF(appr.date_value is null,null,(SELECT snapshot FROM entity_snapshots WHERE recordable_id = products.id AND recordable_type = 'Product' AND created_at <= appr.date_value ORDER BY created_at DESC LIMIT 1))) as 'ES'"
+    flds << "products.id"
+    "SELECT #{flds.join(", ")} "
+  end
+  def make_where 
+    wheres = self.search_criterions.collect do |sc| 
+      v = sc.where_value
+      if v.respond_to? :collect
+        v = v.collect {|val| ActiveRecord::Base.sanitize val}.join(",")
+      else
+        v = ActiveRecord::Base.sanitize v
+      end
+      sc.where_clause(v).gsub("?",v)
+    end
+    wheres << "appr.date_value < sap.date_value" 
+    "WHERE (#{wheres.join(") AND (")})"
+  end
+end
---
+++ spec/models/custom_report_ann_sap_changes_spec.rb
@@ -0,0 +1,145 @@
+require 'spec_helper'
+
+describe CustomReportAnnSapChanges do
+  before :each do
+    @u = Factory(:master_user)
+    MasterSetup.any_instance.stub(:custom_feature?).and_return(true)
+  end
+  describe :static_methods do
+    it "should not allow users not from master company" do
+      Company.any_instance.stub(:master?).and_return(false)
+      described_class.can_view?(@u).should be_false
+    end
+    it "should not allow if Ann SAP not enabled" do
+      MasterSetup.any_instance.should_receive(:custom_feature?).with('Ann SAP').and_return(false)
+      described_class.can_view?(@u).should be_false
+    end
+    it "should allow product, classification fields" do
+      avail = described_class.column_fields_available(@u)
+      avail.size.should == CoreModule::PRODUCT.model_fields.values.size + CoreModule::CLASSIFICATION.model_fields.values.size
+      avail.should include(ModelField.find_by_uid(:prod_uid))
+      avail.should include(ModelField.find_by_uid(:class_cntry_iso))
+    end
+    it "should allow parameters from product, classifcation" do
+      avail = described_class.criterion_fields_available(@u)
+      avail.size.should == CoreModule::PRODUCT.model_fields.values.size + CoreModule::CLASSIFICATION.model_fields.values.size
+      avail.should include(ModelField.find_by_uid(:prod_uid))
+      avail.should include(ModelField.find_by_uid(:class_cntry_iso))
+    end
+
+  end
+
+  describe :run do
+    def make_eligible_product
+      tr = Factory(:tariff_record)
+      cls = tr.classification
+      cls.update_custom_value! @appr, 2.days.ago
+      cls.product.update_custom_value! @sap, 1.day.ago
+      cls.product
+    end
+    before :each do
+      @rc = described_class.new
+      @rc.search_columns.build(:model_field_uid=>:prod_uid,:rank=>1)
+      @rc.search_columns.build(:model_field_uid=>:class_cntry_iso,:rank=>2)
+      @cdefs = @rc.prep_custom_definitions OpenChain::CustomHandler::AnnInc::AnnSapProductHandler::SAP_REVISED_PRODUCT_FIELDS + [:sap_revised_date,:approved_date]
+      @appr = @cdefs[:approved_date]
+      @sap = @cdefs[:sap_revised_date]
+    end
+    it "should only include products with SAP Revised Date after approved date" do
+      p = make_eligible_product
+      tr2 = Factory(:tariff_record)
+      cls2 = tr2.classification
+      cls2.update_custom_value! @appr, 2.days.ago
+      cls2.product.update_custom_value! @sap, 3.days.ago
+      arrays = @rc.to_arrays @u
+      arrays.should have(2).rows
+      arrays[1][0].should == p.unique_identifier
+      arrays[1][1].should == p.classifications.first.country.iso_code
+    end
+    it "should write difference columns" do
+      p = make_eligible_product
+      diffs = [:origin,:import,:missy,:petite,:tall,:cost]
+      diffs.each {|d| p.update_custom_value! @cdefs[d], "OLD-#{d}" }
+      es = p.create_snapshot @u
+      es.update_attributes(created_at:3.days.ago)
+      diffs.each {|d| p.update_custom_value! @cdefs[d], "NEW-#{d}" }
+      arrays = @rc.to_arrays @u
+      arrays.should have(2).rows
+      row = arrays[1]
+      diffs.each_with_index do |d,i|
+        new_val = row[3+(i*2)]
+        new_val.should == "NEW-#{d}"
+
+        old_val = row[2+(i*2)]
+        old_val.should == "OLD-#{d}"
+      end
+    end
+    it "should include web links" do
+      @rc.include_links = true
+      MasterSetup.any_instance.stub(:request_host).and_return('http://xxxx')
+      p = make_eligible_product
+      arrays = @rc.to_arrays @u
+      arrays[1][0].should == "http://xxxx/products/#{p.id}"
+      arrays[1][1].should == p.unique_identifier
+    end
+    it "should not include classification with approval after sap revised date even if another classification is before" do
+      p = make_eligible_product
+      cls = Factory(:classification,product:p)
+      cls.update_custom_value! @appr, 0.days.ago
+      arrays = @rc.to_arrays @u
+      arrays.size.should == 2
+      arrays[1][1].should == p.classifications.first.country.iso_code
+    end
+    it "should filter on extra parameters" do
+      find_me = make_eligible_product
+      dont_find_me = make_eligible_product
+      @rc.search_criterions.build(model_field_uid:'prod_uid',operator:'eq',value:find_me.unique_identifier)
+      arrays = @rc.to_arrays @u
+      arrays.size.should == 2
+      arrays[1][0].should == find_me.unique_identifier 
+    end
+    it "should handle records with no snapshots" do
+      p = make_eligible_product
+      arrays = @rc.to_arrays @u
+      arrays.size.should == 2
+      arrays[1][2].should == ""
+    end
+    it "should handle records with no snapshots before approved date" do
+      p = make_eligible_product
+      diffs = [:origin,:import,:missy,:petite,:tall,:cost]
+      diffs.each {|d| p.update_custom_value! @cdefs[d], "OLD-#{d}" }
+      es = p.create_snapshot @u
+      diffs.each {|d| p.update_custom_value! @cdefs[d], "NEW-#{d}" }
+      arrays = @rc.to_arrays @u
+      arrays.should have(2).rows
+      arrays[1][2].should == "" 
+    end
+    it "should use most recent snapshot before approved date" do
+      p = make_eligible_product
+      diffs = [:origin,:import,:missy,:petite,:tall,:cost]
+      diffs.each {|d| p.update_custom_value! @cdefs[d], "REALYOLD-#{d}" }
+      es = p.create_snapshot @u
+      es.update_attributes(created_at:1.year.ago)
+      diffs.each {|d| p.update_custom_value! @cdefs[d], "OLD-#{d}" }
+      es = p.create_snapshot @u
+      es.update_attributes(created_at:3.days.ago)
+      diffs.each {|d| p.update_custom_value! @cdefs[d], "AFTERAPPROVAL-#{d}" }
+      es = p.create_snapshot @u
+      es.update_attributes(created_at:1.days.ago)
+      diffs.each {|d| p.update_custom_value! @cdefs[d], "NEW-#{d}" }
+      arrays = @rc.to_arrays @u
+      arrays.should have(2).rows
+      arrays[1][2].should == "OLD-origin"
+      arrays[1][3].should == "NEW-origin"
+    end
+    it "should write headings" do
+      p = make_eligible_product
+      expected = [ModelField.find_by_uid(:prod_uid).label,ModelField.find_by_uid(:class_cntry_iso).label]
+      OpenChain::CustomHandler::AnnInc::AnnSapProductHandler::SAP_REVISED_PRODUCT_FIELDS.each do |d|
+        expected << "Old #{@cdefs[d].model_field.label}"
+        expected << "New #{@cdefs[d].model_field.label}"
+      end
+      @rc.to_arrays(@u).first.should == expected
+    end
+  end
+end
