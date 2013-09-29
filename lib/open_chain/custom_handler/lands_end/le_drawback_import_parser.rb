ca2c690aa8adaadc78eb2fbcd59acf048eac067f
Brian Glick <brian@brian-glick.com>
2013-09-28 22:40:20 -0400

    extra logging for LE drawback parser

--- lib/open_chain/custom_handler/lands_end/le_drawback_import_parser.rb
+++ lib/open_chain/custom_handler/lands_end/le_drawback_import_parser.rb
@@ -14,34 +14,41 @@ module OpenChain
         end
 
         def parse data
+          cursor = 1
           #this all has to be in a long running transaction because if there's an error, 
           #we've incremented values which MUST be undone for the whole file so it can be
           #reprocessed
-          ActiveRecord::Base.transaction do
-            CSV.parse(data,headers:true) do |row|
-              next if row.blank?
-              part_number = row[5].split('-').first.strip
-              q = DrawbackImportLine.where(importer_id:@company.id,
-                entry_number:row[0],part_number:part_number).
-                where("id NOT IN (select drawback_import_line_id from duty_calc_import_file_lines)")
-              d = q.first_or_create!(
-                product_id:Product.where(unique_identifier:"LANDSEND-#{part_number}").first_or_create!.id,
-                description:description(row[5]),
-                port_code:row[1],
-                import_date:format_date(row[2]),
-                received_date:format_date(row[3]),
-                hts_code:row[4],
-                quantity:0,
-                unit_of_measure:row[7],
-                unit_price:row[8].gsub('$',''),
-                rate:( BigDecimal(row[9].gsub('%',''))*BigDecimal('0.01') )
-              )
-              d.quantity += BigDecimal(row[6])
-              key = "#{d.entry_number}-#{d.part_number}"
-              kj = KeyJsonItem.lands_end_cd(key).first
-              d.duty_per_unit = kj.data['duty_per_unit'] if kj
-              d.save!
+          begin
+            ActiveRecord::Base.transaction do
+              CSV.parse(data,headers:true) do |row|
+                next if row.blank?
+                part_number = row[5].split('-').first.strip
+                q = DrawbackImportLine.where(importer_id:@company.id,
+                  entry_number:row[0],part_number:part_number).
+                  where("id NOT IN (select drawback_import_line_id from duty_calc_import_file_lines)")
+                d = q.first_or_create!(
+                  product_id:Product.where(unique_identifier:"LANDSEND-#{part_number}").first_or_create!.id,
+                  description:description(row[5]),
+                  port_code:row[1],
+                  import_date:format_date(row[2]),
+                  received_date:format_date(row[3]),
+                  hts_code:row[4],
+                  quantity:0,
+                  unit_of_measure:row[7],
+                  unit_price:row[8].gsub('$',''),
+                  rate:( BigDecimal(row[9].gsub('%',''))*BigDecimal('0.01') )
+                )
+                d.quantity += BigDecimal(row[6])
+                key = "#{d.entry_number}-#{d.part_number}"
+                kj = KeyJsonItem.lands_end_cd(key).first
+                d.duty_per_unit = kj.data['duty_per_unit'] if kj
+                d.save!
+                cursor += 1
+              end
             end
+          rescue
+            $!.log_me ["Lands End Drawback Import Parser", "Row #{cursor}"]
+            raise
           end
         end
 
