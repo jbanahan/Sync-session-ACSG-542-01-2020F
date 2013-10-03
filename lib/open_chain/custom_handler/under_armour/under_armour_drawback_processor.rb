b196faea80813ebf9ec3d7c687abf2f13382360a
Brian Glick <brian@brian-glick.com>
2013-10-03 15:50:53 -0400

    under armour handle bad part number

--- lib/open_chain/custom_handler/under_armour/under_armour_drawback_processor.rb
+++ lib/open_chain/custom_handler/under_armour/under_armour_drawback_processor.rb
@@ -10,7 +10,9 @@ module OpenChain; module CustomHandler; module UnderArmour
 
     def find_shipment_lines commercial_invoice_line
       entry = commercial_invoice_line.entry
+      return [] if commercial_invoice_line.part_number.blank?
       pn, size = commercial_invoice_line.part_number.split('-')
+      return [] if size.blank?
       po_search = SearchCriterion.new(:model_field_uid=>"*cf_#{@cdefs[:po].id}",:operator=>"eq",:value=>commercial_invoice_line.po_number)
       no_past_search = SearchCriterion.new(:model_field_uid=>"*cf_#{@cdefs[:del_date].id}",:operator=>"gt",:value=>entry.arrival_date-1.day)
       size_search = SearchCriterion.new(:model_field_uid=>"*cf_#{@cdefs[:color].id}",:operator=>'eq',:value=>size)
