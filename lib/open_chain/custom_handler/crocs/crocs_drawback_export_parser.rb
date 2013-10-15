d0e591d3d2cbbe7e14d15e2d7b2da4b11b007f00
Brian Glick <brian@brian-glick.com>
2013-10-15 13:43:00 -0400

    updates to crocs export parser

--- lib/open_chain/custom_handler/crocs/crocs_drawback_export_parser.rb
+++ lib/open_chain/custom_handler/crocs/crocs_drawback_export_parser.rb
@@ -5,12 +5,15 @@ module OpenChain
       class CrocsDrawbackExportParser < OpenChain::DrawbackExportParser
         def self.parse_csv_line r, row_num, importer
           return nil if r.size <= 1 && r[0].blank?
-          raise "Line #{row_num} had #{r.size} elements.  All lines must have 19 elements." unless r.size==19
+          raise "Line #{row_num} had #{r.size} elements.  All lines must have 18 elements." unless r.size==18
+          return nil unless r[15]=='Pairs'
+          exp_d = r[3].split('/')
+          exp_d[2] = "20#{exp_d[2]}" if(exp_d[2].length==2)
           d = DutyCalcExportFileLine.new
-          d.export_date = Date.strptime(r[3],"%m-%d-%Y")
+          d.export_date = Date.new(exp_d[2].to_i,exp_d[0].to_i,exp_d[1].to_i)
           d.ship_date = d.export_date
           d.importer = importer
-          d.part_number = r[11]
+          d.part_number = "#{r[11]}-#{r[13]}"
           d.carrier = r[17]
           d.ref_1 = r[0]
           d.ref_2 = r[1]
