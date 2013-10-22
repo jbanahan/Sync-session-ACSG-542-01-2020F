527a89aaac6db17c4cf58000aa6d7636d6537b7f
Brian Glick <brian@brian-glick.com>
2013-10-22 11:45:08 -0400

    remove bad test

--- spec/models/survey_response_spec.rb
+++ spec/models/survey_response_spec.rb
@@ -34,17 +34,6 @@ describe SurveyResponse do
     sr = SurveyResponse.new(:survey=>Factory(:survey))
     sr.save.should be_false
   end
-  it "should accept nested attributes for answers" do
-    s = Factory(:survey)
-    s.questions.create!(:content=>'1234567890123',:rank=>1)
-    q = s.questions.first
-    sr = Factory(:survey_response,:survey=>s)
-    sr.update_attributes(:answers_attributes=>[{:question_id=>q.id,:choice=>'123',:rating=>'NI'}])
-    a = sr.answers.first
-    a.question.should == q
-    a.choice.should == '123'
-    a.rating.should == 'NI'
-  end
   it "should protect date attributes" do
     sr = Factory(:survey_response)
     d = 1.day.ago
