c0038e69e065a7808cd12f19c82969a76bef2fe9
Brian Glick <brian@brian-glick.com>
2013-12-20 09:40:58 -0500

    track jcrew ups ref num

--- lib/open_chain/custom_handler/j_crew/j_crew_drawback_export_parser.rb
+++ lib/open_chain/custom_handler/j_crew/j_crew_drawback_export_parser.rb
@@ -4,15 +4,16 @@ module OpenChain; module CustomHandler; module JCrew
     def self.parse_csv_line r, row_number, importer
       return nil if r.size <= 1 && r[0].blank? # stray rows
       raise "Line #{row_number} had #{r.size} elements.  All lines must have 167 elements." unless r.size==167
-      return nil if r[121].blank? || r[121] == '0'
+      quanity = 
       d = DutyCalcExportFileLine.new
       d.ship_date = Date.strptime(r[105],'%m/%d/%Y')
       d.export_date = Date.strptime(r[106],'%m/%d/%Y')
       d.part_number = r[164]
       d.ref_1 = r[116]
       d.ref_2 = r[8]
+      d.ref_3 = r[5]
       d.destination_country = 'CA'
-      d.quantity = r[121]
+      d.quantity = r[121].blank? ? '0' : r[121]
       d.description = r[122]
       d.uom = 'EA'
       d.exporter = 'J Crew'
