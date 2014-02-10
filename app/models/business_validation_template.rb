85e97f19cadf5b6054cd3864ecd1ad639ea9bbbf
Brian Glick <brian@brian-glick.com>
2014-02-10 10:46:18 -0500

    allow bulk update from template, #1019

--- app/models/business_validation_template.rb
+++ app/models/business_validation_template.rb
@@ -6,13 +6,24 @@ class BusinessValidationTemplate < ActiveRecord::Base
   has_many :business_validation_results, inverse_of: :business_validation_template
   has_many :search_criterions, dependent: :destroy
 
-  def create_result! obj
+  #run create
+  def create_results! run_validation = false
+    cm = CoreModule.find_by_class_name(self.module_type)
+    klass = cm.klass 
+    srch = klass.where("#{cm.table_name}.updated_at > business_validation_results.updated_at OR business_validation_results.updated_at is null")
+    srch = srch.joins("LEFT OUTER JOIN business_validation_results ON business_validation_results.validatable_type = '#{self.module_type}' AND business_validation_results.validatable_id = #{cm.table_name}.id")
+    self.search_criterions.each {|sc| srch = sc.apply(srch)}
+    srch.each do |obj|
+      self.create_result! obj, run_validation
+    end
+  end
+  def create_result! obj, run_validation = false
     bvr = nil
     self.search_criterions.each do |sc|
       return nil unless sc.test?(obj)
     end
     self.class.transaction do
-      bvr = self.business_validation_results.new
+      bvr = self.business_validation_results.where(validatable_type:obj.class.name,validatable_id:obj.id).first_or_create!
       bvr.validatable = obj
       bvr.save!
       self.business_validation_rules.each do |rule|
@@ -20,6 +31,11 @@ class BusinessValidationTemplate < ActiveRecord::Base
         rr.business_validation_rule = rule
         rr.save!
       end
+    end
+    if run_validation
+      bvr.run_validation
+      bvr.updated_at = Time.now #force save
+      bvr.save!
     end
     bvr
   end
