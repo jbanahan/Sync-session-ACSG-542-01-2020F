14457baab47705294076d99233283aecd42b5bd1
Jeremy Hulford <jhulford@vandegriftinc.com>
2014-02-24 15:04:37 -0600

    Transliterate Alliance files to ASCII. #1029

--- lib/open_chain/custom_handler/alliance_product_support.rb
+++ lib/open_chain/custom_handler/alliance_product_support.rb
@@ -4,6 +4,31 @@ module OpenChain
       def ftp_credentials
         {:username=>'VFITRACK',:password=>'RL2VFftp',:server=>'ftp2.vandegriftinc.com',:folder=>'to_ecs/alliance_products',:remote_file_name=>remote_file_name}
       end
+
+      def preprocess_row row
+        row.each do |column, val|
+          # So, what we're doing here is attempting to transliterate any NON-ASCII data...
+          # If that's not possible, we're using an ASCII bell character.
+
+          # If the translated text then returns that we have a bell character (which really should never
+          # occurr naturally in data), then we know we have an untranslatable char and we'll hard stop.
+          
+          if val.is_a? String
+            translated = ActiveSupport::Inflector.transliterate(val, "\007")
+            if translated =~ /\x07/
+              raise "Untranslatable Non-ASCII character for Part Number '#{row[0]}' found at string index #{$~.begin(0)} in product query column #{column}: '#{val}'."
+            else
+              row[column] = translated
+            end
+          end
+        end
+
+        super(row)
+      rescue => e
+        # Don't let a single product stop the rest of them from being sent.
+        e.log_me
+        nil
+      end
     end
   end
 end
