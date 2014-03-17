7ef0388bc75c17bbca7fab7104dce5e77d810aba
Brian Glick <brian@brian-glick.com>
2014-03-17 13:00:04 -0400

    Make business validations auto skip on search criterions

--- spec/models/business_validation_rule_spec.rb
+++ spec/models/business_validation_rule_spec.rb
@@ -1,8 +1,19 @@
 require 'spec_helper'
 
 describe BusinessValidationRule do
-  it "should default should_skip? to false" do
-    bvr = BusinessValidationRule.new
-    expect(bvr.should_skip?(Order.new)).to be_false
-  end
+  describe :should_skip?
+    it "should base should_skip? on search_criterions" do
+      pass_ent = Entry.new(entry_number:'9')
+      fail_ent = Entry.new(entry_number:'7')
+      bvr = BusinessValidationRule.new
+      bvr.search_criterions.build(model_field_uid:'ent_entry_num',operator:'eq',value:'9')
+      expect(bvr.should_skip?(pass_ent)).to be_false
+      expect(bvr.should_skip?(fail_ent)).to be_true
+    end
+    it "should raise exception if search_criterion's model field CoreModule doesn't equal object's CoreModule" do
+      bvr = BusinessValidationRule.new
+      bvr.search_criterions.build(model_field_uid:'ent_entry_num',operator:'eq',value:'9')
+      ci = CommercialInvoiceLine.new
+      expect {bvr.should_skip? ci}.to raise_error /Invalid object expected Entry got CommercialInvoiceLine/
+    end
 end
