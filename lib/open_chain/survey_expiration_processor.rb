8fdfc4cbfaa474eb7954d2691a1ce05e4d3ce38b
Jeff Conway <jconway@arcwebtech.com>
2014-03-17 15:18:36 -0400

    #1057 Slightly more efficient processing by using sets and adding a variable

--- lib/open_chain/survey_expiration_processor.rb
+++ lib/open_chain/survey_expiration_processor.rb
@@ -1,16 +1,17 @@
+require 'set'
+
 class OpenChain::SurveyExpirationProcessor
 	def run_schedulable
-		surveys_with_expired_responses = []
-		SurveyResponse.reminder_email_needed.each do |sr| 
-			surveys_with_expired_responses << sr.survey unless surveys_with_expired_responses.include? sr.survey
-		end
+		surveys_with_expired_responses = Set.new # There's a high chance of repetition in the surveys; sets handle this best
+		SurveyResponse.reminder_email_needed.each { |sr| surveys_with_expired_responses << sr.survey }
 		surveys_with_expired_responses.each do |expired_survey|
+			responses = expired_survey.survey_responses.reminder_email_needed
 			recipient_addresses = []
 			expired_survey.survey_subscriptions.each { |survey_subscription| recipient_addresses << survey_subscription.user.email }
 			recipient_addresses.each do |recipient|
-				OpenMailer.send_survey_expiration_reminder(recipient, expired_survey, expired_survey.survey_responses.reminder_email_needed).deliver!
+				OpenMailer.send_survey_expiration_reminder(recipient, expired_survey, responses).deliver!
 			end
-			expired_survey.survey_responses.reminder_email_needed.each do |response|
+			responses.each do |response|
 				response.expiration_notification_sent_at = Time.now
 				response.save!
 			end
