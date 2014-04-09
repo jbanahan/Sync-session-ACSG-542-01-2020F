c6dc1514d07090560f642dc4e0e7c08a2e18dde1
Jeff Conway <jconway@arcwebtech.com>
2014-04-09 10:22:18 -0400

    Update attachment emailer and attachment partial #1120
    * Update definition of sendEmailAttachments in chain.js.coffee
    * Update call to email_attachments in attachments_controller.rb
    * Create action for auto_send_attachments in open_mailer.rb
    * Update email_attachments definition in attachment.rb
    * Make UI edits to show_email_attachable.html.erb
    * Center button in _attachments.html.erb
    * Test auto_send_attachments

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -66,7 +66,7 @@ class AttachmentsController < ApplicationController
   end
 
   def send_email_attachable
-    Attachment.delay.email_attachments({to_address: params[:to_address], email_subject: params[:email_subject], email_body: params[:email_body], ids_to_include: params[:ids_to_include]})
+    Attachment.delay.email_attachments({to_address: params[:to_address], email_subject: params[:email_subject], email_body: params[:email_body], ids_to_include: params[:ids_to_include], full_name: params[:full_name], email: params[:email]})
     render text: "OK"
   end
 
