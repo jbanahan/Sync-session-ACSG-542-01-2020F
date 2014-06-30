be0b29ab5d3fba6fa3f68d5604e75fa30eff0fea
Brian Glick <brian@brian-glick.com>
2014-06-30 09:23:50 -0400

    Add another UTF conversion for RL SAP

--- lib/open_chain/custom_handler/polo_sap_product_generator.rb
+++ lib/open_chain/custom_handler/polo_sap_product_generator.rb
@@ -56,6 +56,7 @@ module OpenChain
             "\u{201C}" => "\"", # Left quote-mark “
             "\u{00BC}" => "1/4",
             "\u{00BD}" => "1/2",
+            "\u{00FA}" => "u", #u w/ acute http://www.fileformat.info/info/unicode/char/fa/index.htm
             "\u{2019}" => "'" # Right single quote-mark '
           }
           # First convert the UTF-8 text to ascii w/ our conversion table
