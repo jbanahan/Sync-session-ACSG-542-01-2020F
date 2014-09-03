4d53155e18db63937e02c839592e0c774f5ea3de
Brian Glick <brian@brian-glick.com>
2014-09-03 15:34:44 -0400

    order closed model fields

--- app/models/model_field.rb
+++ app/models/model_field.rb
@@ -1468,7 +1468,16 @@ and classifications.product_id = products.id
         [10,:ord_window_start,:ship_window_start,'Ship Window Start Date',{data_type: :date}],
         [11,:ord_window_end,:ship_window_end,'Ship Window End Date',{data_type: :date}],
         [12,:ord_first_exp_del,:first_expected_delivery_date,'First Expected Delivery Date',{data_type: :date}],
-        [13,:ord_fob_point,:fob_point,'FOB Point',{data_type: :string}]
+        [13,:ord_fob_point,:fob_point,'FOB Point',{data_type: :string}],
+        [14,:ord_closed_at,:closed_at,'Closed At',{data_type: :datetime}],
+        [15,:ord_closed_by,:username,"Closed By", {
+          :import_lambda => lambda {|a,b| return "Closed By cannot be set by import, ignored."},
+          :export_lambda => lambda {|obj| 
+            obj.closed_by.blank? ? "" : obj.closed_by.username
+          },
+          :qualified_field_name => "(SELECT username FROM users where users.id = orders.closed_by_id)",
+          :data_type=>:string
+        }]
       ]
       add_fields CoreModule::ORDER, make_vendor_arrays(100,"ord","orders")
       add_fields CoreModule::ORDER, make_ship_to_arrays(200,"ord","orders")
