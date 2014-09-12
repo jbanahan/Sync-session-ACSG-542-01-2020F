ad82d10cd758bd357707a2749a5c68dedea58375
Jeremy Hulford <jhulford@vandegriftinc.com>
2014-09-12 12:05:50 -0500

    Add Posting Date to Intacct Payables/Receivables. #1351

--- lib/open_chain/custom_handler/intacct/intacct_xml_generator.rb
+++ lib/open_chain/custom_handler/intacct/intacct_xml_generator.rb
@@ -14,6 +14,7 @@ module OpenChain; module CustomHandler; module Intacct; class IntacctXmlGenerato
       trans = add_element func, "create_sotransaction"
       add_element trans, "transactiontype", receivable.receivable_type
       add_date trans, "datecreated", receivable.invoice_date
+      add_date trans, "dateposted", receivable.created_at.in_time_zone("Eastern Time (US & Canada)")
       add_element trans, "customerid", receivable.customer_number
       add_element trans, "documentno", receivable.invoice_number
       add_element trans, "referenceno", receivable.customer_reference
@@ -48,6 +49,7 @@ module OpenChain; module CustomHandler; module Intacct; class IntacctXmlGenerato
       bill = add_element func, "create_bill"
       add_element bill, "vendorid", payable.vendor_number
       add_date bill, "datecreated", payable.bill_date
+      add_date bill, "dateposted", payable.created_at.in_time_zone("Eastern Time (US & Canada)")
       add_element bill, "termname", termname
       add_element bill, "billno", payable.bill_number, allow_blank: false
       add_element bill, "externalid", payable.vendor_reference, allow_blank: false
