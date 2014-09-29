8b96588e2f3f74d416c2c50535b8ca7f023d269b
Brian Glick <brian@brian-glick.com>
2014-09-29 08:23:08 -0400

    don't include vendor in shipment ref search

--- lib/open_chain/custom_handler/under_armour/under_armour_receiving_parser.rb
+++ lib/open_chain/custom_handler/under_armour/under_armour_receiving_parser.rb
@@ -84,7 +84,7 @@ module OpenChain; module CustomHandler; module UnderArmour
       sys_code = trim_numeric row[0]
       ref = trim_numeric row[6]
       vendor = Company.find_or_create_by_system_code_and_vendor(sys_code,true,:name=>row[1])
-      @shipment = Shipment.find_by_reference_and_vendor_id ref, vendor.id
+      @shipment = Shipment.find_by_reference ref
       @shipment = Shipment.new(:reference=>ref,:vendor=>vendor,:importer=>Company.find_by_importer(true)) unless @shipment
     end
 
