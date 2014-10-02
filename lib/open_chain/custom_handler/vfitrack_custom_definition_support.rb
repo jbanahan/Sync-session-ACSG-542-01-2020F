b9595c0b17b87e010a063b3948016d5ff0279ba1
Jeremy Hulford <jhulford@vandegriftinc.com>
2014-10-02 12:29:23 -0500

    Fix custom def datatype.

--- lib/open_chain/custom_handler/vfitrack_custom_definition_support.rb
+++ lib/open_chain/custom_handler/vfitrack_custom_definition_support.rb
@@ -29,7 +29,7 @@ module OpenChain; module CustomHandler; module VfitrackCustomDefinitionSupport
     prod_fda_contact_phone: {label: "FDA Contact Phone", data_type: :string, module_type: "Product"},
     prod_fda_affirmation_compliance: {label: "FDA Affirmation of Compliance", data_type: :string, module_type: "Product"},
     ord_invoicing_system: {label: "Invoicing System", data_type: :string, module_type: "Order"},
-    ord_invoiced: {label: "Invoice Received?", data_type: :string, module_type: "Order"}
+    ord_invoiced: {label: "Invoice Received?", data_type: :boolean, module_type: "Order"}
   } 
   
   def self.included(base)
