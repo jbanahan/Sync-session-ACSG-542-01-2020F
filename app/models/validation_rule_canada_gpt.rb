da052ef80afd268734dbcf8608077cc6983620bd
Jeremy Hulford <jhulford@vandegriftinc.com>
2014-10-16 16:22:34 -0500

    Update GPT Country list.

--- app/models/validation_rule_canada_gpt.rb
+++ app/models/validation_rule_canada_gpt.rb
@@ -6,7 +6,12 @@ class ValidationRuleCanadaGpt < BusinessValidationRule
     'CK', 'CI', 'DJ', 'EG', 'SV', 'ER', 'ET', 'FK', 'FJ', 'GM', 'GE', 'GH', 'GT', 'GN', 'GW', 'GY', 'HT', 'HN', 'IQ', 'KE', 'KI', 'LA',
     'LS', 'LR', 'MG', 'MW', 'ML', 'MH', 'MR', 'FM', 'MD', 'MN', 'MS', 'MA', 'MZ', 'NR', 'NP', 'NI', 'NE', 'NG', 'NU', 'NF', 'PK', 'PG', 
     'PY', 'PH', 'PN', 'RW', 'WS', 'SH', 'ST', 'SN', 'SL', 'SB', 'SO', 'LK', 'SD', 'SZ', 'SY', 'TJ', 'TZ', 'TL', 'TG', 'TK', 'TO', 'TM', 
-    'TV', 'UG', 'UA', 'UZ', 'VU', 'VN', 'BG', 'YE', 'ZM', 'ZW'
+    'TV', 'UG', 'UA', 'UZ', 'VU', 'VN', 'BG', 'YE', 'ZM', 'ZW',
+    # All countries listed after this point have GPT expire 1/1/2015
+    'DZ', 'AS', 'AG', 'AR', 'AZ', 'BS', 'BH', 'BB', 'BM', 'BA', 'BW', 'BR', 'KY', 'CL', 'CN', 'CO', 'CR', 'HR', 'CU', 'DM', 'DO', 'EC', 
+    'CQ', 'PF', 'GA', 'GI', 'GD', 'DU', 'HK', 'IN', 'ID', 'IR', 'IL', 'JM', 'JO', 'KZ', 'KW', 'LB', 'MO', 'MK', 'MY', 'MV', 'MU', 'MX', 
+    'NA', 'NC', 'OM', 'PW', 'PA', 'PE', 'QA', 'RU', 'KN', 'LC', 'VC', 'SC', 'SG', 'ZA', 'KR', 'ZA', 'SR', 'TH', 'TT', 'TN', 'TR', 'TC', 
+    'AE', 'UY', 'VE', 'VI'
   ]
 
   def run_child_validation invoice_line
