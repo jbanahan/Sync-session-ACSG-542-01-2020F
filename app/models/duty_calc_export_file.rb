701668ba5b65093e4f83c59621675fcb8a9cc5f1
Brian Glick <brian@brian-glick.com>
2014-12-01 09:31:53 -0500

    fix for primitive importer

--- app/models/duty_calc_export_file.rb
+++ app/models/duty_calc_export_file.rb
@@ -8,11 +8,11 @@ class DutyCalcExportFile < ActiveRecord::Base
   # Make a new DutyCalcExportFile with an excel zip attached including all unused export lines for the 
   # given importer
   def self.generate_for_importer importer, run_by=nil, file_path=nil, extra_where=nil, max_lines_per_file = 65000, max_files_per_zip = 3
+    importer = Company.find(importer) if importer.is_a?(Fixnum) || importer.is_a?(String)
     r = []
     begin
       while DutyCalcExportFileLine.where(importer_id:importer.id).where(extra_where ? extra_where : "1=1").where('duty_calc_export_file_id is null').count > 0
         ActiveRecord::Base.transaction do
-          importer = Company.find(importer) if importer.is_a?(Fixnum) || importer.is_a?(String)
           fn = "duty_calc_export_#{importer.system_code.blank? ? importer.alliance_customer_number : importer.system_code}_#{Time.now.to_i}.zip"
           fp = "tmp/#{fn}"
           unless file_path.nil? 
