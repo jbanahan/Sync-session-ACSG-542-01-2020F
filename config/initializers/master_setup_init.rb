431b8d244f33ea9993ae5c77bdcef9a0947ff0fe
Brian Glick <brian@brian-glick.com>
2015-01-15 08:52:47 -0500

    don't pull all tables in init

--- config/initializers/master_setup_init.rb
+++ config/initializers/master_setup_init.rb
@@ -1,11 +1,11 @@
 # The check for tables occurs so these initializations don't run during a rake db:migrate
 # on an empty database (say from our CI servers).
-if ActiveRecord::Base.connection.table_exists? 'master_setups'
+conn = ActiveRecord::Base.connection
+if conn.table_exists? 'master_setups'
   MasterSetup.init_base_setup
 end
 
-if (["companies","users"] - ActiveRecord::Base.connection.tables).length == 0
-
+if conn.table_exists?('companies') && conn.table_exists?('users')
   c = Company.first_or_create!(name:'My Company',master:true) 
   if c.users.empty?
     pass = 'init_pass'
