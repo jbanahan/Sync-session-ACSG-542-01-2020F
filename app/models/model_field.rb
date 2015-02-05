46ea9bc7864a6f130727321877bdf3250e145e1e
Jeremy Hulford <jhulford@vandegriftinc.com>
2015-02-05 13:17:23 -0600

    Add Last Changed By Full Name field.

--- app/models/model_field.rb
+++ app/models/model_field.rb
@@ -778,15 +778,28 @@ class ModelField
   end
   def self.make_last_changed_by rank, uid_prefix, base_class
     table_name = base_class.table_name
-    [rank,"#{uid_prefix}_last_changed_by".to_sym,:username,"Last Changed By", {
-      :import_lambda => lambda {|a,b| return "Last Changed By cannot be set by import, ignored."},
-      :export_lambda => lambda {|obj| 
-        obj.last_updated_by.blank? ? "" : obj.last_updated_by.username
-      },
-      :qualified_field_name => "(SELECT username FROM users where users.id = #{table_name}.last_updated_by_id)",
-      :data_type=>:string,
-      :history_ignore => true
-    }]
+    [
+      [rank,"#{uid_prefix}_last_changed_by".to_sym,:username,"Last Changed By", {
+        :import_lambda => lambda {|a,b| return "Last Changed By cannot be set by import, ignored."},
+        :export_lambda => lambda {|obj| 
+          obj.last_updated_by.blank? ? "" : obj.last_updated_by.username
+        },
+        :qualified_field_name => "(SELECT username FROM users where users.id = #{table_name}.last_updated_by_id)",
+        :data_type=>:string,
+        :history_ignore => true
+      }],
+      [rank + 1,"#{uid_prefix}_last_changed_by_full_name".to_sym,:username,"Last Changed By Full Name", {
+        :import_lambda => lambda {|a,b| return "Last Changed By cannot be set by import, ignored."},
+        :export_lambda => lambda {|obj| 
+          # purposefully not using the fullname user method, since it falls back to returning username if names are blank, which we don't want here
+          obj.last_updated_by.blank? ? "" : ("#{obj.last_updated_by.first_name} #{obj.last_updated_by.last_name}")
+        },
+        :qualified_field_name => "(SELECT CONCAT_WS(' ', IFNULL(first_name, ''), IFNULL(last_name, '')) FROM users where users.id = #{table_name}.last_updated_by_id)",
+        :data_type=>:string,
+        :history_ignore => true
+      }]
+    ]
+    
   end
   def self.make_broker_invoice_entry_field sequence_number, mf_uid,field_reference,label,data_type,ent_exp_lambda,can_view_lambda=nil
     h = {:data_type=>data_type,
@@ -1799,7 +1812,7 @@ and classifications.product_id = products.id
           :data_type=>:integer
         }],
       ]
-      add_fields CoreModule::PRODUCT, [make_last_changed_by(12,'prod',Product)]
+      add_fields CoreModule::PRODUCT, make_last_changed_by(12,'prod',Product)
       add_fields CoreModule::PRODUCT, make_vendor_arrays(5,"prod","products")
       add_fields CoreModule::PRODUCT, make_division_arrays(100,"prod","products")
       add_fields CoreModule::PRODUCT, make_master_setup_array(200,"prod")
