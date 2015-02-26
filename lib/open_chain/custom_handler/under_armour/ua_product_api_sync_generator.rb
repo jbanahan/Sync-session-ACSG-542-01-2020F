ccf82682c89a25c77a932dd1590f66c004771387
Jeremy Hulford <jhulford@vandegriftinc.com>
2015-02-26 09:46:07 -0600

    Send all UA style-color combos to VFI Track.

--- lib/open_chain/custom_handler/under_armour/ua_product_api_sync_generator.rb
+++ lib/open_chain/custom_handler/under_armour/ua_product_api_sync_generator.rb
@@ -12,41 +12,31 @@ module OpenChain; module CustomHandler; module UnderArmour; class UaProductApiSy
   end
 
   def process_query_result row, opts
-    @us_plant_codes ||= Set.new(DataCrossReference.where(cross_reference_type: DataCrossReference::UA_PLANT_TO_ISO, value: "US").pluck :key)
     
     # UA stores their data at the style level, but UA's unique product identifier (from an entry standpoint) 
-    # is Style + Color.  Because of this we need to examine the colors custom value associated w/ the product
-    # figure out from the plant xref which ones are valid and then expload each line potentially into multiple
-    # style - color combinations.
-    plants = row[5].to_s.split(/\s*\r?\n\s*/)
-    colors = row[6].to_s.split(/\s*\r?\n\s*/)
+    # is Style + Color.  So, split out the color value and send every style + color combo to VFI Track.
+    colors = row[5].to_s.split(/\s*\r?\n\s*/)
 
     api_objects = []
-
-    # We can skip any colors we've already generated data for the product
     sent_codes = Set.new
-    plants.each do |plant|
-      next unless @us_plant_codes.include?(plant)
 
-      colors.each do |color|
-        unique_identifier = "#{row[1]}-#{color}"
-        next if sent_codes.include?(unique_identifier)
-        next unless DataCrossReference.find_ua_material_color_plant(row[1], color, plant)
+    colors.each do |color|
+      unique_identifier = "#{row[1]}-#{color}"
+      next if color.blank? || sent_codes.include?(unique_identifier)
 
-        product = {}
-        product['id'] = row[0]
-        product['prod_imp_syscode'] = vfitrack_importer_syscode(nil)
-        product['prod_uid'] = unique_identifier
-        product["prod_part_number"] = unique_identifier
-        product['class_cntry_iso'] = row[2]
-        tariff = {}
-        product['tariff_records'] = [tariff]
-        tariff['hts_line_number'] = row[3]
-        tariff['hts_hts_1'] = row[4]
+      product = {}
+      product['id'] = row[0]
+      product['prod_imp_syscode'] = vfitrack_importer_syscode(nil)
+      product['prod_uid'] = unique_identifier
+      product["prod_part_number"] = unique_identifier
+      product['class_cntry_iso'] = row[2]
+      tariff = {}
+      product['tariff_records'] = [tariff]
+      tariff['hts_line_number'] = row[3]
+      tariff['hts_hts_1'] = row[4]
 
-        api_objects << ApiSyncObject.new(row[0], product)
-        sent_codes << unique_identifier
-      end
+      api_objects << ApiSyncObject.new(row[0], product)
+      sent_codes << unique_identifier
     end
 
     # If there's no records to send, then we want to make sure that we still log a sync record against this product 
@@ -76,12 +66,11 @@ module OpenChain; module CustomHandler; module UnderArmour; class UaProductApiSy
   def query
     # We only want 1 row per product, we're handling exploading out the records in the query processing method
     qry = <<-QRY
-SELECT products.id, products.unique_identifier, iso.iso_code, r.line_number, r.hts_1, plant.text_value, color.text_value
+SELECT products.id, products.unique_identifier, iso.iso_code, r.line_number, r.hts_1, color.text_value
 FROM products products
 INNER JOIN classifications c on products.id = c.product_id
 INNER JOIN tariff_records r on r.classification_id = c.id and line_number = 1
 INNER JOIN countries iso on iso.id = c.country_id and iso.iso_code = 'US'
-INNER JOIN custom_values plant on plant.customizable_id = products.id and plant.customizable_type = 'Product' and plant.custom_definition_id = #{@plant_cd.id} and length(plant.text_value) > 0
 INNER JOIN custom_values color on color.customizable_id = products.id and color.customizable_type = 'Product' and color.custom_definition_id = #{@colors_cd.id} and length(color.text_value) > 0
 QRY
     if @custom_where.blank?
