597969cfbaf9c6d0c5589c832f694f7d5b852514
Brian Glick <brian@brian-glick.com>
2015-04-07 10:20:14 -0400

    don't need to check for old data in migrations anymore

--- db/migrate/20110422211854_add_module_type_to_imported_file.rb
+++ db/migrate/20110422211854_add_module_type_to_imported_file.rb
@@ -1,13 +1,6 @@
 class AddModuleTypeToImportedFile < ActiveRecord::Migration
   def self.up
     add_column :imported_files, :module_type, :string
-    ImportedFile.all.each do |f|
-      s = f.search_setup
-      unless s.nil?
-        f.module_type = s.module_type
-        f.save!
-      end
-    end
   end
 
   def self.down
