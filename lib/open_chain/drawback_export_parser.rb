252ece5878cef19b1c606f576a18dbee9eccc867
Jeremy Hulford <jhulford@vandegriftinc.com>
2015-05-11 09:53:30 -0500

    Add additional encoding instruction to replace invalid chars w/ space.

--- lib/open_chain/drawback_export_parser.rb
+++ lib/open_chain/drawback_export_parser.rb
@@ -5,7 +5,7 @@ module OpenChain
       f = File.new(file_path)
       f.each_line do |line|
         unless count == 0
-          ln = line.encode(Encoding.find("US-ASCII"),:undef=>:replace, :replace=>' ', :fallback=>' ')
+          ln = line.encode(Encoding.find("US-ASCII"),:undef=>:replace, :invalid=>:replace, :replace=>' ', :fallback=>' ')
           CSV.parse(ln) do |r|
             d = parse_csv_line r, count, importer
             d.save! unless d.nil?
