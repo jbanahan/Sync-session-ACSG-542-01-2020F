93aa079f54c1396ee074869ddf55f86593a02493
Max Furman <max@arcweb.co>
2015-05-12 13:09:35 -0400

    Fixed data type of marks & numbers migration

--- db/migrate/20150512163851_add_marks_and_numbers_to_shipment.rb
+++ db/migrate/20150512163851_add_marks_and_numbers_to_shipment.rb
@@ -1,5 +1,5 @@
 class AddMarksAndNumbersToShipment < ActiveRecord::Migration
   def change
-    add_column :shipments, :marks_and_numbers, :string
+    add_column :shipments, :marks_and_numbers, :text
   end
 end
