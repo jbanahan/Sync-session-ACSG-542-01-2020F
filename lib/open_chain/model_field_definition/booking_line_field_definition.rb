162091c4046d720cabe2fc17ecc8b55e2fbb90ba
Max Furman <max@arcweb.co>
2015-05-14 16:17:18 -0400

    Added fields for order and order line

--- lib/open_chain/model_field_definition/booking_line_field_definition.rb
+++ lib/open_chain/model_field_definition/booking_line_field_definition.rb
@@ -8,7 +8,15 @@ module OpenChain
       end
 
       def add_booking_line_fields
-        _add_fields [[1,:bkln_quantity, :quantity, "Quantity Booked", {type: :decimal}]]
+        _add_fields [[1,:bkln_quantity, :quantity, "Quantity Booked", {type: :decimal}],
+                     [2,:bkln_order_number, :order_number, "Order", {
+                           type: :string,
+                           qualified_field_name: 'select order_number from orders where orders.id = booking_line.order_id'
+                       }],
+                     [3,:bkln_order_line_number, :order_line_number, "Order Line", {
+                           type: :integer,
+                           qualified_field_name: 'select line_number from order_lines where order_lines.id = booking_line.order_line_id'
+                       }]]
         _add_fields make_product_arrays(100,PREFIX,"booking_lines")
       end
 
