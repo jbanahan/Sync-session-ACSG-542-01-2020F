fb490e6602f9e23fd02863d5162b0c0e77bafda1
Max Furman <max@arcweb.co>
2015-05-20 10:31:33 -0400

    Extracted form data from the example

--- spec/lib/custom_handler/generic_pack_manifest_parser_spec.rb
+++ spec/lib/custom_handler/generic_pack_manifest_parser_spec.rb
@@ -1,22 +1,18 @@
 require 'spec_helper'
+require Rails.root.join('spec/fixtures/files/standard_booking_form')
 
 describe OpenChain::CustomHandler::GenericPackManifestParser do
 
-  let(:file) { File.open(Rails.root.join 'spec/fixtures/files/Standardized Booking Form (SO).xls') }
-  let(:user) { FactoryGirl.create(:user) }
-  let(:shipment) { FactoryGirl.create(:shipment) }
+  let(:user) { double(:user) }
+  let(:shipment) { double(:shipment) }
   let(:attachment) { double :attachment }
 
   before do
-    attachment.stub(:attached).and_return file
-  end
-
-  it 'finds the file' do
-    expect(file).not_to be_nil
+    shipment.stub(:can_edit?).and_return true
   end
 
   it 'parses it' do
-    result = described_class.process_attachment shipment, attachment, user
+    result = described_class.new.process_rows shipment, FORM_ARRAY, user
     expect(result).to be_present?
   end
 end
