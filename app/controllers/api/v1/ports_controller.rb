bc5384613f8daff084aacca4527c1a88bf6fdb98
Max Furman <max@arcweb.co>
2015-05-26 18:21:30 -0400

    Checks type and filters accordingly. All ports are searched by default

--- app/controllers/api/v1/ports_controller.rb
+++ app/controllers/api/v1/ports_controller.rb
@@ -3,6 +3,8 @@ module Api; module V1; class PortsController < Api::V1::ApiController
   def autocomplete
     p = Port.order(:name)
     p = p.where('name like ?',"%#{params[:n]}%") unless params[:n].blank?
+    p = p.where(active_origin: true) if params[:type] == 'origin'
+    p = p.where(active_destination: true) if params[:type] == 'destination'
     p = p.paginate(per_page:10,page:params[:page])
     render json: p.collect {|port| {name:port.name,id:port.id}}
 
