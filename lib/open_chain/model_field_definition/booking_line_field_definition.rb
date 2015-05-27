4ff4a769590fc1d13b35a1995d323bd196b34d12
Max Furman <max@arcweb.co>
2015-05-27 12:42:03 -0400

    Line numbers

--- lib/open_chain/model_field_definition/booking_line_field_definition.rb
+++ lib/open_chain/model_field_definition/booking_line_field_definition.rb
@@ -4,12 +4,12 @@ module OpenChain
 
       def add_booking_line_fields
         add_fields CoreModule::BOOKING_LINE, [
-         [0, :bkln_line_number, :line_number, "Line Number", {:type=>:integer}],
-         [0, :bkln_quantity, :quantity, "Quantity Booked", {:type=>:decimal, :description=>"Quantity Booked"}],
-         [0, :bkln_gross_kgs, :gross_kgs, "Gross Weights (KGS)", {:type=>:integer, :description=>"Gross Weights (KGS)"}],
-         [0, :bkln_cbms, :cbms, "CBMS", {:type=>:integer, :description=>"CBMS"}],
-         [0, :bkln_carton_qty, :carton_qty, "Carton Quantity", {:type=>:integer, :description=>"Carton Quantity"}],
-         [12,:bkln_carton_set_uid,:carton_set_id,"Carton Set Unique ID",
+         [1, :bkln_line_number, :line_number, "Line Number", {:data_type=>:integer}],
+         [2, :bkln_quantity, :quantity, "Quantity Booked", {:data_type=>:decimal}],
+         [3, :bkln_gross_kgs, :gross_kgs, "Gross Weights (KGS)", {:data_type=>:integer}],
+         [4, :bkln_cbms, :cbms, "CBMS", {:data_type=>:integer}],
+         [5, :bkln_carton_qty, :carton_qty, "Carton Quantity", {:data_type=>:integer}],
+         [6,:bkln_carton_set_uid,:carton_set_id,"Carton Set Unique ID",
           {data_type: :integer,
            import_lambda: lambda {|sl,id|
              return "#{ModelField.find_by_uid(:bkln_carton_set_uid).label} was blank." if id.blank?
@@ -20,7 +20,7 @@ module OpenChain
              "#{ModelField.find_by_uid(:bkln_carton_set_uid).label} set to #{cs.id}."
            }
           }]]
-          add_fields CoreModule::BOOKING_LINE, make_product_arrays(100,'bkln',CoreModule::BOOKING_LINE.table_name)
+          add_fields CoreModule::BOOKING_LINE, make_product_arrays(7,'bkln',CoreModule::BOOKING_LINE.table_name)
        end
     end
   end
