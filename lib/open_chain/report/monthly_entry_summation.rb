a6fcbe099e777403fe314d7ccd5a868778125aaf
Max Furman <max@arcweb.co>
2015-06-08 15:48:31 -0400

    Fix dates to be midnight and exclusive

--- lib/open_chain/report/monthly_entry_summation.rb
+++ lib/open_chain/report/monthly_entry_summation.rb
@@ -14,7 +14,11 @@ module OpenChain
       def run(run_by, settings)
         workbook = Spreadsheet::Workbook.new
         sheet = workbook.create_worksheet name: "Entry Summation"
-        table_from_query sheet, entry_summation_query(settings['start_date'], settings['end_date'], settings['customer_number'])
+
+        start_date = settings['start_date'].to_datetime.midnight
+        end_date = settings['end_date'].to_datetime.midnight - 1.second
+
+        table_from_query sheet, entry_summation_query(start_date, end_date, settings['customer_number'])
         workbook_to_tempfile workbook, 'MonthlyEntrySummation-'
       end
 
