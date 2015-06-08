79bf0fef40f6f3a60d72f88db5d4da9da0e52936
Max Furman <max@arcweb.co>
2015-06-08 14:46:46 -0400

    Escape settings correctly

--- lib/open_chain/report/monthly_entry_summation.rb
+++ lib/open_chain/report/monthly_entry_summation.rb
@@ -14,7 +14,7 @@ module OpenChain
       def run(run_by, settings)
         workbook = Spreadsheet::Workbook.new
         sheet = workbook.create_worksheet name: "Entry Summation"
-        table_from_query sheet, entry_summation_query(settings[:start_date], settings[:end_date], settings[:customer_number])
+        table_from_query sheet, entry_summation_query(settings['start_date'], settings['end_date'], settings['customer_number'])
         workbook_to_tempfile workbook, 'MonthlyEntrySummation-'
       end
 
@@ -29,8 +29,8 @@ SELECT
   (SELECT SUM(charge_amount) FROM broker_invoices JOIN broker_invoice_lines ON broker_invoice_id = broker_invoices.id WHERE charge_type <> 'F' AND broker_invoices.entry_id = entries.id ) AS `Total Freight`,
   (SELECT SUM(charge_amount) FROM broker_invoices JOIN broker_invoice_lines ON broker_invoice_id = broker_invoices.id WHERE charge_type NOT IN ('F','D') AND broker_invoices.entry_id = entries.id) AS `Total Brokerage Fees`
 FROM entries
-WHERE entries.release_date BETWEEN #{start_date} AND #{end_date}
-  AND entries.customer_number = #{customer_number}
+WHERE entries.release_date BETWEEN '#{start_date}' AND '#{end_date}'
+  AND entries.customer_number = '#{customer_number}'
 GROUP BY `Year / Month`
 QRY
         q
