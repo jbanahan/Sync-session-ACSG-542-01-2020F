521f5033bcdbbacf0e932de3ef69b53ddcf6c3d6
Max Furman <max@arcweb.co>
2015-06-08 16:11:19 -0400

    No more dollars, working subquery joins

--- lib/open_chain/report/monthly_entry_summation.rb
+++ lib/open_chain/report/monthly_entry_summation.rb
@@ -27,11 +27,11 @@ module OpenChain
 SELECT
   DATE_FORMAT(entries.release_date,'%Y-%m') AS `Year / Month`,
   (SELECT COUNT(DISTINCT container_number) FROM containers JOIN entries AS entry ON entry_id = entry.id WHERE DATE_FORMAT(entry.release_date,'%Y-%m') = `Year / Month`) AS `Total Containers`,
-  CONCAT('$', FORMAT(SUM(entries.total_invoiced_value),2)) AS `Total Invoice Value`,
-  CONCAT('$', FORMAT(SUM(entries.total_duty + entries.total_duty_direct),2)) AS `Total Duty`,
-  CONCAT('$', FORMAT(SUM(entries.total_fees),2)) AS `Total Fees`,
-  (SELECT CONCAT('$', FORMAT(SUM(charge_amount),2)) FROM broker_invoices JOIN broker_invoice_lines ON broker_invoice_id = broker_invoices.id WHERE charge_type <> 'F' AND broker_invoices.entry_id = entries.id ) AS `Total Freight`,
-  (SELECT CONCAT('$', FORMAT(SUM(charge_amount),2)) FROM broker_invoices JOIN broker_invoice_lines ON broker_invoice_id = broker_invoices.id WHERE charge_type NOT IN ('F','D') AND broker_invoices.entry_id = entries.id) AS `Total Brokerage Fees`
+  SUM(entries.total_invoiced_value) AS `Total Invoice Value`,
+  SUM(entries.total_duty + entries.total_duty_direct) AS `Total Duty`,
+  SUM(entries.total_fees) AS `Total Fees`,
+  (SELECT SUM(charge_amount) FROM broker_invoices JOIN broker_invoice_lines ON broker_invoice_id = broker_invoices.id JOIN entries AS entry ON broker_invoices.entry_id = entry.id WHERE DATE_FORMAT(entry.release_date,'%Y-%m') = `Year / Month` AND charge_type <> 'F' ) AS `Total Freight`,
+  (SELECT SUM(charge_amount) FROM broker_invoices JOIN broker_invoice_lines ON broker_invoice_id = broker_invoices.id JOIN entries AS entry ON broker_invoices.entry_id = entry.id WHERE DATE_FORMAT(entry.release_date,'%Y-%m') = `Year / Month` AND charge_type NOT IN ('F','D')) AS `Total Brokerage Fees`
 FROM entries
 WHERE entries.release_date BETWEEN '#{start_date}' AND '#{end_date}'
   AND entries.customer_number = '#{customer_number}'
