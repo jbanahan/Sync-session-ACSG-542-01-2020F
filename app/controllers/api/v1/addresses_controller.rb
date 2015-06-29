7087306473a93b4a0bc41af7fd550dfe024954c1
Max Furman <max@arcweb.co>
2015-06-29 09:58:20 -0400

    Address autocomplete component

--- app/controllers/api/v1/addresses_controller.rb
+++ app/controllers/api/v1/addresses_controller.rb
@@ -1,8 +1,8 @@
 module Api; module V1; class AddressesController < ApiController
 
   def autocomplete
-    result = Address.where('name LIKE %?%',params[:n]).where(in_address_book:true)
-    render json: {addresses: result}
+    result = Address.where('name like ?',"%#{params[:n]}%").where(in_address_book:true)
+    render json: result, root: false
   end
 
   def create
