dbe77ae5972a06f715117e270f4b384661f85979
Max Furman <max@arcweb.co>
2015-07-07 15:13:42 -0400

    Avoid 'redefining constant' error

--- lib/open_chain/custom_handler/isf_xml_generator.rb
+++ lib/open_chain/custom_handler/isf_xml_generator.rb
@@ -5,7 +5,7 @@ module OpenChain; module CustomHandler; class ISFXMLGenerator
   include OpenChain::XmlBuilder
   include OpenChain::FtpFileSupport
 
-  EdiLine = Struct.new('EdiLine',:order_number, :hts_number, :country_of_origin, :container_number)
+  EdiLine ||= Struct.new('EdiLine',:order_number, :hts_number, :country_of_origin, :container_number)
 
   def self.generate_and_send(shipment_id, first_time)
     new(shipment_id, first_time).generate_and_send!
