664561db634d32a1c3d35f9f88106fcfd0b772d5
Max Furman <max@arcweb.co>
2015-07-08 14:26:52 -0400

    Multiple manufacturers in the ISF

--- lib/open_chain/custom_handler/isf_xml_generator.rb
+++ lib/open_chain/custom_handler/isf_xml_generator.rb
@@ -5,7 +5,7 @@ module OpenChain; module CustomHandler; class ISFXMLGenerator
   include OpenChain::XmlBuilder
   include OpenChain::FtpFileSupport
 
-  EdiLine ||= Struct.new('EdiLine',:order_number, :hts_number, :country_of_origin, :container_number)
+  EdiLine ||= Struct.new('EdiLine',:order_number, :hts_number, :country_of_origin, :container_number, :manufacturer_supplier_code)
 
   def self.generate_and_send(shipment_id, first_time)
     new(shipment_id, first_time).generate_and_send!
@@ -15,15 +15,16 @@ module OpenChain; module CustomHandler; class ISFXMLGenerator
     @shipment = Shipment.find(shipment_id)
     @first_time = first_time
     raise 'Shipment is missing information required for ISF!' unless @shipment.valid_isf?
-    @edi_lines = Set.new
+    @address_ids = @shipment.shipment_lines.map(&:manufacturer_address_id).uniq
 
-    @edi_lines.merge(
+    @edi_lines = Set.new(
       @shipment.shipment_lines.map do |line|
         EdiLine.new(
           line.order_lines.first.order.customer_order_number,
           line.us_hts_number,
           line.country_of_origin,
-          line.container.try(:container_number)
+          line.container.try(:container_number),
+          @address_ids.index(line.manufacturer_address_id) + 1
         )
       end
     )
@@ -58,7 +59,7 @@ module OpenChain; module CustomHandler; class ISFXMLGenerator
     add_element root, 'ACTION_CD', @first_time ? 'A' : 'R'
     add_element root, 'IMPORTER_ACCT_CD', @shipment.importer.alliance_customer_number
     add_element root, 'OWNER_ACCT_CD', options[:isf_cust_acct]
-    add_element root, 'EST_LOAD_DATE', @shipment.est_load_date.in_time_zone('US/Eastern').iso8601
+    add_element root, 'EST_LOAD_DATE', @shipment.est_load_date ? @shipment.est_load_date.in_time_zone('US/Eastern').iso8601 : nil
     add_element root, 'BOOKING_NBR', @shipment.booking_number
 
     if @shipment.house_bill_of_lading.present?
@@ -84,30 +85,31 @@ module OpenChain; module CustomHandler; class ISFXMLGenerator
     add_element importer_entity, 'BROKERAGE_ACCT_CD', @shipment.importer.alliance_customer_number
 
     consignee_entity = add_element root, 'EdiEntity'
-    add_element consignee_entity, 'ENTITY_TYPE_CD', 'CS'
+    add_element consignee_entity, 'ENTITY_TYPE_CD', 'CN'
     add_element consignee_entity, 'ENTITY_ID', @shipment.consignee.irs_number
     add_element consignee_entity, 'ENTITY_ID_TYPE_CD', 'EI'
     add_element consignee_entity, 'NAME', @shipment.consignee.name
-
-    #TODO: Manufacturers
 
     add_address_entity root, @shipment.seller_address, 'SE'
     add_address_entity root, @shipment.buyer_address, 'BY'
     add_address_entity root, @shipment.ship_to_address, 'ST'
     add_address_entity root, @shipment.container_stuffing_address, 'LG'
     add_address_entity root, @shipment.consolidator_address, 'CS'
-    add_address_entity root, @shipment.manufacturer_address, 'MF', 1 if @shipment.manufacturer_address
 
-    @edi_lines.each do |line|
-      line_element = add_element root, 'EdiLine'
-      add_element line_element, 'MFG_SUPPLIER_CD', 1 if @shipment.manufacturer_address
-      add_element line_element, 'PO_NBR', line.order_number
-      add_element line_element, 'TARIFF_CD', line.hts_number
-      add_element line_element, 'ORIGIN_COUNTRY_CD', line.country_of_origin
-      add_element line_element, 'CONTAINER_NBR', line.container_number
-    end
+    @address_ids.each_with_index { |address_id, idx| add_address_entity root, Address.find(address_id), 'MF', idx+1 }
+
+    @edi_lines.each { |line| add_edi_line root, line }
 
     doc
+  end
+
+  def add_edi_line(root, line)
+    line_element = add_element root, 'EdiLine'
+    add_element line_element, 'MFG_SUPPLIER_CD', line.manufacturer_supplier_code
+    add_element line_element, 'PO_NBR', line.order_number
+    add_element line_element, 'TARIFF_CD', line.hts_number
+    add_element line_element, 'ORIGIN_COUNTRY_CD', line.country_of_origin
+    add_element line_element, 'CONTAINER_NBR', line.container_number
   end
 
   def add_address_entity(root, address, entity_type, index=nil)
