8ff74364bec8c465d73fbc2b90e39fe4177147e0
Jeremy Hulford <jhulford@vandegriftinc.com>
2015-07-08 11:34:55 -0500

    Fix Monthly Entry Summation report.

--- lib/open_chain/report/monthly_entry_summation.rb
+++ lib/open_chain/report/monthly_entry_summation.rb
@@ -14,12 +14,23 @@ module OpenChain
       def run(run_by, settings)
         workbook = Spreadsheet::Workbook.new
         sheet = workbook.create_worksheet name: "Entry Summation"
+        timezone = ActiveSupport::TimeZone[run_by.time_zone]
+        start_date = timezone.parse(settings['start_date']).in_time_zone("UTC")
+        end_date = timezone.parse(settings['end_date']).in_time_zone("UTC")
+        timezone_id = timezone.tzinfo.identifier
+        cust_no = settings['customer_number']
 
-        start_date = settings['start_date'].to_datetime.midnight
-        end_date = settings['end_date'].to_datetime.midnight - 1.second
-        timezone = ActiveSupport::TimeZone[run_by.time_zone].tzinfo.identifier
+        query = entry_summation_query(start_date, end_date, settings['customer_number'], timezone_id)
 
-        table_from_query sheet, entry_summation_query(start_date, end_date, settings['customer_number'], timezone)
+        data_conversions = {}
+        data_conversions[5] = lambda do |result_set_row, raw_column_value|
+          monthly_charge_sum(result_set_row[0], timezone, cust_no, ["'F'"])
+        end
+        data_conversions[6] = lambda do |result_set_row, raw_column_value|
+          monthly_charge_sum(result_set_row[0], timezone, cust_no, ["'F'", "'D'"], true)
+        end
+
+        table_from_query sheet, query, data_conversions
         workbook_to_tempfile workbook, 'MonthlyEntrySummation-'
       end
 
@@ -31,14 +42,30 @@ SELECT
   SUM(entries.total_invoiced_value) AS `Total Invoice Value`,
   SUM(entries.total_duty + entries.total_duty_direct) AS `Total Duty`,
   SUM(entries.total_fees) AS `Total Fees`,
-  (SELECT SUM(charge_amount) FROM broker_invoices JOIN broker_invoice_lines ON broker_invoice_id = broker_invoices.id JOIN entries AS entry ON broker_invoices.entry_id = entry.id WHERE DATE_FORMAT(convert_tz(entry.release_date, 'UTC', '#{timezone}'),'%Y-%m') = `Year / Month` AND entry.customer_number = '#{customer_number}') AND charge_type = 'F' ) AS `Total Freight`,
-  (SELECT SUM(charge_amount) FROM broker_invoices JOIN broker_invoice_lines ON broker_invoice_id = broker_invoices.id JOIN entries AS entry ON broker_invoices.entry_id = entry.id WHERE DATE_FORMAT(convert_tz(entry.release_date, 'UTC', '#{timezone}'),'%Y-%m') = `Year / Month` AND entry.customer_number = '#{customer_number}') AND charge_type NOT IN ('F','D')) AS `Total Brokerage Fees`
+  '' AS `Total Freight`,
+  '' AS `Total Brokerage Fees`
 FROM entries
-WHERE convert_tz(entries.release_date, 'UTC', '#{timezone}') BETWEEN '#{start_date}' AND '#{end_date}'
+WHERE entries.release_date >= '#{start_date}' AND entries.release_date < '#{end_date}'
   AND entries.customer_number = '#{customer_number}'
 GROUP BY `Year / Month`
 QRY
         q
+      end
+
+      def monthly_charge_sum year_month, timezone, customer_number, charge_types, not_in = false
+        first_of_month = timezone.parse("#{year_month}-01")
+        end_of_month = first_of_month + 1.month
+        qry = <<QRY
+SELECT SUM(charge_amount) 
+FROM broker_invoices 
+INNER JOIN broker_invoice_lines ON broker_invoice_id = broker_invoices.id 
+INNER JOIN entries ON broker_invoices.entry_id = entries.id 
+WHERE entries.release_date >= '#{first_of_month.in_time_zone("UTC")}' 
+AND entries.release_date < '#{end_of_month.in_time_zone("UTC")}' AND entries.customer_number = '#{customer_number}' AND broker_invoice_lines.charge_type #{not_in ? "NOT IN" : "IN"} (#{charge_types.join(", ")})
+AND broker_invoices.invoice_date >= '#{first_of_month.strftime("%Y-%m-%d")}' AND broker_invoices.invoice_date < '#{end_of_month.strftime("%Y-%m-%d")}' AND broker_invoices.customer_number = '#{customer_number}'
+QRY
+        val = ActiveRecord::Base.connection.execute(qry).try(:first).try(:first)
+        val.presence || BigDecimal("0")
       end
 
     end
