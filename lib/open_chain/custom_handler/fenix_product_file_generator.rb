b3c88db159fb1ca443f62aac873d418df182c00c
Jeremy Hulford <jhulford@vandegriftinc.com>
2015-07-27 14:06:22 -0500

    Strip forbidden B3 chars from Fenix product feed.

--- lib/open_chain/custom_handler/fenix_product_file_generator.rb
+++ lib/open_chain/custom_handler/fenix_product_file_generator.rb
@@ -88,6 +88,8 @@ module OpenChain
             line += force_fixed(p.get_custom_value(@cdefs[:prod_country_of_origin]).value.to_s, 3)
           end
 
+          #Because Canada doesn't like these chars in electronic b3's, replace any of the following chars w/ spaces: ! ' : | + ? 
+          line = line.gsub(/[!':|+?]/, " ") 
           line += "\r\n"
         end
 
