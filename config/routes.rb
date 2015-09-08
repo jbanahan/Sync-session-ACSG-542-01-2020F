62f0d8fdf5eebbdf31abd40aaee099a5e22857f5
Jay Davis <jdavis@vandegriftinc.com>
2015-09-08 15:53:22 -0400

    Add rl tariff report (class, routes, controllers/tests, views)

--- config/routes.rb
+++ config/routes.rb
@@ -384,6 +384,8 @@ OpenChain::Application.routes.draw do
   match "/reports/run_j_jill_weekly_freight_summary" => "reports#run_j_jill_weekly_freight_summary", :via=>:post
   match "/reports/show_drawback_audit_report" => "reports#show_drawback_audit_report", :via=>:get
   match "/reports/run_drawback_audit_report" => "reports#run_drawback_audit_report", :via=>:post
+  match "/reports/show_rl_tariff_totals" =>"reports#show_rl_tariff_totals", :via=>:get
+  match "/reports/run_rl_tariff_totals" =>"reports#run_rl_tariff_totals", :via=>:post
   get "/reports/show_pvh_container_log" => "reports#show_pvh_container_log"
   post "/reports/run_pvh_container_log" => "reports#run_pvh_container_log"
   get "reports/show_monthly_entry_summation" => "reports#show_monthly_entry_summation"
