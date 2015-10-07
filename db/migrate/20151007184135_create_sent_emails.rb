113a76c68c1d35d5f8d417a594eb92c64290b10c
Jay Davis <jdavis@vandegriftinc.com>
2015-10-07 16:05:06 -0400

    Add SentEmail migration, model

---
+++ db/migrate/20151007184135_create_sent_emails.rb
@@ -0,0 +1,20 @@
+class CreateSentEmails < ActiveRecord::Migration
+  def self.up
+    create_table :sent_emails do |t|
+      t.string :email_subject
+      t.string :email_to
+      t.string :email_cc
+      t.string :email_bcc
+      t.string :email_from
+      t.string :email_reply_to
+      t.timestamp :email_date
+      t.text :email_body
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :sent_emails
+  end
+end
