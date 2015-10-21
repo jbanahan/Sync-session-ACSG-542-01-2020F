f55647ecf2a20dd312c8417f634876cc9836ec11
Brian Glick <brian@brian-glick.com>
2015-10-20 22:07:13 -0400

    fixes for test cases

--- app/models/core_module_definitions.rb
+++ app/models/core_module_definitions.rb
@@ -203,9 +203,9 @@ module CoreModuleDefinitions
   })
   COMMERCIAL_INVOICE = CoreModule.new("CommercialInvoice","Invoice",{
       :enabled_lambda => lambda {MasterSetup.get.entry_enabled?},
-      :unique_id_field_name => :invoice_number,
+      :unique_id_field_name => :ci_invoice_number,
       :show_field_prefix=>true,
-      :key_model_field_uids=>[:invoice_number],
+      :key_model_field_uids=>[:ci_invoice_number],
       :children => [COMMERCIAL_INVOICE_LINE],
       :child_lambdas => {COMMERCIAL_INVOICE_LINE=> lambda {|i| i.commercial_invoice_lines}},
       :child_joins => {COMMERCIAL_INVOICE_LINE => "LEFT OUTER JOIN commercial_invoice_lines on commercial_invoices.id = commercial_invoice_lines.commercial_invoice_id"}
@@ -229,7 +229,11 @@ module CoreModuleDefinitions
        :logical_key_lambda => lambda {|obj| "#{obj.source_system}_#{obj.broker_reference}"}
    })
   OFFICIAL_TARIFF = CoreModule.new("OfficialTariff","HTS Regulation",:default_search_columns=>[:ot_hts_code,:ot_cntry_iso,:ot_full_desc,:ot_common_rate], :quicksearch_fields=> [:ot_hts_code,:ot_full_desc])
-  PLANT_PRODUCT_GROUP_ASSIGNMENT = CoreModule.new('PlantProductGroupAssignment','Plant Product Group Assignment',default_search_columns:[:ppga_pg_name], show_field_prefix: true)
+  PLANT_PRODUCT_GROUP_ASSIGNMENT = CoreModule.new('PlantProductGroupAssignment','Plant Product Group Assignment',
+    unique_id_field_name: :ppga_pg_name,
+    default_search_columns:[:ppga_pg_name], 
+    key_model_field_uids: [:ppga_pg_name],
+    show_field_prefix: true)
   PLANT = CoreModule.new("Plant","Plant",
     default_search_columns: [:plant_name],
     unique_id_field_name: :plant_name,
