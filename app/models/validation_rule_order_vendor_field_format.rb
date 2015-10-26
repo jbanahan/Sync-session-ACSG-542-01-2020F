43976eb9dd160e0e807c02609751d595070ed66f
Brian Glick <brian@brian-glick.com>
2015-10-26 09:28:39 -0400

    fix return value

--- app/models/validation_rule_order_vendor_field_format.rb
+++ app/models/validation_rule_order_vendor_field_format.rb
@@ -4,7 +4,7 @@ class ValidationRuleOrderVendorFieldFormat < BusinessValidationRule
   def run_validation ord
     vend = ord.vendor
     if vend.nil?
-      "Vendor must be set."
+      return "Vendor must be set."
     end
     validate_field_format(vend) do |mf, val, regex|
       "All #{mf.label} value does not match '#{regex}' format for Vendor #{vend.name}."
