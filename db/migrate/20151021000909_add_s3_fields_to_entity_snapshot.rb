bbb3f367c8170253ba0ddee8dc06642f13523ac9
Jeremy Hulford <jhulford@vandegriftinc.com>
2015-11-03 09:07:12 -0600

    Rewrite migration to be a single table change call.

--- db/migrate/20151021000909_add_s3_fields_to_entity_snapshot.rb
+++ db/migrate/20151021000909_add_s3_fields_to_entity_snapshot.rb
@@ -1,9 +1,18 @@
 class AddS3FieldsToEntitySnapshot < ActiveRecord::Migration
-  def change
-    add_column :entity_snapshots, :bucket, :string
-    add_column :entity_snapshots, :doc_path, :string
-    add_column :entity_snapshots, :version, :string
-    add_column :entity_snapshots, :compared_at, :datetime
+  def up
+    change_table :entity_snapshots, bulk: true do |t|
+      t.string :bucket
+      t.string :doc_path
+      t.string :version
+      t.datetime :compared_at
+    end
     add_index :entity_snapshots, [:bucket, :doc_path, :compared_at], name: 'Uncompared Items'
+  end
+
+  def down
+    change_table :entity_snapshots, bulk: true do |t|
+      # By virtue of removing every column referenced by the index above, the index itself is dropped
+      t.remove :bucket, :doc_path, :version, :compared_at
+    end
   end
 end
