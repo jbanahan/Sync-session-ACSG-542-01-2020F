2d8eeef6da692fd1ab24a60623e8a53329b283d8
Brian Glick <brian@brian-glick.com>
2015-12-07 21:56:46 -0500

    fix for new system bootstrap

--- config/initializers/register_view_selector.rb
+++ config/initializers/register_view_selector.rb
@@ -1,6 +1,8 @@
 require 'open_chain/custom_handler/custom_view_selector'
 require 'open_chain/custom_handler/lumber_liquidators/lumber_view_selector'
 
-if MasterSetup.get.system_code = 'll' 
-  OpenChain::CustomHandler::CustomViewSelector.register_handler OpenChain::CustomHandler::LumberLiquidators::LumberViewSelector
+if ActiveRecord::Base.connection.table_exists?('master_setups')
+  if MasterSetup.get.system_code = 'll' 
+    OpenChain::CustomHandler::CustomViewSelector.register_handler OpenChain::CustomHandler::LumberLiquidators::LumberViewSelector
+  end
 end
