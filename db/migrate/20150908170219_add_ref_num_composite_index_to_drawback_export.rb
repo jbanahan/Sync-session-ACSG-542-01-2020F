33cc48552b334cf0f19d124c0f3675d1a3973db3
Brian Glick <brian@brian-glick.com>
2015-12-30 14:34:33 +0000

    adapt index for Cloud9 IDE

--- db/migrate/20150908170219_add_ref_num_composite_index_to_drawback_export.rb
+++ db/migrate/20150908170219_add_ref_num_composite_index_to_drawback_export.rb
@@ -2,7 +2,7 @@ class AddRefNumCompositeIndexToDrawbackExport < ActiveRecord::Migration
   def change
     # The default charset in CircleCI is different than our production server, so the key lengths on the index is too long in Circle
     # Just work around that by using only part of the column for the index
-    if ENV["CIRCLECI"]
+    if ENV["CIRCLECI"] || File.exist?('.c9/project.settings')
       execute "CREATE INDEX unique_refs ON duty_calc_export_file_lines(ref_1(100), ref_2(100), ref_3(100), ref_4(100), part_number(100), importer_id)"
     else
       add_index :duty_calc_export_file_lines, [:ref_1,:ref_2,:ref_3,:ref_4,:part_number,:importer_id], {name: :unique_refs}
