7aeed6c3bf4dd8751221b819aaa6d9360934c964
Jeremy Hulford <jhulford@vandegriftinc.com>
2016-01-04 15:01:25 -0600

    Add retry

--- spec/lib/s3_spec.rb
+++ spec/lib/s3_spec.rb
@@ -205,27 +205,30 @@ describe OpenChain::S3, s3: true do
           # Bizarrely, if I put the sleep AFTER doing the integration keys call the upload_file call below
           # (specifically the s3_object.write method) blocks for like 20 seconds.  Possible aws-sdk bug or ruby 2
           # bug with leaking socket handle or something?  This wasn't happening w/ 1.9.3.
-          sleep 2
-          OpenChain::S3.should_receive(:integration_bucket_name).at_least(2).times.and_return(@bucket)
-          found_keys = []
-          OpenChain::S3.integration_keys(Date.new(2011,12,26), "subfolder/2") {|key| found_keys << key }
-          found_keys[0].should == @my_keys[0]
-          found_keys[1].should == @my_keys[1]
-
-          Tempfile.open('test') do |f|
-            f.binmode
-            f << "Test"
-            f.flush
-
-            # Lets update key[0] and it should then be returned second
-            # Need to use a different file with different content, otherwise S3 object is too smart
-            # and doesn't actually send the data if it didn't change
-            OpenChain::S3.upload_file @bucket, @my_keys[0], f
+          retry_expect {
+            OpenChain::S3.should_receive(:integration_bucket_name).at_least(2).times.and_return(@bucket)
             found_keys = []
             OpenChain::S3.integration_keys(Date.new(2011,12,26), "subfolder/2") {|key| found_keys << key }
-            found_keys[0].should == @my_keys[1]
-            found_keys[1].should == @my_keys[0]
-          end
+            found_keys[0].should == @my_keys[0]
+            found_keys[1].should == @my_keys[1]
+          }
+
+          retry_expect {
+            Tempfile.open('test') do |f|
+              f.binmode
+              f << "Test"
+              f.flush
+
+              # Lets update key[0] and it should then be returned second
+              # Need to use a different file with different content, otherwise S3 object is too smart
+              # and doesn't actually send the data if it didn't change
+              OpenChain::S3.upload_file @bucket, @my_keys[0], f
+              found_keys = []
+              OpenChain::S3.integration_keys(Date.new(2011,12,26), "subfolder/2") {|key| found_keys << key }
+              found_keys[0].should == @my_keys[1]
+              found_keys[1].should == @my_keys[0]
+            end
+          }
         end
 
         it 'strips leading slashes from subfolder name' do
