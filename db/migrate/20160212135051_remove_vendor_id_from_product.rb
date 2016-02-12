b68956d9978148e2cba1dcdb6c5c9269e6a76582
Brian Glick <brian@brian-glick.com>
2016-02-12 13:23:03 -0500

    migrate existing data to new structure

--- db/migrate/20160212135051_remove_vendor_id_from_product.rb
+++ db/migrate/20160212135051_remove_vendor_id_from_product.rb
@@ -1,5 +1,6 @@
 class RemoveVendorIdFromProduct < ActiveRecord::Migration
   def up
+    execute "INSERT INTO product_vendor_assignments (product_id, vendor_id, created_at, updated_at) SELECT id, vendor_id, now(), now() FROM products WHERE vendor_id is not null"
     remove_column :products, :vendor_id
   end
 
