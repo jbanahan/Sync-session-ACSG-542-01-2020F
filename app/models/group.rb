651f8374012a06f0a4313959898359a105f2ba7d
Brian Glick <brian@brian-glick.com>
2016-02-17 21:49:17 -0500

    put back new group method signature

--- app/models/group.rb
+++ app/models/group.rb
@@ -31,7 +31,7 @@ QRY
   }
   # Finds (and creates if not found) the system group w/ the given system code
   # Uses group_name only when creating the group
-  def self.use_system_group system_code, name = nil, create = true
+  def self.use_system_group system_code, name: nil, create: true
     group_name = system_code if group_name.nil?
     g = Group.where(system_code: system_code)
     if create
