5ea9e69d1fd40248f90e99c6c4823375435f1b55
Jay Davis <jdavis@vandegriftinc.com>
2016-03-02 11:17:10 -0500

    Condense SQL query, change vendor_sap_# split character

--- lib/open_chain/report/ll_prod_risk_report.rb
+++ lib/open_chain/report/ll_prod_risk_report.rb
@@ -1,7 +1,9 @@
+require 'open_chain/custom_handler/lumber_liquidators/lumber_custom_definition_support'
 require 'open_chain/report/report_helper'
 module OpenChain
   module Report
     class LlProdRiskReport
+      include OpenChain::CustomHandler::LumberLiquidators::LumberCustomDefinitionSupport
       include OpenChain::Report::ReportHelper
       
       def self.permission? user
@@ -18,13 +20,14 @@ module OpenChain
 
       def create_workbook
         wb = XlsMaker.create_workbook "Products Needing Risk Assignment"
-        table_from_query wb.worksheet(0), query, {'Vendor SAP #' => link_lambda} 
+        custom_defs = self.class.prep_custom_definitions([:prodven_risk,:cmp_sap_company])
+        table_from_query wb.worksheet(0), query(custom_defs[:prodven_risk], custom_defs[:cmp_sap_company]), {'Vendor SAP #' => link_lambda} 
         wb
       end
 
       def link_lambda
         lambda do |result_set_row, raw_column_value| 
-          vendor_sap, vendor_id = raw_column_value.split("/")
+          vendor_sap, vendor_id = raw_column_value.split("~*~")
           vendor_sap_link = XlsMaker.excel_url("/vendors/" + vendor_id)
           XlsMaker.create_link_cell(vendor_sap_link, vendor_sap)
         end
@@ -45,11 +48,11 @@ module OpenChain
         end
       end
 
-      def query
+      def query(cd_prodven_risk, cd_cmp_sap_company)
         <<-SQL
           SELECT (CASE 
-                  WHEN cv_ven.string_value IS NULL OR cv_ven.string_value = '' THEN CONCAT("None","/",c.id)
-                  ELSE CONCAT(cv_ven.string_value,"/",c.id)
+                  WHEN cv_ven.string_value IS NULL OR cv_ven.string_value = '' THEN CONCAT("None","~*~",c.id)
+                  ELSE CONCAT(cv_ven.string_value,"~*~",c.id)
                   END) AS "Vendor SAP #", 
             c.name AS "Vendor Name", 
             p.unique_identifier AS "Product SAP #", 
@@ -57,10 +60,10 @@ module OpenChain
           FROM companies as c
             INNER JOIN product_vendor_assignments AS pva ON c.id = pva.vendor_id
             INNER JOIN products AS p on p.id = pva.product_id
-            LEFT OUTER JOIN custom_values AS cv_ven ON (cv_ven.customizable_type = "company" AND cv_ven.customizable_id = c.id)
-            LEFT OUTER JOIN custom_definitions AS cd_ven ON (cd_ven.module_type = "Company" AND cd_ven.label = 'SAP Company #')
-            LEFT OUTER JOIN custom_values AS cv_prodven ON (cv_prodven.customizable_type = "ProductVendorAssignment" AND cv_prodven.customizable_id = pva.id) 
-            LEFT OUTER JOIN custom_definitions AS cd_prodven ON (cd_prodven.module_type = "ProductVendorAssignment" AND cd_prodven.label = 'Risk')
+            LEFT OUTER JOIN custom_values AS cv_ven ON (cv_ven.customizable_type = "company" AND cv_ven.customizable_id = c.id 
+                                                        AND cv_ven.custom_definition_id = #{cd_cmp_sap_company.id})
+            LEFT OUTER JOIN custom_values AS cv_prodven ON (cv_prodven.customizable_type = "ProductVendorAssignment" AND cv_prodven.customizable_id = pva.id
+                                                        AND cv_prodven.custom_definition_id = #{cd_prodven_risk.id})
           WHERE cv_prodven.string_value IS NULL OR cv_prodven.string_value = ''
           ORDER BY c.name
         SQL
