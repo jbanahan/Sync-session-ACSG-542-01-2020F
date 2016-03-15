3dcc226adf2945699d1250fc076a6d9dc265c44c
Jay Davis <jdavis@vandegriftinc.com>
2016-03-15 17:17:41 -0400

    Add parser and auxiliary validation class

---
+++ lib/open_chain/custom_handler/hm/hm_i1_interface.rb
@@ -0,0 +1,123 @@
+require 'open_chain/custom_handler/vfitrack_custom_definition_support'
+module OpenChain
+  module CustomHandler
+    module Hm
+      class HmI1Interface
+        include OpenChain::CustomHandler::VfitrackCustomDefinitionSupport
+        
+        def initialize
+           @cust_id = Company.where(alliance_customer_number: 'HENNE').first.id
+           @cdefs = (self.class.prep_custom_definitions [:prod_po_numbers, :prod_sku_number, :prod_earliest_ship_date, :prod_earliest_arrival_date, 
+            :prod_part_number, :prod_season, :prod_suggested_tariff, :prod_countries_of_origin])
+        end
+
+        def self.parse file_content
+          self.new.process file_content
+        end
+
+        def process file_content
+          header = true
+          ActiveRecord::Base.transaction do
+            CSV.parse(file_content, skip_blanks: true) do |row|
+              if header
+                header = false
+                next
+              end
+              *, uid = get_part_number_and_uid(row[3])
+              p = Product.where(unique_identifier: uid).first
+              p ? (update_product p, row, @cdefs) : (create_product row, @cdefs, @cust_id)
+            end
+          end        
+        end
+
+        def create_product row, cdefs, cust_id
+          valid = HmI1Validator.new row
+          p = Product.new
+          part_number, uid = (valid.check(3) ? get_part_number_and_uid(row[3]) : nil)
+          p.unique_identifier = uid
+          p.name = row[5]
+          p.importer_id = cust_id
+          p.save!
+          p.update_custom_value! cdefs[:prod_po_numbers], valid.filter(0, row[0])
+          p.update_custom_value! cdefs[:prod_earliest_ship_date], valid.filter(1, Date.strptime(row[1], '%m/%d/%Y'))
+          p.update_custom_value! cdefs[:prod_earliest_arrival_date], valid.filter(2, Date.strptime(row[2], '%m/%d/%Y'))
+          p.update_custom_value! cdefs[:prod_sku_number], valid.filter(3, row[3])
+          p.update_custom_value! cdefs[:prod_part_number], part_number
+          p.update_custom_value! cdefs[:prod_season], valid.filter(4, row[4])
+          p.update_custom_value! cdefs[:prod_suggested_tariff], valid.filter(6, row[6])
+          p.update_custom_value! cdefs[:prod_countries_of_origin], valid.filter(7, row[7])
+          p
+        end
+
+        def update_product product, row, cdefs
+          valid = HmI1Validator.new row
+          unless product.name == row[5]
+            product.name = valid.filter(5, row[5])
+            product.save!
+          end
+          cv_concat product, :prod_po_numbers, valid.filter(0, row[0]), cdefs
+          assign_earlier product, :prod_earliest_ship_date, valid.filter(1, row[1]), cdefs
+          assign_earlier product, :prod_earliest_arrival_date, valid.filter(2, row[2]), cdefs
+          product.update_custom_value! cdefs[:prod_sku_number], valid.filter(3, row[3])
+          cv_concat product, :prod_season, valid.filter(4, row[4]), cdefs
+          product.update_custom_value! cdefs[:prod_suggested_tariff], valid.filter(6, row[6])
+          cv_concat product, :prod_countries_of_origin, valid.filter(7, row[7]), cdefs
+          product
+        end
+
+        def cv_concat product, cv_uid, str, cdefs
+          old_str = (product.get_custom_value cdefs[cv_uid]).value
+          arr = old_str.blank? ? [] : old_str.split("\n ")
+          arr << str unless arr.include? str
+          new_str = arr.join("\n ")
+          product.update_custom_value! cdefs[cv_uid], new_str unless new_str == old_str
+          product
+        end
+
+        def assign_earlier product, cv_uid, date_str, cdefs
+          current_date = (product.get_custom_value cdefs[cv_uid]).value
+          parsed_date = Date.strptime(date_str,'%m/%d/%Y')
+          product.update_custom_value!(cdefs[cv_uid], parsed_date) if current_date.nil? || parsed_date < current_date
+          product
+        end   
+      
+        private
+
+        def get_part_number_and_uid sku
+          trunc_sku = sku[0..6]
+          [trunc_sku, "HENNE-#{trunc_sku}"]
+        end
+      end
+
+      class HmI1Validator
+        def initialize row
+          @valid_arr = validate_row row
+        end
+        
+        def filter index, new_val
+          @valid_arr[index] ? new_val : nil
+        end
+
+        def check index
+          @valid_arr[index]
+        end
+
+        private
+        
+        def validate_row row
+          date_pattern = /^\d{2}\/\d{2}\/\d{4}$/
+          arr = []
+          arr << (row[0] =~ /^\d{6}$/ ? true : false)
+          arr << (row[1] =~ date_pattern ? true : false)
+          arr << (row[2] =~ date_pattern ? true : false)
+          arr << (row[3] =~ /^\d{13}$/ ? true : false)
+          arr << (row[4] =~ /^\d{6}$/ ? true : false)
+          arr << true #placeholder to keep indexing consistent
+          arr << (row[6] =~ /^\d{8}$/ ? true : false)
+          arr << (row[7] =~ /^[A-Z]{2}$/ ? true : false)
+        end
+      end
+    
+    end 
+  end 
+end
---
+++ spec/lib/custom_handler/hm/hm_i1_interface_spec.rb
@@ -0,0 +1,223 @@
+require 'spec_helper'
+
+describe OpenChain::CustomHandler::Hm::HmI1Interface do
+  def load_cdefs
+    @cdefs = {
+        prod_po_numbers: CustomDefinition.where(label: "PO Numbers").first,
+        prod_part_number: CustomDefinition.where(label: "Part Number").first,
+        prod_earliest_ship_date: CustomDefinition.where(label: "Earliest Ship Date").first,
+        prod_earliest_arrival_date: CustomDefinition.where(label: "Earliest Arrival Date").first,
+        prod_sku_number: CustomDefinition.where(label: "SKU Number").first,
+        prod_season: CustomDefinition.where(label: "Season").first,
+        prod_suggested_tariff: CustomDefinition.where(label: "Suggested Tariff").first,
+        prod_countries_of_origin: CustomDefinition.where(label: "Countries of Origin").first
+      }
+  end
+
+  before(:each){ @cust_id = Factory(:company, alliance_customer_number: 'HENNE').id }
+
+  before(:all) do
+    @lines = [
+      "\"hmOrderReferenceNumber\",\"hmOrderDate\",\"expectedReceiptDate\",\"sku\",\"season\",\"skuName\",\"commodityCode\",\"countryOfOrigin\"",
+      "\"182909\",\"01/02/2016\",\"01/02/2016\",\"0148762001002\",\"201501\",\"Large Blue Hooded Sweatshirt\",\"61101190\",\"US\"",
+      "\"909281\",\"01/12/2015\",\"01/10/2015\",\"2001002678410\",\"105102\",\"Small Red T-Shirt\",\"09110116\",\"CA\"",
+      "\"293010\",\"03/03/2016\",\"03/03/2016\",\"1259873112113\",\"312612\",\"Medium Black Sweatpants\",\"72212201\",\"US\"",
+      "\"293010\",\"03/03/2016\",\"02/15/2016\",\"1259873112113\",\"216213\",\"Large Black Sweatpants\",\"10221227\",\"CA\""
+    ]
+  end
+  
+  describe :parse do
+    it "delegates to #process" do
+      file_content = "data"
+      described_class.any_instance.should_receive(:process).with(file_content)
+      described_class.parse file_content
+    end
+  end
+
+  describe :process do
+    it "parses CSV into Product objects" do
+      @t = Tempfile.new("csv")
+      @t << @lines.join("\n")
+      @t.flush
+      parser = described_class.new
+      load_cdefs
+      parser.process(IO.read @t)
+
+      expect(Product.count).to eq 3
+      products = Product.all
+      p1, p2, p3 = products[0], products[1], products[2]
+
+      expect(p1.unique_identifier).to eq "HENNE-0148762"
+      expect(p1.name).to eq "Large Blue Hooded Sweatshirt"
+      expect(p1.importer_id).to eq @cust_id
+      expect((p1.get_custom_value @cdefs[:prod_po_numbers]).value).to eq "182909"
+      expect((p1.get_custom_value @cdefs[:prod_earliest_ship_date]).value).to eq Date.new(2016,1,2)
+      expect((p1.get_custom_value @cdefs[:prod_earliest_arrival_date]).value).to eq Date.new(2016,1,2)
+      expect((p1.get_custom_value @cdefs[:prod_part_number]).value).to eq "0148762" 
+      expect((p1.get_custom_value @cdefs[:prod_sku_number]).value).to eq "0148762001002"
+      expect((p1.get_custom_value @cdefs[:prod_season]).value).to eq "201501"
+      expect((p1.get_custom_value @cdefs[:prod_suggested_tariff]).value).to eq "61101190"
+      expect((p1.get_custom_value @cdefs[:prod_countries_of_origin]).value).to eq "US"
+
+      expect(p2.unique_identifier).to eq "HENNE-2001002"
+      expect(p2.name).to eq "Small Red T-Shirt"
+      expect(p2.importer_id).to eq @cust_id
+      expect((p2.get_custom_value @cdefs[:prod_po_numbers]).value).to eq "909281"
+      expect((p2.get_custom_value @cdefs[:prod_earliest_ship_date]).value).to eq Date.new(2015,1,12)
+      expect((p2.get_custom_value @cdefs[:prod_earliest_arrival_date]).value).to eq Date.new(2015,1,10)
+      expect((p2.get_custom_value @cdefs[:prod_part_number]).value).to eq "2001002" 
+      expect((p2.get_custom_value @cdefs[:prod_sku_number]).value).to eq "2001002678410"
+      expect((p2.get_custom_value @cdefs[:prod_season]).value).to eq "105102"
+      expect((p2.get_custom_value @cdefs[:prod_suggested_tariff]).value).to eq "09110116"
+      expect((p2.get_custom_value @cdefs[:prod_countries_of_origin]).value).to eq "CA"
+
+      expect(p3.unique_identifier).to eq "HENNE-1259873"
+      expect(p3.name).to eq "Large Black Sweatpants"
+      expect(p3.importer_id).to eq @cust_id
+      expect((p3.get_custom_value @cdefs[:prod_po_numbers]).value).to eq "293010"
+      expect((p3.get_custom_value @cdefs[:prod_earliest_ship_date]).value).to eq Date.new(2016,3,3)
+      expect((p3.get_custom_value @cdefs[:prod_earliest_arrival_date]).value).to eq Date.new(2016,2,15)
+      expect((p3.get_custom_value @cdefs[:prod_part_number]).value).to eq "1259873" 
+      expect((p3.get_custom_value @cdefs[:prod_sku_number]).value).to eq "1259873112113"
+      expect((p3.get_custom_value @cdefs[:prod_season]).value).to eq "312612\n 216213"
+      expect((p3.get_custom_value @cdefs[:prod_suggested_tariff]).value).to eq "10221227"
+      expect((p3.get_custom_value @cdefs[:prod_countries_of_origin]).value).to eq "US\n CA"
+
+      @t.unlink
+    end
+  end
+  
+  describe :create_product do
+    it "creates a new Product from a CSV row" do
+      parser = described_class.new
+      load_cdefs
+      parser.create_product CSV.parse_line(@lines[1]), @cdefs, @cust_id
+      prod = Product.first
+      
+      expect(prod.unique_identifier).to eq "HENNE-0148762"
+      expect(prod.name).to eq "Large Blue Hooded Sweatshirt"
+      expect(prod.importer_id).to eq @cust_id
+      expect((prod.get_custom_value @cdefs[:prod_po_numbers]).value).to eq "182909"
+      expect((prod.get_custom_value @cdefs[:prod_earliest_ship_date]).value).to eq Date.new(2016,1,2)
+      expect((prod.get_custom_value @cdefs[:prod_earliest_arrival_date]).value).to eq Date.new(2016,1,2)
+      expect((prod.get_custom_value @cdefs[:prod_part_number]).value).to eq "0148762" 
+      expect((prod.get_custom_value @cdefs[:prod_sku_number]).value).to eq "0148762001002"
+      expect((prod.get_custom_value @cdefs[:prod_season]).value).to eq "201501"
+      expect((prod.get_custom_value @cdefs[:prod_suggested_tariff]).value).to eq "61101190"
+      expect((prod.get_custom_value @cdefs[:prod_countries_of_origin]).value).to eq "US"
+    end
+  end
+
+  describe :update_product do
+    it "updates an existing Product from a CSV row" do
+      parser = described_class.new
+      load_cdefs
+
+      prod = Factory(:product, unique_identifier: "HENNE-0148762", name: "Large Blue Hooded Sweatshirt", importer_id: @cust_id)
+      prod.update_custom_value! @cdefs[:prod_po_numbers], "182909"
+      prod.update_custom_value! @cdefs[:prod_earliest_ship_date], Date.new(2016,1,2)
+      prod.update_custom_value! @cdefs[:prod_earliest_arrival_date], Date.new(2016,1,2)
+      prod.update_custom_value! @cdefs[:prod_part_number], "0148762"
+      prod.update_custom_value! @cdefs[:prod_sku_number], "0148762001002"
+      prod.update_custom_value! @cdefs[:prod_season], "201501"
+      prod.update_custom_value! @cdefs[:prod_suggested_tariff], "61101190"
+      prod.update_custom_value! @cdefs[:prod_countries_of_origin], "US"
+
+      line = CSV.parse_line(@lines[2])
+      line[3] = "0148762001002" #uid of @lines[1]
+      parser.update_product prod, line, @cdefs
+
+      expect(prod.unique_identifier).to eq "HENNE-0148762"
+      expect(prod.name).to eq "Small Red T-Shirt"
+      expect(prod.importer_id).to eq @cust_id
+      expect((prod.get_custom_value @cdefs[:prod_po_numbers]).value).to eq "182909\n 909281"
+      expect((prod.get_custom_value @cdefs[:prod_earliest_ship_date]).value).to eq Date.new(2015,1,12)
+      expect((prod.get_custom_value @cdefs[:prod_earliest_arrival_date]).value).to eq Date.new(2015,1,10)
+      expect((prod.get_custom_value @cdefs[:prod_part_number]).value).to eq "0148762" 
+      expect((prod.get_custom_value @cdefs[:prod_sku_number]).value).to eq "0148762001002"
+      expect((prod.get_custom_value @cdefs[:prod_season]).value).to eq "201501\n 105102"
+      expect((prod.get_custom_value @cdefs[:prod_suggested_tariff]).value).to eq "09110116"
+      expect((prod.get_custom_value @cdefs[:prod_countries_of_origin]).value).to eq "US\n CA"
+    end
+  end
+
+  describe :cv_concat do
+          
+    before :each do
+      @parser = described_class.new
+      load_cdefs
+      @prod = Factory(:product, unique_identifier: "HENNE-0148762")
+      @prod.update_custom_value! @cdefs[:prod_po_numbers], "182909"
+    end
+    
+    it "concats a string to a custom value if it isn't already included" do
+      @parser.cv_concat @prod, :prod_po_numbers, "123456", @cdefs
+      expect((@prod.get_custom_value @cdefs[:prod_po_numbers]).value).to eq "182909\n 123456"
+    end
+
+    it "assigns string to a custom value if the value is blank" do
+      @prod.update_custom_value! @cdefs[:prod_po_numbers], nil
+      @parser.cv_concat @prod, :prod_po_numbers, "123456", @cdefs
+      expect((@prod.get_custom_value @cdefs[:prod_po_numbers]).value).to eq "123456"
+    end
+
+    it "leaves custom value unchanged if the string is already present" do
+      @parser.cv_concat @prod, :prod_po_numbers, "182909", @cdefs
+      expect((@prod.get_custom_value @cdefs[:prod_po_numbers]).value).to eq "182909"
+    end
+  end
+
+  describe :assign_earlier do
+    before :each do
+      @parser = described_class.new
+      load_cdefs
+      @prod = Factory(:product, unique_identifier: "HENNE-0148762")
+      @prod.update_custom_value! @cdefs[:prod_earliest_ship_date], Date.new(2015,1,12)
+    end
+
+    it "assigns input date to custom value if it's earlier than the existing date" do
+      @parser.assign_earlier @prod, :prod_earliest_ship_date, "01/05/2015", @cdefs
+      expect((@prod.get_custom_value @cdefs[:prod_earliest_ship_date]).value).to eq Date.new(2015,1,5)
+    end
+
+    it "assigns input date to custom value if the value is blank" do
+      @prod.update_custom_value! @cdefs[:prod_earliest_ship_date], nil
+      @parser.assign_earlier @prod, :prod_earliest_ship_date, "01/05/2015", @cdefs
+      expect((@prod.get_custom_value @cdefs[:prod_earliest_ship_date]).value).to eq Date.new(2015,1,5)
+    end
+
+    it "leaves custom value unchanged if it's earlier than input date" do
+      @parser.assign_earlier @prod, :prod_earliest_ship_date, "02/12/2015", @cdefs
+      expect((@prod.get_custom_value @cdefs[:prod_earliest_ship_date]).value).to eq Date.new(2015,1,12)
+    end
+  end
+end
+
+describe OpenChain::CustomHandler::Hm::HmI1Validator do
+  describe :filter do
+    it "returns the value for assignment if the input field is valid" do
+      row = ["182909","01/02/2016","01/02/2016","0148762001002","201501","Large Blue Hooded Sweatshirt","61101190","US"]
+      valid = described_class.new row
+      results = []
+      (0..7).each{ |i| results << valid.filter(i, "yay") }
+      expect(results).to eq ["yay", "yay", "yay", "yay", "yay", "yay", "yay", "yay"]
+    end
+
+    it "returns nil if the input field is invalid" do
+      row = ["18290","1/02/2016","01/2/2016","014876200100","20150","Large Blue Hooded Sweatshirt","6110119","USA"]
+      valid = described_class.new row
+      results = []
+      (0..7).each{ |i| results << valid.filter(i, "yay") }
+      expect(results).to eq [nil, nil, nil, nil, nil, "yay", nil, nil]
+    end
+  end
+
+  describe :check do
+    it "returns true if the field is valid and false otherwise" do
+      row = ["182909","1/02/2016","01/02/2016","0148762001002","201501","Large Blue Hooded Sweatshirt","61101190","US"]
+      valid = described_class.new row
+      expect(valid.check 0).to eq true
+      expect(valid.check 1).to eq false
+    end
+  end
+end
