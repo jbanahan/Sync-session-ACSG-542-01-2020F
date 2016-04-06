3526cae89576303a46f3b0cc9cec0f15a274da9c
Brian Glick <brian@brian-glick.com>
2016-04-06 10:53:12 -0400

    add require statements

--- lib/open_chain/custom_handler/lumber_liquidators/lumber_system_init.rb
+++ lib/open_chain/custom_handler/lumber_liquidators/lumber_system_init.rb
@@ -2,6 +2,7 @@ require 'open_chain/custom_handler/custom_view_selector'
 require 'open_chain/custom_handler/lumber_liquidators/lumber_view_selector'
 require 'open_chain/custom_handler/lumber_liquidators/lumber_order_change_comparator'
 require 'open_chain/custom_handler/lumber_liquidators/lumber_product_vendor_assignment_change_comparator'
+require 'open_chain/entity_compare/comparator_registry'
 
 module OpenChain; module CustomHandler; module LumberLiquidators; class LumberSystemInit
   def self.init
