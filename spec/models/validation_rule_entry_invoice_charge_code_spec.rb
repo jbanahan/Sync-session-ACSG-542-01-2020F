18ff93af8e2ba7d03e2c72c9635243720c799f98
Jay Davis <jdavis@vandegriftinc.com>
2016-04-13 17:44:36 -0400

    Rewrite validation to allow filter, blacklisting

--- spec/models/validation_rule_entry_invoice_charge_code_spec.rb
+++ spec/models/validation_rule_entry_invoice_charge_code_spec.rb
@@ -1,31 +1,92 @@
 require 'spec_helper'
 
 describe ValidationRuleEntryInvoiceChargeCode do
-  before :each do
-    @rule = ValidationRuleEntryInvoiceChargeCode.new(rule_attributes_json: {charge_codes: ['123', '456', '789']}.to_json)
-    @entry = Factory(:entry)
-    @broker_invoice = Factory(:broker_invoice, entry: @entry)
-    @inv_line_1 = Factory(:broker_invoice_line, broker_invoice: @broker_invoice, charge_code: 123, charge_amount: 5)
-    @inv_line_2 = Factory(:broker_invoice_line, broker_invoice: @broker_invoice, charge_code: 123, charge_amount: 10)
-    @inv_line_3 = Factory(:broker_invoice_line, broker_invoice: @broker_invoice, charge_code: 456, charge_amount: 15)
+    before :each do
+      @entry = Factory(:entry)
+      @inv_no_suffix = Factory(:broker_invoice, entry: @entry)
+      Factory(:broker_invoice_line, broker_invoice: @inv_no_suffix, charge_code: '123', charge_amount: 5)
+      Factory(:broker_invoice_line, broker_invoice: @inv_no_suffix, charge_code: '456', charge_amount: 10)
 
+      @inv_suffix = Factory(:broker_invoice, entry: @entry, suffix: 'AB')
+      Factory(:broker_invoice_line, broker_invoice: @inv_suffix, charge_code: '123', charge_amount: 5)
+      Factory(:broker_invoice_line, broker_invoice: @inv_suffix, charge_code: '123', charge_amount: 10)
+      Factory(:broker_invoice_line, broker_invoice: @inv_suffix, charge_code: '456', charge_amount: 15)
+
+      Factory(:broker_invoice_line, charge_code: '999', charge_amount: 17) #unrelated entry
+    end
+
+  describe :run_validation do
+      
+    it "passes if invoice lines with white-listed charge codes have a non-zero sum" do
+      rule = ValidationRuleEntryInvoiceChargeCode.new(rule_attributes_json: {charge_codes: ['123', '456', '789']}.to_json)
+      expect(rule.run_validation(@entry)).to be_nil
+    end
+
+    it "fails if invoice lines with black-listed charge codes have a non-zero sum" do
+      rule = ValidationRuleEntryInvoiceChargeCode.new(rule_attributes_json: {blacklist_charge_codes: ['123', '456', '789']}.to_json)
+
+      expect(rule.run_validation(@entry)).to eq "The following invalid charge codes were found: 123, 456"
+    end
+
+    it "passes if invoice lines without blacklisted charge codes have a non-zero sum" do
+      rule = ValidationRuleEntryInvoiceChargeCode.new(rule_attributes_json: {blacklist_charge_codes: ['123', '456', '789']}.to_json)
+      Factory(:broker_invoice_line, broker_invoice: @inv_suffix, charge_code: '123', charge_amount: -20)
+      Factory(:broker_invoice_line, broker_invoice: @inv_suffix, charge_code: '456', charge_amount: -25)
+      expect(rule.run_validation(@entry)).to be_nil
+    end
+
+    it "passes if suffix-filtered invoice lines with white-listed charge codes have a non-zero sum" do
+      rule = ValidationRuleEntryInvoiceChargeCode.new(rule_attributes_json: {charge_codes: ['123'], 
+                                                                             filter: 'suffix'}.to_json)
+      Factory(:broker_invoice_line, broker_invoice: @inv_suffix, charge_code: '456', charge_amount: -15)
+      expect(rule.run_validation(@entry)).to be_nil
+    end
+
+    it "passes if non-suffix-filtered invoice lines with white-listed charge codes have a non-zero sum" do
+      rule = ValidationRuleEntryInvoiceChargeCode.new(rule_attributes_json: {charge_codes: ['123'], 
+                                                                             filter: 'no_suffix'}.to_json)
+      Factory(:broker_invoice_line, broker_invoice: @inv_no_suffix, charge_code: '456', charge_amount: -10)
+      expect(rule.run_validation(@entry)).to be_nil
+    end
   end
 
-  it "passes if invoice lines with white-listed charge codes have a non-zero sum" do
-    expect(@rule.run_validation(@entry)).to be_nil
+  describe :query do
+    before :each do 
+      @results = []
+      @rule = ValidationRuleEntryInvoiceChargeCode.new(rule_attributes_json: {charge_codes: ['123', '456', '789']}.to_json)
+    end
+
+    it "sums amounts for each charge code of every invoice associated with an entry" do
+      @rule.query(@entry.id).each{ |row| @results << row }
+      expect(@results).to eq [{'charge_code' => '123', 'amount' => 20}, {'charge_code' => '456', 'amount' => 25}]
+    end
+
+    it "sums amounts for each charge code of every suffixed invoice" do
+      @rule.query(@entry.id, 'suffix').each{ |row| @results << row }
+      expect(@results).to eq [{'charge_code' => '123', 'amount' => 15}, {'charge_code' => '456', 'amount' => 15}]
+    end
+
+    it "sums amounts for each charge code of every non-suffixed invoice" do
+      @rule.query(@entry.id, 'no_suffix').each{ |row| @results << row }
+      expect(@results).to eq [{'charge_code' => '123', 'amount' => 5}, {'charge_code' => '456', 'amount' => 10}]
+    end
   end
 
-  it "passes if invoice lines not on white list have a zero sum" do
-    Factory(:broker_invoice_line, broker_invoice: @broker_invoice, charge_code: 604, charge_amount: 5)
-    Factory(:broker_invoice_line, broker_invoice: @broker_invoice, charge_code: 604, charge_amount: -5)
-    expect(@rule.run_validation(@entry)).to be_nil
-  end
-  
-  it "fails if invoice lines with charge codes not on white list have a non-zero sum" do
-    Factory(:broker_invoice_line, broker_invoice: @broker_invoice, charge_code: 604, charge_amount: 5)
-    Factory(:broker_invoice_line, broker_invoice: @broker_invoice, charge_code: 604, charge_amount: 10)
-    expect(@rule.run_validation(@entry)).to eq "The following invalid charge codes were found: 604."
-  end
+  describe :check_list do
+    before :each do
+      @rule = ValidationRuleEntryInvoiceChargeCode.new(rule_attributes_json: {charge_codes: ['123', '456', '789']}.to_json)
+      @totals = [{'charge_code' => '123', 'amount' => 5}, 
+                {'charge_code' => '777', 'amount' => 14},
+                {'charge_code' => '888', 'amount' => 0},
+                {'charge_code' => '999', 'amount' => 10}]
+    end
+    it "returns codes not appearing on white list when associated amount greater than 0" do
+      expect(@rule.check_list(@totals, ['123', '888', '999'], :white)).to eq ['777']
+    end
 
+    it "returns codes appearing on black list when associated amount greater than 0" do
+      expect(@rule.check_list(@totals, ['123', '888', '999'], :black)).to eq ['123', '999']
+    end
+  end
 
 end
