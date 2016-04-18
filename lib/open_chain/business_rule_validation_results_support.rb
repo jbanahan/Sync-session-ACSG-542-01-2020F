9753a7303438b90a4f068571a0c1c2fef511638f
Jay Davis <jdavis@vandegriftinc.com>
2016-04-18 13:11:51 -0400

    Refactor #generic_validations_results, add #run_validations

--- lib/open_chain/business_rule_validation_results_support.rb
+++ lib/open_chain/business_rule_validation_results_support.rb
@@ -1,40 +1,60 @@
 module OpenChain; module BusinessRuleValidationResultsSupport
   include ValidationResultsHelper
 
-    def generic_validation_results obj
-        cm = CoreModule.find_by_object(obj)
-        respond_to do |format|
-        format.html {
-          action_secure(obj.can_view?(current_user) && current_user.view_business_validation_results?,obj,{:lock_check=>false,:verb=>"view",:module_name=>cm.label.downcase}) {
-            @validation_object = obj
-          }
-        }
-        format.json {
-          
-          r = {
-            object_number:cm.unique_id_field.process_export(obj,current_user),
-            state:obj.business_rules_state,
-            object_updated_at:obj.updated_at,
-            single_object: obj.class.to_s,
-            bv_results:[]
-          }
-          obj.business_validation_results.each do |bvr|
-            return render_json_error "You do not have permission to view this object", 401 unless bvr.can_view?(current_user)
-            h = {
-              id:bvr.id,
-              state:bvr.state,
-              template:{name:bvr.business_validation_template.name},
-              updated_at:bvr.updated_at,
-              rule_results:[]
-            }
-            bvr.business_validation_rule_results.each do |rr|
-              h[:rule_results] << business_validation_rule_result_json(rr, current_user)
+  def generic_validation_results obj  
+    cm = CoreModule.find_by_object(obj)
+    respond_to do |format|
+      format.html {
+        action_secure(obj.can_view?(current_user) && current_user.view_business_validation_results?,
+          obj,{:lock_check=>false,:verb=>"view",:module_name=>cm.label.downcase}) { @validation_object = obj }
+      }
+      format.json {
+        action_secure(obj.can_view?(current_user) && current_user.view_business_validation_results?,
+          obj,{:lock_check=>false,:verb=>"view",:module_name=>cm.label.downcase, :json=>true}) {
+            bvr = results_to_hsh current_user, obj, cm
+            if bvr
+              render json: {business_validation_result: bvr}
+            else
+              render json: {error:"You do not have permission to view this validation result"}, status: 401
             end
-            r[:bv_results] << h
-          end
-          render json: {business_validation_result:r}
-        }
-        end
+          }
+      }
     end
+  end
+
+  def results_to_hsh run_by, obj, core_module=nil
+    core_module ||= CoreModule.find_by_object(obj)
+    r = {
+          object_number:core_module.unique_id_field.process_export(obj,run_by),
+          state:obj.business_rules_state,
+          object_updated_at:obj.updated_at,
+          single_object: obj.class.to_s,
+          can_run_validations: obj.can_run_validations?(run_by),
+          bv_results:[]
+        }
+    obj.business_validation_results.each do |bvr|
+      return nil unless bvr.can_view?(run_by)
+      h = {
+            id:bvr.id,
+            state:bvr.state,
+            template:{name:bvr.business_validation_template.name},
+            updated_at:bvr.updated_at,
+            rule_results:[]
+          }
+      bvr.business_validation_rule_results.each do |rr|
+        h[:rule_results] << business_validation_rule_result_json(rr, run_by)
+      end
+      r[:bv_results] << h
+    end
+    r
+  end
+
+  def run_validations obj
+    action_secure(obj.can_run_validations?(current_user) && obj.can_view?(current_user), obj, 
+      {verb: "update validation results for"}) {
+        BusinessValidationTemplate.create_results_for_object! obj
+        generic_validation_results obj
+    }
+  end
 
 end; end
---
+++ spec/lib/business_rule_validation_results_support_spec.rb
@@ -0,0 +1,229 @@
+require 'spec_helper'
+
+def create_validation_data user
+  @obj = Factory(:entry, broker_reference: "123456")
+  @bvt = Factory(:business_validation_template, module_type: 'Entry', name: "Entry rules")
+  @bvr_1 = Factory(:business_validation_result, business_validation_template: @bvt, validatable: @obj, state: "Fail")
+  @bvr_2 = Factory(:business_validation_result, business_validation_template: @bvt, validatable: @obj, state: "Pass")
+  @bvru_1 = Factory(:business_validation_rule, business_validation_template: @bvt, name: "Rule no. 1", description: "desc of rule 1")
+  @bvru_2 = Factory(:business_validation_rule, business_validation_template: @bvt, name: "Rule no. 2", description: "desc of rule 2")
+  @bvru_3 = Factory(:business_validation_rule, business_validation_template: @bvt, name: "Rule no. 3", description: "desc of rule 3")
+  
+  @bvrr_1 = Factory(:business_validation_rule_result, business_validation_rule: @bvru_1, state: "Pass")
+  @bvrr_1.business_validation_result = @bvr_1; @bvrr_1.save!
+  
+  @bvrr_2 = Factory(:business_validation_rule_result, business_validation_rule: @bvru_2, state: "Fail", note: "Why is this failing?", message: "This entry's no good!")
+  @bvrr_2.business_validation_result = @bvr_1; @bvrr_2.save!
+  
+  @bvrr_3 = Factory(:business_validation_rule_result, business_validation_rule: @bvru_3, state: "Pass", overridden_at: DateTime.now, overridden_by: user)
+  @bvrr_3.business_validation_result = @bvr_2; @bvrr_3.save!
+end
+
+#harness for testing generic_validation_results
+
+class FakeHtmlController < ApplicationController
+  include OpenChain::BusinessRuleValidationResultsSupport
+
+  def validation_results
+    generic_validation_results(Entry.find params[:id])
+    render nothing: true unless performed?
+  end
+end
+
+describe FakeHtmlController, :type => :controller do
+  before(:each) do 
+    @obj = Factory(:entry)
+    @u = Factory(:user)
+    sign_in_as @u
+    @routes.draw { get "validation_results" => "fake_html#validation_results" }
+  end
+
+  describe :generic_validation_results do
+    context "with HTML" do
+      it "provides object to view if user has rights to view object and business validation results" do
+        @u.stub(:view_business_validation_results?).and_return true
+        Entry.any_instance.stub(:can_view?).with(@u).and_return true
+        get :validation_results, id: @obj.id
+        expect(assigns(:validation_object)).to eq @obj
+        expect(response).to be_success
+      end
+
+      it "redirects with error if user can't view object" do
+        @u.stub(:view_business_validation_results?).and_return true
+        get :validation_results, id: @obj.id
+        expect(response).to be_redirect
+        expect(flash[:errors]).to include "You do not have permission to view this entry."
+      end
+      
+      it "redirects with error if user can't view business results" do
+        Entry.any_instance.stub(:can_view?).with(@u).and_return true
+        get :validation_results, id: @obj.id
+        expect(response).to be_redirect
+        expect(flash[:errors]).to include "You do not have permission to view this entry."
+      end
+    end
+
+    context "with JSON" do
+      before(:each) { create_validation_data @u }
+      
+      it "returns JSON object if user has right to view object and business validation results" do
+        @u.stub(:view_business_validation_results?).and_return true
+        Entry.any_instance.stub(:can_view?).with(@u).and_return true
+        get :validation_results, id: @obj.id, :format => 'json'
+        r = JSON.parse(response.body)["business_validation_result"]
+        expect(r["object_number"]).to eq "123456"
+        expect(r["bv_results"].first["state"]).to eq "Fail"
+      end
+
+      it "returns error if user can't view object" do
+        @u.stub(:view_business_validation_results?).and_return true
+        get :validation_results, id: @obj.id, :format => 'json'
+        expect(JSON.parse(response.body)["error"]).to eq "You do not have permission to view this entry."
+      end
+
+      it "redirects with error if user can't view business results" do
+        Entry.any_instance.stub(:can_view?).with(@u).and_return true
+        get :validation_results, id: @obj.id, :format => 'json'
+        expect(JSON.parse(response.body)["error"]).to eq "You do not have permission to view this entry."
+      end
+    end
+  end
+end
+
+#harness for testing run_validations
+
+class FakeJsonController < Api::V1::ApiController
+  include OpenChain::BusinessRuleValidationResultsSupport
+
+  def validation_runner 
+    run_validations Entry.find(params[:id])
+    render nothing: true unless performed?
+  end
+end
+
+describe FakeJsonController, :type => :controller do
+  describe :run_validations do
+    before :each do 
+      MasterSetup.get.update_attributes(:entry_enabled=>true)
+      @u = Factory(:master_user,entry_view:true)
+      allow_api_access @u
+      @routes.draw{ post "validation_runner" => "fake_json#validation_runner" }
+    end
+
+    it "delegates to BusinessValidationTemplate.create_results_for_object!" do
+      BusinessValidationTemplate.destroy_all
+      bvt = BusinessValidationTemplate.create!(module_type:'Entry')
+      bvt.search_criterions.create! model_field_uid: "ent_entry_num", operator: "nq", value: "XXXXXXXXXX"
+      ent = Factory(:entry)
+      post :validation_runner, id: ent.id, :format => 'json'
+      expect(bvt.business_validation_results.first.validatable).to eq ent
+    end
+    
+    it "returns results hash" do
+      obj = Factory(:entry)
+      BusinessValidationResult.any_instance.stub(:can_view?).with(@u).and_return true
+      post :validation_runner, id: obj.id, :format => 'json'
+      expect(JSON.parse(response.body)["business_validation_result"]["single_object"]).to eq "Entry"
+    end
+
+    it "renders error if user doesn't have permission to view object" do
+      obj = Factory(:entry)
+      Entry.any_instance.stub(:can_view?).and_return false
+      post :validation_runner, id: obj.id, :format => 'json'
+      expect(JSON.parse(response.body)["errors"]).to eq ["You do not have permission to update validation results for this object."]
+    end
+
+    it "renders error if there are any rule results user doesn't have permission to edit" do
+      create_validation_data @u
+      @bvru_1.group = Factory(:group); @bvru_1.save!
+      post :validation_runner, id: @obj.id, :format => 'json'
+      expect(JSON.parse(response.body)["errors"]).to eq ["You do not have permission to update validation results for this object."]
+    end
+  end
+end
+
+describe OpenChain::BusinessRuleValidationResultsSupport do
+  
+  class FakeController
+    include OpenChain::BusinessRuleValidationResultsSupport 
+  end
+
+  before :each do
+    @klass = FakeController.new
+    @u = Factory(:user, first_name: "Nigel", last_name: "Tufnel")
+    @u.stub(:edit_business_validation_rule_results?).and_return true
+  end
+
+  describe :results_to_hsh do
+
+    before :each do
+      create_validation_data(@u)
+      @target = {
+        object_number: "123456",
+        state: "Fail",
+        object_updated_at: @obj.updated_at,
+        single_object: "Entry",
+        can_run_validations: true,
+        bv_results: [
+          {
+            id: @bvr_1.id,
+            state: "Fail",
+            template:{name:"Entry rules"},
+            updated_at: @bvr_1.updated_at,
+            rule_results:[
+              {
+                id: @bvrr_1.id,
+                state: "Pass",
+                rule: {name: "Rule no. 1", description: "desc of rule 1"},
+                note: nil,
+                message: nil,
+                overridden_by: nil,
+                overridden_at: nil,
+                editable: true
+                },
+              {
+                id: @bvrr_2.id,
+                state: "Fail",
+                rule: {name: "Rule no. 2", description: "desc of rule 2"},
+                note: "Why is this failing?",
+                message: "This entry's no good!",
+                overridden_by: nil,
+                overridden_at: nil,
+                editable: true
+                }
+              ]
+            },
+          {
+            id: @bvr_2.id,
+            state: "Pass",
+            template:{name:"Entry rules"},
+            updated_at: @bvr_2.updated_at,
+            rule_results:[
+              {
+                id: @bvrr_3.id,
+                state: "Pass",
+                rule: {name: "Rule no. 3", description: "desc of rule 3"},
+                note: nil,
+                message: nil,
+                overridden_by: {full_name: "Nigel Tufnel"},
+                overridden_at: @bvrr_3.overridden_at,
+                editable: true
+                }
+              ]  
+          }
+        ]
+      }
+    end
+
+    it "returns a hash of the business rule results associated with an object if the user has permission to view ALL of them" do
+      BusinessValidationResult.any_instance.stub(:can_view?).with(@u).and_return true
+      r = @klass.results_to_hsh @u, @obj
+      expect(r.to_json).to eq @target.to_json #to_json resolves millisecond discrepancy
+    end
+
+    it "returns nil if the business rule results have rule that user does not have permission to view" do
+      r = @klass.results_to_hsh @u, @obj
+      expect(r).to be_nil
+    end
+  end
+end
