5912eb80cefcf654d6882150a25ca54272752bee
Jay Davis <jdavis@vandegriftinc.com>
2016-04-20 15:28:48 -0400

    Add ability to check multiple fields, fail only if all fields invalid

--- app/models/validates_field_format.rb
+++ app/models/validates_field_format.rb
@@ -3,40 +3,47 @@ module ValidatesFieldFormat
   def validate_field_format obj, opt = {}
     opt = {yield_matches: false, yield_failures: true}.merge opt
 
-    # Memoize the attributes and model field, there's no need to have to deserialze them and do the model field lookup repeatedly
-    # for any rule that runs over child objects. These values shouldn't change over the validation lifespan of the rule.
-    val = model_field.process_export(obj,nil,true)
-    reg = match_expression
+    messages = []
+    validation_expressions.each_pair do |model_field, attrs|
+      
+      # Memoize the attributes and model field, there's no need to have to deserialze them and do the model field lookup repeatedly
+      # for any rule that runs over child objects. These values shouldn't change over the validation lifespan of the rule.
+      val = model_field.process_export(obj,nil,true)
+      reg = attrs['regex']
+      allow_blank = attrs['allow_blank'].to_s.to_boolean
 
-    message = nil
-    unless allow_blank? && val.blank?
-      if !val.to_s.match(reg)
-        if opt[:yield_failures] && block_given?
-          message = yield model_field, val, reg
-        else
-          message = "#{model_field.label} must match '#{reg}' format, but was '#{val}'"      
+      unless allow_blank && val.blank?
+        if !val.to_s.match(reg)
+          if opt[:yield_failures] && block_given?
+            messages << yield(model_field, val, reg)
+          else
+            messages << "#{model_field.label} must match '#{reg}' format, but was '#{val}'"      
+          end
+        elsif opt[:yield_matches] && block_given?
+          yield model_field, val, reg
         end
-      elsif opt[:yield_matches] && block_given?
-        yield model_field, val, reg
       end
     end
-    
-    message
+
+    messages.join("\n").presence if rule_attribute('model_field_uid') || self.rule_attributes.count == messages.count
   end
 
-  def match_expression
-    @regex ||= rule_attribute('regex')
-    @regex
-  end
+  def validation_expressions
+    unless defined?(@expressions)
+      @expressions = {}
+      if rule_attribute('model_field_uid')
+        attrs = {}
+        @expressions[ModelField.find_by_uid(rule_attribute('model_field_uid'))] = attrs
+        attrs['regex'] = rule_attribute('regex')
+        attrs['allow_blank'] = rule_attribute('allow_blank')
+      else
+        self.rule_attributes.each_pair do |uid, attrs|
+          @expressions[ModelField.find_by_uid(uid)] = attrs
+        end
+      end
+    end
 
-  def model_field
-    @model_field ||= ModelField.find_by_uid rule_attribute('model_field_uid')
-    @model_field
-  end
-
-  def allow_blank?
-    @allow_blank ||= rule_attribute('allow_blank')
-    @allow_blank
+    @expressions
   end
 
   def rule_attribute key
---
+++ spec/models/validates_field_format_spec.rb
@@ -0,0 +1,87 @@
+require 'spec_helper'
+
+describe ValidatesFieldFormat do
+  
+  class Rule
+    include ValidatesFieldFormat
+    attr_reader :rule_attributes
+    def initialize(init)
+      @rule_attributes = init
+    end
+    def rule_attribute key
+      @rule_attributes[key]
+    end
+  end
+
+  before :each do
+    @rule = Rule.new('regex' => 'ABC', 'model_field_uid' => 'ordln_hts')
+    @block = Proc.new {|mf, val, regex| "All #{mf.label} values do not match '#{regex}' format."}
+    @order_line = Factory(:order_line)
+    @mf_double = double("MF")
+    @mf_double.stub(:label).and_return "HTS Code"
+    ModelField.should_receive(:find_by_uid).with('ordln_hts').and_return @mf_double  
+  end
+
+  describe :validate_field_format do
+    it "returns nil if the value is blank and allow_blank? is true" do
+      @rule = Rule.new('regex' => 'ABC', 'model_field_uid' => 'ordln_hts', 'allow_blank' => 'true')
+      @mf_double.should_receive(:process_export).with(@order_line, nil, true).and_return nil
+      expect(@rule.validate_field_format(@order_line, &@block)).to be_nil
+    end
+  
+    context "failure" do
+      before(:each) { @mf_double.should_receive(:process_export).with(@order_line, nil, true).and_return 'foo' }
+
+      it "returns result of block if yield_failures enabled" do
+        expect(@rule.validate_field_format(@order_line, {yield_failures: true}, &@block)).to eq "All HTS Code values do not match 'ABC' format."
+      end
+      
+      it "returns form message if yield_failures not enabled" do
+        expect(@rule.validate_field_format(@order_line, {yield_failures: false}, &@block)).to eq "HTS Code must match 'ABC' format, but was 'foo'"
+      end
+      
+      it "returns form message if block missing" do
+        expect(@rule.validate_field_format(@order_line, yield_failures: true)).to eq "HTS Code must match 'ABC' format, but was 'foo'"
+      end
+    end
+
+    context "success" do
+      before(:each) { @mf_double.should_receive(:process_export).with(@order_line, nil, true).and_return 'ABC' }
+
+      it "executes block if yield_matches enabled" do
+        expect{ |block| @rule.validate_field_format(@order_line, {yield_matches: true}, &block) }.to yield_with_args(@mf_double, 'ABC', 'ABC')
+      end
+
+      it "returns nil if yield_matches enabled" do
+        expect(@rule.validate_field_format(@order_line, {yield_matches: true}, &@block)).to be_nil
+      end
+    end
+
+    context "multiple validations" do
+      before :each do
+        @mf_double.should_receive(:process_export).with(@order_line, nil, true).and_return 'foo'
+
+        @mf_double_2 = double("MF2")
+        @mf_double_2.stub(:label).and_return "Currency"
+        @mf_double_2.should_receive(:process_export).with(@order_line, nil, true).and_return 'bar'
+        ModelField.should_receive(:find_by_uid).with('ordln_currency').and_return @mf_double_2
+
+        @mf_double_3 = double("MF3")
+        @mf_double_3.stub(:label).and_return "SKU"
+        @mf_double_3.should_receive(:process_export).with(@order_line, nil, true).and_return 'baz'
+        ModelField.should_receive(:find_by_uid).with('ordln_sku').and_return @mf_double_3
+      end
+
+      it "returns all errors if none of the validations match" do
+        @rule = Rule.new({'ordln_hts' => {'regex' => 'ABC'}, 'ordln_currency' => {'regex' => 'DEF'}, 'ordln_sku' => {'regex' => 'GHI'}})
+        expect(@rule.validate_field_format(@order_line, {yield_failures: true}, &@block)).to eq "All HTS Code values do not match 'ABC' format.\nAll Currency values do not match 'DEF' format.\n""All SKU values do not match 'GHI' format."
+      end
+
+      it "returns no errors if any of the validations match" do
+        @rule = Rule.new({'ordln_hts' => {'regex' => 'foo'}, 'ordln_currency' => {'regex' => 'DEF'}, 'ordln_sku' => {'regex' => 'GHI'}})
+        expect(@rule.validate_field_format(@order_line, {yield_failures: true}, &@block)).to be_nil
+      end
+    end
+
+  end
+end
