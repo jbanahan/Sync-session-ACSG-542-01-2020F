19d33884a7c5c3c24eef0c5512d3260df640d8c9
Brian Glick <brian@brian-glick.com>
2016-04-21 14:22:03 -0400

    update product risk report to only include items on orders

--- lib/open_chain/report/ll_prod_risk_report.rb
+++ lib/open_chain/report/ll_prod_risk_report.rb
@@ -5,7 +5,7 @@ module OpenChain
     class LlProdRiskReport
       include OpenChain::CustomHandler::LumberLiquidators::LumberCustomDefinitionSupport
       include OpenChain::Report::ReportHelper
-      
+
       def self.permission? user
         user.view_products? && user.company.master? && MasterSetup.get.system_code=='ll'
       end
@@ -21,12 +21,12 @@ module OpenChain
       def create_workbook
         wb = XlsMaker.create_workbook "Products Needing Risk Assignment"
         custom_defs = self.class.prep_custom_definitions([:prodven_risk,:cmp_sap_company])
-        table_from_query wb.worksheet(0), query(custom_defs[:prodven_risk], custom_defs[:cmp_sap_company]), {'Vendor SAP #' => link_lambda} 
+        table_from_query wb.worksheet(0), query(custom_defs[:prodven_risk], custom_defs[:cmp_sap_company]), {'Vendor SAP #' => link_lambda}
         wb
       end
 
       def link_lambda
-        lambda do |result_set_row, raw_column_value| 
+        lambda do |result_set_row, raw_column_value|
           vendor_sap, vendor_id = raw_column_value.split("~*~")
           vendor_sap_link = XlsMaker.excel_url("/vendors/" + vendor_id)
           XlsMaker.create_link_cell(vendor_sap_link, vendor_sap)
@@ -40,7 +40,7 @@ module OpenChain
 
       def send_email(settings)
         wb = create_workbook
-        
+
         workbook_to_tempfile wb, 'LlProdRiskReport-' do |t|
           subject = "Products Needing Risk Assignment"
           body = "<p>Report attached.<br>--This is an automated message, please do not reply.<br>This message was generated from VFI Track</p>".html_safe
@@ -50,21 +50,21 @@ module OpenChain
 
       def query(cd_prodven_risk, cd_cmp_sap_company)
         <<-SQL
-          SELECT (CASE 
+          SELECT (CASE
                   WHEN cv_ven.string_value IS NULL OR cv_ven.string_value = '' THEN CONCAT("None","~*~",c.id)
                   ELSE CONCAT(cv_ven.string_value,"~*~",c.id)
-                  END) AS "Vendor SAP #", 
-            c.name AS "Vendor Name", 
-            p.unique_identifier AS "Product SAP #", 
+                  END) AS "Vendor SAP #",
+            c.name AS "Vendor Name",
+            p.unique_identifier AS "Product SAP #",
             p.name AS "Product Name"
           FROM companies as c
             INNER JOIN product_vendor_assignments AS pva ON c.id = pva.vendor_id
             INNER JOIN products AS p on p.id = pva.product_id
-            LEFT OUTER JOIN custom_values AS cv_ven ON (cv_ven.customizable_type = "company" AND cv_ven.customizable_id = c.id 
+            LEFT OUTER JOIN custom_values AS cv_ven ON (cv_ven.customizable_type = "company" AND cv_ven.customizable_id = c.id
                                                         AND cv_ven.custom_definition_id = #{cd_cmp_sap_company.id})
             LEFT OUTER JOIN custom_values AS cv_prodven ON (cv_prodven.customizable_type = "ProductVendorAssignment" AND cv_prodven.customizable_id = pva.id
                                                         AND cv_prodven.custom_definition_id = #{cd_prodven_risk.id})
-          WHERE cv_prodven.string_value IS NULL OR cv_prodven.string_value = ''
+          WHERE cv_prodven.string_value IS NULL OR cv_prodven.string_value = '' and p.id IN (SELECT product_id from order_lines inner join orders on order_lines.order_id = orders.id where orders.closed_at is null)
           ORDER BY c.name
         SQL
       end
