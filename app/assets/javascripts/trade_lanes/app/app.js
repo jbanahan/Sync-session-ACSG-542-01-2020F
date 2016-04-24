eb025ef9236e36b914da1a74ddf585d07617f7d2
Brian Glick <brian@brian-glick.com>
2016-04-24 13:28:13 -0400

    trade lane with edit

--- app/assets/javascripts/trade_lanes/app/app.js
+++ app/assets/javascripts/trade_lanes/app/app.js
@@ -24,7 +24,48 @@
         url: '/show/:id',
         controller: 'ShowCtrl',
         templateUrl: "trade_lanes/partials/show.html"
+      }).state('edit', {
+        url: '/edit/:id',
+        controller: 'EditCtrl',
+        templateUrl: "trade_lanes/partials/edit.html"
       });
+    }
+  ]);
+
+}).call(this);
+
+(function() {
+  angular.module('ChainCommon').controller('EditCtrl', [
+    '$scope', '$stateParams', '$state', 'chainApiSvc', 'chainDomainerSvc', function($scope, $stateParams, $state, chainApiSvc, chainDomainerSvc) {
+      $scope.init = function(id) {
+        $scope.loading = 'loading';
+        return chainDomainerSvc.withDictionary().then(function(d) {
+          $scope.dict = d;
+          return chainApiSvc.TradeLane.get(id).then(function(r) {
+            $scope.lane = r;
+            return delete $scope.loading;
+          });
+        });
+      };
+      $scope.save = function(lane) {
+        $scope.loading = 'loading';
+        return chainApiSvc.TradeLane.save(lane).then(function(resp) {
+          return $state.transitionTo('show', {
+            id: resp.id
+          });
+        });
+      };
+      $scope.cancel = function(lane) {
+        $scope.loading = 'loading';
+        return chainApiSvc.TradeLane.load(lane.id).then(function(resp) {
+          return $state.transitionTo('show', {
+            id: resp.id
+          });
+        });
+      };
+      if (!$scope.$root.isTest) {
+        return $scope.init($stateParams.id);
+      }
     }
   ]);
 
@@ -61,7 +102,12 @@
 
 }).call(this);
 
-angular.module('ChainTradeLanes-Templates', ['trade_lanes/partials/index.html', 'trade_lanes/partials/new.html', 'trade_lanes/partials/show.html']);
+angular.module('ChainTradeLanes-Templates', ['trade_lanes/partials/edit.html', 'trade_lanes/partials/index.html', 'trade_lanes/partials/new.html', 'trade_lanes/partials/show.html']);
+
+angular.module("trade_lanes/partials/edit.html", []).run(["$templateCache", function($templateCache) {
+  $templateCache.put("trade_lanes/partials/edit.html",
+    "<chain-loading-wrapper loading=\"{{loading}}\"><div class=\"container\"><div class=\"row\"><div class=\"col-md-12 text-center\"><h1>Edit Trade Lane</h1><h3><chain-flag-icon img-class=\"mini-flag\" iso-code=\"{{lane.lane_origin_cntry_iso}}\"></chain-flag-icon>{{lane.lane_origin_cntry_name}} <i class=\"fa fa-arrow-right\"></i><chain-flag-icon img-class=\"mini-flag\" iso-code=\"{{lane.lane_destination_cntry_iso}}\"></chain-flag-icon>{{lane.lane_destination_cntry_name}}</h3></div></div><div class=\"row\"><div class=\"col-md-12\"><div class=\"panel panel-primary\"><div class=\"panel-heading\"><h3 class=\"panel-title\">Attributes</h3></div><ul class=\"list-group\" ng-if=\"lane\"><li class=\"list-group-item\" ng-repeat=\"fld in dict.fieldsByRecordType(dict.recordTypes.TradeLane) | chainSkipReadOnly | chainViewFields:['lane_destination_cntry_iso','lane_destination_cntry_name','lane_origin_cntry_iso','lane_origin_cntry_name']:true track by fld.uid\"><label class=\"control-label\">{{fld.label}}</label><p class=\"form-control-static\"><chain-field-input model=\"lane\" field=\"fld\"></chain-field-input></p></li></ul><div class=\"panel-footer text-right\"><button class=\"btn btn-sm btn-default\" ng-click=\"cancel(lane)\">Cancel</button> <button class=\"btn btn-sm btn-success\" ng-click=\"save(lane)\">Save</button></div></div></div></div></div></chain-loading-wrapper>");
+}]);
 
 angular.module("trade_lanes/partials/index.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("trade_lanes/partials/index.html",
@@ -70,16 +116,12 @@ angular.module("trade_lanes/partials/index.html", []).run(["$templateCache", fun
 
 angular.module("trade_lanes/partials/new.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("trade_lanes/partials/new.html",
-    "<chain-loading-wrapper loading=\"{{loading}}\"><div class=\"container\"><div class=\"row\"><div class=\"col-md-12\"><div class=\"panel panel-primary\"><div class=\"panel-heading\"><h3 class=\"panel-title\">New Trade Lane</h3></div><div class=\"panel-body\"><label for=\"origin-country\">Origin</label><select class=\"form-control\" id=\"origin-country\" ng-model=\"vals.origin\" ng-options=\"c as c.name for c in countries track by c.id\"></select><label for=\"destination-country\">Destination</label><select class=\"form-control\" id=\"destination-country\" ng-model=\"vals.destination\" ng-options=\"c as c.name for c in countries track by c.id\"></select></div><div class=\"panel-footer text-right\">&nbsp; <span ng-if=\"existingLane\">Trade Lane already exists. <a ui-sref=\"show({id:existingLane.id})\">Click here to view.</a></span> <button ng-click=\"create(vals.origin,vals.destination)\" class=\"btn btn-success\" ng-show=\"isShowCreate()\">Create</button></div></div><pre>\n" +
-    "          {{vals.origin}}\n" +
-    "          ----------------------\n" +
-    "          {{vals.destination}}\n" +
-    "        </pre></div></div></div></chain-loading-wrapper>");
+    "<chain-loading-wrapper loading=\"{{loading}}\"><div class=\"container\"><div class=\"row\"><div class=\"col-md-12\"><div class=\"panel panel-primary\"><div class=\"panel-heading\"><h3 class=\"panel-title\">New Trade Lane</h3></div><div class=\"panel-body\"><label for=\"origin-country\">Origin</label><select class=\"form-control\" id=\"origin-country\" ng-model=\"vals.origin\" ng-options=\"c as c.name for c in countries track by c.id\"></select><label for=\"destination-country\">Destination</label><select class=\"form-control\" id=\"destination-country\" ng-model=\"vals.destination\" ng-options=\"c as c.name for c in countries track by c.id\"></select></div><div class=\"panel-footer text-right\">&nbsp; <span ng-if=\"existingLane\">Trade Lane already exists. <a ui-sref=\"show({id:existingLane.id})\">Click here to view.</a></span> <button ng-click=\"create(vals.origin,vals.destination)\" class=\"btn btn-success\" ng-show=\"isShowCreate()\">Create</button></div></div></div></div></div></chain-loading-wrapper>");
 }]);
 
 angular.module("trade_lanes/partials/show.html", []).run(["$templateCache", function($templateCache) {
   $templateCache.put("trade_lanes/partials/show.html",
-    "<div class=\"container\"><div class=\"row\"><div class=\"col-md-12 text-center\"><h1>Trade Lane</h1><h3>{{lane.lane_origin_cntry_name}} <i class=\"fa fa-arrow-right\"></i> {{lane.lane_destination_cntry_name}}</h3></div></div><div class=\"row\"><div class=\"col-md-6\"><div class=\"panel panel-primary\"><div class=\"panel-heading\"><h3 class=\"panel-title\">Attributes</h3></div><div class=\"panel-body\"></div><div class=\"panel-footer\"></div></div><div class=\"panel panel-primary\"><div class=\"panel-heading\"><h3 class=\"panel-title\">Preference Programs</h3></div><div class=\"panel-body\"></div><div class=\"panel-footer\"></div></div></div><div class=\"col-md-6\"><div><chain-flag-icon iso-code=\"{{lane.lane_origin_cntry_iso}}\"></chain-flag-icon></div><div><chain-flag-icon iso-code=\"{{lane.lane_destination_cntry_iso}}\"></chain-flag-icon></div></div></div></div>");
+    "<chain-loading-wrapper loading=\"{{loading}}\"><div class=\"container\"><div class=\"row\"><div class=\"col-md-12 text-center\"><h1>Trade Lane</h1><h3><chain-flag-icon img-class=\"mini-flag\" iso-code=\"{{lane.lane_origin_cntry_iso}}\"></chain-flag-icon>{{lane.lane_origin_cntry_name}} <i class=\"fa fa-arrow-right\"></i><chain-flag-icon img-class=\"mini-flag\" iso-code=\"{{lane.lane_destination_cntry_iso}}\"></chain-flag-icon>{{lane.lane_destination_cntry_name}}</h3></div></div><div class=\"row\"><div class=\"col-md-6\"><div class=\"panel panel-primary\"><div class=\"panel-heading\"><h3 class=\"panel-title\">Attributes</h3></div><ul class=\"list-group\" ng-if=\"lane\"><li class=\"list-group-item\" ng-repeat=\"fld in dict.fieldsByRecordType(dict.recordTypes.TradeLane) | chainFieldsWithValues:lane | chainViewFields:['lane_destination_cntry_iso','lane_destination_cntry_name','lane_origin_cntry_iso','lane_origin_cntry_name']:true track by fld.uid\"><label class=\"control-label\">{{fld.label}}</label><p class=\"form-control-static\"><chain-field-value model=\"lane\" field=\"fld\"></chain-field-value></p></li></ul><div class=\"panel-footer text-right\"><button ng-click=\"edit(lane)\" class=\"btn btn-xs btn-default\"><i class=\"fa fa-edit\"></i></button></div></div><div class=\"panel panel-primary\"><div class=\"panel-heading\"><h3 class=\"panel-title\">Preference Programs</h3></div><div class=\"panel-body\"></div><div class=\"panel-footer\"></div></div></div><div class=\"col-md-6\"><div></div><div></div></div></div></div></chain-loading-wrapper>");
 }]);
 
 (function() {
@@ -150,12 +192,21 @@ angular.module("trade_lanes/partials/show.html", []).run(["$templateCache", func
 
 (function() {
   angular.module('ChainTradeLanes').controller('ShowCtrl', [
-    '$scope', '$stateParams', 'chainApiSvc', function($scope, $stateParams, chainApiSvc) {
+    '$scope', '$stateParams', '$state', 'chainApiSvc', 'chainDomainerSvc', function($scope, $stateParams, $state, chainApiSvc, chainDomainerSvc) {
       $scope.init = function(id) {
         $scope.loading = 'loading';
-        return chainApiSvc.TradeLane.get(id).then(function(r) {
-          $scope.lane = r;
-          return delete $scope.loading;
+        return chainDomainerSvc.withDictionary().then(function(d) {
+          $scope.dict = d;
+          return chainApiSvc.TradeLane.get(id).then(function(r) {
+            $scope.lane = r;
+            return delete $scope.loading;
+          });
+        });
+      };
+      $scope.edit = function(lane) {
+        $scope.loading = 'loading';
+        return $state.transitionTo('edit', {
+          id: lane.id
         });
       };
       if (!$scope.$root.isTest) {
