ccb492ced243250add78399a5896d6e748d5e594
Jeremy Hulford <jhulford@vandegriftinc.com>
2016-04-29 13:47:04 -0500

    Lumber PPQ Report

---
+++ lib/open_chain/custom_handler/lumber_liquidators/lumber_ppq_report.rb
@@ -0,0 +1,71 @@
+require 'open_chain/report/report_helper'
+
+module OpenChain; module CustomHandler; module LumberLiquidators; class LumberPpqReport
+  include OpenChain::Report::ReportHelper
+
+  def self.run_schedulable opts = {}
+    self.new.email_report *start_end_dates, opts['email_to']
+  end
+
+  def email_report start_date, end_date, email_to
+    run_time = Time.zone.now.in_time_zone(user_timezone).to_date
+    wb = run_report(start_date, end_date)
+    subject = "PPQ Report #{run_time.strftime('%m/%d/%y')}"
+    workbook_to_tempfile(wb, "Lumber_PPQ", file_name: "PPQ Report #{run_time.strftime('%m-%d-%y')}.xls") do |temp|
+      OpenMailer.send_simple_html(email_to, subject, "Attached is the PPQ Report for #{run_time.strftime('%m/%d/%y')}.", temp).deliver!
+    end
+  end
+
+  def run_report start_date, end_date
+    wb, sheet = XlsMaker.create_workbook_and_sheet "Lacey Data #{start_date.in_time_zone(user_timezone).strftime '%m/%d/%y'} - #{(end_date.in_time_zone(user_timezone) - 1.day).strftime '%m/%d/%y'}", nil
+    table_from_query sheet, query(start_date, end_date), conversions
+    wb
+  end
+
+  def query start_date, end_date
+    <<-SQL
+      SELECT '' as 'Unique Id', e.entry_number 'Entry Number', e.master_bills_of_lading 'B/L No(s)', e.container_numbers 'Container No(s)', date(convert_tz(e.arrival_date, 'UTC', 'America/New_York')) 'Arrival Date', i.mfid 'Manufacturer ID', l.part_number 'Part No', l.po_number 'PO No', t.hts_code 'HTS No',
+      lc.detailed_description 'Description', lc.quantity 'Quantity of Constituent Element', lc.unit_of_measure 'UOM', 0  'Percent of Constituent Element', lc.value  'PGA Line Value', lc.genus  'Scientific Genus Name', lc.species  'Scientific Species Name',
+      lc.harvested_from_country  'Source Country Code'
+      FROM entries e
+      INNER JOIN commercial_invoices i ON e.id = i.entry_id
+      INNER JOIN commercial_invoice_lines l ON i.id = l.commercial_invoice_id
+      INNER JOIN commercial_invoice_tariffs t ON l.id = t.commercial_invoice_line_id
+      LEFT OUTER JOIN commercial_invoice_lacey_components lc ON lc.commercial_invoice_tariff_id = t.id
+      WHERE e.customer_number = 'LUMBER' AND e.source_system = 'Alliance' AND e.release_date >= '#{start_date.to_s(:db)}' and e.release_date < '#{end_date.to_s(:db)}'
+      ORDER BY e.arrival_date, e.broker_reference, i.id, l.line_number, t.id, lc.id
+    SQL
+  end
+
+  def conversions
+    c = {}
+
+    # The first column needs to be a counter that literally just counts the lines on the report
+    counter = 0
+    c['Unique Id'] = lambda {|row, value| counter += 1 }
+    c['HTS No'] = lambda {|row, value| value.to_s.hts_format }
+    c['B/L No(s)']= csv_translation_lambda
+    c['Container No(s)']= csv_translation_lambda
+    c
+  end
+
+  def self.user_timezone
+    "America/New_York"
+  end
+
+  def user_timezone
+    self.class.user_timezone
+  end
+
+  def self.start_end_dates
+    # Calculate start/end dates using the run date as the previous workweek (Monday - Sunday)
+    now = Time.zone.now.in_time_zone(user_timezone)
+    start_date = (now - 7.days)
+    # Subtract days until we're at a Monday
+    start_date -= 1.day while start_date.wday != 1
+    # Basically, we're formatting these dates so the represent the Monday @ Midnight and the following Monday @ midnight, relying on the 
+    # where clause being >= && <.  We don't want any results showing that are actually on the following Monday based on Eastern timezone
+    [start_date.beginning_of_day.in_time_zone("UTC"), (start_date + 7.days).beginning_of_day.in_time_zone("UTC")]
+  end
+
+end; end; end; end;
---
+++ spec/lib/custom_handler/lumber_liquidators/lumber_ppq_report_spec.rb
@@ -0,0 +1,87 @@
+require 'spec_helper'
+
+describe OpenChain::CustomHandler::LumberLiquidators::LumberPpqReport do
+
+  describe "run_schedulable" do
+    let (:entry) {
+      tariff = Factory(:commercial_invoice_tariff, hts_code: "1111111111", 
+                          commercial_invoice_line: Factory(:commercial_invoice_line, part_number: "Part", po_number: "PO", 
+                            commercial_invoice: Factory(:commercial_invoice, mfid: "MID",
+                              entry: Factory(:entry, customer_number: "LUMBER", source_system: "Alliance", release_date: "2016-05-01", master_bills_of_lading: "MBOL1\nMBOL2", container_numbers: "CONT1\n CONT2", entry_number: "ENTNUM", arrival_date: "2016-04-30 05:00")
+                            )
+                          )
+                        )
+      tariff.commercial_invoice_lacey_components.create! detailed_description: "DESC", quantity: 10, unit_of_measure: "UOM", genus: "GENUS", species: "SPECIES", harvested_from_country: "CA", value: 100
+      tariff.commercial_invoice_lacey_components.create! detailed_description: "DESC2", quantity: 20, unit_of_measure: "UOM2", genus: "GENUS2", species: "SPECIES2", harvested_from_country: "CN", value: 50
+      tariff.commercial_invoice_line.entry
+    }
+
+    def validate_no_report_data attachment_name
+      expect(ActionMailer::Base.deliveries.length).to eq 1
+      m = ActionMailer::Base.deliveries.first
+      sheet = Spreadsheet.open(StringIO.new(m.attachments[attachment_name].read)).worksheets.first
+      expect(sheet.rows.length).to eq 1
+    end
+
+    context "with_valid_run_dates" do
+      around(:each) do |ex|
+        Timecop.freeze(ActiveSupport::TimeZone["America/New_York"].parse("2016-05-02").in_time_zone("UTC")) do 
+          ex.run
+        end
+      end
+
+      it "sends report" do
+        entry
+        described_class.run_schedulable({'email_to' => ["user@there.com"]})
+
+        expect(ActionMailer::Base.deliveries.length).to eq 1
+        m = ActionMailer::Base.deliveries.first
+        expect(m.to).to eq ["user@there.com"]
+        expect(m.subject).to eq "PPQ Report 05/02/16"
+        expect(m.body.raw_source).to include "Attached is the PPQ Report for 05/02/16."
+        expect(m.attachments["PPQ Report 05-02-16.xls"]).not_to be_nil
+
+        sheet = Spreadsheet.open(StringIO.new(m.attachments["PPQ Report 05-02-16.xls"].read)).worksheets.first
+        expect(sheet.rows.length).to eq 3
+
+        expect(sheet.row(0)).to eq ["Unique Id","Entry Number","B/L No(s)","Container No(s)","Arrival Date","Manufacturer ID","Part No","PO No","HTS No","Description","Quantity of Constituent Element","UOM","Percent of Constituent Element","PGA Line Value","Scientific Genus Name","Scientific Species Name","Source Country Code"]
+        expect(sheet.row(1)).to eq [1, "ENTNUM", "MBOL1, MBOL2", "CONT1, CONT2", excel_date(Date.new(2016, 4, 30)), "MID", "Part", "PO", "1111.11.1111", "DESC", 10, "UOM", 0, 100, "GENUS", "SPECIES", "CA"]
+        expect(sheet.row(2)).to eq [2, "ENTNUM", "MBOL1, MBOL2", "CONT1, CONT2", excel_date(Date.new(2016, 4, 30)), "MID", "Part", "PO", "1111.11.1111", "DESC2", 20, "UOM2", 0, 50, "GENUS2", "SPECIES2", "CN"]
+      end
+
+      context "with invalid entry attribute" do 
+        after :each do
+          validate_no_report_data "PPQ Report 05-02-16.xls"
+        end
+
+        it "does not find non-Lumber entries" do
+          entry.update_attributes! customer_number: "NOTLUMBER"
+          described_class.run_schedulable({'email_to' => ["user@there.com"]})
+        end
+
+        it "does not find non-Kewill entries" do
+          entry.update_attributes! source_system: "NOTALLIANCE"
+          described_class.run_schedulable({'email_to' => ["user@there.com"]})
+        end
+      end
+    end
+    
+
+    it "does not find entries before run period" do
+      entry
+      Timecop.freeze(ActiveSupport::TimeZone["America/New_York"].parse("2016-05-09").in_time_zone("UTC")) do 
+        described_class.run_schedulable({'email_to' => ["user@there.com"]})
+      end
+      validate_no_report_data "PPQ Report 05-09-16.xls"
+    end
+
+    it "does not find entries after run period" do
+      entry
+      Timecop.freeze(ActiveSupport::TimeZone["America/New_York"].parse("2016-04-25").in_time_zone("UTC")) do 
+        described_class.run_schedulable({'email_to' => ["user@there.com"]})
+      end
+      validate_no_report_data "PPQ Report 04-25-16.xls"
+      
+    end
+  end
+end
