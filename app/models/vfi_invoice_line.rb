3ce29ecb56ae677876a9e9f95cc5050e88246fd2
Jay Davis <jdavis@vandegriftinc.com>
2016-06-06 14:27:34 -0400

    Extract line-number functionality from LineSupport into separate module; add it to to VFI Invoice Line

--- app/models/vfi_invoice_line.rb
+++ app/models/vfi_invoice_line.rb
@@ -1,8 +1,21 @@
 class VfiInvoiceLine < ActiveRecord::Base
+  include DefaultLineNumberSupport
   belongs_to :vfi_invoice, :inverse_of => :vfi_invoice_lines, :touch => true
   has_many :invoiced_events
 
+  before_validation :default_line_number
   validates :vfi_invoice_id, :presence => true
   validates :charge_description, :presence => true
   validates :charge_amount, :presence => true
+
+  private
+  
+  def parent_obj #supporting method for LinesSupport
+    self.vfi_invoice
+  end
+  
+  def parent_id_where #supporting method for LinesSupport
+    return :vfi_invoice_id => self.vfi_invoice.id
+  end
+
 end
