ea4e73d8278da9286aa59127da7fc6b7a25ac551
Jay Davis <jdavis@vandegriftinc.com>
2016-06-07 16:03:47 -0400

    Add file_logged_date requirement, make company id a parameter in Masterbrand invoice generator

--- lib/open_chain/custom_handler/masterbrand/masterbrand_invoice_generator.rb
+++ lib/open_chain/custom_handler/masterbrand/masterbrand_invoice_generator.rb
@@ -1,50 +1,75 @@
 module OpenChain; module CustomHandler; module Masterbrand; class MasterbrandInvoiceGenerator
     
-    def self.run_schedulable
-      ActiveRecord::Base.transaction do
-        billables = get_new_billables
-        inv = create_invoice
-        bill_new_entries(billables, inv)
-        bill_monthly_charge inv
+  def self.run_schedulable
+    ActiveRecord::Base.transaction do
+      co = get_company
+      billables = get_new_billables
+      invoiceable_ids = get_new_invoiceable_ids(co)
+      inv = create_invoice(co)
+      bill_new_entries(billables, invoiceable_ids, inv)
+      bill_monthly_charge inv
+    end
+  end
+
+  def self.create_invoice(company)
+    VfiInvoice.next_invoice_number { |n| VfiInvoice.create!(customer: company, invoice_date: Date.today, invoice_number: n, currency: "USD")}
+  end
+
+  def self.get_company
+    Company.where(master: true).first
+  end
+
+  def self.bill_monthly_charge invoice
+    invoice.vfi_invoice_lines.create! vfi_invoice: invoice, charge_amount: 1000, quantity: 1, unit: "ea", unit_price: 1000, charge_description: "monthly charge"
+  end
+
+  def self.get_new_billables
+    BillableEvent.joins('LEFT OUTER JOIN invoiced_events ie ON billable_events.id = ie.billable_event_id AND ie.invoice_generator_name = "MasterbrandInvoiceGenerator"').where('ie.id IS NULL')
+  end
+
+  def self.get_new_invoiceable_ids company
+    BillableEvent.connection.execute(entry_query company).map(&:first)
+  end
+
+  def self.bill_new_entries new_billables, invoiceable_ids, invoice
+    new_entries, others = split_billables(new_billables, invoiceable_ids)
+    qty_to_bill = new_entries.count# - 250
+    charge_amount = qty_to_bill * 2.50
+    if qty_to_bill > 0
+      line = invoice.vfi_invoice_lines.create! charge_amount: charge_amount, quantity: qty_to_bill, unit: "ea", unit_price: 2.50, charge_description: "new entry exceeding 250/mo. limit"
+      write_invoiced_events new_entries, line
+    else
+      write_invoiced_events new_entries
+    end
+    write_invoiced_events others
+  end
+
+  def self.split_billables billables, invoiceable_ids
+    billables.partition{ |b| invoiceable_ids.include? b[:billable_eventable_id] }
+  end
+
+  def self.write_invoiced_events billables, invoice_line=nil
+    BillableEvent.transaction do
+      billables.each do |e|
+        InvoicedEvent.create!(billable_event_id: e[:id], vfi_invoice_line: invoice_line, invoice_generator_name: 'MasterbrandInvoiceGenerator', charge_type: 'unified_entry_line')
       end
     end
+  end
 
-    def self.create_invoice
-      co = Company.where(master: true).first
-      VfiInvoice.next_invoice_number { |n| VfiInvoice.create!(customer: co, invoice_date: Date.today, invoice_number: n, currency: "USD")}
-    end
+  private
 
-    def self.bill_monthly_charge invoice
-      invoice.vfi_invoice_lines.create! vfi_invoice: invoice, charge_amount: 1000, quantity: 1, unit: "ea", unit_price: 1000, charge_description: "monthly charge"
-    end
-
-    def self.get_new_billables
-      BillableEvent.joins('LEFT OUTER JOIN invoiced_events ie ON billable_events.id = ie.billable_event_id AND ie.invoice_generator_name = "MasterbrandInvoiceGenerator"').where('ie.id IS NULL')
-    end
-
-    def self.bill_new_entries new_billables, invoice
-      new_entries, others = split_billables new_billables
-      qty_to_bill = new_entries.count - 250
-      charge_amount = qty_to_bill * 2.50
-      if qty_to_bill > 0
-        line = invoice.vfi_invoice_lines.create! charge_amount: charge_amount, quantity: qty_to_bill, unit: "ea", unit_price: 2.50, charge_description: "new entry exceeding 250/mo. limit"
-        write_invoiced_events new_entries, line
-      else
-        write_invoiced_events new_entries
-      end
-      write_invoiced_events others
-    end
-
-    def self.split_billables new_billables
-      new_billables.partition{ |c| c[:event_type] == "entry_new"}
-    end
-
-    def self.write_invoiced_events billables, invoice_line=nil
-      BillableEvent.transaction do
-        billables.each do |e|
-          InvoicedEvent.create!(billable_event_id: e[:id], vfi_invoice_line: invoice_line, invoice_generator_name: 'MasterbrandInvoiceGenerator', charge_type: 'unified_entry_line')
-        end
-      end
-    end
+  def self.entry_query company
+    <<-SQL
+      SELECT e.id
+      FROM billable_events be
+        INNER JOIN entries e ON be.billable_eventable_type = "Entry" AND be.billable_eventable_id = e.id
+        INNER JOIN companies c ON c.id = e.importer_id
+        LEFT OUTER JOIN invoiced_events ie ON be.id = ie.billable_event_id AND ie.invoice_generator_name = "MasterbrandInvoiceGenerator"
+      WHERE ie.billable_event_id IS NULL 
+        AND be.event_type = "entry_new"
+        AND c.id = #{company.id}
+        AND (e.file_logged_date >= "2016-05-01" OR e.file_logged_date IS NULL)
+    SQL
+  end
 
 end; end; end; end
--- spec/lib/custom_handler/masterbrand/masterbrand_invoice_generator_spec.rb
+++ spec/lib/custom_handler/masterbrand/masterbrand_invoice_generator_spec.rb
@@ -5,13 +5,26 @@ describe OpenChain::CustomHandler::Masterbrand::MasterbrandInvoiceGenerator do
   describe :run_schedulable do
     it "finds the new billable events, creates a VFI invoice, bills new entries and monthly charges" do
       new_billables = double("new billables")
+      invoiceable_ids = double("invoiceable ids")
       invoice = double("vfi invoice")
+      company = double("company")
+      described_class.should_receive(:get_company).and_return company
       described_class.should_receive(:get_new_billables).and_return new_billables
+      described_class.should_receive(:get_new_invoiceable_ids).with(company).and_return invoiceable_ids
       described_class.should_receive(:create_invoice).and_return invoice
-      described_class.should_receive(:bill_new_entries).with(new_billables, invoice)
+      described_class.should_receive(:bill_new_entries).with(new_billables, invoiceable_ids, invoice)
       described_class.should_receive(:bill_monthly_charge).with invoice
 
       described_class.run_schedulable
+    end
+  end
+
+  describe :get_company do
+    it "retrieves master company" do
+      co = Factory(:company)
+      expect(described_class.get_company).to be_nil
+      co.update_attributes(master: true)
+      expect(described_class.get_company).to eq co
     end
   end
 
@@ -31,10 +44,45 @@ describe OpenChain::CustomHandler::Masterbrand::MasterbrandInvoiceGenerator do
     end
   end
 
+  describe :get_new_invoiceable_ids do
+    before :each do
+      @co = Factory(:company, alliance_customer_number: 'mbci')
+      @e_1 = Factory(:entry, importer: @co)
+      @e_2 = Factory(:entry, importer: @co)
+      @be_1 = Factory(:billable_event, billable_eventable: @e_1, entity_snapshot: Factory(:entity_snapshot), event_type: "entry_new")
+      @be_2 = Factory(:billable_event, billable_eventable: @e_2, entity_snapshot: Factory(:entity_snapshot), event_type: "entry_new")
+    end
+
+    it "excludes entry events with a file_logged_date earlier than 5/1/16" do
+      @e_2.update_attributes(file_logged_date: "2015/1/1")
+      expect(described_class.get_new_invoiceable_ids @co).to eq [@e_1.id]
+    end
+
+    it "excludes entry events belonging to any company other than the specified one" do
+      @e_2.update_attributes(importer: Factory(:company))
+      expect(described_class.get_new_invoiceable_ids @co).to eq [@e_1.id]
+    end
+
+    it "excludes events with wrong type" do
+      @be_2.update_attributes(event_type: "classification_new")
+      expect(described_class.get_new_invoiceable_ids @co).to eq [@e_1.id]
+    end
+
+    it "excludes events that aren't new (i.e. a billable with an invoiced event created by this generator)" do
+      Factory(:invoiced_event, billable_event: @be_2, invoice_generator_name: "MasterbrandInvoiceGenerator")
+      expect(described_class.get_new_invoiceable_ids @co).to eq [@e_1.id]
+    end
+
+    it "includes events that have been invoiced by another generator" do
+      Factory(:invoiced_event, billable_event: @be_2, invoice_generator_name: "FooGenerator")
+      expect(described_class.get_new_invoiceable_ids @co).to eq [@e_1.id, @e_2.id]
+    end    
+  end
+
   describe :create_invoice do
     it "creates a new VFI invoice" do
-      mbci = Factory(:master_company)
-      inv = described_class.create_invoice
+      mbci = Factory(:company)
+      inv = described_class.create_invoice mbci
       expected = [mbci, Date.today, "VFI-1", "USD"]
       expect([inv.customer, inv.invoice_date, inv.invoice_number, inv.currency]).to eq expected
     end
@@ -43,7 +91,7 @@ describe OpenChain::CustomHandler::Masterbrand::MasterbrandInvoiceGenerator do
   describe :bill_new_entries do
     before :each do
       @inv = double("vfi invoice")
-      
+      @invoiceable_ids = double("int_array")
       @new_billables = [{billable_event_id: 1, billable_eventable_id: 2, event_type: "entry_new"},
                        {billable_event_id: 5, billable_eventable_id: 6, event_type: "entry_new"},
                        {billable_event_id: 3, billable_eventable_id: 4, event_type: "classification_new"}]
@@ -58,7 +106,7 @@ describe OpenChain::CustomHandler::Masterbrand::MasterbrandInvoiceGenerator do
                           {billable_event_id: 5, billable_eventable_id: 6, event_type: "entry_new"}],
                          [{billable_event_id: 3, billable_eventable_id: 4, event_type: "classification_new"}]]
 
-      described_class.should_receive(:split_billables).with(@new_billables).and_return split_billables
+      described_class.should_receive(:split_billables).with(@new_billables, @invoiceable_ids).and_return split_billables
       split_billables[0].should_receive(:count).and_return(qty_to_be_invoiced)
       
       @inv.should_receive(:vfi_invoice_lines).and_return invoice_lines_relation
@@ -66,20 +114,20 @@ describe OpenChain::CustomHandler::Masterbrand::MasterbrandInvoiceGenerator do
       described_class.should_receive(:write_invoiced_events).with(split_billables[0], inv_line)
       described_class.should_receive(:write_invoiced_events).with(split_billables[1])
 
-      described_class.bill_new_entries @new_billables, @inv
+      described_class.bill_new_entries @new_billables, @invoiceable_ids, @inv
     end
 
     it "doesn't write any invoice lines if there aren't any events of type 'entry_new" do
       split_billables = [[],
                          [{billable_event_id: 5, billable_eventable_id: 6, event_type: "classification_new"}]]
 
-      described_class.should_receive(:split_billables).with(@new_billables).and_return split_billables
+      described_class.should_receive(:split_billables).with(@new_billables, @invoiceable_ids).and_return split_billables
       split_billables[0].should_receive(:count).and_return(0)
       
       described_class.should_receive(:write_invoiced_events).with(split_billables[0])
       described_class.should_receive(:write_invoiced_events).with(split_billables[1])
 
-      described_class.bill_new_entries @new_billables, @inv
+      described_class.bill_new_entries @new_billables, @invoiceable_ids, @inv
     end
   end
 
@@ -94,16 +142,13 @@ describe OpenChain::CustomHandler::Masterbrand::MasterbrandInvoiceGenerator do
   end
 
   describe :split_billables do
-    it "partitions billables into those with event_type 'new_entry' and remainder" do
-      new_billables = [{billable_event_id: 1, billable_eventable_id: 2, event_type: "entry_new"},
-                       {billable_event_id: 5, billable_eventable_id: 6, event_type: "entry_new"},
-                       {billable_event_id: 3, billable_eventable_id: 4, event_type: "classification_new"}]
+    it "partitions separates billables that meet invoicing requirements from the rest" do
+      billables = [{id: 1, billable_eventable_id: 2}, {id: 3, billable_eventable_id: 4}, {id: 5, billable_eventable_id: 6}]
+      invoiceable_ids = [2,4]
+      split_billables = [[{id: 1, billable_eventable_id: 2}, {id: 3, billable_eventable_id: 4}],
+                         [{id: 5, billable_eventable_id: 6}]]
       
-      split_billables = [[{billable_event_id: 1, billable_eventable_id: 2, event_type: "entry_new"}, 
-                          {billable_event_id: 5, billable_eventable_id: 6, event_type: "entry_new"}],
-                         [{billable_event_id: 3, billable_eventable_id: 4, event_type: "classification_new"}]]
-
-      expect(described_class.split_billables new_billables).to eq split_billables
+      expect(described_class.split_billables billables, invoiceable_ids).to eq split_billables
     end
   end
 
