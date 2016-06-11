08b45bc330a0e140d660612bc60443b96a5e79ac
Brian Glick <brian@brian-glick.com>
2016-06-11 11:00:54 -0400

    add dates to controller response

--- app/controllers/api/v1/tpp_hts_overrides_controller.rb
+++ app/controllers/api/v1/tpp_hts_overrides_controller.rb
@@ -24,7 +24,10 @@ module Api; module V1; class TppHtsOverridesController < Api::V1::ApiCoreModuleC
       :tpphtso_hts_code,
       :tpphtso_rate,
       :tpphtso_note,
-      :tpphtso_trade_preference_program_id
+      :tpphtso_trade_preference_program_id,
+      :tpphtso_start_date,
+      :tpphtso_end_date,
+      :tpphtso_active
     ] + custom_field_keys(core_module))
 
     h = to_entity_hash(obj, headers_to_render)
