42a1ca5dda2dc7771b391ffef2f79f3e65e4da13
Brian Glick <brian@brian-glick.com>
2016-06-30 20:42:15 -0400

    add model field caching by user and load time

--- app/controllers/api/v1/model_fields_controller.rb
+++ app/controllers/api/v1/model_fields_controller.rb
@@ -1,47 +1,50 @@
 require 'digest/md5'
 
 module Api; module V1; class ModelFieldsController < Api::V1::ApiController
-  API_MODULES ||= [CoreModule::PRODUCT, 
-    CoreModule::CLASSIFICATION, 
-    CoreModule::TARIFF, 
+  API_MODULES ||= [CoreModule::PRODUCT,
+    CoreModule::CLASSIFICATION,
+    CoreModule::TARIFF,
     CoreModule::ORDER,
     CoreModule::ORDER_LINE,
-    CoreModule::ENTRY, 
-    CoreModule::OFFICIAL_TARIFF, 
+    CoreModule::ENTRY,
+    CoreModule::OFFICIAL_TARIFF,
     CoreModule::VARIANT]
 
   def index
-    validator_rules = Hash[FieldValidatorRule.all.map{|fvr| [fvr.model_field_uid.to_sym, fvr]}]
-    h = {}
-    h['recordTypes'] = []
-    h['fields'] = []
-    h['cache_key'] = make_cache_key
-    API_MODULES.each do |cm|
-      next unless cm.view?(current_user)
-      cm_class_name = cm.class_name
-      h['recordTypes'] << {'uid'=>cm_class_name,label:cm.label}
-      ModelField.find_by_core_module(cm).each do |mf|
-        next if !mf.can_view?(current_user) || !mf.user_accessible
-        mf_h = {'uid'=>mf.uid, 'label'=>mf.label(false), 'data_type'=>mf.data_type, 'record_type_uid'=>cm_class_name, 'read_only' => mf.read_only?}
-        select_opts = mf.select_options
-        mf_h['select_options'] = select_opts
-        mf_h['autocomplete'] = mf.autocomplete unless mf.autocomplete.blank?
-        fvr = validator_rules[mf.uid.to_sym]
-        if fvr
-          mf_h['remote_validate'] = true
-          if fvr.one_of_array.length > 0
-            #clobber the hard coded options with the customer configured ones if they both exist.
-            #This is on purpose - BSG 2015-09-16
-            mf_h['select_options'] = fvr.one_of_array.collect {|a| [a,a]} #api expects 2 dimensional array
+    text_to_render = Rails.cache.fetch("Api::V1::ModelFields#index-#{ModelField.last_loaded}-#{current_user.id}-#{current_user.updated_at.to_i}") do
+      validator_rules = Hash[FieldValidatorRule.all.map{|fvr| [fvr.model_field_uid.to_sym, fvr]}]
+      h = {}
+      h['recordTypes'] = []
+      h['fields'] = []
+      h['cache_key'] = make_cache_key
+      API_MODULES.each do |cm|
+        next unless cm.view?(current_user)
+        cm_class_name = cm.class_name
+        h['recordTypes'] << {'uid'=>cm_class_name,label:cm.label}
+        ModelField.find_by_core_module(cm).each do |mf|
+          next if !mf.can_view?(current_user) || !mf.user_accessible
+          mf_h = {'uid'=>mf.uid, 'label'=>mf.label(false), 'data_type'=>mf.data_type, 'record_type_uid'=>cm_class_name, 'read_only' => mf.read_only?}
+          select_opts = mf.select_options
+          mf_h['select_options'] = select_opts
+          mf_h['autocomplete'] = mf.autocomplete unless mf.autocomplete.blank?
+          fvr = validator_rules[mf.uid.to_sym]
+          if fvr
+            mf_h['remote_validate'] = true
+            if fvr.one_of_array.length > 0
+              #clobber the hard coded options with the customer configured ones if they both exist.
+              #This is on purpose - BSG 2015-09-16
+              mf_h['select_options'] = fvr.one_of_array.collect {|a| [a,a]} #api expects 2 dimensional array
+            end
           end
+          mf_h['user_id_field'] = true if mf.user_id_field?
+          mf_h['user_field'] = true if mf.user_field?
+          mf_h['user_full_name_field'] = true if mf.user_full_name_field?
+          h['fields'] << mf_h
         end
-        mf_h['user_id_field'] = true if mf.user_id_field?
-        mf_h['user_field'] = true if mf.user_field?
-        mf_h['user_full_name_field'] = true if mf.user_full_name_field?
-        h['fields'] << mf_h
       end
+      h.to_json
     end
-    render json: h
+    render json: text_to_render
   end
 
   def cache_key
