57d0b43965877514a98f87fef8f9b4527e79386c
Jay Davis <jdavis@vandegriftinc.com>
2016-07-13 12:13:23 -0400

    Have H&M billing generator only create invoiced_events for CA classifications

--- lib/open_chain/custom_handler/hm/hm_invoice_generator.rb
+++ lib/open_chain/custom_handler/hm/hm_invoice_generator.rb
@@ -5,18 +5,16 @@ module OpenChain; module CustomHandler; module Hm; class HmInvoiceGenerator
   def self.run_schedulable
     ActiveRecord::Base.transaction do
       billables = get_new_billables
-      invoiceable_ids = get_new_invoiceable_ids
       inv = create_invoice
-      bill_new_classifications(billables, invoiceable_ids, inv)
+      bill_new_classifications(billables, inv)
     end
   end
 
   def self.get_new_billables
-    BillableEvent.joins('LEFT OUTER JOIN invoiced_events ie ON billable_events.id = ie.billable_event_id AND ie.invoice_generator_name = "HmInvoiceGenerator"').where('ie.id IS NULL')
-  end
-
-  def self.get_new_invoiceable_ids
-    BillableEvent.connection.execute(ca_classification_qry).map(&:first)
+    BillableEvent.joins('LEFT OUTER JOIN invoiced_events ie ON billable_events.id = ie.billable_event_id AND ie.invoice_generator_name = "HmInvoiceGenerator"')
+                 .where('ie.id IS NULL')
+                 .where('billable_eventable_type = "Classification"')
+                 .where('billable_eventable_id IN (SELECT cla.id FROM classifications cla INNER JOIN products p ON p.id = cla.product_id INNER JOIN countries cou ON cou.id = cla.country_id INNER JOIN companies com ON com.id = p.importer_id WHERE cou.iso_code = "CA" AND com.alliance_customer_number = "HENNE")')
   end
 
   def self.create_invoice
@@ -24,41 +22,20 @@ module OpenChain; module CustomHandler; module Hm; class HmInvoiceGenerator
     VfiInvoice.next_invoice_number { |n| VfiInvoice.create!(customer: co, invoice_date: Date.today, invoice_number: n, currency: "USD")}
   end
 
-  def self.bill_new_classifications billables, invoiceable_ids, invoice
-    to_be_invoiced, others = split_billables(billables, invoiceable_ids)
-    qty_to_bill = to_be_invoiced.count
+  def self.bill_new_classifications billables, invoice
+    qty_to_bill = billables.count
     if qty_to_bill > 0
       line = invoice.vfi_invoice_lines.create!(charge_description: "new Canadian classification", quantity: qty_to_bill, unit: "ea", unit_price: UNIT_PRICE)
-      write_invoiced_events(to_be_invoiced, line)
+      write_invoiced_events(billables, line)
     end
-    write_invoiced_events others
   end
 
-  def self.split_billables billables, invoiceable_ids
-    billables.partition{ |b| invoiceable_ids.include? b[:billable_eventable_id] }
-  end
-
-  def self.write_invoiced_events billables, invoice_line=nil
+  def self.write_invoiced_events billables, invoice_line
     BillableEvent.transaction do
       billables.each do |e|
         InvoicedEvent.create!(billable_event_id: e[:id], vfi_invoice_line: invoice_line, invoice_generator_name: 'HmInvoiceGenerator', charge_type: 'classification_ca')
       end
     end
-  end
-
-  private
-
-  def self.ca_classification_qry
-    <<-SQL
-      SELECT cl.id
-      FROM billable_events be
-        INNER JOIN classifications cl ON be.billable_eventable_type = "Classification" AND be.billable_eventable_id = cl.id
-        INNER JOIN products p ON p.id = cl.product_id
-        INNER JOIN countries cou ON cou.id = cl.country_id
-        INNER JOIN companies com ON com.id = p.importer_id
-        LEFT OUTER JOIN invoiced_events ie ON be.id = ie.billable_event_id AND ie.invoice_generator_name = "HmInvoiceGenerator"
-      WHERE com.alliance_customer_number = "HENNE" AND cou.iso_code = 'CA' AND ie.billable_event_id IS NULL AND be.event_type ='classification_new'
-    SQL
   end
 
 end; end; end; end
--- spec/lib/custom_handler/hm/hm_invoice_generator_spec.rb
+++ spec/lib/custom_handler/hm/hm_invoice_generator_spec.rb
@@ -5,72 +5,64 @@ describe OpenChain::CustomHandler::Hm::HmInvoiceGenerator do
   describe :run_schedulable do
     it "finds the new billable events, new invoiceable events, creates a VFI invoice, bills new entries" do
       new_billables = double("new billables")
-      new_invoiceable_ids = double("new_invoiceables")
       invoice = double("vfi invoice")
   
       described_class.should_receive(:get_new_billables).and_return new_billables
-      described_class.should_receive(:get_new_invoiceable_ids).and_return new_invoiceable_ids
       described_class.should_receive(:create_invoice).and_return invoice
-      described_class.should_receive(:bill_new_classifications).with(new_billables, new_invoiceable_ids, invoice)
+      described_class.should_receive(:bill_new_classifications).with(new_billables, invoice)
 
       described_class.run_schedulable
     end
   end
 
-  describe :get_new_billabes do
-    it "gets new billables" do      
-      be_1 = Factory(:billable_event, billable_eventable: Factory(:classification), entity_snapshot: Factory(:entity_snapshot), event_type: "classification_new")
-      be_2 = Factory(:billable_event, billable_eventable: Factory(:classification), entity_snapshot: Factory(:entity_snapshot), event_type: "classification_new")
-      be_3 = Factory(:billable_event, billable_eventable: Factory(:classification), entity_snapshot: Factory(:entity_snapshot), event_type: "entry_new")
-      Factory(:invoiced_event, billable_event: be_3, invoice_generator_name: "HmInvoiceGenerator")
+  describe :get_new_billables do
+    let(:country_ca) { Factory(:country, iso_code: "CA", name: "CANADA") }
+    let(:country_us) { Factory(:country, iso_code: "US", name: "UNITED STATES") }
+    
+    context "H&M classifications" do
+      let(:prod) { Factory(:product, importer: Factory(:company, alliance_customer_number: "HENNE", name: "H&M"), unique_identifier: "foo") }
 
-      filtered_results = [{id: be_1.id, billable_eventable_id: be_1.billable_eventable.id, event_type: "classification_new"},
-                          {id: be_2.id, billable_eventable_id: be_2.billable_eventable.id, event_type: "classification_new"}]
-      
-      new_billables = described_class.get_new_billables.sort_by{ |b| b.id }
-      expect(new_billables.map{|b| {id: b.id, billable_eventable_id: b.billable_eventable.id, event_type: b.event_type} })
-                          .to eq filtered_results
+      it "returns no results for CA classifications that are already invoiced" do
+        cl = Factory(:classification, product: prod, country: country_ca)
+        be = Factory(:billable_event, billable_eventable: cl, entity_snapshot: Factory(:entity_snapshot), event_type: "classification_new")
+        Factory(:invoiced_event, billable_event: be, invoice_generator_name: "HmInvoiceGenerator")
+        results = described_class.get_new_billables
+        expect(results.count).to eq 0
+      end
+
+      it "returns results for CA classifications that haven't been invoiced" do
+        cl = Factory(:classification, product: prod, country: country_ca)
+        Factory(:billable_event, billable_eventable: cl, entity_snapshot: Factory(:entity_snapshot), event_type: "classification_new")
+        results = described_class.get_new_billables
+        expect(results.count).to eq 1
+      end
+
+      it "returns no results for non-CA classifications that haven't been invoiced" do
+        cl = Factory(:classification, product: prod, country: country_us)
+        Factory(:billable_event, billable_eventable: cl, entity_snapshot: Factory(:entity_snapshot), event_type: "classification_new")
+        results = described_class.get_new_billables
+        expect(results.count).to eq 0
+      end
+    end
+
+    context "Non-H&M classifications" do
+      let(:prod) { Factory(:product, importer: Factory(:company), unique_identifier: "foo") }
+
+      it "returns no results for CA classifications that haven't been invoiced" do
+        cl = Factory(:classification, product: prod, country: country_ca)
+        Factory(:billable_event, billable_eventable: cl, entity_snapshot: Factory(:entity_snapshot), event_type: "classification_new")
+        results = described_class.get_new_billables
+        expect(results.count).to eq 0
+      end
+
+      it "returns no results for non-CA classifications that haven't been invoiced" do
+        cl = Factory(:classification, product: prod, country: country_us)
+        Factory(:billable_event, billable_eventable: cl, entity_snapshot: Factory(:entity_snapshot), event_type: "classification_new")
+        results = described_class.get_new_billables
+        expect(results.count).to eq 0
+      end
     end
   end
-
-  describe :get_new_invoiceable_ids do
-    context "when retrieving billables that will be invoiced" do
-      before :each do
-        cou = Factory(:country, iso_code: "CA")
-        com = Factory(:company, alliance_customer_number: "HENNE")
-        @class_1 = Factory(:classification, product: Factory(:product, importer: com), country: cou)
-        @class_2 = Factory(:classification, product: Factory(:product, importer: com), country: cou)
-        @be_1 = Factory(:billable_event, billable_eventable: @class_1, entity_snapshot: Factory(:entity_snapshot), event_type: "classification_new")
-        @be_2 = Factory(:billable_event, billable_eventable: @class_2, entity_snapshot: Factory(:entity_snapshot), event_type: "classification_new")
-      end
-
-      it "excludes non-Canadian classification events" do
-        @class_2.update_attributes(country: Factory(:country))
-        expect(described_class.get_new_invoiceable_ids).to eq [@class_1.id]
-      end
-
-      it "excludes classification events not belonging to H&M" do
-        p = @class_2.product
-        p.update_attributes(importer: Factory(:company))
-        expect(described_class.get_new_invoiceable_ids).to eq [@class_1.id]
-      end
-
-      it "excludes events with wrong type" do
-        @be_2.update_attributes(event_type: "entry_new")
-        expect(described_class.get_new_invoiceable_ids).to eq [@class_1.id]
-      end
-
-      it "excludes events that aren't new (i.e. a billable with an invoiced event created by this generator)" do
-        Factory(:invoiced_event, billable_event: @be_2, invoice_generator_name: "HmInvoiceGenerator")
-        expect(described_class.get_new_invoiceable_ids).to eq [@class_1.id]
-      end
-
-      it "includes events that have been invoiced by another generator" do
-        Factory(:invoiced_event, billable_event: @be_2, invoice_generator_name: "FooGenerator")
-        expect(described_class.get_new_invoiceable_ids.sort).to eq [@class_1.id, @class_2.id]
-      end
-    end
-  end  
 
   describe :create_invoice do
     it "creates a new VFI invoice" do
@@ -82,50 +74,28 @@ describe OpenChain::CustomHandler::Hm::HmInvoiceGenerator do
   end
 
   describe :bill_new_classifications do
-    before :each do
-      @billables = [{id: 1, billable_eventable_id: 2}, {id: 3, billable_eventable_id: 4}, {id: 5, billable_eventable_id: 6}]
-      @invoiceable_ids = double("int_array")
-      @invoice = double("invoice")
-    end
+    let(:invoice) { double("invoice") }
 
     it "creates invoiced events for all billables and an invoice line for billables with invoiceable ids" do
+      billables = [{id: 1, billable_eventable_id: 2}, {id: 3, billable_eventable_id: 4}, {id: 5, billable_eventable_id: 6}]
       inv_line = double("invoice_line")
       inv_lines_relation = double("inv_lines_relation")
-      split_billables = [[{id: 1, billable_eventable_id: 2}, {id: 3, billable_eventable_id: 4}],
-                         [{id: 5, billable_eventable_id: 6}]]
       
-      described_class.should_receive(:split_billables).with(@billables, @invoiceable_ids).and_return split_billables
-      
-      @invoice.should_receive(:vfi_invoice_lines).and_return inv_lines_relation
-      inv_lines_relation.should_receive(:create!).with(charge_description: "new Canadian classification", quantity: 2, 
+      invoice.should_receive(:vfi_invoice_lines).and_return inv_lines_relation
+      inv_lines_relation.should_receive(:create!).with(charge_description: "new Canadian classification", quantity: 3, 
                                                        unit: "ea", unit_price: 2.00).and_return inv_line
-      described_class.should_receive(:write_invoiced_events).with(split_billables[0], inv_line)
-      described_class.should_receive(:write_invoiced_events).with(split_billables[1])
+      described_class.should_receive(:write_invoiced_events).with(billables, inv_line)
 
-      described_class.bill_new_classifications @billables, @invoiceable_ids, @invoice
+      described_class.bill_new_classifications billables, invoice
     end
 
     it "doesn't create an invoice line if there are no invoiceable events" do
-      split_billables = [[],
-                         [{id: 5, billable_eventable_id: 6}]]
+      billables = []
       
-      described_class.should_receive(:split_billables).with(@billables, @invoiceable_ids).and_return split_billables
-      @invoice.should_not_receive(:vfi_invoice_lines)
-      described_class.should_not_receive(:write_invoiced_events).with(split_billables[0])
-      described_class.should_receive(:write_invoiced_events).with(split_billables[1])
+      invoice.should_not_receive(:vfi_invoice_lines)
+      described_class.should_not_receive(:write_invoiced_events)
 
-      described_class.bill_new_classifications @billables, @invoiceable_ids, @invoice
-    end
-  end
-
-  describe :split_billables do
-    it "partitions separates billables that meet invoicing requirements from the rest" do
-      billables = [{id: 1, billable_eventable_id: 2}, {id: 3, billable_eventable_id: 4}, {id: 5, billable_eventable_id: 6}]
-      invoiceable_ids = [2,4]
-      split_billables = [[{id: 1, billable_eventable_id: 2}, {id: 3, billable_eventable_id: 4}],
-                         [{id: 5, billable_eventable_id: 6}]]
-      
-      expect(described_class.split_billables billables, invoiceable_ids).to eq split_billables
+      described_class.bill_new_classifications billables, @invoice
     end
   end
 
