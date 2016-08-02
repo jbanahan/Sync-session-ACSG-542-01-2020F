55c06e60e6d810d86ef034ceecc16244d73c0621
Brian Glick <brian@brian-glick.com>
2016-08-02 14:48:11 -0400

    add unique ids for core modules

--- lib/open_chain/model_field_definition/group_field_definition.rb
+++ lib/open_chain/model_field_definition/group_field_definition.rb
@@ -3,7 +3,10 @@ module OpenChain; module ModelFieldDefinition; module GroupFieldDefinition
     add_fields CoreModule::GROUP, [
       [1, :grp_name, :name, "Name", {data_type: :string}],
       [2, :grp_system_code, :system_code, "System Code", {data_type: :string, read_only: true}],
-      [3, :grp_description, :description, "Description", {data_type: :string}]
+      [3, :grp_description, :description, "Description", {data_type: :string}],
+      [4, :grp_unique_identifier, :unique_identifier, "Unique Identifier", {data_type: :string, read_only: true,
+          export_lambda: lambda {|grp| "#{grp.id}-#{grp.name}"},
+          qualified_field_name: "CONCAT(groups.id, '-', ifnull(groups.attached_file_name, ''))"}]
     ]
   end
 end; end; end;
