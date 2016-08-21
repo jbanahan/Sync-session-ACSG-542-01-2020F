3336e487806163bb26ada00b3aed92d1a398c2fd
Brian Glick <brian@brian-glick.com>
2016-08-21 16:39:39 -0400

    remove deprectated mock expectation

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -7,10 +7,11 @@ describe Comment do
       u = Factory(:user)
       t = Time.now
       allow(Time).to receive(:now).and_return t
-      expect(OpenChain::EventPublisher).to receive(:publish).with {|type,obj|
+      expect(OpenChain::EventPublisher).to receive(:publish) do |type,obj|
         expect(type).to eq :comment_create
         expect(obj).to be_instance_of Comment
-      }
+        nil
+      end
       o.comments.create!(user_id:u.id,body:'abc',subject:'def')
     end
   end
