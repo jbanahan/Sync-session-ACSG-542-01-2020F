7b65a3080f0c99d80a1b86f83fe1433997692f2e
Jeremy Hulford <jhulford@vandegriftinc.com>
2016-10-03 08:17:43 -0500

    Pepsi SAML IP update.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -12,15 +12,15 @@ if File.exist?("config/google_auth.json")
   Rails.application.config.use_google_auth = true
 end
 
-if File.exist?("config/pepsi.pem")
+if File.exist?("config/pepsi.pem") && ["pepsi", "pepsitest"].include?(MasterSetup.get.system_code)
   spid = "pepsi.vfitrack.net"
-  saml_target = (Rails.env.production? ? "https://affiliateportal.mypepsico.com/affwebservices/public/saml2sso" : "https://affiliateportal.ite.mypepsico.com/affwebservices/public/saml2sso")
+  saml_target = ("pepsi" == MasterSetup.get.system_code) ? "https://idp.mypepsico.com/affwebservices/public/saml2sso" : "https://idp.ite.mypepsico.com/affwebservices/public/saml2sso"
   # Pepsi's SSO SAML system does not appear to work like a "standard" system does.  All we need to do is include our SPID (Service Provider ID - pepsi.vfitrack.net) 
   # as a query parameter into the SSO link, rather than forwarding a full-fledged SAML AuthnRequest XML.  So, basically, the whole request_phase that the Omniauth-SAML 
   # gem provides is totally unused, only the callback is utilized (hence the reason why I'm killing it in the request_path below with a lambda always returning false).
   Rails.application.config.middleware.use OmniAuth::Builder do
     provider :saml,
-      :assertion_consumer_service_url     => (Rails.env.production? ? "https://pepsi.vfitrack.net#{OmniAuth.config.path_prefix}/pepsi-saml/callback" : "http://development.vfitrack.net#{OmniAuth.config.path_prefix}/pepsi-saml/callback"),
+      :assertion_consumer_service_url     => "https://#{MasterSetup.get.system_code}.vfitrack.net#{OmniAuth.config.path_prefix}/pepsi-saml/callback",
       :issuer                             => spid,
       :idp_sso_target_url                 => saml_target,
       :idp_cert                           => IO.read("config/pepsi.pem"),
