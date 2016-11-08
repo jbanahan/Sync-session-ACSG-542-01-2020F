33c50360677ecafdfe8b40feb7e29935be3bd523
Jeremy Hulford <jhulford@vandegriftinc.com>
2016-11-08 11:33:58 -0600

    TestInstanceManager adjustments.

--- lib/open_chain/test_instance_manager.rb
+++ lib/open_chain/test_instance_manager.rb
@@ -20,9 +20,14 @@ module OpenChain
 
     def update_master_setup! req_host
       uuid = UUIDTools::UUID.timestamp_create.to_s
-      ms = MasterSetup.get
+      # Load the master setup from the database (NOT from memcache)
+      # This is because other test instances may be running against a previous database version -
+      # since this is pretty much the first step to run when deploying a new test database.
+      ms = MasterSetup.first
       ms.system_code = req_host.split(".")[0] # system code should equal the machine name of the system (.ie www from www.vfitrack.net)
-      ms.uuid = UUIDTools::UUID.timestamp_create.to_s
+      # Use the request host name as the basis for the UUID.  This way we can just keep using the same uuid for progressive iterations
+      # of the same test system and not end up creating different S3 buckets for everything.
+      ms.uuid = UUIDTools::UUID.md5_create(UUIDTools::UUID_DNS_NAMESPACE, req_host).to_s
       ms.request_host = req_host
       ms.ftp_polling_active = false
       ms.custom_features = ''
