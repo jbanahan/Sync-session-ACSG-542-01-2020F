254455e88b81e32b96f578c6938b474939b386b0
Jay Davis <planarian@users.noreply.github.com>
2016-12-07 10:52:20 -0500

    Add history_ignore to order comment fields (#1809)

--- lib/open_chain/model_field_definition/order_field_definition.rb
+++ lib/open_chain/model_field_definition/order_field_definition.rb
@@ -81,17 +81,17 @@ module OpenChain; module ModelFieldDefinition; module OrderFieldDefinition
         },
         qualified_field_name: "(SELECT GROUP_CONCAT(DISTINCT shipments.reference ORDER BY shipments.reference SEPARATOR \"\n\") FROM order_lines INNER JOIN piece_sets ON piece_sets.order_line_id = order_lines.id INNER JOIN shipment_lines ON shipment_lines.id = piece_sets.shipment_line_id INNER JOIN shipments ON shipments.id = shipment_lines.shipment_id WHERE order_lines.order_id = orders.id)"
       }],
-      [31, :ord_comment_last_7_days, :comment_last_7_days, "Comments - Last 7 Days", :data_type=>:string, :read_only=>true,
+      [31, :ord_comment_last_7_days, :comment_last_7_days, "Comments - Last 7 Days", :data_type=>:string, :read_only=>true, :history_ignore=>true,
         :import_lambda => lambda {|a,b| return "Comments - Last 7 Days cannot be set by import, ignored."},
         :export_lambda => lambda {|obj| Comment.gather(obj, DateTime.now - 7.days)},
         :qualified_field_name => "(SELECT GROUP_CONCAT(CONCAT(DATE_FORMAT(CONVERT_TZ(c.updated_at, 'UTC', '#{Time.zone.tzinfo.name}'),'%m-%d %H:%i'),\" \",c.subject,\": \",c.body) ORDER BY c.updated_at DESC SEPARATOR \"\n \n\") FROM comments c WHERE c.commentable_id = orders.id AND DATE_SUB(NOW(), INTERVAL 7 DAY) <= c.updated_at)"
       ],
-      [32, :ord_comment_last_24_hrs, :comment_last_24_hrs, "Comments - Last 24 Hours", :data_type=>:string, :read_only=>true,
+      [32, :ord_comment_last_24_hrs, :comment_last_24_hrs, "Comments - Last 24 Hours", :data_type=>:string, :read_only=>true, :history_ignore=>true,
         :import_lambda => lambda {|a,b| return "Comments - Last 24 Hours cannot be set by import, ignored."},
         :export_lambda => lambda {|obj| Comment.gather(obj, DateTime.now - 24.hours)},
         :qualified_field_name => "(SELECT GROUP_CONCAT(CONCAT(DATE_FORMAT(CONVERT_TZ(c.updated_at, 'UTC', '#{Time.zone.tzinfo.name}'),'%m-%d %H:%i'),\" \",c.subject,\": \",c.body) ORDER BY c.updated_at DESC SEPARATOR \"\n \n\") FROM comments c WHERE c.commentable_id = orders.id AND DATE_SUB(NOW(), INTERVAL 24 DAY_HOUR) <= c.updated_at)"
       ],
-      [33, :ord_comment_last_25, :comment_last_25, "Comments - Last 25", :data_type=>:string, :read_only=>true,
+      [33, :ord_comment_last_25, :comment_last_25, "Comments - Last 25", :data_type=>:string, :read_only=>true, :history_ignore=>true,
         :import_lambda => lambda {|a,b| return "Comments - Last 25 cannot be set by import, ignored."},
         :export_lambda => lambda {|obj| Comment.gather(obj, nil, 25)},
         :qualified_field_name => "(SELECT GROUP_CONCAT(CONCAT(DATE_FORMAT(CONVERT_TZ(c.updated_at, 'UTC', '#{Time.zone.tzinfo.name}'),'%m-%d %H:%i'),\" \",c.subject,\": \",c.body) ORDER BY c.updated_at DESC SEPARATOR \"\n \n\") FROM comments c WHERE c.commentable_id = orders.id LIMIT 25)"
