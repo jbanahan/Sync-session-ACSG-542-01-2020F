af38f7120140e5a08042ab085c0f8fdeb0dc638b
Brian Bommarito <brian@bommarito.io>
2016-12-30 11:15:15 -0500

    ISF monitoring script. (#1825)
    * ISF monitoring script.
    
    * Fixing EST conversion.

---
+++ lib/open_chain/isf_monitor.rb
@@ -0,0 +1,74 @@
+require 'net/ftp'
+
+module OpenChain
+  class IsfMonitor
+    def self.run_schedulable opts = {}
+      hostname = opts['hostname']
+      username = opts['username']
+      password = opts['password']
+      directory = opts['directory']
+
+      raise "hostname, username, password and directory must be passed in" if hostname.blank? || username.blank? || password.blank? || directory.blank?
+
+      self.new.run(hostname, username, password, directory)
+    end
+
+    def run(hostname, username, password, directory)
+      ftp = ftp_connection(hostname)
+      ftp_login(ftp, username, password)
+      ftp_chdir(ftp, directory)
+      contents = ftp.ls
+      return unless contents.present?
+      sorted_dates = process_ftp_ls(contents)
+      if is_backed_up?(sorted_dates[0])
+        to = ['isf@vandegriftinc.com', 'mzeitlin@vandegriftinc.com', 'agriffin@vandegriftinc.com', 'bglick@vandegriftinc.com', 'support@vandegriftinc.com']
+        subject = "ISF PROCESSING STUCK"
+        body = "Kewill EDI ISF processing is suck. The oldest file in the folder is from #{sorted_dates[0]} and there are #{contents.length} files in the folder<br />Please call Brian Glick - 215-821-6595 to escalate."
+        OpenMailer::send_simple_html(to, subject, body).deliver
+      end
+    end
+
+    def is_backed_up?(date)
+      Time.zone.now.utc < date || date < 16.minutes.ago.utc
+    end
+
+    def process_ftp_ls(contents)
+      dates = parse_ftp_dates(contents)
+      est_dates = ftp_dates_to_est(dates)
+      utc_dates = est_dates_to_utc_dates(est_dates)
+      sort_utc_dates(utc_dates)
+    end
+
+    def sort_utc_dates(dates)
+      dates.sort { |a, b| a <=> b }
+    end
+
+    def est_dates_to_utc_dates(dates)
+      dates.map { |date| date.utc }
+    end
+
+    def ftp_dates_to_est(dates)
+      dates.map { |a| Time.parse(a).in_time_zone('Eastern Time (US & Canada)') }
+    end
+
+    def parse_ftp_dates(contents)
+      dates = nil
+      dates = contents.map { |listing| /\d\d:\d\d/.match listing }
+      dates = dates.map { |date| date[0] }
+
+      dates
+    end
+
+    def ftp_chdir(ftp, directory)
+      ftp.chdir(directory)
+    end
+
+    def ftp_connection(hostname)
+      Net::FTP.new(hostname)
+    end
+
+    def ftp_login(ftp, username, password)
+      ftp.login(username, password)
+    end
+  end
+end
---
+++ spec/lib/isf_monitor_spec.rb
@@ -0,0 +1,64 @@
+require 'spec_helper'
+
+RSpec.describe 'ISF Monitor' do
+  let(:ls_output) {
+    ["-rw-------   1 user group         3471 Dec 30 08:57 VAND0323_EDI_LUMBER6690558_20161230085703.xml",
+          "-rw-------   1 user group         3471 Dec 30 08:56 VAND0323_EDI_LUMBER6690558_20161230085703.xml",
+          "-rw-------   1 user group         3471 Dec 30 08:58 VAND0323_EDI_LUMBER6690558_20161230085703.xml"]
+  }
+
+  describe '#parse_ftp_dates' do
+    it 'returns just the times' do
+      expect(OpenChain::IsfMonitor.new.parse_ftp_dates(ls_output)).to eql(['08:57', '08:56', '08:58'])
+    end
+  end
+
+  describe '#is_backed_up?' do
+    it 'considers the ftp backed up if oldest date is before current date' do
+      future_date = 10.minutes.from_now.utc
+
+      expect(OpenChain::IsfMonitor.new.is_backed_up?(future_date)).to be true
+    end
+
+    it 'considers the ftp backed up if oldest date is older than now - 15 minutes' do
+      past_date = 16.minutes.ago
+
+      expect(OpenChain::IsfMonitor.new.is_backed_up?(past_date)).to be true
+    end
+  end
+
+  describe '#sort_utc_dates' do
+    it 'sorts an array of dates oldest date first' do
+      date1 = Time.zone.parse("08:56").utc
+      date2 = Time.zone.parse("08:57").utc
+      date3 = Time.zone.parse("08:58").utc
+      dates = [date2, date3, date1]
+      expected = [date1, date2, date3]
+
+      expect(OpenChain::IsfMonitor.new.sort_utc_dates(dates)).to eql(expected)
+    end
+  end
+  describe '#est_dates_to_utc_dates' do
+    it 'converts the est dates to utc dates and returns an array' do
+      date1 = Time.zone.parse("08:56").utc
+      date2 = Time.zone.parse("08:57").utc
+      date3 = Time.zone.parse("08:58").utc
+      expected = [date1.utc, date2.utc, date3.utc]
+      dates = [date1, date2, date3]
+
+      expect(OpenChain::IsfMonitor.new.est_dates_to_utc_dates(dates)).to eql(expected)
+    end
+  end
+
+  describe '#ftp_dates_to_est' do
+    it 'converts the ftp times to eastern time' do
+      Time.zone = 'Eastern Time (US & Canada)'
+      date1 = Time.zone.parse("08:57")
+      date2 = Time.zone.parse("08:56")
+      date3 = Time.zone.parse("08:58")
+
+      times = OpenChain::IsfMonitor.new.parse_ftp_dates(ls_output)
+      expect(OpenChain::IsfMonitor.new.ftp_dates_to_est(times)).to eql([date1, date2, date3])
+    end
+  end
+end
