9ccf0cef12ac71157fded2f66c5bb077d05a077d
Brian Bommarito <brian@bommarito.io>
2017-01-06 12:56:42 -0500

    I really, truly hate timezones. Correcting an issue where the EST con… (#1838)
    * Simplified isf monitor, thanks to some suggestions from Jeremy.
    * Removing future date check in #is_backed_up?

--- spec/lib/isf_monitor_spec.rb
+++ spec/lib/isf_monitor_spec.rb
@@ -1,29 +1,38 @@
 require 'spec_helper'
+require 'net/ftp'
 
 RSpec.describe 'ISF Monitor' do
-  let(:ls_output) {
-    ["-rw-------   1 user group         3471 Dec 30 08:57 VAND0323_EDI_LUMBER6690558_20161230085703.xml",
-          "-rw-------   1 user group         3471 Dec 30 08:56 VAND0323_EDI_LUMBER6690558_20161230085703.xml",
-          "-rw-------   1 user group         3471 Dec 30 08:58 VAND0323_EDI_LUMBER6690558_20161230085703.xml"]
-  }
+  let!(:ftp_files) { ["file1.txt", "file2.txt", "file3.txt"] }
+  let!(:ftp_dates) { [16.minutes.ago.utc, 15.minutes.ago.utc, 5.minutes.ago.utc] }
+  let!(:ftp) { Net::FTP.new }
 
-  describe '#parse_ftp_dates' do
-    it 'returns just the times' do
-      expect(OpenChain::IsfMonitor.new.parse_ftp_dates(ls_output)).to eql(['08:57', '08:56', '08:58'])
+  before do
+    allow(Net::FTP).to receive(:new).and_return(ftp)
+  end
+
+  describe '#process_ftp_conents' do
+    it 'returns the dates of the files' do
+      dates = ftp_dates.dup
+      ftp_files.each do |file|
+        expect(ftp).to receive(:mtime).with(file).and_return(dates.shift)
+      end
+
+      expect(OpenChain::IsfMonitor.new.process_ftp_contents(ftp, ftp_files)).to eql(ftp_dates)
     end
   end
 
   describe '#is_backed_up?' do
-    it 'considers the ftp backed up if oldest date is before current date' do
-      future_date = 10.minutes.from_now.utc
-
-      expect(OpenChain::IsfMonitor.new.is_backed_up?(future_date, 15)).to be true
-    end
-
     it 'considers the ftp backed up if oldest date is older than now - 15 minutes' do
-      past_date = 16.minutes.ago
+      past_date = 16.minutes.ago.utc
 
       expect(OpenChain::IsfMonitor.new.is_backed_up?(past_date, 15)).to be true
+    end
+  end
+
+  describe '#utc_to_est_date' do
+    it 'converts the passed in date to an est date' do
+      expect(OpenChain::IsfMonitor.new.utc_to_est_date(ftp_dates[0])).
+          to eql(ftp_dates[0].in_time_zone("America/New_York"))
     end
   end
 
@@ -36,28 +45,6 @@ RSpec.describe 'ISF Monitor' do
       expected = [date1, date2, date3]
 
       expect(OpenChain::IsfMonitor.new.sort_utc_dates(dates)).to eql(expected)
-    end
-  end
-  describe '#est_dates_to_utc_dates' do
-    it 'converts the est dates to utc dates and returns an array' do
-      date1 = Time.zone.parse("08:56").utc
-      date2 = Time.zone.parse("08:57").utc
-      date3 = Time.zone.parse("08:58").utc
-      expected = [date1.utc, date2.utc, date3.utc]
-      dates = [date1, date2, date3]
-
-      expect(OpenChain::IsfMonitor.new.est_dates_to_utc_dates(dates)).to eql(expected)
-    end
-  end
-
-  describe '#ftp_dates_to_est' do
-    it 'converts the ftp times to eastern time' do
-      date1 = Time.parse("08:57").in_time_zone('Eastern Time (US & Canada)')
-      date2 = Time.parse("08:56").in_time_zone('Eastern Time (US & Canada)')
-      date3 = Time.parse("08:58").in_time_zone('Eastern Time (US & Canada)')
-
-      times = OpenChain::IsfMonitor.new.parse_ftp_dates(ls_output)
-      expect(OpenChain::IsfMonitor.new.ftp_dates_to_est(times)).to eql([date1, date2, date3])
     end
   end
 end
