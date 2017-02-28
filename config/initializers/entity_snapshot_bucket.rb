856a0b5da2278ef4ae8fc402b95b257b103b64bd
Jeremy Hulford <jhulford@vandegriftinc.com>
2017-02-28 13:08:44 -0600

    Comment out s3 check in startup.

--- config/initializers/entity_snapshot_bucket.rb
+++ config/initializers/entity_snapshot_bucket.rb
@@ -1,3 +1,5 @@
 if Rails.env.production? && ActiveRecord::Base.connection.table_exists?('entity_snapshots')
-  EntitySnapshot.create_bucket_if_needed!
+  # TODO Make some sort of attribute somewhere that notes the bucket is already created
+  # so we don't have to rely on s3 on startup.
+  #EntitySnapshot.create_bucket_if_needed!
 end
