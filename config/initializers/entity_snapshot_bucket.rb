2ac537f39eb9a102c40b16041e824d0d007f5581
jhulford <jhulford@vandegriftinc.com>
2017-03-02 08:43:13 -0600

    Fix Snapshot Bucket Initializer to not fail if S3 is down. (#1916)

--- config/initializers/entity_snapshot_bucket.rb
+++ config/initializers/entity_snapshot_bucket.rb
@@ -1,5 +1,7 @@
 if Rails.env.production? && ActiveRecord::Base.connection.table_exists?('entity_snapshots')
-  # TODO Make some sort of attribute somewhere that notes the bucket is already created
-  # so we don't have to rely on s3 on startup.
-  #EntitySnapshot.create_bucket_if_needed!
+  begin
+    EntitySnapshot.create_bucket_if_needed!
+  rescue => e
+    e.log_me
+  end
 end
