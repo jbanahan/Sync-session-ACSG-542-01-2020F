e74c909cfd480675a9752f5fe58a2caace291c8b
Jeremy Hulford <jhulford@vandegriftinc.com>
2017-03-02 10:38:18 -0600

    Make change be batched.

--- db/migrate/20170228213349_make_s3_columns_nullable_on_business_rule_snapshot.rb
+++ db/migrate/20170228213349_make_s3_columns_nullable_on_business_rule_snapshot.rb
@@ -1,11 +1,12 @@
 class MakeS3ColumnsNullableOnBusinessRuleSnapshot < ActiveRecord::Migration
   def up
-    change_column :business_rule_snapshots, :bucket, :string, :null => true
-    change_column :business_rule_snapshots, :doc_path, :string, :null => true
+    change_table(:business_rule_snapshots, bulk: true) do |t|
+      t.change :bucket, :string, null: true
+      t.change :doc_path, :string, null: true
+    end
   end
 
   def down
-    change_column :business_rule_snapshots, :bucket, :string, :null => false
-    change_column :business_rule_snapshots, :doc_path, :string, :null => false
+    # This migration is not reversible due to the probability that there are now nulls in the table
   end
 end
