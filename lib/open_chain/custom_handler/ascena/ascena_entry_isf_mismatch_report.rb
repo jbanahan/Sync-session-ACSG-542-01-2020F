a877e14a737d46f0c4aa3c98e20b248537bfda7a
jhulford <jhulford@vandegriftinc.com>
2017-03-20 08:25:29 -0500

    Ascena Entry/ISF Mismatch Report (#1931)

---
+++ lib/open_chain/custom_handler/ascena/ascena_entry_isf_mismatch_report.rb
@@ -0,0 +1,159 @@
+require 'open_chain/report/report_helper'
+
+module OpenChain; module CustomHandler; module Ascena; class AscenaEntryIsfMismatchReport
+  include OpenChain::Report::ReportHelper
+
+  def self.run_schedulable config = {}
+    start_date, end_date = dates
+    tf = self.new.run_report ascena, start_date, end_date
+    OpenMailer.send_simple_html(config["email"], "Ascena Entry/ISF Mismatch #{start_date.to_date} - #{end_date.to_date}", "The Entry/ISF Mismatch report for Entry Summary Sent Dates between #{start_date.to_date} - #{end_date.to_date} is attached.", [tf]).deliver!
+  end
+
+  def self.dates 
+    now = Time.zone.now.in_time_zone("America/New_York").midnight
+    start_date = now - 7.days
+    end_date = now + 7.days
+    [start_date, end_date]
+  end
+
+  def self.ascena
+    Company.importers.where(alliance_customer_number: "ASCE").first
+  end
+
+  def run_report importer, start_date, end_date
+    wb, sheet = XlsMaker.create_workbook_and_sheet "Entry / ISF Match"
+
+    row_number = 0
+    column_widths = [20, 25, 20, 20, 30, 30, 20, 20, 20, 20, 10, 10, 10, 10, 10, 10, 10]
+    XlsMaker.set_column_widths sheet, column_widths
+    XlsMaker.add_header_row sheet, 0, report_headers, column_widths
+
+    entries = Entry.where(importer_id: importer.id, transport_mode_code: ["10", "11"]).where("first_entry_sent_date >= ? AND first_entry_sent_date < ?", start_date.in_time_zone("UTC"), end_date.in_time_zone("UTC")).pluck :id
+    entries.each do |id|
+      entry = Entry.where(id: id).includes(:commercial_invoices => [:commercial_invoice_lines=>[:commercial_invoice_tariffs]]).first
+
+      isf = SecurityFiling.where(importer_id: entry.importer_id).where("entry_reference_numbers LIKE ?", "%#{entry.broker_reference}%").includes(:security_filing_lines).first
+
+      # Kewill's ISF system is terribly bad at not VFI Track updates containing the entry numbers.  So, we're going to also see if we can find 
+      # an ISF by the master bill.
+      isf = SecurityFiling.where(importer_id: entry.importer_id).where("master_bill_of_lading = ?", entry.master_bills_of_lading).includes(:security_filing_lines).first
+
+      if isf.nil?
+        add_exception_row(sheet, (row_number+=1), column_widths, entry, nil, nil, nil, nil, nil, nil, nil, nil, nil)
+      else
+        entry.commercial_invoices.each do |invoice|
+          invoice.commercial_invoice_lines.each do |line|
+            line.commercial_invoice_tariffs.each do |tariff|
+              match = matches(line, tariff, isf)
+
+              if !match.matches?
+                add_exception_row sheet, (row_number += 1), column_widths, entry, line, tariff, isf, match.isf_line, true, match.hts_match, match.coo_match, match.po_match, match.style_match
+              end
+            end
+          end
+        end
+      end
+    end
+
+    workbook_to_tempfile wb, "EntryISfMatch", file_name: "Entry ISF Match.xls"
+  end
+
+  def report_headers 
+    ["Transaction Number", "Master Bill", "Container Number", "Entry Number", "Country of Origin Code (ISF)", "Country of Origin Code (Entry)", "PO Number (ISF)", "PO Number (Entry)", "Part Number (ISF)", "Part Number (Entry)", "HTS Code (ISF)", "HTS Code (Entry)", "ISF Match", "HTS Match", "COO Match", "PO Match", "Style Match"]
+  end
+
+
+  def add_exception_row sheet, row_number, column_widths, entry, invoice_line, tariff_line, isf, isf_line, isf_match, hts_match, coo_match, po_match, style_match
+    row = []
+    row << field(isf.try(:transaction_number), "")
+    row << field(isf.try(:master_bill_of_lading), entry.master_bills_of_lading)
+    row << field(isf.try(:container_numbers), entry.container_numbers)
+    row << entry.entry_number
+    row << isf_line.try(:country_of_origin_code)
+    row << invoice_line.try(:country_origin_code)
+    row << isf_line.try(:po_number)
+    row << invoice_line.try(:po_number)
+    row << isf_line.try(:part_number)
+    row << invoice_line.try(:part_number)
+    row << isf_line.try(:hts_code).to_s.hts_format
+    row << tariff_line.try(:hts_code).to_s.hts_format
+    row << (isf_match ? "Y" : "N")
+    row << (hts_match ? "Y" : "N")
+    row << (coo_match ? "Y" : "N")
+    row << (po_match ? "Y" : "N")
+    row << (style_match ? "Y" : "N")
+
+    XlsMaker.add_body_row sheet, row_number, row, column_widths
+  end
+
+  def field isf_value, entry_value
+    isf_value.presence || entry_value
+  end
+
+  def matches line, tariff, isf
+    # basically, we're looking for a single line that as close as possible matches the invoice line / tariff...so what
+    # we'll do is evaluate all the lines to see how well each of the criteria matches...and if we find a line that 
+    # matches them all, we'll say it matches...if we don't fine one, then we'll return the best match
+    matches = []
+
+    isf.security_filing_lines.each do |isf_line|
+      match = IsfMatch.new isf_line
+
+      match.hts_match = isf_line.hts_code.strip.to_s[0..5] == tariff.hts_code.strip.to_s[0..5]
+      match.coo_match = isf_line.country_of_origin_code.to_s.strip.upcase == line.country_origin_code.to_s.strip.upcase
+      match.po_match = isf_line.po_number.to_s.strip.upcase == line.po_number.to_s.strip.upcase
+      match.style_match = isf_line.part_number.to_s.strip.upcase == line.part_number.to_s.strip.upcase
+
+      matches << match
+
+      # no use continuing to look for rows if all the criteria match
+      break if match.matches?
+    end
+
+    # Return the "most matched" result
+    matches.sort!
+
+    matches.first
+  end
+
+  def full_match? matches_hash
+    matches_hash.values.uniq.first == true
+  end
+
+  class IsfMatch 
+
+    attr_reader :isf_line
+    attr_accessor :hts_match, :coo_match, :po_match, :style_match
+
+    def initialize isf_line
+      @isf_line = isf_line
+    end
+
+    def <=> m
+      # Evaluate whatever match has the most matches as the 
+      v = m.match_count <=> match_count
+
+      if v == 0
+        v = m.isf_line.line_number <=> isf_line.line_number
+      end
+
+      v
+    end
+
+    def match_count
+      count = 0
+      count += 1 if hts_match
+      count += 1 if coo_match
+      count += 1 if po_match
+      count += 1 if style_match
+
+      count
+    end
+
+    def matches?
+      match_count == 4
+    end
+  end
+
+
+end; end; end; end
---
+++ spec/lib/custom_handler/ascena/ascena_entry_isf_mismatch_report_spec.rb
@@ -0,0 +1,161 @@
+describe OpenChain::CustomHandler::Ascena::AscenaEntryIsfMismatchReport do
+
+
+  describe "run_report" do
+    let (:importer) { Factory(:importer) }
+    let! (:entry) {
+      e = Factory(:entry, importer: importer, broker_reference: "REF", entry_number: "ENT", master_bills_of_lading: "MBOL", container_numbers: "CONT", transport_mode_code: "10", first_entry_sent_date: ActiveSupport::TimeZone["UTC"].parse("2017-03-01 12:00"))
+      i = e.commercial_invoices.create! invoice_number: "INV"
+      l = i.commercial_invoice_lines.create! po_number: "PO", part_number: "PART", country_origin_code: "CO"
+      t = l.commercial_invoice_tariffs.create! hts_code: "1234567890"
+
+      e
+    }
+
+    let! (:isf) {
+      sf = Factory(:security_filing, importer: importer, transaction_number: "TRANS", entry_reference_numbers: "REF", master_bill_of_lading: "MBOL")
+      sf.security_filing_lines.create! po_number: "PO", part_number: "PART", hts_code: "1234567890", country_of_origin_code: "CO"
+
+      sf
+    }
+
+    let (:tz) {
+      ActiveSupport::TimeZone["UTC"]
+    }
+
+    it "reports nothing if entry matches isf by reference number" do
+      file = subject.run_report importer, tz.parse("2017-03-01 07:00"), tz.parse("2017-03-01 12:30")
+      wb = XlsMaker.open_workbook file
+      sheet = wb.worksheet "Entry / ISF Match"
+      expect(sheet).not_to be_nil
+      expect(sheet.rows.length).to eq 1
+      expect(sheet.row(0)).to eq ["Transaction Number", "Master Bill", "Container Number", "Entry Number", "Country of Origin Code (ISF)", "Country of Origin Code (Entry)", "PO Number (ISF)", "PO Number (Entry)", "Part Number (ISF)", "Part Number (Entry)", "HTS Code (ISF)", "HTS Code (Entry)", "ISF Match", "HTS Match", "COO Match", "PO Match", "Style Match"]
+    end
+
+    it "reports nothing if entry matches isf by master bill" do
+      isf.update_attributes! entry_reference_numbers: "NO MATCH"
+
+      file = subject.run_report importer, tz.parse("2017-03-01 07:00"), tz.parse("2017-03-01 12:30")
+      wb = XlsMaker.open_workbook file
+      sheet = wb.worksheet "Entry / ISF Match"
+      expect(sheet.rows.length).to eq 1
+    end
+
+    it "reports if entry does not match to an isf" do
+      isf.update_attributes! entry_reference_numbers: "NO MATCH", master_bill_of_lading: "NO MATCH"
+
+      file = subject.run_report importer, tz.parse("2017-03-01 07:00"), tz.parse("2017-03-01 12:30")
+      wb = XlsMaker.open_workbook file
+      sheet = wb.worksheet "Entry / ISF Match"
+      expect(sheet.rows.length).to eq 2
+
+      expect(sheet.row(1)).to eq [nil, "MBOL", "CONT", "ENT", nil, nil, nil, nil, nil, nil, nil, nil, "N", "N", "N", "N", "N" ]
+    end
+
+    it "reports if entry does not match to country origin" do
+      isf.security_filing_lines.first.update_attributes! country_of_origin_code: "ISF"
+
+      file = subject.run_report importer, tz.parse("2017-03-01 07:00"), tz.parse("2017-03-01 12:30")
+      wb = XlsMaker.open_workbook file
+      sheet = wb.worksheet "Entry / ISF Match"
+      expect(sheet.rows.length).to eq 2
+
+      expect(sheet.row(1)).to eq ["TRANS", "MBOL", "CONT", "ENT", "ISF", "CO", "PO", "PO", "PART", "PART", "1234.56.7890", "1234.56.7890", "Y", "Y", "N", "Y", "Y"]
+    end
+
+    it "reports if entry does not match to PO" do
+      isf.security_filing_lines.first.update_attributes! po_number: "ISF"
+
+      file = subject.run_report importer, tz.parse("2017-03-01 07:00"), tz.parse("2017-03-01 12:30")
+      wb = XlsMaker.open_workbook file
+      sheet = wb.worksheet "Entry / ISF Match"
+      expect(sheet.rows.length).to eq 2
+
+      expect(sheet.row(1)).to eq ["TRANS", "MBOL", "CONT", "ENT", "CO", "CO", "ISF", "PO", "PART", "PART", "1234.56.7890", "1234.56.7890", "Y", "Y", "Y", "N", "Y"]
+    end
+
+    it "reports if entry does not match to Part" do
+      isf.security_filing_lines.first.update_attributes! part_number: "ISF"
+
+      file = subject.run_report importer, tz.parse("2017-03-01 07:00"), tz.parse("2017-03-01 12:30")
+      wb = XlsMaker.open_workbook file
+      sheet = wb.worksheet "Entry / ISF Match"
+      expect(sheet.rows.length).to eq 2
+
+      expect(sheet.row(1)).to eq ["TRANS", "MBOL", "CONT", "ENT", "CO", "CO", "PO", "PO", "ISF", "PART", "1234.56.7890", "1234.56.7890", "Y", "Y", "Y", "Y", "N"]
+    end
+
+    it "reports if entry does not match to HTS" do
+      isf.security_filing_lines.first.update_attributes! hts_code: "1234578906"
+
+      file = subject.run_report importer, tz.parse("2017-03-01 07:00"), tz.parse("2017-03-01 12:30")
+      wb = XlsMaker.open_workbook file
+      sheet = wb.worksheet "Entry / ISF Match"
+      expect(sheet.rows.length).to eq 2
+
+      expect(sheet.row(1)).to eq ["TRANS", "MBOL", "CONT", "ENT", "CO", "CO", "PO", "PO", "PART", "PART", "1234.57.8906", "1234.56.7890", "Y", "N", "Y", "Y", "Y"]
+    end
+
+    it "does not report if hts matches first 6 digits, but doesn't match the rest of the digits" do
+      isf.security_filing_lines.first.update_attributes! hts_code: "1234561234"
+
+      file = subject.run_report importer, tz.parse("2017-03-01 07:00"), tz.parse("2017-03-01 12:30")
+      wb = XlsMaker.open_workbook file
+      sheet = wb.worksheet "Entry / ISF Match"
+      expect(sheet.rows.length).to eq 1
+    end
+
+    it "does not report entries that are not non-Ocean transport modes" do
+      entry.update_attributes! transport_mode_code: "40"
+      isf.security_filing_lines.first.update_attributes! po_number: "ISF"
+
+      file = subject.run_report importer, tz.parse("2017-03-01 07:00"), tz.parse("2017-03-01 12:30")
+      wb = XlsMaker.open_workbook file
+      sheet = wb.worksheet "Entry / ISF Match"
+      expect(sheet.rows.length).to eq 1
+    end
+
+    it "does not report entries that are outside the time range" do
+      entry.update_attributes! first_entry_sent_date: tz.parse("2017-03-01 12:31")
+
+      file = subject.run_report importer, tz.parse("2017-03-01 07:00"), tz.parse("2017-03-01 12:30")
+      wb = XlsMaker.open_workbook file
+      sheet = wb.worksheet "Entry / ISF Match"
+      expect(sheet.rows.length).to eq 1
+    end
+  end
+
+
+  describe "run_schedulable" do
+
+    let (:tempfile) {
+      t = Tempfile.new ["file", ".txt"]
+      Attachment.add_original_filename_method t, "file.txt"
+      t
+    }
+
+    let! (:ascena) { Factory(:importer, alliance_customer_number: "ASCE")}
+
+    after (:each) { tempfile.close! }
+
+    it "runs and emails report over previous and next week" do
+      now = Time.zone.now
+
+      expected_start = now.in_time_zone("America/New_York").midnight - 7.days
+      expected_end = now.in_time_zone("America/New_York").midnight + 7.days
+
+      expect_any_instance_of(described_class).to receive(:run_report).with(ascena, expected_start, expected_end).and_return tempfile
+
+      Timecop.freeze(now) do 
+        described_class.run_schedulable({"email" => ["me@there.com"]})
+      end
+
+      expect(ActionMailer::Base.deliveries.size).to eq 1
+
+      m = ActionMailer::Base.deliveries.first
+      expect(m.to).to eq ["me@there.com"]
+      expect(m.subject).to eq "Ascena Entry/ISF Mismatch #{expected_start.to_date} - #{expected_end.to_date}"
+      expect(m.body).to include "The Entry/ISF Mismatch report for Entry Summary Sent Dates between #{expected_start.to_date} - #{expected_end.to_date} is attached."
+    end
+  end
+end
