494d87f5c81563b8704c957578f47061534cae49
jhulford <jhulford@vandegriftinc.com>
2017-05-26 09:09:17 -0500

    Add locking / lookups. (#2018)

--- lib/open_chain/custom_handler/eddie_bauer/eddie_bauer_po_parser.rb
+++ lib/open_chain/custom_handler/eddie_bauer/eddie_bauer_po_parser.rb
@@ -35,13 +35,14 @@ module OpenChain; module CustomHandler; module EddieBauer; class EddieBauerPoPar
 
   private
   def process_lines lines
-    Order.transaction do
-      order = find_or_create_order lines.first
+    products = find_or_create_products lines
+
+    find_or_create_order(lines.first) do |order|
       order.order_lines.destroy_all
       lines.each_with_index do |ln,i|
         order.order_lines.build(
           line_number:i+1,
-          product:find_or_create_product(ln),
+          product:products[product_style(ln)],
           quantity:BigDecimal(ln[323,7])
         )
       end
@@ -49,16 +50,37 @@ module OpenChain; module CustomHandler; module EddieBauer; class EddieBauerPoPar
     end
   end
 
-  def find_or_create_product line
-    Product.where(importer_id:@eddie.id,unique_identifier:"EDDIE-#{line[228,3]}-#{line[231,4]}").first_or_create!
+  def find_or_create_products lines
+    products = {}
+    lines.each do |line|
+      style = product_style(line)
+      unique_identifier = "EDDIE-#{style}"
+
+      Lock.acquire("Product-#{unique_identifier}") do 
+        products[style] = Product.where(importer_id:@eddie.id, unique_identifier:unique_identifier).first_or_create!
+      end
+    end
+
+    products
+  end
+
+  def product_style line
+    "#{line[228,3]}-#{line[231,4]}"
   end
 
   def find_or_create_order line
+    order = nil
     clean_order_number = line[0,14].sub('-','')
-    importer = clean_order_number.ends_with?('0002') || clean_order_number.ends_with?('0004') ? @ebcc : @eddie
-    ord = Order.where(importer_id:importer.id,order_number:"EDDIE-#{clean_order_number}").first_or_create!
-    ord.customer_order_number = clean_order_number
-    ord.vendor = Company.where(system_code:"EDDIE-#{line[301,8]}").first_or_create!(name:line[263,35].strip)
-    return ord
+    order_number = "EDDIE-#{clean_order_number}"
+
+    Lock.acquire("Order-#{order_number}") do 
+      importer = clean_order_number.ends_with?('0002') || clean_order_number.ends_with?('0004') ? @ebcc : @eddie
+      order = Order.where(importer_id:importer.id,order_number:order_number).first_or_create!
+      order.customer_order_number = clean_order_number
+      order.vendor = Company.where(system_code:"EDDIE-#{line[301,8]}").first_or_create!(name:line[263,35].strip)
+      yield order
+    end
+
+    order
   end
 end; end; end; end
