6f6e7bcce66d271987d341713c00179eadfcb747
Jeremy Hulford <jhulford@vandegriftinc.com>
2017-07-12 09:59:19 -0500

    Update layout of Schedule B file.

--- app/models/official_schedule_b_code.rb
+++ app/models/official_schedule_b_code.rb
@@ -3,20 +3,17 @@ class OfficialScheduleBCode < ActiveRecord::Base
   #clears current schedule b list and reloads it from census file
   def self.load_from_census_file file_path
     OfficialScheduleBCode.transaction do 
-      OfficialScheduleBCode.destroy_all
-      IO.foreach(file_path) {|line|
+      OfficialScheduleBCode.delete_all
+      CSV.foreach(file_path) {|row|
         data_hash = {
-          :hts_code => line[0,10].strip,
-          :short_description => line[14,51].strip,
-          :long_description => line[69,150].strip,
-          :quantity_1 => line[224,3].strip,
-          :quantity_2 => line[232,3].strip,
-          :sitc_code => line[240,5].strip,
-          :end_use_classification => line[250,5].strip,
-          :usda_code => line[260,1].strip,
-          :naics_classification => line[265,6].strip,
-          :hitech_classification => line[276,2].strip
+          :hts_code => row[0].to_s.strip,
+          :short_description => row[1].to_s.strip,
+          :long_description => row[1].to_s.strip,
+          :quantity_1 => row[2].to_s.strip,
+          :quantity_2 => row[3].to_s.strip
         }
+        next if data_hash[:hts_code].to_s.blank? || data_hash[:hts_code].to_i <= 0
+
         osb = OfficialScheduleBCode.find_by_hts_code(data_hash[:hts_code])
         osb = OfficialScheduleBCode.new unless osb
         osb.update_attributes data_hash
