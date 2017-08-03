cb527e57d38db1239b8545445a3644ce42d2a83a
Jeremy Hulford <jhulford@vandegriftinc.com>
2017-08-03 14:31:06 -0500

    Allow for passing uuid.

--- lib/open_chain/test_instance_manager.rb
+++ lib/open_chain/test_instance_manager.rb
@@ -2,12 +2,12 @@ module OpenChain
   class TestInstanceManager
     # call this method to prep the test instance
     # NEVER CALL THIS IN PRODUCTION, IT CLEARS SCHEDULES AND EDITS USERS
-    def self.prep_test_instance new_request_host
+    def self.prep_test_instance new_request_host, uuid
       m = self.new
-      m.update_master_setup! new_request_host
+      m.update_master_setup! new_request_host, uuid
       m.clear_schedulable_jobs!
       m.clear_scheduled_reports!
-      m.update_users!
+      m.update_users! new_request_host
     end
 
     def clear_schedulable_jobs!
@@ -18,16 +18,20 @@ module OpenChain
       SearchSchedule.scoped.destroy_all
     end
 
-    def update_master_setup! req_host
-      uuid = UUIDTools::UUID.timestamp_create.to_s
+    def update_master_setup! req_host, uuid
       # Load the master setup from the database (NOT from memcache)
       # This is because other test instances may be running against a previous database version -
       # since this is pretty much the first step to run when deploying a new test database.
       ms = MasterSetup.first
       ms.system_code = req_host.split(".")[0] # system code should equal the machine name of the system (.ie www from www.vfitrack.net)
-      # Use the request host name as the basis for the UUID.  This way we can just keep using the same uuid for progressive iterations
-      # of the same test system and not end up creating different S3 buckets for everything.
-      ms.uuid = UUIDTools::UUID.md5_create(UUIDTools::UUID_DNS_NAMESPACE, req_host).to_s
+      if uuid.blank?
+        # Use the request host name as the basis for the UUID.  This way we can just keep using the same uuid for progressive iterations
+        # of the same test system and not end up creating different S3 buckets for everything.
+        ms.uuid = UUIDTools::UUID.md5_create(UUIDTools::UUID_DNS_NAMESPACE, req_host).to_s
+      else
+        ms.uuid = uuid
+      end
+      
       ms.request_host = req_host
       ms.ftp_polling_active = false
       ms.custom_features = ''
@@ -35,8 +39,8 @@ module OpenChain
       ms.save!
     end
 
-    def update_users!
-      User.scoped.update_all(tariff_subscribed:false)
+    def update_users! request_host
+      User.scoped.update_all(tariff_subscribed:false, host_with_port: request_host)
       EventSubscription.delete_all
     end
   end
--- spec/lib/test_instance_manager_spec.rb
+++ spec/lib/test_instance_manager_spec.rb
@@ -3,11 +3,11 @@ require 'spec_helper'
 describe OpenChain::TestInstanceManager do
   describe "prep_test_instance" do
     it "should perform prep tasks" do
-      expect_any_instance_of(described_class).to receive(:update_master_setup!).with('req')
+      expect_any_instance_of(described_class).to receive(:update_master_setup!).with('req', "uuid")
       expect_any_instance_of(described_class).to receive(:clear_schedulable_jobs!)
       expect_any_instance_of(described_class).to receive(:clear_scheduled_reports!)
-      expect_any_instance_of(described_class).to receive(:update_users!)
-      described_class.prep_test_instance 'req'
+      expect_any_instance_of(described_class).to receive(:update_users!).with('req')
+      described_class.prep_test_instance 'req', "uuid"
     end
   end
   describe "clear_schedulable_jobs" do
@@ -25,32 +25,42 @@ describe OpenChain::TestInstanceManager do
     end
   end
   describe "update_master_setup" do
-    before :each do
+    before :each do 
       MasterSetup.get.update_attributes(system_code:'oldcode',uuid:'olduuid',ftp_polling_active:true,custom_features:'cf',request_host:'oldhost')
-      described_class.new.update_master_setup! 'new.request.host'
     end
-    it "should change system code" do
-      expect(MasterSetup.get.system_code).to eq('new') 
+    context "without uuid" do
+      before :each do      
+        described_class.new.update_master_setup! 'new.request.host', nil
+      end
+      it "should change system code" do
+        expect(MasterSetup.get.system_code).to eq('new') 
+      end
+      it "should change UUID" do
+        expect(MasterSetup.get.uuid).to eq UUIDTools::UUID.md5_create(UUIDTools::UUID_DNS_NAMESPACE, 'new.request.host').to_s
+      end
+      it "should disable FTP polling" do
+        expect(MasterSetup.get).not_to be_ftp_polling_active
+      end
+      it "should clear custom features" do
+        expect(MasterSetup.get.custom_features).to be_blank
+      end
+      it "should set request host" do
+        expect(MasterSetup.get.request_host).to eq('new.request.host')
+      end
     end
-    it "should change UUID" do
-      expect(MasterSetup.get.uuid.size).to eq(36)
-    end
-    it "should disable FTP polling" do
-      expect(MasterSetup.get).not_to be_ftp_polling_active
-    end
-    it "should clear custom features" do
-      expect(MasterSetup.get.custom_features).to be_blank
-    end
-    it "should set request host" do
-      expect(MasterSetup.get.request_host).to eq('new.request.host')
+
+    it "reuses uuid if given" do
+      described_class.new.update_master_setup! 'new.request.host', "uuid"
+      expect(MasterSetup.get.uuid).to eq "uuid"
     end
   end
   describe "update_users" do
     it "should clear tariff_subscribed flags" do
       u = Factory(:user,tariff_subscribed:true)
-      described_class.new.update_users!
+      described_class.new.update_users! "localhost"
       u.reload
       expect(u).not_to be_tariff_subscribed
+      expect(u.host_with_port).to eq "localhost"
     end
   end
 end
