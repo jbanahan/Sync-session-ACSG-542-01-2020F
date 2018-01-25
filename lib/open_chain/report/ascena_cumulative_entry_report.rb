2c7dfdd6f5b92780041b960e04ef0ea18979ff5e
Jay Davis <planarian@users.noreply.github.com>
2018-01-25 10:53:53 -0500

    Add Ascena Cumulative Entry Report (#2254)

---
+++ lib/open_chain/report/ascena_cumulative_entry_report.rb
@@ -0,0 +1,91 @@
+require 'open_chain/report/report_helper'
+require 'open_chain/fiscal_calendar_scheduling_support'
+
+module OpenChain; module Report; class AscenaCumulativeEntryReport
+  include OpenChain::Report::ReportHelper
+  extend OpenChain::FiscalCalendarSchedulingSupport
+
+  CUSTOMER_NUMBER = "ASCE"
+
+  def self.run_schedulable settings={}
+    settings['company'] = 'ASCENA'
+    run_if_configured(settings) do |fiscal_month, fiscal_date|
+      self.new.run(settings['email'], fiscal_month.back(1))
+    end
+  end
+
+  def run email, fiscal_month
+    wb = create_workbook(fiscal_month)
+    send_email(email, wb, fiscal_month.fiscal_descriptor)
+  end
+
+  def create_workbook fiscal_month
+    fm_num, fy = fiscal_month.month_number, fiscal_month.year
+    start_date = fiscal_month.start_date.strftime("%Y-%m-%d")
+    end_date = fiscal_month.end_date.strftime("%Y-%m-%d")
+    
+    wb = XlsMaker.new_workbook
+    add_main_sheet wb, fm_num, fy
+    add_isf_sheet wb, start_date, end_date
+    wb
+  end
+
+  def add_main_sheet wb, fiscal_month, fiscal_year
+    XlsMaker.create_sheet wb, "Main"
+    table_from_query wb.worksheets.last, main_query(fiscal_month, fiscal_year)
+  end
+
+  def add_isf_sheet wb, start_date, end_date
+    XlsMaker.create_sheet wb, "ISF"
+    table_from_query wb.worksheets.last, isf_query(start_date, end_date)
+  end
+
+  def send_email addresses, workbook, fiscal_descriptor
+    title = "Ascena Cumulative Entry Report for #{fiscal_descriptor}"
+    body = "Attached is the #{title}."
+    workbook_to_tempfile(workbook, "report", file_name: "#{title}.xls") do |t|
+      workbook.write t
+      t.flush
+      OpenMailer.send_simple_html(addresses, title, "Attached is the #{title}", t).deliver!
+    end
+  end
+
+  def us
+    @country ||= Country.where(iso_code: "US").first
+    raise "United States not found." unless @country
+    @country
+  end
+
+  def main_query fiscal_month, fiscal_year
+    <<-SQL
+      SELECT COUNT(entry_number) AS 'Total Entries', 
+             SUM(IF(transport_mode_code IN (40, 41),1,0)) AS 'Air Entries',  
+             SUM(IF(transport_mode_code IN (10, 11),1,0)) AS 'Ocean Entries', 
+             SUM((SELECT COUNT(*) 
+                  FROM commercial_invoices 
+                    INNER JOIN commercial_invoice_lines ON commercial_invoices.id = commercial_invoice_lines.commercial_invoice_id 
+                  WHERE commercial_invoices.entry_id = entries.id)) AS 'Invoice Line Count',
+             SUM((SELECT COUNT(*) 
+                  FROM commercial_invoices 
+                  WHERE commercial_invoices.entry_id = entries.id)) AS 'Invoice Count',
+             SUM(IF(transport_mode_code IN (40,41),gross_weight,0)) AS 'Air Weight',
+             SUM(IF(transport_mode_code IN (10,11),gross_weight,0)) AS 'Ocean Weight',
+             SUM(entries.mpf) AS 'MPF',
+             SUM(entries.entered_value) AS 'Entered Value',
+             SUM(entries.total_duty) AS 'Total Duty'
+      FROM entries 
+      WHERE import_country_id = #{us.id} AND customer_number = '#{CUSTOMER_NUMBER}' 
+            AND fiscal_month = #{fiscal_month} AND fiscal_year = #{fiscal_year}
+    SQL
+  end
+
+  def isf_query start, finish
+    <<-SQL
+      SELECT COUNT(*) AS "Count"
+      FROM security_filings
+      WHERE security_filings.importer_account_code = '#{CUSTOMER_NUMBER}' AND first_sent_date between '#{start}' AND '#{finish}'
+    SQL
+  end
+
+
+end; end; end;
---
+++ spec/lib/report/ascena_cumulative_entry_report_spec.rb
@@ -0,0 +1,112 @@
+require 'spec_helper'
+
+describe OpenChain::Report::AscenaCumulativeEntryReport do
+  let(:parser) { described_class.new }
+  let(:us) { Factory :country, iso_code: "US" }
+
+  describe "run_schedulable" do
+    it "emails report" do
+      ascena = Factory(:company, system_code: "ASCENA")
+      this_month = Factory :fiscal_month, company: ascena, month_number: 6, year: 2018, start_date: Date.new(2017,12,31), end_date: Date.new(2018,1,27)
+      last_month = Factory :fiscal_month, company: ascena, month_number: 5, year: 2018, start_date: Date.new(2017,11,26), end_date: Date.new(2017,12,30)
+      us
+      
+      expect_any_instance_of(described_class).to receive(:main_query).with(5, 2018).and_call_original
+      expect_any_instance_of(described_class).to receive(:isf_query).with("2017-11-26", "2017-12-30").and_call_original
+
+      Timecop.freeze(DateTime.new(2017,12,31,12,0)) do
+        described_class.run_schedulable('email'=>'st-hubbins@hellhole.co.uk')
+      end
+
+      mail = ActionMailer::Base.deliveries.pop
+      expect(mail.to).to eq ['st-hubbins@hellhole.co.uk']
+      expect(mail.subject).to eq "Ascena Cumulative Entry Report for 2018-05"
+      expect(mail.body.raw_source).to match(/Attached is the Ascena Cumulative Entry Report for 2018-05/)
+      expect(mail.attachments.length).to eq 1
+      att = mail.attachments["Ascena Cumulative Entry Report for 2018-05.xls"]
+      expect(att).to_not be_nil
+      Tempfile.open('temp') do |t|
+        t.binmode
+        t << att.read
+        t.flush
+        wb = XlsMaker.open_workbook t.path
+        sheet1 = wb.worksheet(0)
+        expect(sheet1.row(0)[0]).to eq "Total Entries"
+        sheet2 = wb.worksheet(1)
+        expect(sheet2.row(0)[0]).to eq "Count"
+      end
+    end
+  end
+
+  describe "main_query" do
+    let(:ca) { Factory :country, iso_code: "CA" }
+    let!(:e1) { Factory :entry, customer_number: "ASCE", import_country: us, entry_number: "12345", transport_mode_code: 40, gross_weight: 2, mpf: 3, total_duty: 4, entered_value: 17, fiscal_month: 1, fiscal_year: 2018 }
+    let!(:e2) { Factory :entry, customer_number: "ASCE", import_country: us, entry_number: "12346", transport_mode_code: 41, gross_weight: 5, mpf: 6, total_duty: 7, entered_value: 18, fiscal_month: 1, fiscal_year: 2018 }
+    let!(:e3) { Factory :entry, customer_number: "ASCE", import_country: us, entry_number: "12347", transport_mode_code: 10, gross_weight: 8, mpf: 9, total_duty: 10, entered_value: 19, fiscal_month: 1, fiscal_year: 2018 }
+    let!(:e4) { Factory :entry, customer_number: "ASCE", import_country: us, entry_number: "12348", transport_mode_code: 11, gross_weight: 11, mpf: 12, total_duty: 13, entered_value: 20, fiscal_month: 1, fiscal_year: 2018 }
+    let!(:e5) { Factory :entry, customer_number: "ASCE", import_country: us, entry_number: "12349", transport_mode_code: 99, gross_weight: 14, mpf: 15, total_duty: 16, entered_value: 21, fiscal_month: 1, fiscal_year: 2018 }
+    let!(:ci1) { Factory :commercial_invoice, entry: e1 }
+    let!(:ci2) { Factory :commercial_invoice, entry: e2 }
+    let!(:cil1) { Factory :commercial_invoice_line, commercial_invoice: ci1 }
+    let!(:cil2) { Factory :commercial_invoice_line, commercial_invoice: ci1 }
+    let!(:cil3) { Factory :commercial_invoice_line, commercial_invoice: ci2 }
+    
+    it "produces expected results" do
+      results = ActiveRecord::Base.connection.execute parser.main_query(1,2018)
+      expect(results.fields).to eq ["Total Entries", "Air Entries", "Ocean Entries", "Invoice Line Count", "Invoice Count", "Air Weight", "Ocean Weight", "MPF", "Entered Value", "Total Duty"]
+      expect(results.count).to eq 1
+      r = results.first
+      expect(r).to eq [5, 2, 2, 3, 2, 7, 19, 45, 95, 50]
+    end
+
+    it "skips non-US entries" do
+      e5.update_attributes! import_country: ca
+      results = ActiveRecord::Base.connection.execute parser.main_query(1,2018)
+      expect(results.first[0]).to eq 4
+    end
+
+    it "skips non-Ascena entries" do
+      e5.update_attributes! customer_number: "ACME"
+      results = ActiveRecord::Base.connection.execute parser.main_query(1,2018)
+      expect(results.first[0]).to eq 4
+    end
+
+    it "skips entries with wrong fiscal month" do
+      e5.update_attributes! fiscal_month: 2
+      results = ActiveRecord::Base.connection.execute parser.main_query(1,2018)
+      expect(results.first[0]).to eq 4
+    end
+
+    it "skips entries with wrong fiscal year" do
+      e5.update_attributes! fiscal_year: 2017
+      results = ActiveRecord::Base.connection.execute parser.main_query(1,2018)
+      expect(results.first[0]).to eq 4
+    end
+  end
+
+  describe "isf_query" do
+    let!(:sf) { Factory :security_filing, importer_account_code: "ASCE", first_sent_date: Date.new(2018,1,15) }
+    let!(:sf2) { Factory :security_filing, importer_account_code: "ASCE", first_sent_date: Date.new(2018,1,25) }
+    
+    it "produces expected results" do
+      results = ActiveRecord::Base.connection.execute parser.isf_query("2018-01-01","2018-01-31")
+      expect(results.fields).to eq ["Count"]
+      expect(results.count).to eq 1
+      expect(results.first).to eq [2]
+    end
+
+    it "skips filings with wrong customer number" do
+      sf.update_attributes! importer_account_code: "ACME"
+      results = ActiveRecord::Base.connection.execute parser.isf_query("2018-01-01","2018-01-31")
+      expect(results.first).to eq [1]
+    end
+
+    it "skips filings with first_sent_date outside of range" do
+      sf.update_attributes! first_sent_date: Date.new(2018,2,1)
+      results = ActiveRecord::Base.connection.execute parser.isf_query("2018-01-01","2018-01-31")
+      expect(results.first).to eq [1]
+    end
+  end
+
+
+end
