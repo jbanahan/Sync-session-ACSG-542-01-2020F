367f49b070ba63e8dae35bfd603abcb4825aaa6e
jhulford <jhulford@vandegriftinc.com>
2018-05-23 16:00:56 -0500

    Add locking to csm parser (#2404)
    * Fix locking, update cdef handling.
    
    * Adjust indentation style.

--- lib/open_chain/custom_handler/polo_csm_sync_handler.rb
+++ lib/open_chain/custom_handler/polo_csm_sync_handler.rb
@@ -1,171 +1,172 @@
 require 'open_chain/xl_client'
-module OpenChain
-  module CustomHandler
-    # Updates CSM number for existing styles based on spreadsheet sent from team in Italy
-    class PoloCsmSyncHandler
+require 'open_chain/custom_handler/polo/polo_custom_definition_support'
 
-      def self.can_view? user
-        MasterSetup.get.custom_feature?("CSM Sync") && user.edit_products?
+# Updates CSM number for existing styles based on spreadsheet sent from team in Italy
+module OpenChain; module CustomHandler; class PoloCsmSyncHandler
+  include OpenChain::CustomHandler::Polo::PoloCustomDefinitionSupport
+
+  def self.can_view? user
+    MasterSetup.get.custom_feature?("CSM Sync") && user.edit_products?
+  end
+
+  def can_view? user
+    self.class.can_view? user
+  end
+
+  def initialize(custom_file,file_received_at=0.seconds.ago)
+    @custom_file = custom_file
+    @received_date = file_received_at 
+  end
+
+  def process user, first_row = 1
+    system_errors = false
+    current_style = ''
+    csm_errors = []
+    begin
+      xlc = XLClient.new(@custom_file.attached.path)
+      xlc.raise_errors = true
+      last_row = xlc.last_row_number(0)
+      style_map = {}
+      dept_map = {}
+      season_map = {}
+      (first_row..last_row).each do |n|
+        row_hash = xlc.get_row_as_column_hash 0, n
+        us_style_cell = row_hash[8]
+        us_style = (us_style_cell.blank? || us_style_cell['value'].blank?) ? nil : us_style_cell['value']
+        csm = get_csm_number row_hash
+        next if csm.blank? || us_style.blank?
+        raise(CsmError, "File failed: CSM Number at row #{n + 1} was not 18 digits \"#{csm}\"") unless csm.size == 18
+
+        style_map[us_style] ||= Set.new
+        style_map[us_style] << csm
+
+        department = row_hash[13]['value']
+        dept_map[us_style] = department
+
+        season_map[us_style] ||= Set.new
+        season_map[us_style] << row_hash[0]['value']
       end
-
-      def can_view? user
-        self.class.can_view? user
-      end
-
-      def initialize(custom_file,file_received_at=0.seconds.ago)
-        @custom_file = custom_file
-        @csm_cd = CustomDefinition.find_or_create_by_label("CSM Number",:module_type=>'Product',:data_type=>'text')
-        @dept_cd = CustomDefinition.find_or_create_by_label("CSM Department",:module_type=>'Product',:data_type=>'text')
-        @season_cd = CustomDefinition.find_or_create_by_label('CSM Season',:module_type=>'Product',:data_type=>'text')
-        @first_csm_date_cd = CustomDefinition.find_or_create_by_label('CSM Received Date (First)',:module_type=>'Product',:data_type=>'date')  
-        @last_csm_date_cd = CustomDefinition.find_or_create_by_label('CSM Received Date (Last)',:module_type=>'Product',:data_type=>'date')
-        [@csm_cd,@dept_cd,@season_cd,@first_csm_date_cd,@last_csm_date_cd].each do |cd|
-          fvr = FieldValidatorRule.where(custom_definition_id:cd.id,model_field_uid:"*cf_#{cd.id}",module_type:cd.module_type).first_or_create!(read_only:true)
-          fvr.update_attributes(read_only:true) unless fvr.read_only?
-        end
-        @received_date = file_received_at 
-      end
-
-      def process user, first_row = 1
-        system_errors = false
-        current_style = ''
-        csm_errors = []
+      style_map.each do |us_style,csm_set|
+        current_style = us_style
         begin
-          xlc = XLClient.new(@custom_file.attached.path)
-          xlc.raise_errors = true
-          last_row = xlc.last_row_number(0)
-          style_map = {}
-          dept_map = {}
-          season_map = {}
-          (first_row..last_row).each do |n|
-            row_hash = xlc.get_row_as_column_hash 0, n
-            us_style_cell = row_hash[8]
-            us_style = (us_style_cell.blank? || us_style_cell['value'].blank?) ? nil : us_style_cell['value']
-            csm = get_csm_number row_hash
-            next if csm.blank? || us_style.blank?
-            raise(CsmError, "File failed: CSM Number at row #{n + 1} was not 18 digits \"#{csm}\"") unless csm.size == 18
-
-            style_map[us_style] ||= Set.new
-            style_map[us_style] << csm
-
-            department = row_hash[13]['value']
-            dept_map[us_style] = department
-
-            season_map[us_style] ||= Set.new
-            season_map[us_style] << row_hash[0]['value']
+          p = nil
+          Lock.acquire("Product-#{us_style}") do 
+            p = Product.where(:unique_identifier=>us_style).first_or_create!
           end
-          style_map.each do |us_style,csm_set|
-            current_style = us_style
-            begin
-              Product.transaction do 
-                p = Product.where(:unique_identifier=>us_style).first_or_create!
-                raise(CsmError, "File failed: #{user.full_name} can't edit product #{p.unique_identifier}") unless p.can_edit?(user)
-                Lock.with_lock_retry(p) do
-                  p.update_custom_value! @csm_cd, csm_set.to_a.join("\n")
-                  p.update_custom_value! @dept_cd, dept_map[us_style]
-                  update_season p, season_map[us_style]
-                  update_first_csm_date p
-                  update_last_csm_date p
 
-                  OpenChain::FieldLogicValidator.validate!(p) 
-                  p.update_attributes! last_updated_by: user
-                  p.create_snapshot user
-                end
-                
-              end
-            rescue OpenChain::ValidationLogicError => e
-              # Prefix the errors with the style so the user knows which one needs to be fixed
-              if e.base_object.errors[:base]
-                e.base_object.errors[:base].each do |err|
-                  csm_errors << "Style: #{current_style} - #{err}"
-                end
-              end
+          raise(CsmError, "File failed: #{user.full_name} can't edit product #{p.unique_identifier}") unless p.can_edit?(user)
+
+          Lock.db_lock(p) do 
+            p.update_custom_value! cdefs[:csm_numbers], csm_set.to_a.join("\n")
+            p.update_custom_value! cdefs[:csm_department], dept_map[us_style]
+            update_season p, season_map[us_style]
+            update_first_csm_date p
+            update_last_csm_date p
+
+            OpenChain::FieldLogicValidator.validate!(p) 
+            p.update_attributes! last_updated_by: user
+            p.create_snapshot user
+          end
+        rescue OpenChain::ValidationLogicError => e
+          # Prefix the errors with the style so the user knows which one needs to be fixed
+          if e.base_object.errors[:base]
+            e.base_object.errors[:base].each do |err|
+              csm_errors << "Style: #{current_style} - #{err}"
             end
           end
-        rescue CsmError => e
-          # Since RL wants us to detect field validation errors, we're going to route those (above) and other logic issues
-          # with the file into user messages that they will have to deal with themselves so we don't have to foward emails any longer.
-          csm_errors << e.message
-        rescue
-          system_errors = true
-          $!.log_me ["Custom File ID: #{@custom_file.id}","Style: #{current_style}"]
-        end
-         
-        subject = "CSM Sync Complete"
-        if system_errors || csm_errors.size > 0
-          subject += " with Errors"
-        end
-
-        body = "<p>Your CSM Sync job has completed.  You can download the updated file <a href='/custom_features/csm_sync'>here</a>.</p>"
-        if system_errors
-          body += "<p>Support has received notification regarding system errors in your file and will be researching them.</p>"
-        end
-
-        if csm_errors.size > 0
-          user_errors_message = ""
-          csm_errors.each do |e|
-            user_errors_message += "<li>#{e}</li>"
-          end
-          user_errors_message = "<p>The following CSM data errors were encountered:<ul>#{user_errors_message}</ul></p>"
-
-          body += user_errors_message
-        end
-
-        user.messages.create(subject: subject, body: body)
-      end
-
-      def self.process_from_s3 bucket, path, options = {}
-        OpenChain::S3.download_to_tempfile(bucket, path, original_filename: options[:original_filename]) do |tmp|
-          cf = CustomFile.new(:file_type=>'OpenChain::CustomHandler::PoloCsmSyncHandler',:uploaded_by=>User.find_by_username('rbjork'))
-          cf.attached = tmp
-          cf.save!
-          cf.process(cf.uploaded_by)
         end
       end
+    rescue CsmError => e
+      # Since RL wants us to detect field validation errors, we're going to route those (above) and other logic issues
+      # with the file into user messages that they will have to deal with themselves so we don't have to foward emails any longer.
+      csm_errors << e.message
+    rescue
+      system_errors = true
+      $!.log_me ["Custom File ID: #{@custom_file.id}","Style: #{current_style}"]
+    end
+     
+    subject = "CSM Sync Complete"
+    if system_errors || csm_errors.size > 0
+      subject += " with Errors"
+    end
 
-      private
-      def get_csm_number row_hash
-        r = ""
-        (2..5).each do |i|
-          cell_hash = row_hash[i]
-          return nil unless cell_hash && cell_hash['value']
-          r << string_val(cell_hash['value'])
-        end
-        r
-      end
-      def string_val val 
-        r = val
-        return r if r.nil?
-        r = r[0,r.size-2] if r.end_with? '.0' #fix numerics
-        r
-      end
-      def update_season product, season_set
-        cv = product.get_custom_value @season_cd  
-        current_vals = cv.value.blank? ? "" : cv.value
-        current_vals.split("\n").each {|v| season_set << v} unless current_vals.blank?
-        new_value = season_set.to_a.sort.join("\n")
-        if new_value!=cv.value
-          cv.value = new_value 
-          cv.save!
-        end
-      end
-      def update_first_csm_date product
-        cv = product.get_custom_value @first_csm_date_cd
-        if cv.value.blank?
-          cv.value = @received_date 
-          cv.save!
-        end
-      end
-      def update_last_csm_date product
-        cv = product.get_custom_value @last_csm_date_cd
-        if cv.value.blank? || cv.value.to_datetime.to_i < @received_date.to_i
-          cv.value = @received_date 
-          cv.save!
-        end
-      end
+    body = "<p>Your CSM Sync job has completed.  You can download the updated file <a href='/custom_features/csm_sync'>here</a>.</p>"
+    if system_errors
+      body += "<p>Support has received notification regarding system errors in your file and will be researching them.</p>"
+    end
 
-      class CsmError < StandardError; end
+    if csm_errors.size > 0
+      user_errors_message = ""
+      csm_errors.each do |e|
+        user_errors_message += "<li>#{e}</li>"
+      end
+      user_errors_message = "<p>The following CSM data errors were encountered:<ul>#{user_errors_message}</ul></p>"
 
+      body += user_errors_message
+    end
+
+    user.messages.create(subject: subject, body: body)
+  end
+
+  def self.process_from_s3 bucket, path, options = {}
+    OpenChain::S3.download_to_tempfile(bucket, path, original_filename: options[:original_filename]) do |tmp|
+      cf = CustomFile.new(:file_type=>'OpenChain::CustomHandler::PoloCsmSyncHandler',:uploaded_by=>User.find_by_username('rbjork'))
+      cf.attached = tmp
+      cf.save!
+      cf.process(cf.uploaded_by)
     end
   end
-end
+
+  class CsmError < StandardError; end
+
+  private
+  
+    def get_csm_number row_hash
+      r = ""
+      (2..5).each do |i|
+        cell_hash = row_hash[i]
+        return nil unless cell_hash && cell_hash['value']
+        r << string_val(cell_hash['value'])
+      end
+      r
+    end
+    
+    def string_val val 
+      r = val
+      return r if r.nil?
+      r = r[0,r.size-2] if r.end_with? '.0' #fix numerics
+      r
+    end
+
+    def update_season product, season_set
+      cv = product.get_custom_value cdefs[:csm_season]
+      current_vals = cv.value.blank? ? "" : cv.value
+      current_vals.split("\n").each {|v| season_set << v} unless current_vals.blank?
+      new_value = season_set.to_a.sort.join("\n")
+      if new_value!=cv.value
+        cv.value = new_value 
+        cv.save!
+      end
+    end
+    
+    def update_first_csm_date product
+      cv = product.get_custom_value cdefs[:csm_received_date_first]
+      if cv.value.blank?
+        cv.value = @received_date 
+        cv.save!
+      end
+    end
+    
+    def update_last_csm_date product
+      cv = product.get_custom_value cdefs[:csm_received_date_last]
+      if cv.value.blank? || cv.value.to_datetime.to_i < @received_date.to_i
+        cv.value = @received_date 
+        cv.save!
+      end
+    end
+
+    def cdefs 
+      @cdefs ||= self.class.prep_custom_definitions([:csm_numbers, :csm_department, :csm_season, :csm_received_date_first, :csm_received_date_last])
+    end
+
+end; end; end
--- spec/lib/custom_handler/polo_csm_sync_hander_spec.rb
+++ spec/lib/custom_handler/polo_csm_sync_hander_spec.rb
@@ -2,8 +2,19 @@ require 'spec_helper'
 
 describe OpenChain::CustomHandler::PoloCsmSyncHandler do
 
+  before :all do
+    described_class.new(nil).send(:cdefs)
+  end
+
+  after :all do 
+    CustomDefinition.destroy_all
+  end
+
 
   describe "process" do
+
+    let (:cdefs) { @h.send(:cdefs) }
+
     before :each do
       @xlc = double('xl_client')
       allow(@xlc).to receive(:raise_errors=)
@@ -13,11 +24,6 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
       allow(@cf).to receive(:attached).and_return(@att)
       allow(@cf).to receive(:update_attributes)
       expect(OpenChain::XLClient).to receive(:new).with('/path/to').and_return(@xlc)
-      @csm = Factory(:custom_definition,:module_type=>'Product',:label=>"CSM Number",:data_type=>'text')
-      @dept = Factory(:custom_definition,:module_type=>'Product',:label=>"CSM Department",:data_type=>'text')
-      @season = Factory(:custom_definition,:module_type=>'Product',:label=>"CSM Season",:data_type=>'text')
-      @first_csm_date_cd = Factory(:custom_definition,:module_type=>"Product",:data_type=>'date',:label=>"CSM Received Date (First)")
-      @last_csm_date_cd = Factory(:custom_definition,:module_type=>"Product",:data_type=>'date',:label=>"CSM Received Date (Last)")
       @h = described_class.new @cf
       allow_any_instance_of(Product).to receive(:can_edit?).and_return(true)
       @snapshot_created = false
@@ -29,9 +35,10 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
     end
 
     context "csm_season" do
+
       it "should set CSM Season for existing products" do
         p = Factory(:product)
-        p.update_custom_value! @season, 'someval'
+        p.update_custom_value! cdefs[:csm_season], 'someval'
         expect(@xlc).to receive(:last_row_number).and_return(1)
         expect(@xlc).to receive(:get_row_as_column_hash).with(0,1).and_return(
           0=>{'value'=>'seas','datatype'=>'string'},
@@ -45,10 +52,11 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
         u = Factory(:user)
         @h.process u
         p.reload
-        expect(p.get_custom_value(@season).value).to eq("seas\nsomeval")
+        expect(p.custom_value(cdefs[:csm_season])).to eq("seas\nsomeval")
         expect(@snapshot_created).to be_truthy
         expect(@snapshot_user).to eq u
       end
+
       it "should set CSM Season for new product" do
         expect(@xlc).to receive(:last_row_number).and_return(1)
         expect(@xlc).to receive(:get_row_as_column_hash).with(0,1).and_return(
@@ -62,26 +70,9 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
         )
         @h.process Factory(:user)
         p = Product.find_by_unique_identifier 'newproduid'
-        expect(p.get_custom_value(@season).value).to eq('seas')
+        expect(p.custom_value(cdefs[:csm_season])).to eq('seas')
       end
-      it "should create CSM Season field if it doesn't exist" do
-        id = @season.id
-        @season.destroy
-        expect(CustomDefinition.find_by_id(id)).to be_nil
-        @h = described_class.new @cf
-        expect(@xlc).to receive(:last_row_number).and_return(1)
-        expect(@xlc).to receive(:get_row_as_column_hash).with(0,1).and_return(
-          0=>{'value'=>'seas','datatype'=>'string'},
-          2=>{'value'=>'140','datatype'=>'string'},
-          3=>{'value'=>'ABCDE','datatype'=>'string'},
-          4=>{'value'=>'FGHIJ','datatype'=>'string'},
-          5=>{'value'=>'KLMNO','datatype'=>'string'},
-          8=>{'value'=>'newproduid','datatype'=>'string'},
-          13=>{'value'=>'CSMDEPT','datatype'=>'string'}
-        )
-        @h.process Factory(:user)
-        expect(CustomDefinition.find_by_module_type_and_label('Product','CSM Season').data_type).to eq('text')
-      end
+
       it "should accumulate CSM Seasons" do
         p = Factory(:product)
         expect(@xlc).to receive(:last_row_number).and_return(2)
@@ -104,8 +95,7 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
           13=>{'value'=>'CSMDEPT','datatype'=>'string'}
         )
         @h.process Factory(:user)
-        p.reload
-        expect(p.get_custom_value(@season).value).to eq("aaa\nseas")
+        expect(p.custom_value(cdefs[:csm_season])).to eq("aaa\nseas")
       end
     end
 
@@ -134,8 +124,9 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
         13=>{'value'=>'CSMDEPT','datatype'=>'string'}
       )
       @h.process Factory(:user)
-      expect(p.get_custom_value(@csm).value).to eq("140ABCDEFGHIJKLMNO\nZZZPQRSTUVWXYZ1234")
+      expect(p.custom_value(cdefs[:csm_numbers])).to eq("140ABCDEFGHIJKLMNO\nZZZPQRSTUVWXYZ1234")
     end
+    
     it "should include non-contiguous CSM numbers for a product" do
       p = Factory(:product)
       expect(@xlc).to receive(:last_row_number).and_return(3)
@@ -170,9 +161,10 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
 
       )
       @h.process Factory(:user)
-      expect(p.get_custom_value(@csm).value).to eq("140ABCDEFGHIJKLMNO\nZZZPQRSTUVWXYZ1234")
-      expect(Product.find_by_unique_identifier('something else').get_custom_value(@csm).value).to eq("140ABCDEFGHIJKLMNO")
+      expect(p.custom_value(cdefs[:csm_numbers])).to eq("140ABCDEFGHIJKLMNO\nZZZPQRSTUVWXYZ1234")
+      expect(Product.where(unique_identifier: 'something else').first.custom_value(cdefs[:csm_numbers])).to eq("140ABCDEFGHIJKLMNO")
     end
+    
     it "should create new CSM number for new product" do
       expect(@xlc).to receive(:last_row_number).and_return(1)
       expect(@xlc).to receive(:get_row_as_column_hash).with(0,1).and_return(
@@ -186,13 +178,14 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
 
       )
       @h.process Factory(:user)
-      p = Product.find_by_unique_identifier('something else')
-      expect(p.get_custom_value(@csm).value).to eq("140ABCDEFGHIJKLMNO")
-      expect(p.get_custom_value(@dept).value).to eq('CSMDEPT')
+      p = Product.where(unique_identifier: 'something else').first
+      expect(p.custom_value(cdefs[:csm_numbers])).to eq("140ABCDEFGHIJKLMNO")
+      expect(p.custom_value(cdefs[:csm_department])).to eq('CSMDEPT')
     end
+    
     it "should drop existing CSM numbers not in file" do
       p = Factory(:product)
-      p.update_custom_value!(@csm,'XZY')
+      p.update_custom_value!(cdefs[:csm_numbers],'XZY')
       expect(@xlc).to receive(:last_row_number).and_return(1)
       expect(@xlc).to receive(:get_row_as_column_hash).with(0,1).and_return(
         0=>{'value'=>'seas','datatype'=>'string'},
@@ -205,8 +198,9 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
       )
       @h.process Factory(:user)
       p = Product.find p.id
-      expect(p.get_custom_value(@csm).value).to eq("140ABCDEFGHIJKLMNO")
+      expect(p.custom_value(cdefs[:csm_numbers])).to eq("140ABCDEFGHIJKLMNO")
     end
+    
     it "should fail if CSM number is not 18 digits" do
       p = Factory(:product)
       expect(@xlc).to receive(:last_row_number).and_return(1)
@@ -221,15 +215,17 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
       )
       u = Factory(:user)
       @h.process u
-      expect(p.get_custom_value(@csm).value).to be_blank
+      expect(p.custom_value(cdefs[:csm_numbers])).to be_blank
       expect(u.messages.size).to eq(1)
       expect(u.messages[0].body).to include("File failed: CSM Number at row 2 was not 18 digits \"140XXABCDEFGHIJKLMNO\"")
     end
+    
     it "should not fail for empty lines" do
       expect(@xlc).to receive(:last_row_number).and_return(1)
       expect(@xlc).to receive(:get_row_as_column_hash).with(0,1).and_return({})
       expect{@h.process Factory(:user)}.to_not change(ErrorLogEntry,:count)
     end
+
     it "should fail if user cannot edit products" do
       allow_any_instance_of(Product).to receive(:can_edit?).and_return false
       p = Factory(:product)
@@ -246,7 +242,7 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
       )
       u = Factory(:user)
       @h.process u
-      expect(p.get_custom_value(@csm).value).to be_blank
+      expect(p.custom_value(cdefs[:csm_numbers])).to be_blank
       expect(u.messages.size).to eq(1)
       expect(u.messages[0].body).to include("File failed: #{u.full_name} can't edit product #{p.unique_identifier}")
     end
@@ -266,7 +262,7 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
       )
       u = Factory(:user)
       @h.process u
-      expect(p.get_custom_value(@csm).value).to be_blank
+      expect(p.custom_value(cdefs[:csm_numbers])).to be_blank
       expect(u.messages.size).to eq(1)
       # Don't bother trying to determine what the error will be..
       expect(u.messages[0].body).to include("<p>The following CSM data errors were encountered:<ul><li>")
@@ -274,34 +270,7 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
     end
 
     context "dates" do
-      it "should create csm date custom fields" do
-        id = @first_csm_date_cd.id
-        @first_csm_date_cd.destroy
-        expect(CustomDefinition.find_by_id(id)).to be_nil
-        id = @last_csm_date_cd.id
-        @last_csm_date_cd.destroy
-        expect(CustomDefinition.find_by_id(id)).to be_nil
-        p = Factory(:product)
-        expect(@xlc).to receive(:last_row_number).and_return(1)
-        expect(@xlc).to receive(:get_row_as_column_hash).with(0,1).and_return(
-          0=>{'value'=>'seas','datatype'=>'string'},
-          2=>{'value'=>'140','datatype'=>'string'},
-          3=>{'value'=>'ABCDE','datatype'=>'string'},
-          4=>{'value'=>'FGHIJ','datatype'=>'string'},
-          5=>{'value'=>'KLMNO','datatype'=>'string'},
-          8=>{'value'=>p.unique_identifier,'datatype'=>'string'},
-          13=>{'value'=>'CSMDEPT','datatype'=>'string'}
-        )
-        @h = described_class.new @cf
-        @h.process Factory(:user)
-        p.reload
-        f = CustomDefinition.find_by_label("CSM Received Date (First)")
-        expect(f.module_type).to eq("Product")
-        expect(f.data_type).to eq("date")
-        l = CustomDefinition.find_by_label("CSM Received Date (Last)")
-        expect(l.module_type).to eq("Product")
-        expect(l.data_type).to eq("date")
-      end
+
       it "should set first/last csm received dates" do
         p = Factory(:product)
         expect(@xlc).to receive(:last_row_number).and_return(1)
@@ -315,13 +284,13 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
           13=>{'value'=>'CSMDEPT','datatype'=>'string'}
         )
         @h.process Factory(:user)
-        p.reload
-        expect(p.get_custom_value(@first_csm_date_cd).value.strftime("%y%m%d")).to eq(0.seconds.ago.strftime("%y%m%d"))
-        expect(p.get_custom_value(@last_csm_date_cd).value.strftime("%y%m%d")).to eq(0.seconds.ago.strftime("%y%m%d"))
+        expect(p.custom_value(cdefs[:csm_received_date_first]).strftime("%y%m%d")).to eq(0.seconds.ago.strftime("%y%m%d"))
+        expect(p.custom_value(cdefs[:csm_received_date_last]).strftime("%y%m%d")).to eq(0.seconds.ago.strftime("%y%m%d"))
       end
+
       it "should not change existing first csm received date" do
         p = Factory(:product)
-        p.update_custom_value! @first_csm_date_cd, 1.day.ago
+        p.update_custom_value! cdefs[:csm_received_date_first], 1.day.ago
         expect(@xlc).to receive(:last_row_number).and_return(1)
         expect(@xlc).to receive(:get_row_as_column_hash).with(0,1).and_return(
           0=>{'value'=>'seas','datatype'=>'string'},
@@ -333,12 +302,11 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
           13=>{'value'=>'CSMDEPT','datatype'=>'string'}
         )
         @h.process Factory(:user)
-        p.reload
-        expect(p.get_custom_value(@first_csm_date_cd).value.strftime("%y%m%d")).to eq(1.day.ago.strftime("%y%m%d"))
+        expect(p.custom_value(cdefs[:csm_received_date_first]).strftime("%y%m%d")).to eq(1.day.ago.strftime("%y%m%d"))
       end
       it "should not move last csm date backwards" do
         p = Factory(:product)
-        p.update_custom_value! @last_csm_date_cd, 1.day.from_now
+        p.update_custom_value! cdefs[:csm_received_date_last], 1.day.from_now
         expect(@xlc).to receive(:last_row_number).and_return(1)
         expect(@xlc).to receive(:get_row_as_column_hash).with(0,1).and_return(
           0=>{'value'=>'seas','datatype'=>'string'},
@@ -350,8 +318,7 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
           13=>{'value'=>'CSMDEPT','datatype'=>'string'}
         )
         @h.process Factory(:user)
-        p.reload
-        expect(p.get_custom_value(@last_csm_date_cd).value.strftime("%y%m%d")).to eq(1.day.from_now.strftime("%y%m%d"))
+        expect(p.custom_value(cdefs[:csm_received_date_last]).strftime("%y%m%d")).to eq(1.day.from_now.strftime("%y%m%d"))
       end
       it "should respect override for file received date" do
         p = Factory(:product)
@@ -367,8 +334,7 @@ describe OpenChain::CustomHandler::PoloCsmSyncHandler do
         )
         @h = described_class.new @cf, 1.day.from_now
         @h.process Factory(:user)
-        p.reload
-        expect(p.get_custom_value(@last_csm_date_cd).value.strftime("%y%m%d")).to eq(1.day.from_now.strftime("%y%m%d"))
+        expect(p.custom_value(cdefs[:csm_received_date_last]).strftime("%y%m%d")).to eq(1.day.from_now.strftime("%y%m%d"))
       end
     end
   end
