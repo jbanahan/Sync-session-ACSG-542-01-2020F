06e22c76814b66aa11b77566f2ec28e526255d2f
Jeremy Hulford <jhulford@vandegriftinc.com>
2018-11-09 14:37:57 -0600

    Background the kill scripts to speed up shutdown.

--- script/killall_delayed_jobs.sh
+++ script/killall_delayed_jobs.sh
@@ -3,6 +3,6 @@
 for D in $(find . -mindepth 1 -maxdepth 1 -type d -print0 | xargs -0 -n1 basename) ; do
   cd $D
   echo "Killing all jobs in $D"
-  script/kill_dj.sh
+  script/kill_dj.sh &
   cd ..
 done
