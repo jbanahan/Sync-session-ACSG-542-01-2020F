17c4cfeb8a38f7675b9294c6126ca12b22366a3b
Brian Henderson <bhenderson@vandegriftinc.com>
2019-05-09 14:00:54 -0400

    1700 invoice date based monthly yoy (#2775)
    * Added the option to switch out the base date on the monthly YoY report. (1700)
    
    * Added the option to switch out the base date on the monthly YoY report. (1700)
    
    * Fixed settings-passing issue. (1700)

--- lib/open_chain/report/monthly_yoy_report.rb
+++ lib/open_chain/report/monthly_yoy_report.rb
@@ -2,11 +2,11 @@ module OpenChain; module Report; class MonthlyYoyReport
   include OpenChain::Report::ReportHelper
 
   def self.run_schedulable opts_hash={}
-    self.new.send_email('email' => opts_hash['email'])
+    self.new.send_email(opts_hash)
   end
 
   def send_email(settings)
-    wb = create_workbook
+    wb = create_workbook settings
     workbook_to_tempfile wb, 'MonthlyYoyReport-' do |t|
       subject = "Monthly YOY Report"
       body = "<p>Report attached.<br>--This is an automated message, please do not reply.<br>This message was generated from VFI Track</p>".html_safe
@@ -14,27 +14,37 @@ module OpenChain; module Report; class MonthlyYoyReport
     end
   end
 
-  def create_workbook
+  def create_workbook settings
     wb = XlsMaker.create_workbook "YOY Report #{Date.today.strftime('%m-%Y')}"
-    table_from_query wb.worksheet(0), query
+    table_from_query wb.worksheet(0), query(get_range_field(settings))
     wb
   end
 
-  def query
+  # Pulls the range field from the settings, allowing only certain values so as to bypass potential SQL injection
+  # issues.  Because of the way these queries are written (this is a dynamic field in the query, not a value for
+  # a field), simply sanitizing the settings value is inadequate.
+  def get_range_field settings
+    range_field = settings['range_field']
+    # Defaults to file logged date if bad.  This was the date field the report was originally written against.
+    ['file_logged_date','invoice_date'].include?(range_field) ? range_field : "file_logged_date"
+  end
+
+  def query range_field
     <<-SQL
-      SELECT CONCAT(year(file_logged_date),"-",LPAD(month(file_logged_date),2,"0")) AS 'Period',
-        year(file_logged_date) AS Year, 
-        month(file_logged_date) AS Month,
+      SELECT CONCAT(year(#{range_field}),"-",LPAD(month(#{range_field}),2,"0")) AS 'Period',
+        year(#{range_field}) AS 'Year', 
+        month(#{range_field}) AS 'Month',
         import_country.iso_code AS 'Country',
         division_number AS 'Division Number',
         customer_number AS 'Customer Number',
         CASE entries.transport_mode_code WHEN '10' THEN 'Ocean' WHEN '11' THEN 'Ocean' WHEN '40' THEN 'Air' WHEN '41' THEN 'Air' ELSE 'Other' END AS 'Mode',
         COUNT(*) AS 'File Count'
       FROM entries
-        INNER JOIN countries import_country ON entries.import_country_id = import_country.id
+        INNER JOIN countries import_country ON entries.import_country_id = import_country.id 
+        #{range_field == "invoice_date" ? "LEFT OUTER JOIN (SELECT entry_id, MIN(invoice_date) AS invoice_date FROM commercial_invoices GROUP by entry_id) AS ci ON entries.id = ci.entry_id " : ""}
       WHERE 
-        entries.file_logged_date > '#{(Time.zone.now.beginning_of_year - 2.years).to_s(:db)}' AND 
-        file_logged_date < '#{Time.zone.now.beginning_of_month.to_s(:db)}' 
+        #{range_field} > '#{(Time.zone.now.beginning_of_year - 2.years).to_s(:db)}' AND 
+        #{range_field} < '#{Time.zone.now.beginning_of_month.to_s(:db)}' 
       GROUP BY period, country, division_number, customer_number, mode
     SQL
   end
--- spec/reports/monthly_yoy_report_spec.rb
+++ spec/reports/monthly_yoy_report_spec.rb
@@ -2,7 +2,6 @@ require 'spec_helper'
 
 describe OpenChain::Report::MonthlyYoyReport do
   
-  let(:report) { described_class.new }
   let(:co1) { Factory(:country, iso_code: "CA") }
   let(:co2) { Factory(:country, iso_code: "US") }
   let(:today) { Date.new(2016,5,15) }
@@ -15,9 +14,9 @@ describe OpenChain::Report::MonthlyYoyReport do
   # Ensures we are including the final day of the previous month.
   let!(:e5) { Factory(:entry, file_logged_date: Date.new(2016,4,30), division_number: '1', customer_number: '2', transport_mode_code: '10', import_country: co2) }
 
-  describe "send_email" do
+  describe "run_schedulable" do
     it "sends email with attached xls" do
-      Timecop.freeze(today) { report.send_email({"email" => "test@vandegriftinc.com"}) }
+      Timecop.freeze(today) { described_class.run_schedulable({"email" => "test@vandegriftinc.com", "range_field" => "file_logged_date"}) }
       mail = ActionMailer::Base.deliveries.pop
       expect(mail.to).to eq [ "test@vandegriftinc.com" ]
       expect(mail.subject).to eq "Monthly YOY Report"
@@ -34,12 +33,34 @@ describe OpenChain::Report::MonthlyYoyReport do
         expect(sheet.row(0)).to eq ["Period", "Year", "Month", "Country", "Division Number", "Customer Number", "Mode", "File Count"]
       end
     end
+
+    it "sends email with attached xls (invoice date)" do
+      ci1 = Factory(:commercial_invoice, entry: e1, invoice_date: two_months_ago)
+
+      Timecop.freeze(today) { described_class.run_schedulable({"email" => "test@vandegriftinc.com", "range_field" => "invoice_date"}) }
+      mail = ActionMailer::Base.deliveries.pop
+      expect(mail.to).to eq [ "test@vandegriftinc.com" ]
+      expect(mail.subject).to eq "Monthly YOY Report"
+      expect(mail.attachments.count).to eq 1
+
+      Tempfile.open('attachment') do |t|
+        t.binmode
+        t << mail.attachments.first.read
+        t.flush
+        wb = Spreadsheet.open t.path
+        sheet = wb.worksheet(0)
+
+        # Should get less results since the query is based off invoice date and only one of the entries has an invoice.
+        expect(sheet.count).to eq 2
+        expect(sheet.row(0)).to eq ["Period", "Year", "Month", "Country", "Division Number", "Customer Number", "Mode", "File Count"]
+      end
+    end
   end
 
   describe "query" do
-    it "produces expected data" do
+    it "produces expected data (for file_logged_date)" do
       res = nil
-      Timecop.freeze(today) { res = ActiveRecord::Base.connection.execute(report.query) }
+      Timecop.freeze(today) { res = ActiveRecord::Base.connection.execute(subject.query("file_logged_date")) }
       results = []
       res.each { |r| results << r }
       expect(results.count).to eq 5
@@ -50,20 +71,48 @@ describe OpenChain::Report::MonthlyYoyReport do
                                       [month_ago.strftime("%Y-%m"), month_ago.year, month_ago.month, "US", "1", "2", "Ocean", 1]]
     end
 
+    it "produces expected data (for invoice_date)" do
+      ci1 = Factory(:commercial_invoice, entry: e1, invoice_date: two_months_ago)
+      ci1b = Factory(:commercial_invoice, entry: e1, invoice_date: nil)
+      ci2 = Factory(:commercial_invoice, entry: e2, invoice_date: two_months_ago)
+      ci2b = Factory(:commercial_invoice, entry: e2, invoice_date: month_ago)
+      ci3 = Factory(:commercial_invoice, entry: e3, invoice_date: month_ago)
+      ci4 = Factory(:commercial_invoice, entry: e4, invoice_date: month_ago)
+
+      res = nil
+      Timecop.freeze(today) { res = ActiveRecord::Base.connection.execute(subject.query("invoice_date")) }
+      results = []
+      res.each { |r| results << r }
+      expect(results.count).to eq 4
+      expect(results).to match_array [[two_months_ago.strftime("%Y-%m"), two_months_ago.year, two_months_ago.month, "CA", "1", "2", "Ocean", 1],
+                                      [two_months_ago.strftime("%Y-%m"), two_months_ago.year, two_months_ago.month, "CA", "2", "2", "Ocean", 1],
+                                      [month_ago.strftime("%Y-%m"), month_ago.year, month_ago.month, "CA", "1", "3", "Ocean", 1],
+                                      [month_ago.strftime("%Y-%m"), month_ago.year, month_ago.month, "CA", "1", "2", "Air", 1]]
+    end
+
     it "skips entries with file_logged_date earlier than January two years ago" do
       # This should be December 31, 2013 based on our 'today' value of May 15, 2016.  January 1, 2014 is included, but
       # one day earlier is too far into the past.
       e1.update_attributes(file_logged_date: (today.at_beginning_of_month - 2.years).at_beginning_of_year - 1.day)
       results = nil
-      Timecop.freeze(today) { results = ActiveRecord::Base.connection.execute(report.query) }
+      Timecop.freeze(today) { results = ActiveRecord::Base.connection.execute(subject.query("file_logged_date")) }
       expect(results.count).to eq 4
     end
 
     it "skips entries with file_logged_date from the current month" do
       e1.update_attributes(file_logged_date: today.at_beginning_of_month)
       results = nil
-      Timecop.freeze(today) { results = ActiveRecord::Base.connection.execute(report.query) }
+      Timecop.freeze(today) { results = ActiveRecord::Base.connection.execute(subject.query("file_logged_date")) }
       expect(results.count).to eq 4
+    end
+  end
+
+  describe "get_range_field" do
+    it "cleans range field value" do
+      expect(subject.get_range_field({"range_field"=>"file_logged_date"})).to eq "file_logged_date"
+      expect(subject.get_range_field({"range_field"=>"invoice_date"})).to eq "invoice_date"
+      expect(subject.get_range_field({"range_field"=>"INVALID"})).to eq "file_logged_date"
+      expect(subject.get_range_field({})).to eq "file_logged_date"
     end
   end
 end
