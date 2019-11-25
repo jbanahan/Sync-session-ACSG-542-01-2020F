185f530cccdc54ce88466068e09c01d9668cb1ab
jhulford <jhulford@vandegriftinc.com>
2019-11-25 07:50:54 -0600

    Allow Order Line HTS/SKU to be editted. (#2983)

--- lib/open_chain/model_field_definition/order_line_field_definition.rb
+++ lib/open_chain/model_field_definition/order_line_field_definition.rb
@@ -12,8 +12,9 @@ module OpenChain; module ModelFieldDefinition; module OrderLineFieldDefinition
       [6,:ordln_currency,:currency,"Currency",{data_type: :string}],
       [7,:ordln_country_of_origin,:country_of_origin,"Country of Origin",{data_type: :string}],
       [8,:ordln_hts,:hts,"HTS Code",{data_type: :string,
-        :export_lambda=> lambda{|obj| obj.hts.blank? ? '' : obj.hts.hts_format},
-        :search_value_preprocess_lambda=> hts_search_value_preprocess_lambda
+        export_lambda: lambda{|obj| obj.hts.blank? ? '' : obj.hts.hts_format },
+        import_lambda: lambda { |obj, data| obj.hts = (data.blank? ? nil : data.to_s.gsub(".", "")) },
+        search_value_preprocess_lambda: hts_search_value_preprocess_lambda()
       }],
       [9,:ordln_sku,:sku,'SKU',{data_type: :string}],
       [10,:ordln_total_cost, :total_cost, "Total Price", {data_type: :decimal, read_only: true,
