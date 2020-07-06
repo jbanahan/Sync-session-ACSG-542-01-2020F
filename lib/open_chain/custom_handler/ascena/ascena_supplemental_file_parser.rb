1a2c43aff363b2d50d3f141bf9a405947aa640a5
Jay Davis <planarian@users.noreply.github.com>
2020-07-06 11:54:13 -0400

    1950 - Send email if missing fields. (#3193)
    * Send email if missing fields.
    
    * Add inbound_file error logging
    
    * Use custom rspec matcher

--- lib/open_chain/custom_handler/ascena/ascena_supplemental_file_parser.rb
+++ lib/open_chain/custom_handler/ascena/ascena_supplemental_file_parser.rb
@@ -4,32 +4,58 @@ module OpenChain; module CustomHandler; module Ascena; class AscenaSupplementalF
   include OpenChain::IntegrationClientParser
 
   def self.parse data, opts = {}
-    csv_data = CSV.parse(data)
-    ent_num = csv_data[1][21]&.gsub("-", "")
-    raise "Entry number missing from supplemental file." unless ent_num
-    brok_ref = csv_data[1][21].split("-")[1]
+    self.new.parse data, opts
+  end
 
+  def parse data, opts = {}
+    csv_data = CSV.parse(data).drop(1)
+    ent_num = csv_data[0][21]&.gsub("-", "")
+    if ent_num.blank?
+      send_error "Entry Number", data, opts[:original_filename]
+    elsif csv_data.any? { |row| row[55].blank? }
+      send_error "PO Number", data, opts[:original_filename]
+    else
+      brok_ref = csv_data[1][21].split("-")[1]
+      attach_to_entry ent_num, brok_ref, data, opts[:original_filename]
+    end
+  end
+
+  def attach_to_entry ent_num, brok_ref, data, file_name
     find_or_create_entry(ent_num, brok_ref) do |entry|
       entry.attachments.find { |att| att.attachment_type == "FTZ Supplemental Data" }&.destroy
-      # only modify fields for new entries
-      attach_file entry, data, opts[:original_filename]
+      attach_file data, file_name do |tempfile|
+        entry.attachments.create! attached: tempfile, uploaded_by: User.integration, attachment_type: "FTZ Supplemental Data",
+                                  attached_file_name: file_name
+      end
       entry.save!
       entry.create_snapshot(User.integration, nil, inbound_file.s3_path)
     end
   end
 
-  def self.attach_file entry, data, file_name
-    Tempfile.open(["suppl", ".csv"]) do |t|
-      t << data
-      t.flush
-      entry.attachments.create! attached: t, uploaded_by: User.integration, attachment_type: "FTZ Supplemental Data",
-                                attached_file_name: file_name
+  def send_error missing_field, data, file_name
+    inbound_file.add_reject_message "#{missing_field}s are missing."
+    to = MailingList.where(system_code: "ascena_ftz_validations").first || "support@vandegriftinc.com"
+    attach_file data, file_name do |tempfile|
+      body = "The Supplemental Data File #{file_name} was not processed due to missing #{missing_field}s. "\
+             "Please add the correct #{missing_field}s to the Supplemental File and resend for processing."
+
+      OpenMailer.send_simple_html(to, "Supplemental Data File was Rejected for Missing Data", body, tempfile).deliver_now
     end
   end
 
-  private_class_method :attach_file
+  private
 
-  def self.find_or_create_entry ent_num, brok_ref
+  def attach_file data, file_name
+    Tempfile.open(["suppl", ".csv"]) do |t|
+      t << data
+      t.flush
+      Attachment.add_original_filename_method t, file_name
+
+      yield t
+    end
+  end
+
+  def find_or_create_entry ent_num, brok_ref
     entry = nil
     Lock.acquire("Entry-#{ent_num}") do
       entry = Entry.where(source_system: Entry::KEWILL_SOURCE_SYSTEM, broker_reference: brok_ref).first_or_create! entry_number: ent_num
@@ -37,7 +63,5 @@ module OpenChain; module CustomHandler; module Ascena; class AscenaSupplementalF
 
     Lock.db_lock(entry) { yield entry }
   end
-
-  private_class_method :find_or_create_entry
 
 end; end; end; end
--- spec/lib/custom_handler/ascena/ascena_supplemental_file_parser_spec.rb
+++ spec/lib/custom_handler/ascena/ascena_supplemental_file_parser_spec.rb
@@ -2,15 +2,16 @@ describe OpenChain::CustomHandler::Ascena::AscenaSupplementalFileParser do
   let(:csv) do
     # header
     ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n" \
-      ",,,,,,,,,,,,,,,,,,,,,316-2523440-1,,,,,,,,,,,,,,,ASCENA TRADE SERVICES,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n" \
-      ",,,,,,,,,,,,,,,,,,,,,316-2523440-1,,,,,,,,,,,,,,,ASCENA TRADE SERVICES,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,"
+      ",,,,,,,,,,,,,,,,,,,,,316-2523440-1,,,,,,,,,,,,,,,ASCENA TRADE SERVICES,,,,,,,,,,,,,,,,,,,PO1,,,,,,,,,,,,,,,,,,,,,,\n" \
+      ",,,,,,,,,,,,,,,,,,,,,316-2523440-1,,,,,,,,,,,,,,,ASCENA TRADE SERVICES,,,,,,,,,,,,,,,,,,,PO2,,,,,,,,,,,,,,,,,,,,,,"
   end
 
   describe "parse" do
     let!(:ent) { Factory(:entry, entry_number: "21612345671", broker_reference: "1234567", source_system: Entry::KEWILL_SOURCE_SYSTEM) }
+    let(:inbound) { InboundFile.new(s3_path: "path/to/some_file.csv") }
 
     before do
-      allow(described_class).to receive(:inbound_file).and_return InboundFile.new(s3_path: "path/to/some_file.csv")
+      allow(subject).to receive(:inbound_file).and_return inbound
     end
 
     it "attaches file to existing entry" do
@@ -19,7 +20,7 @@ describe OpenChain::CustomHandler::Ascena::AscenaSupplementalFileParser do
       t = Tempfile.new
       expect(Tempfile).to receive(:open).with(["suppl", ".csv"]).and_yield t
 
-      described_class.parse csv, original_filename: "some_file.csv"
+      subject.parse csv, original_filename: "some_file.csv"
       ent.reload
 
       expect(ent.attachments.count).to eq 1
@@ -42,7 +43,7 @@ describe OpenChain::CustomHandler::Ascena::AscenaSupplementalFileParser do
       t = Tempfile.new
       expect(Tempfile).to receive(:open).with(["suppl", ".csv"]).and_yield t
 
-      described_class.parse csv, original_filename: "some_file.csv"
+      subject.parse csv, original_filename: "some_file.csv"
       ent = Entry.find_by entry_number: "31625234401"
 
       expect(ent.broker_reference).to eq "2523440"
@@ -63,9 +64,46 @@ describe OpenChain::CustomHandler::Ascena::AscenaSupplementalFileParser do
       expect(snap.context).to eq "path/to/some_file.csv"
     end
 
-    it "raises exception if entry number missing" do
-      csv.gsub! "316-2523440-1", ""
-      expect {described_class.parse csv, original_filename: "some_file.csv"}.to raise_error "Entry number missing from supplemental file."
+    context "errors" do
+      before { Factory(:mailing_list, system_code: "ascena_ftz_validations", email_addresses: "tufnel@stonehenge.biz") }
+
+      it "sends email if first entry number is missing" do
+        csv.gsub! "316-2523440-1", ""
+
+        subject.parse csv, original_filename: "some_file.csv"
+
+        expect(inbound).to have_reject_message "Entry Numbers are missing."
+        expect(ActionMailer::Base.deliveries.length).to eq 1
+        m = ActionMailer::Base.deliveries.first
+        expect(m.to).to eq ["tufnel@stonehenge.biz"]
+        expect(m.subject).to eq "Supplemental Data File was Rejected for Missing Data"
+        expect(m.body).to include "The Supplemental Data File some_file.csv was not processed due to missing Entry Numbers. "\
+                                  "Please add the correct Entry Numbers to the Supplemental File and resend for processing."
+        expect(m.attachments.length).to eq 1
+        expect(m.attachments["some_file.csv"]).not_to be_nil
+        expect(m.attachments["some_file.csv"].read).to eq csv
+
+        expect(ent.entity_snapshots.count).to eq 0
+      end
+
+      it "sends email if any PO number is missing" do
+        csv.gsub! "PO2", ""
+
+        subject.parse csv, original_filename: "some_file.csv"
+
+        expect(inbound).to have_reject_message "PO Numbers are missing."
+        expect(ActionMailer::Base.deliveries.length).to eq 1
+        m = ActionMailer::Base.deliveries.first
+        expect(m.to).to eq ["tufnel@stonehenge.biz"]
+        expect(m.subject).to eq "Supplemental Data File was Rejected for Missing Data"
+        expect(m.body).to include "The Supplemental Data File some_file.csv was not processed due to missing PO Numbers. "\
+                                  "Please add the correct PO Numbers to the Supplemental File and resend for processing."
+        expect(m.attachments.length).to eq 1
+        expect(m.attachments["some_file.csv"]).not_to be_nil
+        expect(m.attachments["some_file.csv"].read).to eq csv
+
+        expect(ent.entity_snapshots.count).to eq 0
+      end
     end
   end
 end
