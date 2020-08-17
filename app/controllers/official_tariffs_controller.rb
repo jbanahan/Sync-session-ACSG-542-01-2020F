c51bdd83f464a80fa1c0ae24c3a63038c7704ad4
Brian Henderson <bhenderson@vandegriftinc.com>
2020-08-17 11:56:15 -0400

    1905 expand hts classify dropdown (#3230)
    * Expanded number of results returned for HTS autocomplete. (1905)
    
    * Rubocop clean-up. (1905)

--- app/controllers/official_tariffs_controller.rb
+++ app/controllers/official_tariffs_controller.rb
@@ -1,28 +1,33 @@
 class OfficialTariffsController < ApplicationController
-  skip_before_filter :require_user, :set_user_time_zone, :log_request, :log_run_as_request, :only=>[:auto_classify, :auto_complete]
+  skip_before_action :require_user, :set_user_time_zone, :log_request, :log_run_as_request, only: [:auto_classify, :auto_complete]
 
   def auto_complete
-    ot = OfficialTariff.limit(10)
-    if !params[:country].blank?
-      ot = ot.where(:country_id=>params[:country])
+    hts_part = TariffRecord.clean_hts(params[:hts])
+    # After the user has entered 8 positions of the HTS, all matching combinations are to be shown.  Realistically,
+    # this can't be more than 1000, with that level of precision.  The possibilities are likely far fewer than
+    # that amount.  For less than 8 chars, 20 options are presented, up from 10 (old value, before summer 2020/SOW 1905).
+    ot = OfficialTariff.limit(hts_part.length >= 8 ? 1000 : 20)
+    if params[:country].present?
+      ot = ot.where(country_id: params[:country])
     else
       ot = ot.where('country_id = (select id from countries where iso_code = ?)', params[:country_iso])
     end
-    tariffs = ot.where("hts_code LIKE ?", "#{TariffRecord.clean_hts(params[:hts])}%")
-                .map { |t| {label: t.hts_code, desc: t.remaining_description} }
-    render :json => params[:description] ? tariffs : tariffs.map { |t| t[:label] }
+    tariffs = ot.where("hts_code LIKE ?", "#{hts_part}%").map { |t| {label: t.hts_code, desc: t.remaining_description} }
+    render json: params[:description] ? tariffs : tariffs.map { |t| t[:label] }
   end
+
   def auto_classify
     h = params[:hts].blank? ? '' : params[:hts].strip.gsub('.', '')
     found = OfficialTariff.auto_classify h
     r = []
     found.each do |k, v|
       hts = []
-      v.each {|ot| hts << {'lacey_act'=>ot.lacey_act?, 'code'=>ot.hts_code, 'desc'=>ot.remaining_description, 'rate'=>ot.common_rate, 'use_count'=>ot.use_count}}
-      r << {'iso'=>k.iso_code, 'country_id'=>k.id, 'hts'=>hts}
+      v.each {|ot| hts << {'lacey_act' => ot.lacey_act?, 'code' => ot.hts_code, 'desc' => ot.remaining_description, 'rate' => ot.common_rate, 'use_count' => ot.use_count}}
+      r << {'iso' => k.iso_code, 'country_id' => k.id, 'hts' => hts}
     end
-    render :json => r
+    render json: r
   end
+
   def index
     flash.keep
     redirect_to advanced_search CoreModule::OFFICIAL_TARIFF, params[:force_search]
@@ -30,38 +35,37 @@ class OfficialTariffsController < ApplicationController
 
   def show
     @official_tariff = OfficialTariff.find(params[:id])
-    raise ActionController::RoutingError.new('Not Found') unless @official_tariff && @official_tariff.can_view?(current_user)
+    raise ActionController::RoutingError, 'Not Found' unless @official_tariff&.can_view?(current_user)
   end
 
   def find
     hts = TariffRecord.clean_hts params[:hts]
     cid = params[:cid]
     ot = nil
-    ot = OfficialTariff.find_cached_by_hts_code_and_country_id hts, cid unless cid.blank?
+    ot = OfficialTariff.find_cached_by_hts_code_and_country_id hts, cid if cid.present?
     c_iso = params[:ciso]
-    ot = OfficialTariff.joins(:country).where(:hts_code=>hts).where("countries.iso_code = ?", c_iso).first unless !ot.nil? || c_iso.blank?
+    ot = OfficialTariff.joins(:country).where(hts_code: hts).where("countries.iso_code = ?", c_iso).first unless !ot.nil? || c_iso.blank?
     if ot.nil?
-      if OfficialTariff.where(:country_id=>cid).empty?
-        render :json => "country not loaded".to_json
+      if OfficialTariff.where(country_id: cid).empty?
+        render json: "country not loaded".to_json
       else
-        render :json => nil.to_json
+        render json: nil.to_json
       end
     else
-      render :json => ot.to_json(methods: :lacey_act, :include=>{:country=>{:only=>[:name]}, :official_quota=>{:only=>[:category, :unit_of_measure, :square_meter_equivalent_factor]}})
+      render json: ot.to_json(methods: :lacey_act, include: {country: {only: [:name]}, official_quota: {only: [:category, :unit_of_measure, :square_meter_equivalent_factor]}})
     end
   end
 
   def find_schedule_b
     hts = TariffRecord.clean_hts params[:hts]
-    osb = OfficialScheduleBCode.where(:hts_code=>hts).first
-    render :json => osb
+    osb = OfficialScheduleBCode.where(hts_code: hts).first
+    render json: osb
   end
 
   def schedule_b_matches
-    ot = OfficialTariff.find_cached_by_hts_code_and_country_id TariffRecord.clean_hts(params[:hts]), Country.where(:iso_code=>'US').first.id
-    render :json => (ot.nil? ? nil.to_json : ot.find_schedule_b_matches)
+    ot = OfficialTariff.find_cached_by_hts_code_and_country_id TariffRecord.clean_hts(params[:hts]), Country.where(iso_code: 'US').first.id
+    render json: (ot.nil? ? nil.to_json : ot.find_schedule_b_matches)
   end
-
 
   def secure base_search
     OfficalTariff.search_secure current_user, base_search
--- spec/controllers/official_tariffs_controller_spec.rb
+++ spec/controllers/official_tariffs_controller_spec.rb
@@ -1,44 +1,76 @@
 describe OfficialTariffsController do
-  before :each do
-    @us = Factory(:country, :iso_code=>"US", import_location: true)
-    @ca = Factory(:country, :iso_code=>"CA", import_location: true)
-    @us_hts_1 = Factory(:official_tariff, :hts_code=>'1234567890', :country=>@us, :remaining_description=>'abc', :general_rate=>'11', :use_count=>5)
-    @us_hts_2 = Factory(:official_tariff, :hts_code=>'1234567899', :country=>@us, :remaining_description=>'def', :general_rate=>'aa', :use_count=>3)
-    @ca_hts = Factory(:official_tariff, :hts_code=>'1234567777', :country=>@ca, :remaining_description=>'xyz', :most_favored_nation_rate=>'123', :use_count=>6)
+  let!(:us) { Factory(:country, iso_code: "US", import_location: true) }
+  let!(:ca) { Factory(:country, iso_code: "CA", import_location: true) }
+
+  before do
+    Factory(:official_tariff, hts_code: '1234567890', country: us, remaining_description: 'abc', general_rate: '11', use_count: 5)
+    Factory(:official_tariff, hts_code: '1234567899', country: us, remaining_description: 'def', general_rate: 'aa', use_count: 3)
+    Factory(:official_tariff, hts_code: '1234567777', country: ca, remaining_description: 'xyz', most_favored_nation_rate: '123', use_count: 6)
   end
+
   describe "auto_complete" do
-    it "should return array of tariffs for given country" do
-      get :auto_complete, 'hts'=>'1234', 'country'=>@us.id.to_s, :format=>:json
+    it "returns array of tariffs for given country" do
+      get :auto_complete, 'hts' => '1234', 'country' => us.id.to_s, :format => :json
       expect(response).to be_success
       expect(JSON.parse(response.body)).to eq(['1234567890', '1234567899'])
     end
 
     it "provides description if specified" do
-      get :auto_complete, 'hts'=>'1234', 'country'=>@us.id.to_s, 'description' => true, :format=>:json
+      get :auto_complete, 'hts' => '1234', 'country' => us.id.to_s, 'description' => true, :format => :json
       expect(response).to be_success
       expect(JSON.parse(response.body)).to eq([{'label' => '1234567890', 'desc' => 'abc'},
                                                {'label' => '1234567899', 'desc' => "def"}])
     end
+
+    context "limits" do
+      before do
+        25.times do |i|
+          Factory(:official_tariff, hts_code: "4455667#{(i + 1).to_s.rjust(3, "0")}", country: us)
+        end
+      end
+
+      it "limits results to 20 when partial HTS is short" do
+        get :auto_complete, 'hts' => '4455667', 'country' => us.id.to_s, :format => :json
+        expect(response).to be_success
+        # Only 20 of the 25 matches should be returned.
+        expect(JSON.parse(response.body)).to eq(['4455667001', '4455667002', '4455667003', '4455667004', '4455667005',
+                                                 '4455667006', '4455667007', '4455667008', '4455667009', '4455667010',
+                                                 '4455667011', '4455667012', '4455667013', '4455667014', '4455667015',
+                                                 '4455667016', '4455667017', '4455667018', '4455667019', '4455667020'])
+      end
+
+      it "includes all matching results when partial HTS is short" do
+        get :auto_complete, 'hts' => '44556670', 'country' => us.id.to_s, :format => :json
+        expect(response).to be_success
+        # All 25 matches should be returned.
+        expect(JSON.parse(response.body)).to eq(['4455667001', '4455667002', '4455667003', '4455667004', '4455667005',
+                                                 '4455667006', '4455667007', '4455667008', '4455667009', '4455667010',
+                                                 '4455667011', '4455667012', '4455667013', '4455667014', '4455667015',
+                                                 '4455667016', '4455667017', '4455667018', '4455667019', '4455667020',
+                                                 '4455667021', '4455667022', '4455667023', '4455667024', '4455667025'])
+      end
+    end
   end
+
   describe "auto_classify" do
-    it "should return import country classifications" do
-      get :auto_classify, 'hts'=>'123456789'
+    it "returns import country classifications" do
+      get :auto_classify, 'hts' => '123456789'
       expect(response).to be_success
       body = JSON.parse response.body
       expect(body.size).to eq(2)
       x = body.first
       y = body.last
-      expect(x['country_id']).to eq(@ca.id)
-      expect(y['country_id']).to eq(@us.id)
+      expect(x['country_id']).to eq(ca.id)
+      expect(y['country_id']).to eq(us.id)
       expect(x['hts']).to eq(
-          [{"lacey_act"=>false, "code"=>"1234567777",
-          "desc"=>"xyz", "rate"=>"123", "use_count"=>6}]
+        [{"lacey_act" => false, "code" => "1234567777",
+          "desc" => "xyz", "rate" => "123", "use_count" => 6}]
       )
       expect(y['hts']).to eq(
-          [{"lacey_act"=>false, "code"=>"1234567890",
-          "desc"=>"abc", "rate"=>"11", "use_count"=>5},
-          {"lacey_act"=>false, "code"=>"1234567899",
-          "desc"=>"def", "rate"=>"aa", "use_count"=>3}]
+        [{"lacey_act" => false, "code" => "1234567890",
+          "desc" => "abc", "rate" => "11", "use_count" => 5},
+        {"lacey_act" => false, "code" => "1234567899",
+         "desc" => "def", "rate" => "aa", "use_count" => 3}]
       )
     end
   end
