ac2dfe173d1e5c204c957633f48b9d9243950d6b
jhulford <jhulford@vandegriftinc.com>
2020-09-14 10:54:28 -0500

    Update brakeman (#3246)
    * Update brakeman
    
    * Update brakeman to new release.
    
    * Simplify Circle gem caching strategy.

--- config/initializers/cookies_serializer.rb
+++ config/initializers/cookies_serializer.rb
@@ -1,6 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-# This can be changed to json once we've had the rails version upgrade deployed for some time..they hybrid just means that
-# it handles Marshal'ed cookes if they come in, but any new cookies are written in json.  After some time, the marshal'ed cookies
-# will be gone and we can just use json.
-Rails.application.config.action_dispatch.cookies_serializer = :hybrid
+Rails.application.config.action_dispatch.cookies_serializer = :json
