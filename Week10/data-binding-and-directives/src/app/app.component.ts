e00e57106a9fa9508d619903732781a6c9c0817c
Jason Banahan <fir3chi3f@gmail.com>
2020-10-29 22:57:34 -0500

    Add databinding and directive example project

---
+++ Week10/data-binding-and-directives/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { faAngular } from '@fortawesome/free-brands-svg-icons';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'data-binding-and-directives';
+  faAngular = faAngular;
+}
