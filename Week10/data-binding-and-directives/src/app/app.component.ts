46557d04819509bf49cc321a73142e93d7f1a09a
Jason Banahan <fir3chi3f@gmail.com>
2020-10-30 20:57:04 -0500

    Include data binding example

--- Week10/data-binding-and-directives/src/app/app.component.ts
+++ Week10/data-binding-and-directives/src/app/app.component.ts
@@ -9,4 +9,14 @@ import { faAngular } from '@fortawesome/free-brands-svg-icons';
 export class AppComponent {
   title = 'data-binding-and-directives';
   faAngular = faAngular;
+  isTrue = false;
+
+  someTextVariable = "";
+
+  myPictureLocation: string = "../assets/images/stegosaurus.jpg"
+
+  someFunctionName = function () {
+    // alert("Hello World!");
+    this.isTrue = !this.isTrue;
+  }
 }
