e116698cfc92475f709c4e1ce8dad8a024af5e9d
Brian Bommarito <brian@bommarito.io>
2020-12-01 10:58:01 -0500

    Adding manifest.js

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,18 @@
+//= link application.css
+//= link login.css
+//= link vendor_portal.css
+//= link trade_lanes.css
+//= link chain_vendor_maint.css
+//= link chain_admin.css
+//= link quill.css
+//
+//= link application.js
+//= link login.js
+//= link legacy.js
+//= link html5shim.js
+//= link vendor_portal.js
+//= link trade_lanes.js
+//= link chain_vendor_maint.js
+//= link chain_admin.js
+//= link pdfobject.js
+//= link jquery_ujs.js
