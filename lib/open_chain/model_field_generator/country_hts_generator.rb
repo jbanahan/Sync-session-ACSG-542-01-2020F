0c5bc9da5d33e288ecbd7ee7e5411e49375d0733
Jay Davis <planarian@users.noreply.github.com>
2021-01-07 12:57:51 -0500

    INC-64530 -- Allow import of blank HTS (#3365)
    * Allow import of blank HTS
    
    * Rubocop corrections

--- lib/open_chain/model_field_generator/country_hts_generator.rb
+++ lib/open_chain/model_field_generator/country_hts_generator.rb
@@ -6,12 +6,17 @@ module OpenChain; module ModelFieldGenerator; module CountryHtsGenerator
 
       # validate HTS
       hts = TariffRecord.clean_hts(d)
-      return "Blank HTS ignored for #{country.iso_code}" if hts.blank?
-      unless OfficialTariff.find_by_country_id_and_hts_code(country.id, hts) || OfficialTariff.where(country_id:country.id).empty?
-        e = "#{d} is not valid for #{country.iso_code} HTS #{hts_number}"
-        # Indicate the message is an error message
-        def e.error?; true; end
-        return e;
+      if hts.blank?
+        hts = nil
+      else
+        unless OfficialTariff.where(country_id: country.id, hts_code: hts).present? || OfficialTariff.where(country_id: country.id).empty?
+          e = "#{d} is not valid for #{country.iso_code} HTS #{hts_number}"
+          # Indicate the message is an error message
+          def e.error?
+            true
+          end
+          return e
+        end
       end
       cls = nil
       # find classifications & tariff records in memory so this can work on objects that are dirty
@@ -19,12 +24,11 @@ module OpenChain; module ModelFieldGenerator; module CountryHtsGenerator
         cls = existing if existing.country_id == country.id
         break if cls
       end
-      cls = p.classifications.build(:country_id=>country.id) unless cls
-      tr = nil
-      tr = cls.tariff_records.sort {|a, b| a.line_number <=> b.line_number}.first
-      tr = cls.tariff_records.build unless tr
+      cls ||= p.classifications.build(country_id: country.id)
+      tr = cls.tariff_records.min {|a, b| a.line_number <=> b.line_number}
+      tr ||= cls.tariff_records.build
       tr.send("hts_#{hts_number}=".to_sym, hts)
-      "#{country.iso_code} HTS #{hts_number} set to #{hts.hts_format}"
+      "#{country.iso_code} HTS #{hts_number} set to #{hts&.hts_format || '[blank]'}"
     end
   end
 
@@ -47,10 +51,10 @@ module OpenChain; module ModelFieldGenerator; module CountryHtsGenerator
   end
 
   def query_subselect hts_number, country, join_table_name, join_table_with_id
-    "(SELECT hts_#{hts_number} FROM classifications hts_#{hts_number}_class INNER JOIN tariff_records hts_#{hts_number}_tariff ON hts_#{hts_number}_tariff.classification_id = hts_#{hts_number}_class.id WHERE hts_#{hts_number}_class.country_id = #{country.id} AND hts_#{hts_number}_class.product_id = #{join_table_name}.#{join_table_with_id ? "id" : "product_id"} ORDER BY hts_#{hts_number}_tariff.line_number LIMIT 1)"
+    "(SELECT hts_#{hts_number} FROM classifications hts_#{hts_number}_class INNER JOIN tariff_records hts_#{hts_number}_tariff ON hts_#{hts_number}_tariff.classification_id = hts_#{hts_number}_class.id WHERE hts_#{hts_number}_class.country_id = #{country.id} AND hts_#{hts_number}_class.product_id = #{join_table_name}.#{join_table_with_id ? "id" : "product_id"} ORDER BY hts_#{hts_number}_tariff.line_number LIMIT 1)" # rubocop:disable Layout/LineLength
   end
 
-  def make_country_hts_fields core_module, join_table_name: core_module.table_name, read_only: true, model_field_suffix: core_module.table_name, hts_numbers: 1..1, label_prefix: "Product - ", starting_index: nil, product_lambda:
+  def make_country_hts_fields core_module, join_table_name: core_module.table_name, read_only: true, model_field_suffix: core_module.table_name, hts_numbers: 1..1, label_prefix: "Product - ", starting_index: nil, product_lambda: # rubocop:disable Layout/LineLength
     model_fields = []
 
     starting_index = starting_index.nil? ? next_index_number(core_module) : starting_index
@@ -59,13 +63,11 @@ module OpenChain; module ModelFieldGenerator; module CountryHtsGenerator
       hts_numbers.each_with_index do |i, x|
         field_name = "*fhts_#{i}_#{c.id}#{model_field_suffix.blank? ? "" : ("_" + model_field_suffix)}".to_sym
         mf = ModelField.new((starting_index + x), field_name, core_module, field_name,
-          {label_override: "#{label_prefix}First HTS #{i} (#{c.iso_code})", data_type: :string, history_ignore: true, read_only: read_only,
-            import_lambda: (read_only ? nil : import_lambda(product_lambda, i, c)),
-            export_lambda: export_lambda(product_lambda, i, c),
-            qualified_field_name: query_subselect(i, c, join_table_name, core_module == CoreModule::PRODUCT),
-            process_query_result_lambda: lambda {|r| r.nil? ? nil : r.hts_format }
-          }
-        )
+                            {label_override: "#{label_prefix}First HTS #{i} (#{c.iso_code})", data_type: :string, history_ignore: true, read_only: read_only,
+                             import_lambda: (read_only ? nil : import_lambda(product_lambda, i, c)),
+                             export_lambda: export_lambda(product_lambda, i, c),
+                             qualified_field_name: query_subselect(i, c, join_table_name, core_module == CoreModule::PRODUCT),
+                             process_query_result_lambda: ->(r) { r.nil? ? nil : r.hts_format }})
         model_fields << mf
       end
     end
